ee5def91cecfe83ced9a5b9f8261c6d1a935fc90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:19 +0000

    Add package tbtc/0.0.2/tbtc-0.0.2.tar.gz

---
+++ packages/tbtc/tbtc-0.0.2.tar.gz/tbtc-0.0.2/setup.py
@@ -0,0 +1,37 @@
+from os import path
+from setuptools import (
+    find_packages,
+    setup,
+)
+
+# managing version
+with open("VERSION", 'r') as f:
+    VERSION = f.read()
+
+# managing readme
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='tbtc',
+    version=VERSION,
+    py_modules=['tbtc'],
+    description="""A python library to interact with the tbtc protocol.""",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Ankit Chiplunkar',
+    author_email='ankitchiplunkar@gmail.com',
+    url='https://github.com/ankitchiplunkar/tbtc.py',
+    include_package_data=True,
+    python_requires='>=3.6, <4',
+    keywords=['ethereum', 'bitcoin'],
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
