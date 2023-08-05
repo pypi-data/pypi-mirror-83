08be39c0229208abd693d4311b3493594bdf7f19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:35 +0000

    Add package domain-eventbus/0.1.11/domain-eventbus-0.1.11.tar.gz

---
+++ packages/domain-eventbus/domain-eventbus-0.1.11.tar.gz/domain-eventbus-0.1.11/HISTORY.rst
@@ -0,0 +1,33 @@
+=======
+History
+=======
+
+0.1.3 (2020-10-01)
+------------------
+
+* First release on PyPI.
+
+0.1.6 (2020-10-05)
+------------------
+
+* Introducing AbstractEventHandler
+
+0.1.7 (2020-10-07)
+------------------
+
+* Cleanup: Docs, Labels
+
+0.1.9 (2020-10-07)
+------------------
+
+* Cleanup: Docs, setup.py
+
+0.1.10 (2020-10-09)
+-------------------
+
+* Cleanup: Docs, setup.py
+
+0.1.11 (2020-10-09)
+-------------------
+
+* Adding possibility to specify custom __last_modified__ during entity instantiation
---
+++ packages/domain-eventbus/domain-eventbus-0.1.11.tar.gz/domain-eventbus-0.1.11/eventbus/domain/eventbus.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from abc import ABC, abstractmethod
+from typing import List, Tuple, Type, Union, Optional
+
+from eventbus.domain.whitehead import TEvent
+
+
+class AbstractEventHandler(ABC):
+    ASTERISK = "*"
+
+    @property
+    @abstractmethod
+    def event_type(self) -> Union[str, Type, Tuple]:
+        pass
+
+    def filter(self, events: List[TEvent]):
+        # When self.event_type == self.ASTERISK -> return all available events
+        return list(filter(lambda elm: self.event_type == self.ASTERISK or isinstance(elm, self.event_type), events))
+
+    def predicate(self, events) -> bool:
+        return len(self.filter(events)) > 0
+
+    @abstractmethod
+    async def handler(self, events: List[TEvent]):
+        pass
+
+
+class AbstractEventBus(ABC):
+    @abstractmethod
+    def subscribe(self, handler: AbstractEventHandler) -> None:
+        pass
+
+    @abstractmethod
+    def unsubscribe(self, handler: AbstractEventHandler) -> None:
+        pass
+
+    @abstractmethod
+    async def publish(self, events: List[TEvent]) -> None:
+        pass
