a748b08ab3444f1d9ff97412d15ea06dc324a222
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:42 +0000

    Add package pycybos/0.0.0/pycybos-0.0.0-py3-none-any.whl

---
+++ packages/pycybos/pycybos-0.0.0-py3-none-any.whl/utest/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+# print(f"{'@'*50} {__name__}")
+import os
+import sys
+PJT_PATH = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(PJT_PATH)
+import setup_env
