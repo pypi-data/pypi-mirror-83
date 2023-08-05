c436b0381864a6d996bae423bf07bd6346b8a84c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:46 +0000

    Add package neuro-flow/20.10.21/neuro_flow-20.10.21-py3-none-any.whl

---
+++ packages/neuro-flow/neuro_flow-20.10.21-py3-none-any.whl/neuro_flow/cli.py
@@ -0,0 +1,176 @@
+import click
+import functools
+import inspect
+from neuromation.cli.asyncio_utils import Runner
+from typing import Any, Awaitable, Callable, Optional, TypeVar
+
+from . import ast
+from .live_runner import LiveRunner
+from .parser import find_live_config, parse_live
+
+
+_T = TypeVar("_T")
+
+
+def wrap_async(callback: Callable[..., Awaitable[_T]],) -> Callable[..., _T]:
+    assert inspect.iscoroutinefunction(callback)
+
+    # N.B. the decorator implies @click.pass_obj
+    @click.pass_obj
+    @functools.wraps(callback)
+    def wrapper(*args: Any, **kwargs: Any) -> _T:
+        with Runner() as runner:
+            return runner.run(callback(*args, **kwargs))
+
+    return wrapper
+
+
+@click.group()
+@click.option(
+    "--config",
+    type=click.Path(dir_okay=True, file_okay=False),
+    required=False,
+    help=(
+        "Path to a directory with .neuro folder inside, "
+        "automatic lookup is performed if not set (default)"
+    ),
+    default=None,
+    metavar="PATH",
+)
+@click.pass_context
+def main(ctx: click.Context, config: Optional[str]) -> None:
+    config_path = find_live_config(config)
+    flow = parse_live(config_path.workspace, config_path.config_file)
+    ctx.obj = flow
+
+
+# #### job commands ####
+
+
+@main.command()
+@wrap_async
+async def ps(flow: ast.LiveFlow) -> None:
+    """List all jobs"""
+    async with LiveRunner(flow) as runner:
+        await runner.ps()
+
+
+@main.command()
+@click.argument("job-id")
+@wrap_async
+async def run(flow: ast.LiveFlow, job_id: str) -> None:
+    """Run a job.
+
+    RUN job JOB-ID or ATTACH to it if the job is already running
+    """
+    async with LiveRunner(flow) as runner:
+        await runner.run(job_id)
+
+
+@main.command()
+@click.argument("job-id")
+@wrap_async
+async def logs(flow: ast.LiveFlow, job_id: str) -> None:
+    """Print logs.
+
+    Displys logs for JOB-ID
+    """
+    async with LiveRunner(flow) as runner:
+        await runner.logs(job_id)
+
+
+@main.command()
+@click.argument("job-id")
+@wrap_async
+async def status(flow: ast.LiveFlow, job_id: str) -> None:
+    """Show job status.
+
+    Print status for JOB-ID
+    """
+    async with LiveRunner(flow) as runner:
+        await runner.status(job_id)
+
+
+@main.command()
+@click.argument("job-id")
+@wrap_async
+async def kill(flow: ast.LiveFlow, job_id: str) -> None:
+    """Kill a job.
+
+    Kill JOB-ID, use `kill ALL` for killing all jobs."""
+    async with LiveRunner(flow) as runner:
+        if job_id != "ALL":
+            await runner.kill(job_id)
+        else:
+            await runner.kill_all()
+
+
+# #### storage commands ####
+
+
+@main.command()
+@click.argument("volume")
+@wrap_async
+async def upload(flow: ast.LiveFlow, volume: str) -> None:
+    """Upload volume.
+
+    Upload local files to remote for VOLUME,
+    use `upload ALL` for uploading all volumes."""
+    async with LiveRunner(flow) as runner:
+        if volume != "ALL":
+            await runner.upload(volume)
+        else:
+            await runner.upload_all()
+
+
+@main.command()
+@click.argument("volume")
+@wrap_async
+async def download(flow: ast.LiveFlow, volume: str) -> None:
+    """Download volume.
+
+    Download remote files to local for VOLUME,
+    use `download ALL` for downloading all volumes."""
+    async with LiveRunner(flow) as runner:
+        if volume != "ALL":
+            await runner.download(volume)
+        else:
+            await runner.download_all()
+
+
+@main.command()
+@click.argument("volume")
+@wrap_async
+async def clean(flow: ast.LiveFlow, volume: str) -> None:
+    """Clean volume.
+
+    Clean remote files on VOLUME,
+    use `clean ALL` for cleaning up all volumes."""
+    async with LiveRunner(flow) as runner:
+        if volume != "ALL":
+            await runner.clean(volume)
+        else:
+            await runner.clean_all()
+
+
+@main.command()
+@wrap_async
+async def mkvolumes(flow: ast.LiveFlow) -> None:
+    """Create all remote folders for volumes."""
+    async with LiveRunner(flow) as runner:
+        await runner.mkvolumes()
+
+
+# #### image commands ####
+
+
+@main.command()
+@click.argument("image")
+@wrap_async
+async def build(flow: ast.LiveFlow, image: str) -> None:
+    """Build an image.
+
+    Assemble the IMAGE remotely and publish it.
+    """
+    async with LiveRunner(flow) as runner:
+        await runner.build(image)
