c6bc8f07c1f593d9f5e0f85f68beeca9bb3f64ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:10 +0000

    Add package upload.py/0.22/upload.py-0.22-py3-none-any.whl

---
+++ packages/upload.py/upload.py-0.22-py3-none-any.whl/upload_py/example.py
@@ -0,0 +1,34 @@
+from upload_py import *
+import os
+
+#anonfiles
+print("Anonfiles:")
+file = anonfiles()
+file.upload(f"{os.getcwd()}/file.txt")
+file.url_short()
+file.url_full()
+file.metadata()
+print("")
+
+#starfiles
+print("starfiles:")
+file2 = starfiles()
+file2.upload(f"{os.getcwd()}/file.txt")
+file2.url_file()
+file2.url_file_direct()
+file2.url_ipa_install()
+file2.metadata()
+print("")
+
+#FilePipe
+print("FilePipe:")
+file3 = filepipe()
+file3.upload(f"{os.getcwd()}/file.txt")
+file3.url_file()
+print("")
+
+#FilePipe
+print("File.io:")
+file3 = fileio()
+file3.upload(f"{os.getcwd()}/file.txt")
+file3.url_file()
