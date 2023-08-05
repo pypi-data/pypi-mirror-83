85da73fe671ae0d27a03829038566371e10cd89d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:20 +0000

    Add package ORobin/0.5.40/ORobin-0.5.40.tar.gz

---
+++ packages/ORobin/ORobin-0.5.40.tar.gz/ORobin-0.5.40/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='ORobin',
+      version='0.5.4',
+      description='It is programming language in russian! Main project - robin_pl!',
+      packages=['ORobin'],
+      author_email='foton-pc@inbox.ru',
+      zip_safe=False,
+      home_page='https://ppbe.ru',
+      author='FotonPC',
+      platform=['win32','linux','macos'])
