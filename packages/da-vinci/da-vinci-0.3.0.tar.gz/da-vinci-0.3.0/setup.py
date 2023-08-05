86f9b9f812961fdfbfaa56d53bc776adbb2e2882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:34 +0000

    Add package da-vinci/0.3.0/da-vinci-0.3.0.tar.gz

---
+++ packages/da-vinci/da-vinci-0.3.0.tar.gz/da-vinci-0.3.0/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='da-vinci',
+    version='0.3.0',
+    author='Selwin Ong',
+    author_email='selwin.ong@gmail.com',
+    packages=['da_vinci'],
+    url='https://github.com/ui/da-vinci',
+    license='MIT',
+    description='A simple image manipulation library aiming to make common image tasks easy.',
+    long_description=open('README.rst').read(),
+    zip_safe=False,
+    include_package_data=True,
+    package_data={'': ['README.rst']},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Multimedia :: Graphics'
+    ]
+)
