cf7ef8d7ca4d0d317117a98bf21bf7b691c9975d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:34 +0000

    Add package mcdbot/0.4.4/mcdbot-0.4.4.tar.gz

---
+++ packages/mcdbot/mcdbot-0.4.4.tar.gz/mcdbot-0.4.4/mcdbot/commands/unregister.py
@@ -0,0 +1,24 @@
+#  Copyright (C) 2020 Jakub Smetana <jakub/AT/smetana/DOT/ml>
+#  =========
+#  SPDX-License-Identifier: MPL-2.0
+#  ---------
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+#  =========
+
+from mcdbot.errors.http_like_errors import ForbiddenError
+from mcdbot.errors.object_errors import McPlayerNotFoundError
+from mcdbot.helpers import check_user
+from mcdbot.mcdbot import Mcdbot
+
+
+async def unregister(main: Mcdbot, msg, context, player: str):
+    if check_user(main, msg.author):
+        if main.redis.get_player_by_user(msg.author) == player:
+            await main.rcon.authme_unregister(player)
+            await main.redis.del_player(player, msg.author)
+        else:
+            raise McPlayerNotFoundError
+    else:
+        raise ForbiddenError
