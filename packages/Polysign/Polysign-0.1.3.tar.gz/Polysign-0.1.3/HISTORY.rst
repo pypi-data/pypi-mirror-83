ad69598a7df56ee135efc89cb17a5e130af69518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:50 +0000

    Add package Polysign/0.1.3/Polysign-0.1.3.tar.gz

---
+++ packages/Polysign/Polysign-0.1.3.tar.gz/Polysign-0.1.3/HISTORY.rst
@@ -0,0 +1,23 @@
+=======
+History
+=======
+
+0.1.3 (2020-10-19)
+------------------
+
+* Bugfix in testnet style addresses
+
+0.1.2 (2020-09-18)
+------------------
+
+* Cleanup V2 Bismuth DB
+
+0.1.1 (2020-09-17)
+------------------
+
+* Support for V2 Bismuth DB
+
+0.1.0 (2019-07-18)
+------------------
+
+* First release on PyPI.
---
+++ packages/Polysign/Polysign-0.1.3.tar.gz/Polysign-0.1.3/setup.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = ['pycryptodomex', 'ed25519', 'pynacl', 'base58', 'coincurve']
+
+setup_requirements = ['pytest-runner', ]
+
+test_requirements = ['pytest', ]
+
+setup(
+    author="EggdraSyl from Bismuth Foundation",
+    author_email='dev@eggpool.net',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    description="Polysign PIP module for Bismuth. Handles the various cryptographic primitives Bismuth Crypto currency supports.",
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='polysign',
+    name='Polysign',
+    packages=find_packages(include=['polysign']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/bismuthfoundation/polysign',
+    version='0.1.3',
+    zip_safe=False,
+)
