b382460f8f05acc593e6d7e92b173dad30b91b4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:19 +0000

    Add package kfc/0.2.2/kfc-0.2.2.tar.gz

---
+++ packages/kfc/kfc-0.2.2.tar.gz/kfc-0.2.2/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+setup(
+    name='kfc',
+    packages=['kfc', 'kfc/utils'],
+    version='0.2.2',
+    license='GNU General Public License v3.0 or later (GPL-3.0-or-later)',
+    description='A library to convert khmer unicode text to limon format and vice versa',
+    author='Socret Lee',
+    author_email='lee.socret@gmail.com',
+    url='https://github.com/Socret360',
+    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',
+    keywords=['KHMER', 'UNICODE', 'LIMON', 'CONVERTER'],
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3',
+    ],
+)
