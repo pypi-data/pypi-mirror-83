7f01be72f1e8bfb546c4c3f7c15e9a89263703d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:17 +0000

    Add package pipetools/0.3.6/pipetools-0.3.6.tar.gz

---
+++ packages/pipetools/pipetools-0.3.6.tar.gz/pipetools-0.3.6/setup.py
@@ -0,0 +1,53 @@
+import io
+import sys
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+
+from pipetools import xpartial, X
+import pipetools
+
+
+class PyTest(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        error_code = pytest.main(self.test_args)
+        sys.exit(error_code)
+
+
+setup(
+    name='pipetools',
+    version=pipetools.__versionstr__,
+    description=('A library that enables function composition similar to '
+        'using Unix pipes.'),
+    long_description='README.rst' > xpartial(io.open, X, encoding="utf-8") | X.read(),
+    author='Petr Pokorny',
+    author_email='petr@innit.cz',
+    license='MIT',
+    url='http://0101.github.com/pipetools/',
+    packages=['pipetools'],
+    include_package_data=True,
+    install_requires=(
+         'setuptools>=0.6b1',
+    ),
+    tests_require=(
+        'pytest',
+    ),
+    cmdclass={'test': PyTest},
+
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+    ]
+)
