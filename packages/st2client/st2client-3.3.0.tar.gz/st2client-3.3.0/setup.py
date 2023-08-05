9f5a2c49adc99c0f761d60e286f12066b985dfbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:39 +0000

    Add package st2client/3.3.0/st2client-3.3.0.tar.gz

---
+++ packages/st2client/st2client-3.3.0.tar.gz/st2client-3.3.0/setup.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python2.7
+# Copyright 2020 The StackStorm Authors.
+# Copyright 2019 Extreme Networks, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os.path
+
+from setuptools import setup, find_packages
+
+from dist_utils import check_pip_version
+from dist_utils import fetch_requirements
+from dist_utils import apply_vagrant_workaround
+
+from st2client import __version__
+
+check_pip_version()
+
+ST2_COMPONENT = 'st2client'
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+REQUIREMENTS_FILE = os.path.join(BASE_DIR, 'requirements.txt')
+
+install_reqs, dep_links = fetch_requirements(REQUIREMENTS_FILE)
+apply_vagrant_workaround()
+
+setup(
+    name=ST2_COMPONENT,
+    version=__version__,
+    description=('Python client library and CLI for the StackStorm (st2) event-driven '
+                 'automation platform.'),
+    author='StackStorm',
+    author_email='info@stackstorm.com',
+    license='Apache License (2.0)',
+    url='https://stackstorm.com/',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7'
+    ],
+    install_requires=install_reqs,
+    dependency_links=dep_links,
+    test_suite=ST2_COMPONENT,
+    zip_safe=False,
+    include_package_data=True,
+    packages=find_packages(exclude=['setuptools', 'tests']),
+    entry_points={
+        'console_scripts': [
+            'st2 = st2client.shell:main'
+        ]
+    }
+)
