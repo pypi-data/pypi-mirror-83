13a80c7132616a37c945374e9010a6aa110ea328
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:38 +0000

    Add package python-bufflog/0.1.1/python-bufflog-0.1.1.tar.gz

---
+++ packages/python-bufflog/python-bufflog-0.1.1.tar.gz/python-bufflog-0.1.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="python-bufflog",
+    version="0.1.1",
+    author="David Gasquez",
+    author_email="davidgasquez@buffer.com",
+    description="Python logger for Buffer services",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/bufferapp/python-bufflog",
+    install_requires=["structlog"],
+    packages=setuptools.find_packages(),
+)
