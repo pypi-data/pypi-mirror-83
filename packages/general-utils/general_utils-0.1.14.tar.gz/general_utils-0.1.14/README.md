c4e3c5c108ac38caaf4d12d0ad3b3b6cfda679b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:11 +0000

    Add package general-utils/0.1.14/general_utils-0.1.14.tar.gz

---
+++ packages/general-utils/general_utils-0.1.14.tar.gz/general_utils-0.1.14/README.md
@@ -0,0 +1,23 @@
+# General Utilities
+
+A General Utilities Library that provides a Python interface to many technologies
+
+Available **Objects**:
+1. RabbitMQ for queueing purpose
+2. Redis as a secondary storage
+3. MinIO for storing and retrieving files
+4. API requests module
+
+Available **Methods**:
+1. Logging module
+
+Kindly reffer the docstrings for further details reagriding the objects and methods :
+
+```python
+from general_utils import utils
+
+help(utils)
+##-- OR --##
+print(utils.__doc__)
+```
+
---
+++ packages/general-utils/general_utils-0.1.14.tar.gz/general_utils-0.1.14/setup.py
@@ -0,0 +1,39 @@
+import os
+from setuptools import setup, find_packages
+from pip._internal.req import parse_requirements
+
+dirname = os.path.dirname(__file__)
+requirements_path = os.path.join(dirname, "general_utils/requirements.txt")
+readme_path = os.path.join(dirname, "README.md")
+
+
+with open(readme_path, "r") as readme_file:
+    readme = readme_file.read()
+
+
+
+# parse_requirements() returns generator of pip.req.InstallRequirement objects
+install_reqs = parse_requirements(requirements_path, session=False)
+
+# for each in install_reqs:
+#     print(each.__dict__)
+
+reqs = [str(ir.requirement) for ir in install_reqs]
+
+
+setup(
+    name="general_utils",
+    version="0.1.14",
+    author="Kashyap Madariyil",
+    author_email="kashyapmadariyil@gmail.com",
+    description="A general package that has useful functionalities",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kashy750/python_package",
+    packages=find_packages(),
+    install_requires=reqs,
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
