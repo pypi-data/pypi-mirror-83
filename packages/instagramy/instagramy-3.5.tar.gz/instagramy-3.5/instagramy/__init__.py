48223372cec74532e841493bab206d4955da1b3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:15 +0000

    Add package instagramy/3.5/instagramy-3.5.tar.gz

---
+++ packages/instagramy/instagramy-3.5.tar.gz/instagramy-3.5/instagramy/__init__.py
@@ -0,0 +1,4 @@
+from instagramy.InstagramHashTag import InstagramHashTag
+from instagramy.InstagramPost import InstagramPost
+from instagramy.InstagramUser import InstagramUser
+from instagramy.InstagramChecks import check_username, check_email, suggest_username
