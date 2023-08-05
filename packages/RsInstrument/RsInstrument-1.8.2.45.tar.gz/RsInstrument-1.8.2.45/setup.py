bc3e0f34faf9b92e5fb9ecd2260dfc7c6f9d1100
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:30 +0000

    Add package RsInstrument/1.8.2.45/RsInstrument-1.8.2.45.tar.gz

---
+++ packages/RsInstrument/RsInstrument-1.8.2.45.tar.gz/RsInstrument-1.8.2.45/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="RsInstrument",
+    version="1.8.2.45",
+    description="VISA or Socket communication module for Rohde & Schwarz instruments",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="Miloslav Macko",
+    author_email="miloslav.macko@rohde-schwarz.com",
+	url="https://gitlab.com/miki2-2-2/rsinstrument",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=(find_packages(include=['RsInstrument', 'RsInstrument.*'])),
+    install_requires=["PyVisa"]
+)
