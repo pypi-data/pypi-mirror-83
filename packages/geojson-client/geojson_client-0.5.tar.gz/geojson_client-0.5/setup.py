7a5ac0822a6ec73a766ba0dd2c5f80196aa112cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:41 +0000

    Add package geojson-client/0.5/geojson_client-0.5.tar.gz

---
+++ packages/geojson-client/geojson_client-0.5.tar.gz/geojson_client-0.5/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+REQUIRES = [
+    'geojson>=2.4.0',
+    'haversine>=1.0.1',
+    'pytz>=2018.04',
+    'requests>=2.20.0',
+]
+
+setup(
+    name="geojson_client",
+    version="0.5",
+    author="Malte Franken",
+    author_email="coding@subspace.de",
+    description="A GeoJSON client library.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/exxamalte/python-geojson-client",
+    packages=find_packages(exclude=("tests*",)),
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=REQUIRES
+)
