a9585e4f978379cfbc999782884c449d45a9a1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:32 +0000

    Add package telegram-menu/0.2.2/telegram_menu-0.2.2.tar.gz

---
+++ packages/telegram-menu/telegram_menu-0.2.2.tar.gz/telegram_menu-0.2.2/setup.py
@@ -0,0 +1,40 @@
+"""telegram_menu package installer."""
+
+import os
+
+from setuptools import find_packages, setup
+
+with open(os.path.join("README.md"), "r") as fh:
+    LONG_DESCRIPTION = fh.read()
+
+__title__ = "telegram_menu"
+__version__ = "0.2.2"
+
+setup(
+    name=__title__,
+    version=__version__,
+    description="A python library to generate navigation menus using Telegram Bot API",
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mevellea/telegram_menu",
+    author="Armel MEVELLEC",
+    author_email="mevellea@gmail.com",
+    license="GNU GPLv3",
+    packages=find_packages(),
+    install_requires=[],
+    tests_require=["tox>=3.5.0,<4.0.0"],
+    platforms=["any"],
+    keywords="telegram",
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Home Automation",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+)
