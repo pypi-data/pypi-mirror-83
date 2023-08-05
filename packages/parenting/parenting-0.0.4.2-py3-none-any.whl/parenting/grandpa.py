f1622ad9408a04440b788713e61614ba65251124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:20 +0000

    Add package parenting/0.0.4.2/parenting-0.0.4.2-py3-none-any.whl

---
+++ packages/parenting/parenting-0.0.4.2-py3-none-any.whl/parenting/grandpa.py
@@ -0,0 +1,97 @@
+# import libs
+from wasabi import Printer
+
+# start the grandpa class
+class grandpa():
+
+    # define class structure
+    def __init__(self, name=None):
+
+        # object slots
+        self.name = name
+
+        # check if name was given
+        if self.name is None:
+
+            # set name to default
+            self.name = "Grandpa"
+
+        # initialize Printer
+        msg = Printer()
+
+        # print heading
+        msg.divider(str("Welcome " + self.name))
+
+        # create info section
+        info_section = """
+
+Congrats to becoming a grandpa. I bet you can still remember the day you 
+became a father and now out of the sudden you are a grandfather. Be there for
+your kids and your grandkids, they need you now, you got the experince and 
+you had more time to practice your dad jokes.
+
+To start the advice section, you can just run the advice() method and I will 
+guide you, hoping that you can please your little wonder.
+
+Cheers and keep it up.
+
+        """
+
+         # print info
+        print (info_section)
+
+    # define advice function
+    def get_advice(self):
+
+        # initialize Printer
+        msg = Printer()
+
+        # print info message
+        msg.info("Okay, " + self.name + " keep calm, I will guide you")
+
+        # check the sex of the baby
+        sex = input("Are you searching for adivce on a boy or a girl? ")
+
+        # check if fed properly
+        fed = input(str("Was your " + sex + 
+                        " fed properly in the last 2 hours? (yes/no) "))
+
+        # evaluate if baby is maybe hungry
+        if fed=="no":
+            
+            # suggest trying to feed the baby
+            msg.warn("Maybe your " + sex + " is hungry, try some nice food!")
+
+            # break out of function
+            return None
+
+        # check if slept properly
+        slept = input(str("Has your " + sex +
+                          " slept properly in the last 1.5 hours? (yes/no) "))
+
+        # evaluate if baby is maybe tired
+        if slept=="no":
+
+            # suggest trying to put the baby to sleep
+            msg.warn("Maybe your " + sex + " is tired and wants to sleep!")
+
+            # break out of function
+            return None    
+
+        # check if baby is maybe bored
+        entertained = input(str("Is your " + sex + 
+                                " active and wants to interact? (yes/no)"))
+        
+        # evaluate if baby might be bored
+        if entertained=="yes":
+
+            # suggest to talk to the baby
+            msg.warn("Maybe your " + sex +
+                     " is bored and you could sit down and have a chat.")
+
+            # break out of function
+            return None
+
+        # if nothing detected so far, give advice
+        msg.info(str("Your little " + sex +
+                " neither hungry, nor tired, nor bored, just cuddle!"))
---
+++ packages/parenting/parenting-0.0.4.2-py3-none-any.whl/parenting/uncle.py
@@ -0,0 +1,96 @@
+# import libs
+from wasabi import Printer
+
+# start the uncle class
+class uncle():
+
+    # define class structure
+    def __init__(self, name=None):
+
+        # object slots
+        self.name = name
+
+        # check if name was given
+        if self.name is None:
+
+            # set name to default
+            self.name = "Uncle"
+
+        # initialize Printer
+        msg = Printer()
+
+        # print heading
+        msg.divider(str("Welcome " + self.name))
+
+        # create info section
+        info_section = """
+
+Congrats to becoming an uncle. You must be really excited about your little
+baby friend. It is often said, that raising a child takes a village. So be
+there for the little baby friend and help him by being the cool uncle.
+
+To start the advice section, you can just run the advice() method and I will 
+guide you, hoping that you can please your little wonder.
+
+Cheers and keep it up.
+
+        """
+
+         # print info
+        print (info_section)
+
+    # define advice function
+    def get_advice(self):
+
+        # initialize Printer
+        msg = Printer()
+
+        # print info message
+        msg.info("Okay, " + self.name + " keep calm, I will guide you")
+
+        # check the sex of the baby
+        sex = input("Are you searching for adivce on a boy or a girl? ")
+
+        # check if fed properly
+        fed = input(str("Was your " + sex + 
+                        " fed properly in the last 2 hours? (yes/no) "))
+
+        # evaluate if baby is maybe hungry
+        if fed=="no":
+            
+            # suggest trying to feed the baby
+            msg.warn("Maybe your " + sex + " is hungry, try some nice food!")
+
+            # break out of function
+            return None
+
+        # check if slept properly
+        slept = input(str("Has your " + sex +
+                          " slept properly in the last 1.5 hours? (yes/no) "))
+
+        # evaluate if baby is maybe tired
+        if slept=="no":
+
+            # suggest trying to put the baby to sleep
+            msg.warn("Maybe your " + sex + " is tired and wants to sleep!")
+
+            # break out of function
+            return None    
+
+        # check if baby is maybe bored
+        entertained = input(str("Is your " + sex + 
+                                " active and wants to interact? (yes/no)"))
+        
+        # evaluate if baby might be bored
+        if entertained=="yes":
+
+            # suggest to talk to the baby
+            msg.warn("Maybe your " + sex +
+                     " is bored and you could sit down and have a chat.")
+
+            # break out of function
+            return None
+
+        # if nothing detected so far, give advice
+        msg.info(str("Your little " + sex +
+                " neither hungry, nor tired, nor bored, just cuddle!"))
