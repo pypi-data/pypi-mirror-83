d3eca2ec7cca3ff6b70b199789a4987f2dec0150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:03 +0000

    Add package pymarker/0.1.0/pymarker-0.1.0-py3-none-any.whl

---
+++ packages/pymarker/pymarker-0.1.0-py3-none-any.whl/pymarker/cli.py
@@ -0,0 +1,28 @@
+import click
+from .core import generate_marker, generate_patt
+
+@click.command()
+@click.argument('filename')
+@click.option('--patt','-p', is_flag=True, default=False)
+@click.option('--marker','-m', is_flag=True, default=False)
+@click.option('--border-size', '-b', default=50) # 50% is based on template hiro marker
+def generate_patt_and_marker(filename, patt, marker, border_size):
+    click.echo("-- Starting PyMarker Generator --".format(filename))
+    if (patt and marker) or (not patt and not marker):
+        generate_patt(filename)
+        generate_marker(filename, border_size)
+        click.echo("Generating patt and marker for {}".format(filename))
+    elif marker:
+        generate_marker(filename, border_size)
+        click.echo("Generating marker for {}".format(filename))
+    elif patt:
+        generate_patt(filename)
+        click.echo("Generating patt for {}".format(filename))
+
+    click.echo("Done.")
+
+def main():
+    generate_patt_and_marker()
+
+if __name__ == "__main__":
+    main()
---
+++ packages/pymarker/pymarker-0.1.0-py3-none-any.whl/pymarker/core.py
@@ -0,0 +1,59 @@
+from .utils import open_image, get_box_coords, remove_extension
+from PIL import Image
+from math import ceil
+
+def get_marker_size(image, border_size):
+    size = image.height + (border_size*2)
+    # Using the size double because the resulting marker should be a square.
+    return (size,size)
+
+def create_empty_patt(filename):
+    patt_name = remove_extension(filename)+".patt"
+    patt_file = open(patt_name, "w")
+    patt_file.close()
+    return patt_name
+
+def generate_patt(filename):
+    image = open_image(filename)
+    # Patt default marker size is 16x16 pixels
+    image = image.resize((16,16))
+    
+    patt_name = create_empty_patt(filename)
+    patt = open(patt_name,"a")
+    for _ in range(0,4):
+        r, g, b = image.split()
+        color_to_file(r, patt)
+        color_to_file(g, patt)
+        color_to_file(b, patt)
+        patt.write('\n')
+        image = image.rotate(90)
+
+    patt.close()
+    return True
+
+def patt_number_format(point):
+    return str(point).rjust(3,' ')    
+
+# Prints all pixels from a split color to the patt file
+def color_to_file(c, patt):
+        n = 1
+        for point in list(c.getdata()):
+            patt.write(patt_number_format(point))
+            if(n != 0 and n % 16 == 0):
+                n = 0
+                patt.write('\n')
+            else:
+                patt.write(' ')
+            n += 1
+        
+def generate_marker(filename, border_percentage):
+    image = open_image(filename)
+
+    border_size = ceil(image.height * (border_percentage/100))
+
+    # Default color is black, setting (0, 0, 0) for clarity, as the border should be black
+    marker_size = get_marker_size(image, border_size)
+    marker = Image.new('RGB', marker_size, (0, 0, 0))
+    marker.paste(image,get_box_coords(image,border_size))
+    marker.save(remove_extension(filename)+"_marker.png", "PNG")
+    return True
