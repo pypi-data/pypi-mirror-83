c6c0e7e9519f74b1974b0c5f72e877bbc36775de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:21 +0000

    Add package django-kelove-admin-lte/0.1.3/django_kelove_admin_lte-0.1.3-py3-none-any.whl

---
+++ packages/django-kelove-admin-lte/django_kelove_admin_lte-0.1.3-py3-none-any.whl/django_kelove_admin_lte/__init__.py
@@ -0,0 +1,11 @@
+"""
+__init__.py
+By IT小强xqitw.cn <mail@xqitw.cn>
+At 2020-08-24 4:42 PM
+"""
+
+PACKAGE_VERSION = '0.1.3'
+
+VERSION = tuple(PACKAGE_VERSION.split("."))
+
+default_app_config = "django_kelove_admin_lte.apps.DjangoKeloveAdminLteConfig"
