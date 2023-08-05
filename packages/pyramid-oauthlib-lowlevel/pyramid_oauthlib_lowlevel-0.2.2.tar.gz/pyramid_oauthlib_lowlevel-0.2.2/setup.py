caa91604ddb8e6fd92b5e54d4d380b166156a656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:31 +0000

    Add package pyramid-oauthlib-lowlevel/0.2.2/pyramid_oauthlib_lowlevel-0.2.2.tar.gz

---
+++ packages/pyramid-oauthlib-lowlevel/pyramid_oauthlib_lowlevel-0.2.2.tar.gz/pyramid_oauthlib_lowlevel-0.2.2/setup.py
@@ -0,0 +1,78 @@
+"""pyramid_oauthlib_lowlevel installation script.
+"""
+import os
+import re
+import sys
+
+from setuptools import setup
+from setuptools import find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+long_description = description = "lowlevel Pyramid implementation of oauthlib"
+
+
+with open(os.path.join(here, "README.md")) as fp:
+    long_description = fp.read()
+
+# store version in the init.py
+with open(
+    os.path.join(here, "pyramid_oauthlib_lowlevel", "__init__.py")
+) as v_file:
+    VERSION = re.compile(r'.*__VERSION__ = "(.*?)"', re.S).match(v_file.read()).group(1)
+
+requires = [
+    "oauthlib",
+    "pyramid",
+]
+tests_require = [
+    "pyramid_formencode_classic",
+    "pyramid_mako",
+    "pyramid_tm",
+    "pyramid",
+    "pytest",
+    "requests_oauthlib",
+    "requests",
+    "responses",
+    "sqlalchemy",
+    "webtest",
+    "zope.sqlalchemy",
+]
+if sys.version_info[0] == 2:
+    # last known twython version to support py27
+    tests_require.append("twython==3.7.0")
+else:
+    tests_require.append("twython")
+testing_extras = tests_require + []
+
+
+setup(
+    name="pyramid_oauthlib_lowlevel",
+    version=VERSION,
+    author="Jonathan Vanasco",
+    author_email="jonathan@findmeon.com",
+    url="https://github.com/jvanasco/pyramid_oauthlib_lowlevel",
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Framework :: Pyramid",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+    ],
+    keywords="web pyramid oauth oauthlib",
+    py_modules=["pyramid_oauthlib_lowlevel"],
+    license="BSD",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    tests_require=tests_require,
+    extras_require={
+        "testing": testing_extras,
+    },
+    test_suite="pyramid_oauthlib_lowlevel.tests",
+)
