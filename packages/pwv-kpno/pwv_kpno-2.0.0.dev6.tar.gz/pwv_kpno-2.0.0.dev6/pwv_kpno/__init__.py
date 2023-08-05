f0a25b395bcdc7a155f40d2a38ff408404599ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:29 +0000

    Add package pwv-kpno/2.0.0.dev6/pwv_kpno-2.0.0.dev6.tar.gz

---
+++ packages/pwv-kpno/pwv_kpno-2.0.0.dev6.tar.gz/pwv_kpno-2.0.0.dev6/pwv_kpno/__init__.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+#    This file is part of the pwv_kpno software package.
+#
+#    The pwv_kpno package is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    The pwv_kpno package is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+#    Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with pwv_kpno.  If not, see <http://www.gnu.org/licenses/>.
+
+"""``pwv_kpno`` provides models for the atmospheric transmission function due
+to precipitable water vapor (PWV). Using PWV measurements published by the
+SuomiNet project (https://www.suominet.ucar.edu), the package provides access
+to the modeled PWV concentration and/or atmospheric transmission for a given
+date and time at customizable geographic locations. The atmospheric
+transmission can also be determined manually for a given set of atmospheric
+parameters.
+
+Package Modules
+---------------
+
++---------------------+---------------------------------------------------------+
+| Module              | Description                                             |
++=====================+=========================================================+
+| :ref:`defaults`     | Prebuilt receiver objects and transmission models.      |
++---------------------+---------------------------------------------------------+
+| :ref:`downloads`    | Handles the downloading of data from SuomiNet servers.  |
++---------------------+---------------------------------------------------------+
+| :ref:`file_parsing` | Parses files written in the the SuomiNet file format.   |
++---------------------+---------------------------------------------------------+
+| :ref:`gps_pwv`      | PWV modeling and data access for given GPS locations.   |
++---------------------+---------------------------------------------------------+
+| :ref:`transmission` | Models the PWV atmospheric transmission function.       |
++---------------------+---------------------------------------------------------+
+| :ref:`types`        | Custom object types for PEP 484 support.                |
++---------------------+---------------------------------------------------------+
+"""
+
+from .gps_pwv import PWVModel
+
+__authors__ = ['MWV Research Group']
+__copyright__ = 'Copyright 2017, MWV Research Group'
+__credits__ = [
+    'Daniel Perrefort'
+    'W. M. Wood-Vasey',
+    'K. Azalee Bostroem',
+    'Jessica Kroboth',
+    'Tom Matheson',
+]
+
+__license__ = 'GPL V3'
+__version__ = '2.0.0.dev6'
+__email__ = 'djperrefort@pitt.edu'
+__status__ = 'Release'
---
+++ packages/pwv-kpno/pwv_kpno-2.0.0.dev6.tar.gz/pwv_kpno-2.0.0.dev6/setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+#    This file is part of the pwv_kpno software package.
+#
+#    The pwv_kpno package is free software: you can redistribute it and/or
+#    modify it under the terms of the GNU General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    The pwv_kpno package is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+#    Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with pwv_kpno.  If not, see <http://www.gnu.org/licenses/>.
+
+# Because I know I will be looking for this thread later on:
+# https://stackoverflow.com/q/7522250
+
+import re
+from pathlib import Path
+
+from setuptools import setup
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+
+def long_description():
+    with open('README.md') as ofile:
+        readme = ofile.readlines()
+
+    description = ' '.join(readme[13:])
+    return description
+
+
+# Get package version
+init_path = Path(__file__).resolve().parent / 'pwv_kpno/__init__.py'
+with open(init_path, 'r') as f:
+    s = f.read()
+
+versionRegExp = re.compile("__version__ = '(.*?)'")
+__version__ = versionRegExp.findall(s)[0]
+
+setup(name='pwv_kpno',
+      version=__version__,
+      packages=['pwv_kpno'],
+      keywords='KPNO atmospheric transmission PWV precipitable water vapor',
+      description='Models the atmospheric transmission function for KPNO',
+      long_description=long_description(),
+      long_description_content_type='text/markdown',
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+          'Topic :: Scientific/Engineering :: Astronomy',
+          'Topic :: Scientific/Engineering :: Atmospheric Science',
+          'Topic :: Scientific/Engineering :: Physics'
+      ],
+
+      author='Daniel Perrefort',
+      author_email='djperrefort@pitt.edu',
+      url='https://mwvgroup.github.io/pwv_kpno/',
+      license='GPL v3',
+
+      python_requires='>=3.6',
+      install_requires=requirements,
+
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      include_package_data=True)
