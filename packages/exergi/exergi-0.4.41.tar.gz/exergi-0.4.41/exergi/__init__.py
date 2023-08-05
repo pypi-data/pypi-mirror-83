b30d1212f453515f4237b4922b0a76cc3cef075f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:55 +0000

    Add package exergi/0.4.41/exergi-0.4.41.tar.gz

---
+++ packages/exergi/exergi-0.4.41.tar.gz/exergi-0.4.41/exergi/__init__.py
@@ -0,0 +1,9 @@
+"""Imports all modules in the exergi package."""
+
+__author__ = "Kasper Janehag, Johan Wållgren"
+__version__ = "0.4.41"
+
+import exergi.aws.s3
+import exergi.aws.rds
+import exergi.plot
+import exergi.logging
