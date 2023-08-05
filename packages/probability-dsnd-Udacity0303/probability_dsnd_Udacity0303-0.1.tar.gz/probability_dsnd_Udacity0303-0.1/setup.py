f63ec62c52c8d16e292b5b2711bacd785a5b7f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:43 +0000

    Add package probability-dsnd-Udacity0303/0.1/probability_dsnd_Udacity0303-0.1.tar.gz

---
+++ packages/probability-dsnd-Udacity0303/probability_dsnd_Udacity0303-0.1.tar.gz/probability_dsnd_Udacity0303-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_dsnd_Udacity0303',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['probability_dsnd_Udacity0303'],
+      author = 'James Bernard Jr',
+      author_email = 'j_bernard0303@yahoo.com',
+      zip_safe=False)
