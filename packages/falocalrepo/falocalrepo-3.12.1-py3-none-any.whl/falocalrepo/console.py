2e282835837708fc1853e49095dd5af6a1a605b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:14 +0000

    Add package falocalrepo/3.12.1/falocalrepo-3.12.1-py3-none-any.whl

---
+++ packages/falocalrepo/falocalrepo-3.12.1-py3-none-any.whl/falocalrepo/console.py
@@ -0,0 +1,455 @@
+from datetime import datetime
+from inspect import cleandoc
+from os import chdir
+from os import environ
+from os.path import abspath
+from os.path import dirname
+from os.path import getsize
+from os.path import join
+from re import match
+from sqlite3 import Connection
+from typing import Dict
+from typing import Iterable
+from typing import List
+from typing import Tuple
+
+from faapi import FAAPI
+from falocalrepo_database import __version__ as __database_version__
+from falocalrepo_database import add_history
+from falocalrepo_database import check_errors
+from falocalrepo_database import connect_database
+from falocalrepo_database import count
+from falocalrepo_database import delete
+from falocalrepo_database import edit_user_remove_journal
+from falocalrepo_database import edit_user_remove_submission
+from falocalrepo_database import find_user_from_journal
+from falocalrepo_database import find_user_from_submission
+from falocalrepo_database import journals_indexes
+from falocalrepo_database import journals_table
+from falocalrepo_database import make_tables
+from falocalrepo_database import merge_database
+from falocalrepo_database import read_history
+from falocalrepo_database import read_setting
+from falocalrepo_database import save_journal
+from falocalrepo_database import save_submission
+from falocalrepo_database import search_journals
+from falocalrepo_database import search_submissions
+from falocalrepo_database import search_users
+from falocalrepo_database import submissions_indexes
+from falocalrepo_database import submissions_table
+from falocalrepo_database import update_database
+from falocalrepo_database import users_indexes
+from falocalrepo_database import users_table
+from falocalrepo_database import vacuum
+from falocalrepo_database import write_setting
+from falocalrepo_server import __version__ as __server_version__
+from falocalrepo_server import server
+
+from .__version__ import __version__
+from .commands import latest_version
+from .commands import make_journal
+from .commands import make_submission
+from .commands import move_files_folder
+from .commands import print_items
+from .commands import print_users
+from .download import clean_username
+from .download import download_journals
+from .download import download_submissions
+from .download import download_users
+from .download import download_users_update
+from .download import load_cookies
+from .download import read_cookies
+from .download import write_cookies
+
+database_path: str
+
+
+class MalformedCommand(Exception):
+    pass
+
+
+class UnknownCommand(Exception):
+    pass
+
+
+def parameters_multi(args: Iterable[str]) -> Dict[str, List[str]]:
+    params: Dict[str, List[str]] = {}
+    for param, value in map(lambda p: p.split("=", 1), args):
+        param = param.strip()
+        params[param] = [*params.get(param, []), value]
+
+    return params
+
+
+def parameters(args: Iterable[str]) -> Dict[str, str]:
+    return {p: v for p, v in map(lambda p: p.split("=", 1), args)}
+
+
+def parse_args(args_raw: Iterable[str]) -> Tuple[Dict[str, str], List[str]]:
+    opts: List[str] = []
+    args: List[str] = []
+
+    for i, arg in enumerate(args_raw):
+        if match(r"\w+=\w+", arg):
+            opts.append(arg)
+        elif arg == "--":
+            args.extend(args_raw[i + 1:])
+            break
+        else:
+            args.extend(args_raw[i:])
+            break
+
+    return parameters(opts), args
+
+
+def check_update(version: str, package: str):
+    if (latest := latest_version(package)) and latest != version:
+        print(f"New {package} version available: {latest} > {version}")
+
+
+def help_(comm: str = None, *_args: str) -> str:
+    """
+    USAGE
+        falocalrepo help [<setting>]
+
+    ARGUMENTS
+        <command>       Command to get the help of
+
+    AVAILABLE COMMANDS
+        help            Display the manual of help
+        init            Display the manual of init
+        config          Display the manual of config
+        download        Display the manual of download
+        database        Display the manual of database
+    """
+
+    if not comm:
+        return cleandoc(console.__doc__)
+    elif comm == help_.__name__.rstrip("_"):
+        return cleandoc(help_.__doc__)
+    elif comm == init.__name__:
+        return cleandoc(console.__doc__)
+    elif comm == config.__name__:
+        return cleandoc(config.__doc__)
+    elif comm == download.__name__:
+        return cleandoc(download.__doc__)
+    elif comm == database.__name__:
+        return cleandoc(database.__doc__)
+    else:
+        raise UnknownCommand(comm)
+
+
+def init():
+    print("Database ready")
+
+
+def config(db: Connection, comm: str = "", *args: str):
+    """
+    USAGE
+        falocalrepo config [<setting>] [<value1>] ... [<valueN>]
+
+    ARGUMENTS
+        <setting>       Setting to read/edit
+        <value>         New setting value
+
+    AVAILABLE SETTINGS
+        list            List settings
+        cookies         Cookies for the API
+        files-folder    Files download folder
+    """
+
+    if not comm or comm == "list":
+        cookie_a, cookie_b = read_cookies(db)
+        folder: str = read_setting(db, "FILESFOLDER")
+        print("cookie a:", cookie_a)
+        print("cookie b:", cookie_b)
+        print("folder  :", folder)
+    elif comm == "cookies":
+        if not args:
+            cookie_a, cookie_b = read_cookies(db)
+            print("cookie a:", cookie_a)
+            print("cookie b:", cookie_b)
+        elif len(args) == 2:
+            write_cookies(db, args[0], args[1])
+        else:
+            raise MalformedCommand("cookies needs two arguments")
+    elif comm == "files-folder":
+        if not args:
+            print("files folder:", read_setting(db, "FILESFOLDER"))
+        elif len(args) == 1:
+            move_files_folder(db, read_setting(db, "FILESFOLDER"), args[0])
+        else:
+            raise MalformedCommand("files-folder needs one argument")
+    else:
+        raise UnknownCommand(f"config {comm}")
+
+
+def download(db: Connection, comm: str = "", *args: str):
+    """
+    USAGE
+        falocalrepo download <command> [<option>=<value>] [<arg1>] ... [<argN>]
+
+    ARGUMENTS
+        <command>       The type of download to execute
+        <arg>           Argument for the download command
+
+    AVAILABLE COMMANDS
+        update          Update database using the users and folders already saved
+        users           Download users
+        submissions     Download single submissions
+        journals        Download single journals
+    """
+
+    if not comm:
+        raise MalformedCommand("download needs a command")
+
+    print((s := "Connecting... "), end="", flush=True)
+    api: FAAPI = FAAPI()
+    load_cookies(api, *read_cookies(db))
+    print("\r" + (" " * len(s)), end="\r", flush=True)
+
+    if not api.connection_status:
+        raise ConnectionError("FAAPI cannot connect to FA")
+    elif comm == "update":
+        users: List[str] = []
+        folders: List[str] = []
+        opts, args = parse_args(args)
+        if args and args[0] != "@":
+            users_tmp: List[str] = list(filter(bool, map(clean_username, args[0].split(","))))
+            users = sorted(set(users_tmp), key=users_tmp.index)
+        if args[1:] and args[1] != "@":
+            folders_tmp: List[str] = list(filter(bool, map(str.strip, args[1].split(","))))
+            folders = sorted(set(folders_tmp), key=folders_tmp.index)
+        download_users_update(api, db, users, folders, int(opts.get("stop", 1)))
+    elif comm == "users":
+        if len(args) != 2:
+            raise MalformedCommand("users needs two arguments")
+        users_tmp: List[str] = list(filter(bool, map(clean_username, args[0].split(","))))
+        users: List[str] = sorted(set(users_tmp), key=users_tmp.index)
+        folders_tmp: List[str] = list(filter(bool, map(str.strip, args[1].split(","))))
+        folders: List[str] = sorted(set(folders_tmp), key=folders_tmp.index)
+        download_users(api, db, users, folders)
+    elif comm == "submissions":
+        if not args:
+            raise MalformedCommand("submissions needs at least one argument")
+        sub_ids_tmp: List[str] = list(filter(str.isdigit, args))
+        sub_ids: List[str] = sorted(set(sub_ids_tmp), key=sub_ids_tmp.index)
+        download_submissions(api, db, sub_ids)
+    elif comm == "journals":
+        if not args:
+            raise MalformedCommand("journals needs at least one argument")
+        journal_ids_tmp: List[str] = list(filter(str.isdigit, args))
+        journal_ids: List[str] = sorted(set(journal_ids_tmp), key=journal_ids_tmp.index)
+        download_journals(api, db, journal_ids)
+    else:
+        raise UnknownCommand(f"download {comm}")
+
+
+def database(db: Connection, comm: str = "", *args: str):
+    """
+    USAGE
+        falocalrepo database [<operation>] [<param1>=<value1>] ...
+                    [<paramN>=<valueN>]
+
+    ARGUMENTS
+        <command>          The database operation to execute
+        <param>            Parameter for the database operation
+        <value>            Value of the parameter
+
+    AVAILABLE COMMANDS
+        info               Show database information
+        history            Show commands history
+        search-users       Search users
+        search-submissions Search submissions
+        search-journals    Search journals
+        add-submission     Add a submission to the database manually
+        add-journal        Add a journal to the database manually
+        remove-users       Remove users from database
+        remove-submissions Remove submissions from database
+        remove-journals    Remove submissions from database
+        server             Start local server to browse database
+        check-errors       Check the database for errors
+        merge              Merge with a second database
+        clean              Clean the database with the VACUUM function
+    """
+    global database_path
+
+    if not comm or comm == "info":
+        size: int = getsize(database_path)
+        sub_n: int = int(read_setting(db, "SUBN"))
+        usr_n: int = int(read_setting(db, "USRN"))
+        jrn_n: int = int(read_setting(db, "JRNN"))
+        history: List[Tuple[float, str]] = read_history(db)
+        version: str = read_setting(db, "VERSION")
+        print("Size        :", f"{size / 1e6:.1f}MB")
+        print("Submissions :", sub_n)
+        print("Users       :", usr_n)
+        print("Journals    :", jrn_n)
+        print("History     :", len(history) - 1)
+        print("Version     :", version)
+    elif comm == "history":
+        for time, command in read_history(db):
+            print(str(datetime.fromtimestamp(float(time))), command)
+    elif comm == "search-users":
+        results: List[tuple] = search_users(db, **{"order": ["USERNAME"], **parameters_multi(args)})
+        print_users(results, users_indexes)
+        print(f"Found {len(results)} results")
+    elif comm == "search-submissions":
+        results: List[tuple] = search_submissions(db, **{"order": ["ID"], **parameters_multi(args)})
+        print_items(results, submissions_indexes)
+        print(f"Found {len(results)} results")
+    elif comm == "search-journals":
+        results: List[tuple] = search_journals(db, **{"order": ["ID"], **parameters_multi(args)})
+        print_items(results, journals_indexes)
+        print(f"Found {len(results)} results")
+    elif comm == "add-submission":
+        make_params = parameters(args)
+        make_params["id_"] = make_params.get("id", "")
+        if "id" in make_params:
+            del make_params["id"]
+        sub, sub_file = make_submission(**make_params)
+        save_submission(db, dict(sub), sub_file)
+    elif comm == "add-journal":
+        make_params = parameters(args)
+        make_params["id_"] = make_params.get("id", "")
+        if "id" in make_params:
+            del make_params["id"]
+        save_journal(db, dict(make_journal(**make_params)))
+    elif comm == "remove-users":
+        for user in map(clean_username, args):
+            print("Deleting", user)
+            delete(db, users_table, "USERNAME", user)
+            db.commit()
+    elif comm == "remove-submissions":
+        for sub in args:
+            print("Deleting", sub)
+            delete(db, submissions_table, "ID", int(sub))
+            for (user, *_) in find_user_from_submission(db, int(sub)).fetchall():
+                edit_user_remove_submission(db, user, int(sub))
+            db.commit()
+    elif comm == "remove-journals":
+        for jrn in args:
+            print("Deleting", jrn)
+            delete(db, journals_table, "ID", int(jrn))
+            for (user, *_) in find_user_from_journal(db, int(jrn)).fetchall():
+                edit_user_remove_journal(db, user, int(jrn))
+            db.commit()
+    elif comm == "server":
+        opts, _ = parse_args(args)
+        server(database_path, **opts)
+        print()
+    elif comm == "check-errors":
+        print("Checking submissions table for errors... ", end="", flush=True)
+        results: List[tuple] = check_errors(db, submissions_table)
+        print("Done")
+        if results:
+            print_items(results, submissions_indexes)
+        print("Checking journals table for errors... ", end="", flush=True)
+        results: List[tuple] = check_errors(db, journals_table)
+        print("Done")
+        if results:
+            print_items(results, journals_indexes)
+    elif comm == "merge":
+        if len(args) != 1:
+            raise MalformedCommand("merge needs one argument")
+        db2_path: str = args[0]
+        with connect_database(db2_path) as db2:
+            print(f"Merging with database {db2_path}...")
+            merge_database(db, ".", db2, dirname(db2_path))
+            print("Done")
+    elif comm == "clean":
+        vacuum(db)
+    else:
+        raise UnknownCommand(f"database {comm}")
+
+
+def console(comm: str = "", *args: str) -> None:
+    """
+    USAGE
+        falocalrepo [-h] [-v] [-d] [<command>] [<arg1>] ... [<argN>]
+
+    ARGUMENTS
+        <command>       The command to execute
+        <arg>           The arguments of the command
+
+    GLOBAL OPTIONS
+        -h, --help      Display this help message
+        -v, --version   Display version
+        -d, --database  Display database version
+        -s, --server    Display server version
+
+    AVAILABLE COMMANDS
+        help            Display the manual of a command
+        init            Create/update the database and exit
+        config          Manage settings
+        download        Perform downloads
+        database        Operate on the database
+    """
+    global database_path
+
+    console.__doc__ = f"""
+    falocalrepo: {__version__}
+    falocalrepo-database: {__database_version__}
+    falocalrepo-server: {__server_version__}
+    """ + console.__doc__
+
+    if not comm:
+        print(help_())
+        return
+    elif comm in ("-h", "--help"):
+        print(help_())
+        return
+    elif comm == "help":
+        print(help_(*args))
+        return
+    elif comm in ("-v", "--version"):
+        print(__version__)
+        return
+    elif comm in ("-d", "--database"):
+        print(__database_version__)
+        return
+    elif comm in ("-s", "--server"):
+        print(__server_version__)
+        return
+    elif comm not in (init.__name__, config.__name__, download.__name__, database.__name__):
+        raise UnknownCommand(comm)
+
+    check_update(__version__, "falocalrepo")
+    check_update(__database_version__, "falocalrepo-database")
+    check_update(__server_version__, "falocalrepo-server")
+
+    # Initialise and prepare database
+    database_path = "FA.db"
+
+    if db_path := environ.get("FALOCALREPO_DATABASE", None):
+        print(f"Using FALOCALREPO_DATABASE: {db_path}")
+        if db_path.endswith(".db"):
+            database_path = db_path
+        else:
+            database_path = join(db_path, database_path)
+
+    database_path = abspath(database_path)
+    chdir(p if (p := dirname(database_path)) else ".")
+
+    db: Connection = connect_database(database_path)
+    make_tables(db)
+    db = update_database(db)
+
+    try:
+        add_history(db, datetime.now().timestamp(), f"{comm} {' '.join(args)}".strip())
+
+        if comm == init.__name__:
+            init()
+        elif comm == config.__name__:
+            config(db, *args)
+        elif comm == download.__name__:
+            download(db, *args)
+        elif comm == database.__name__:
+            database(db, *args)
+    finally:
+        # Close database and update totals
+        write_setting(db, "USRN", str(count(db, "USERS")))
+        write_setting(db, "SUBN", str(count(db, "SUBMISSIONS")))
+        write_setting(db, "JRNN", str(count(db, "JOURNALS")))
+        db.commit()
+        db.close()
---
+++ packages/falocalrepo/falocalrepo-3.12.1-py3-none-any.whl/falocalrepo/download.py
@@ -0,0 +1,269 @@
+from datetime import datetime
+from json import dumps as json_dumps
+from json import loads as json_loads
+from math import log10
+from os import get_terminal_size
+from re import sub as re_sub
+from sqlite3 import Connection
+from time import sleep
+from typing import Callable
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Tuple
+from typing import Union
+
+from faapi import FAAPI
+from faapi import Journal
+from faapi import SubmissionPartial
+from falocalrepo_database import edit_user_field_add
+from falocalrepo_database import edit_user_field_remove
+from falocalrepo_database import exist_journal
+from falocalrepo_database import exist_submission
+from falocalrepo_database import exist_user_field_value
+from falocalrepo_database import new_user
+from falocalrepo_database import read_setting
+from falocalrepo_database import save_journal
+from falocalrepo_database import save_submission
+from falocalrepo_database import select_all
+from falocalrepo_database import write_setting
+
+from .commands import Bar
+
+
+class UnknownFolder(Exception):
+    pass
+
+
+def load_cookies(api: FAAPI, cookie_a: str, cookie_b: str):
+    api.load_cookies([
+        {"name": "a", "value": cookie_a},
+        {"name": "b", "value": cookie_b},
+    ])
+
+
+def read_cookies(db: Connection) -> Tuple[str, str]:
+    cookies: Dict[str, str] = json_loads(read_setting(db, "COOKIES"))
+    return cookies.get("a", ""), cookies.get("b", "")
+
+
+def write_cookies(db: Connection, a: str, b: str):
+    write_setting(db, "COOKIES", json_dumps({"a": a, "b": b}))
+
+
+def clean_username(username: str) -> str:
+    return str(re_sub(r"[^a-zA-Z0-9\-.~,]", "", username.lower().strip()))
+
+
+def clean_string(title: str) -> str:
+    return str(re_sub(r"[^\x20-\x7E]", "", title.strip()))
+
+
+def download_submissions(api: FAAPI, db: Connection, sub_ids: List[str]):
+    if sub_ids_fail := list(filter(lambda i: not i.isdigit(), sub_ids)):
+        print("The following ID's are not correct:", *sub_ids_fail)
+    for sub_id in map(int, filter(lambda i: i.isdigit(), sub_ids)):
+        print(f"Downloading {sub_id:010} ", end="", flush=True)
+        download_submission(api, db, sub_id)
+
+
+def download_submission(api: FAAPI, db: Connection, sub_id: int) -> bool:
+    sub, _ = api.get_sub(sub_id, False)
+    sub_file: bytes = bytes()
+
+    try:
+        sub_file = download_submission_file(api, sub.file_url)
+    except KeyboardInterrupt:
+        raise
+    except (Exception, BaseException):
+        pass
+
+    if not sub.id:
+        return False
+
+    save_submission(db, dict(sub), sub_file)
+
+    return True
+
+
+def download_submission_file(api: FAAPI, sub_file_url: str, speed: int = 100) -> Optional[bytes]:
+    bar: Bar = Bar(10)
+
+    try:
+        if not (file_stream := api.session.get(sub_file_url, stream=True)).ok:
+            file_stream.raise_for_status()
+
+        size: int = int(file_stream.headers.get("Content-Length", 0))
+        file_binary: Optional[bytes] = bytes()
+
+        if not size:
+            file_binary = file_stream.content
+        else:
+            for chunk in file_stream.iter_content(chunk_size=1024):
+                file_binary += chunk
+                bar.update(size, len(file_binary)) if size else None
+                sleep(1 / speed) if speed > 0 else None
+
+        bar.update(1, 1)
+
+        file_stream.close()
+
+        return file_binary
+    except KeyboardInterrupt:
+        print("\b\b  \b\b", end="")
+        bar.delete()
+        bar.__init__(bar.length)
+        bar.message("INTERRUPT")
+        raise
+    except (Exception, BaseException):
+        bar.message("FILE ERR")
+        return None
+    finally:
+        bar.close()
+
+
+def download_journals(api: FAAPI, db: Connection, jrn_ids: List[str]):
+    if sub_ids_fail := list(filter(lambda i: not i.isdigit(), jrn_ids)):
+        print("The following ID's are not correct:", *sub_ids_fail)
+    for sub_id in map(int, filter(lambda i: i.isdigit(), jrn_ids)):
+        print(f"Downloading {sub_id:010} ", end="", flush=True)
+        download_journal(api, db, sub_id)
+
+
+def download_journal(api: FAAPI, db: Connection, jrn_id: int):
+    journal: Journal = api.get_journal(jrn_id)
+    save_journal(db, dict(journal))
+
+
+def download_users_update(api: FAAPI, db: Connection, users: List[str], folders: List[str], stop: int = 1):
+    tot: int = 0
+    fail: int = 0
+    for user, user_folders_str in select_all(db, "USERS", ["USERNAME", "FOLDERS"], ["USERNAME"]):
+        user_folders: List[str] = sorted(user_folders_str.split(","))
+        if users and user not in users:
+            continue
+        elif any(folder.startswith("!") for folder in user_folders):
+            print(f"User {user} disabled")
+            continue
+        elif (user_exists := api.user_exists(user)) != 0:
+            if user_exists == 1:
+                print(f"User {user} disabled")
+                edit_user_field_remove(db, user, "FOLDERS", ["gallery", "scraps", "journals"])
+                edit_user_field_add(db, user, "FOLDERS", ["!gallery", "!scraps", "!journals"])
+            elif user_exists == 2:
+                print(f"User {user} not found")
+            else:
+                print(f"User {user} error {user_exists}")
+            continue
+        user_folders = [f for f in folders if f in user_folders] if folders else user_folders
+        if not user_folders:
+            print(f"User {user} no folders selected")
+        for folder in user_folders:
+            print(f"Downloading: {user}/{folder}")
+            tot_tmp, fail_tmp = download_user(api, db, user, folder, stop)
+            tot += tot_tmp
+            fail += fail_tmp
+    print("Items downloaded:", tot)
+    print("Items failed:", fail) if fail else None
+    write_setting(db, "LASTUPDATE", str(datetime.now().timestamp()))
+
+
+def download_users(api: FAAPI, db: Connection, users: List[str], folders: List[str]):
+    for user, folder in ((u, f) for u in users for f in folders):
+        print(f"Downloading: {user}/{folder}")
+        if (user_exists := api.user_exists(user)) != 0:
+            if user_exists == 1:
+                print(f"User {user} disabled")
+            elif user_exists == 2:
+                print(f"User {user} not found")
+            else:
+                print(f"User {user} error {user_exists}")
+            continue
+        tot, fail = download_user(api, db, user, folder)
+        print("Items downloaded:", tot)
+        print("Items failed:", fail) if fail else None
+
+
+def download_user(api: FAAPI, db: Connection, user: str, folder: str, stop: int = 0) -> Tuple[int, int]:
+    items_total: int = 0
+    items_failed: int = 0
+    page: Union[int, str] = 1
+    page_n: int = 0
+    user = clean_username(user)
+    space_bar: int = 10
+    space_term: int = get_terminal_size()[0]
+    space_line: int = space_term - (space_bar + 2 + 2)
+    found_items: int = 0
+    skip: bool = False
+
+    download: Callable[[str, Union[str, int]], Tuple[List[Union[SubmissionPartial, Journal]], Union[int, str]]]
+    exists: Callable[[Connection, int], bool]
+
+    if folder.startswith("!"):
+        print(f"{user}/{folder} disabled")
+        return 0, 0
+    elif folder in ("mentions", "mentions_all", "list-mentions", "list-mentions_all"):
+        print(f"Unsupported: {user}/{folder}")
+        return 0, 0
+    elif folder in ("gallery", "list-gallery"):
+        download = api.gallery
+        exists = exist_submission
+    elif folder in ("scraps", "list-scraps"):
+        download = api.scraps
+        exists = exist_submission
+    elif folder in ("favorites", "list-favorites"):
+        page = "next"
+        download = api.favorites
+        exists = exist_submission
+    elif folder in ("journals", "list-journals"):
+        download = api.journals
+        exists = exist_journal
+    else:
+        raise UnknownFolder(folder)
+
+    if folder.startswith("list-"):
+        skip = True
+        folder = folder[5:]  # remove list- prefix
+    else:
+        new_user(db, user)
+        edit_user_field_add(db, user, "FOLDERS", [folder.lower()])
+
+    while page:
+        page_n += 1
+        print(f"{page_n}    {user[:space_term - int(log10(page_n)) - 8 - 1]} ...", end="", flush=True)
+        items, page = download(user, page)
+        print("\r" + (" " * (space_term - 1)), end="\r", flush=True)
+        for i, item in enumerate(items, 1):
+            sub_string: str = f"{page_n}/{i:02d} {item.id:010d} {clean_string(item.title)}"
+            print(f"{sub_string[:space_line]:<{space_line}} ", end="", flush=True)
+            bar: Bar = Bar(space_bar)
+            if not item.id:
+                items_failed += 1
+                bar.message("ID ERROR")
+                bar.close()
+            elif exist_user_field_value(db, user, folder.upper(), str(item.id).zfill(10)):
+                if stop and (found_items := found_items + 1) >= stop:
+                    print("\r" + (" " * (space_term - 1)), end="\r", flush=True)
+                    return items_total, items_failed
+                bar.message("IS IN DB")
+                bar.close()
+            elif exists(db, item.id):
+                bar.message("IS IN DB")
+                bar.close()
+                edit_user_field_add(db, user, folder.upper(), [str(item.id).zfill(10)])
+            elif skip:
+                bar.message("SKIPPED")
+                bar.close()
+            elif isinstance(item, SubmissionPartial):
+                bar.delete()
+                if download_submission(api, db, item.id):
+                    edit_user_field_add(db, user, folder.upper(), [str(item.id).zfill(10)])
+                    items_total += 1
+            elif isinstance(item, Journal):
+                save_journal(db, dict(item))
+                bar.update(1, 1)
+                bar.close()
+                edit_user_field_add(db, user, folder.upper(), [str(item.id).zfill(10)])
+                items_total += 1
+
+    return items_total, items_failed
