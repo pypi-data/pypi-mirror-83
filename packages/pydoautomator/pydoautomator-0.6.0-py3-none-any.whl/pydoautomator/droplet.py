14dccbeb8443100fad71a2ce71a479042430c410
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:31 +0000

    Add package pydoautomator/0.6.0/pydoautomator-0.6.0-py3-none-any.whl

---
+++ packages/pydoautomator/pydoautomator-0.6.0-py3-none-any.whl/pydoautomator/droplet.py
@@ -0,0 +1,14 @@
+from pydantic import BaseModel
+from typing import List, Optional
+
+
+class Droplet(BaseModel):
+    id: Optional[int] = None
+    name: str
+    region: str = 'nyc1'
+    size: str = 's-8vcpu-16gb'
+    image: int
+    ssh_keys: List[int] = []
+    private_networking: bool = True
+    vpc_uuid: str
+    monitoring: bool = True
