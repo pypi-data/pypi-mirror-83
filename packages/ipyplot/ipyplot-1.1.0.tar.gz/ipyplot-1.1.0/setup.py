7fb2f62a11ee755959b9108ae404970365e71763
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:55 +0000

    Add package ipyplot/1.1.0/ipyplot-1.1.0.tar.gz

---
+++ packages/ipyplot/ipyplot-1.1.0.tar.gz/ipyplot-1.1.0/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+from setuptools import find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="ipyplot",
+    version="1.1.0",
+    description="Simple package that leverages IPython and HTML for more efficient, reach and interactive plotting of images in Jupyter Notebooks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # This is important!
+    url="http://github.com/karolzak/ipyplot",
+    author="Karol Zak",
+    author_email="karol.zak@hotmail.com",
+    license="MIT",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[
+        "IPython",
+        "numpy",
+        "pillow",
+        "shortuuid"
+    ],
+)
