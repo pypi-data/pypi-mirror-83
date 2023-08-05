807edf6ae2d32767c03f9bf6eb2757cee20950a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:36 +0000

    Add package GreenDeck-GsheetsTest/0.0.16/GreenDeck_GsheetsTest-0.0.16.tar.gz

---
+++ packages/GreenDeck-GsheetsTest/GreenDeck_GsheetsTest-0.0.16.tar.gz/GreenDeck_GsheetsTest-0.0.16/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="GreenDeck_GsheetsTest", # Replace with your own username
+    version="0.0.16",
+    author="Sneh Kothari",
+    author_email="snehkothari1@gmail.com",
+    description="Greendeck Test to plot graph using gsheets",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/snehkothari/GreenDeck_gsheets",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+          'matplotlib',
+          'pandas',
+          'gsheets',
+      ],
+)
