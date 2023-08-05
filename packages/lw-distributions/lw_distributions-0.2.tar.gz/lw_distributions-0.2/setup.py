ca4a8278bc23426b7c0f4ae5b264db9112b1d52b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:15 +0000

    Add package lw-distributions/0.2/lw_distributions-0.2.tar.gz

---
+++ packages/lw-distributions/lw_distributions-0.2.tar.gz/lw_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='lw_distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['lw_distributions'],
+      author= 'Leopold Walther',
+      author_emaiL= 'leopold.walther@gmail.com',
+      zip_safe=False)
