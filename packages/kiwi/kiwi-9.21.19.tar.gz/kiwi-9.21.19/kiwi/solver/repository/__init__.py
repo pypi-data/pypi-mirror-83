dc50b3cb3f95785c658535b0009a64a7f8330586
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:32 +0000

    Add package kiwi/9.21.19/kiwi-9.21.19.tar.gz

---
+++ packages/kiwi/kiwi-9.21.19.tar.gz/kiwi-9.21.19/kiwi/solver/repository/__init__.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2015 SUSE Linux GmbH.  All rights reserved.
+#
+# This file is part of kiwi.
+#
+# kiwi is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# kiwi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with kiwi.  If not, see <http://www.gnu.org/licenses/>
+#
+# project
+from kiwi.solver.repository.suse import SolverRepositorySUSE
+from kiwi.solver.repository.rpm_md import SolverRepositoryRpmMd
+from kiwi.solver.repository.rpm_dir import SolverRepositoryRpmDir
+
+from kiwi.exceptions import KiwiSolverRepositorySetupError
+
+
+class SolverRepository:
+    """
+    **Repository factory for creation of SAT solvables**
+
+    * :param object uri: Instance of :class:`Uri`
+    """
+    def __new__(self, uri, user=None, secret=None):
+        if uri.repo_type == 'yast2':
+            return SolverRepositorySUSE(uri, user, secret)
+        elif uri.repo_type == 'rpm-md':
+            return SolverRepositoryRpmMd(uri, user, secret)
+        elif uri.repo_type == 'rpm-dir':
+            return SolverRepositoryRpmDir(uri, user, secret)
+        else:
+            raise KiwiSolverRepositorySetupError(
+                'Support for %s solver repository type not implemented' %
+                uri.repo_type
+            )
---
+++ packages/kiwi/kiwi-9.21.19.tar.gz/kiwi-9.21.19/kiwi/system/root_import/__init__.py
@@ -0,0 +1,62 @@
+# Copyright (c) 2015 SUSE Linux GmbH.  All rights reserved.
+#
+# This file is part of kiwi.
+#
+# kiwi is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# kiwi is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with kiwi.  If not, see <http://www.gnu.org/licenses/>
+#
+import logging
+
+# project
+from kiwi.system.root_import.oci import RootImportOCI
+from kiwi.exceptions import KiwiRootImportError
+
+log = logging.getLogger('kiwi')
+
+
+class RootImport:
+    """
+    Root import factory
+
+    Attibutes
+
+    * :attr:`root_dir`
+        root directory path name
+
+    * :attr:`image_uri`
+        a uri to an image containing a the root system
+
+    * :attr:`image_type`
+        type of the image to import
+    """
+    def __new__(self, root_dir, image_uri, image_type):
+        log.info(
+            'Importing root from a {0} image type'.format(image_type)
+        )
+        if image_type == 'docker':
+            root_import = RootImportOCI(
+                root_dir, image_uri,
+                custom_args={'archive_transport': 'docker-archive'}
+            )
+        elif image_type == 'oci':
+            root_import = RootImportOCI(
+                root_dir, image_uri,
+                custom_args={'archive_transport': 'oci-archive'}
+            )
+        else:
+            raise KiwiRootImportError(
+                'Support to import {0} images not implemented'.format(
+                    image_type
+                )
+            )
+        return root_import
