567eaf03ee15f9a43f25d52ec94f3e4df3598b67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:52 +0000

    Add package telekinesis/0.1.0a16/telekinesis-0.1.0a16.tar.gz

---
+++ packages/telekinesis/telekinesis-0.1.0a16.tar.gz/telekinesis-0.1.0a16/telekinesis/helpers.py
@@ -0,0 +1,28 @@
+import random
+import aiohttp
+
+from .client import Session, Connection, Route
+from .telekinesis import Telekinesis
+
+async def authenticate(url, print_callback=print, **kwargs):
+
+    url = url if url[-5:] == '.json' else url.rstrip('/') + '/brokers.json'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            brokers = await resp.json()
+
+    bid, b = random.choice(list(brokers.items()))
+
+    s = Session()
+    c = Connection(s, b['url'])
+
+    await c.is_connnecting_lock.wait()
+    assert bid == c.broker_id
+    entrypoint = Telekinesis(Route(**b['entrypoint']), s)
+
+    user = await entrypoint._call(print_callback, **kwargs)
+
+    if not user:
+        raise Exception('Failed to authenticate')
+
+    return user
