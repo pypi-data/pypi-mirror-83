913a009ca8871dd708830177f1cf0aa867e18fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:39 +0000

    Add package cirq-unstable/0.10.0.dev20201020203837/cirq_unstable-0.10.0.dev20201020203837-py3-none-any.whl

---
+++ packages/cirq-unstable/cirq_unstable-0.10.0.dev20201020203837-py3-none-any.whl/cirq/work/__init__.py
@@ -0,0 +1,15 @@
+from cirq.work.collector import (
+    CircuitSampleJob,
+    Collector,
+)
+from cirq.work.pauli_sum_collector import (
+    PauliSumCollector,)
+from cirq.work.observable_settings import (
+    InitObsSetting,
+    _MeasurementSpec,
+    observables_to_settings,
+)
+from cirq.work.sampler import (
+    Sampler,)
+from cirq.work.zeros_sampler import (
+    ZerosSampler,)
