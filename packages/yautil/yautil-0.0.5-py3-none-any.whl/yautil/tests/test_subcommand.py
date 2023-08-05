1a98871f696ba5e00f2dda160e71f7c6be2be803
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:40 +0000

    Add package yautil/0.0.5/yautil-0.0.5-py3-none-any.whl

---
+++ packages/yautil/yautil-0.0.5-py3-none-any.whl/yautil/tests/test_subcommand.py
@@ -0,0 +1,87 @@
+import argparse
+from unittest import TestCase
+
+from yautil import Subcommand, SubcommandParser
+
+
+class TestSubcommand(TestCase):
+
+    def test_basic(self):
+        cmd = ''
+
+        class CmdA(Subcommand):
+            def on_parser_init(self, parser: argparse.ArgumentParser):
+                pass
+
+            def on_command(self, args):
+                assert cmd == 'cmda'
+
+        parser = SubcommandParser()
+
+        parser.add_subcommands(CmdA())
+
+        cmd = 'cmda'
+        args = parser.parse_args([cmd])
+        parser.exec_subcommands()
+
+    def test_subcommand_naming(self):
+        cmd = ''
+
+        class CmdA(Subcommand):
+            def on_parser_init(self, parser: argparse.ArgumentParser):
+                pass
+
+            def on_command(self, args):
+                assert cmd == 'X'
+
+        parser = SubcommandParser()
+
+        parser.add_subcommands(CmdA(name='X'))
+
+        cmd = 'X'
+        args = parser.parse_args([cmd])
+        parser.exec_subcommands()
+
+    def test_multiple_subcommand_regs(self):
+        cmd = ''
+
+        class CmdA(Subcommand):
+            def on_parser_init(self, parser: argparse.ArgumentParser):
+                pass
+
+            def on_command(self, args):
+                assert cmd == 'cmda'
+
+        class CmdB(Subcommand):
+            def on_parser_init(self, parser: argparse.ArgumentParser):
+                pass
+
+            def on_command(self, args):
+                assert cmd == 'cmdb'
+
+        class CmdC(Subcommand):
+            def on_parser_init(self, parser: argparse.ArgumentParser):
+                pass
+
+            def on_command(self, args):
+                assert cmd == 'cmdc'
+
+        parser = SubcommandParser()
+
+        parser.add_subcommands(CmdA())
+        parser.add_subcommands(CmdB(), CmdC())
+
+        cmd = 'cmda'
+        argsA = parser.parse_args([cmd])
+        parser.exec_subcommands()
+
+        cmd = 'cmdb'
+        argsB = parser.parse_args([cmd])
+        parser.exec_subcommands()
+
+        cmd = 'cmdc'
+        argsC = parser.parse_args([cmd])
+        parser.exec_subcommands()
+
+        cmd = 'cmda'
+        parser.exec_subcommands(argsA)
