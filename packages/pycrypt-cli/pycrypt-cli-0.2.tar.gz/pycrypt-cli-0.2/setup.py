6bcb1d4feb67669560542cafa34aa154097860d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:36 +0000

    Add package pycrypt-cli/0.2/pycrypt-cli-0.2.tar.gz

---
+++ packages/pycrypt-cli/pycrypt-cli-0.2.tar.gz/pycrypt-cli-0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import *
+  
+with open('requirements.txt') as f: 
+    requirements = f.readlines() 
+  
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+  
+setup( 
+        name ='pycrypt-cli',
+        version = '0.2',
+        author ='Devansh Singh', 
+        author_email ='devanshamity@gmail.com', 
+        url ='https://github.com/Devansh3712/pycrypt-cli', 
+        description ='Cryption tool', 
+        long_description = long_description, 
+        long_description_content_type ="text/markdown", 
+        license ='MIT',
+        packages = ["pycryptcli"],
+        include_package_data = True,
+        entry_points = {
+        	"console_scripts": [
+        		"pycrypt=pycryptcli.__main__:pycrypt",
+        	]
+        },
+        classifiers =[
+            "Programming Language :: Python :: 3", 
+            "License :: OSI Approved :: MIT License", 
+            "Operating System :: OS Independent", 
+        ],
+        install_requires = requirements,
+) 
