96f824f085d7dae1e7b421284845fcc3d17b2205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:55 +0000

    Add package seven-framework/1.0.94/seven_framework-1.0.94.tar.gz

---
+++ packages/seven-framework/seven_framework-1.0.94.tar.gz/seven_framework-1.0.94/seven_framework/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+@Author: ChenXiaolei
+@Date: 2020-04-16 14:38:22
+@LastEditTime: 2020-08-24 16:51:45
+@LastEditors: ChenXiaolei
+@Description: 
+"""
+
+from . import config
+from .coding import *
+from .crypto import *
+from .mysql import *
+from .log import *
+from .time import *
+from .uuid import *
+from .http import * 
+from .json import *
+from .host import *
+from .notice import *
+from .excel import *
+from .qr_code import *
+from .sign import *
+from .work_wechat import *
