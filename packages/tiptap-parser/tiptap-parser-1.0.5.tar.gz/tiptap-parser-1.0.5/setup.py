f826686c30a030c67daa114c13d969ac71e1aff8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:57 +0000

    Add package tiptap-parser/1.0.5/tiptap-parser-1.0.5.tar.gz

---
+++ packages/tiptap-parser/tiptap-parser-1.0.5.tar.gz/tiptap-parser-1.0.5/setup.py
@@ -0,0 +1,24 @@
+import yaml
+
+from distutils.core import setup
+
+with open("version.yml") as version_yml:
+    version = yaml.load(version_yml, Loader=yaml.FullLoader)['version']
+
+setup(
+  name='tiptap-parser',
+  packages=['tiptapparser'],
+  version=version,
+  license='MIT',
+  author='Daniel Elisenberg',
+  url='https://github.com/DanielElisenberg/tiptap-parser',
+  download_url='https://github.com/DanielElisenberg/tiptap-parser/archive/1.0.2.tar.gz',
+  keywords=['TIPTAP', 'PARSE', 'JSON', 'HTML'],
+  setup_requires=['pyyaml'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3.8',
+  ],
+)
