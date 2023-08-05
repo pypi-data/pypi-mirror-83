61d11e1696af0cd79d0df56683dc1ca2c70bef3a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:25 +0000

    Add package aistac-foundation/2.10.6/aistac-foundation-2.10.6.tar.gz

---
+++ packages/aistac-foundation/aistac-foundation-2.10.6.tar.gz/aistac-foundation-2.10.6/tests/handlers/handler_factory_test.py
@@ -0,0 +1,39 @@
+from aistac.handlers.abstract_handlers import HandlerFactory, ConnectorContract
+import unittest
+
+
+class HandlerFactoryTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_runs(self):
+        """Basic smoke test"""
+        HandlerFactory()
+
+    def test_exceptions(self):
+        # Bad module
+        connector_contract = ConnectorContract(uri='example.csv;file_type=csv',
+                                               module_name='aistac.handlers.none',
+                                               handler='PythonSourceHandler',
+                                               kwargs={'sep': ',', 'encoding': 'latin1'})
+        with self.assertRaises(ModuleNotFoundError) as context:
+            HandlerFactory.instantiate(connector_contract)
+        self.assertTrue("The module 'aistac.handlers.none' could not be found" in str(context.exception))
+        # bad handler
+        connector_contract = ConnectorContract(uri='example.csv', module_name='aistac.handlers.python_handlers',
+                                               handler='none')
+        with self.assertRaises(ImportError) as context:
+            HandlerFactory.instantiate(connector_contract)
+        self.assertTrue("The handler 'none' could not be found in the module 'aistac.handlers.python_handlers'" in str(context.exception))
+        # Everything correct
+        connector_contract = ConnectorContract(uri='example.csv', module_name='aistac.handlers.python_handlers',
+                                               handler='PythonSourceHandler')
+        handler = HandlerFactory.instantiate(connector_contract)
+        self.assertEqual("<class 'aistac.handlers.python_handlers.PythonSourceHandler'>", str(type(handler)))
+
+if __name__ == '__main__':
+    unittest.main()
