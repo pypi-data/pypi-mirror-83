8c62b8711dcac44c94b89fadb33f2654dd3942b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:39 +0000

    Add package PyTrie/0.4.0/PyTrie-0.4.0.tar.gz

---
+++ packages/PyTrie/PyTrie-0.4.0.tar.gz/PyTrie-0.4.0/setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='PyTrie',
+    version='0.4.0',
+    author='George Sakkis',
+    author_email='george.sakkis@gmail.com',
+    url='https://github.com/gsakkis/pytrie/',
+    description='A pure Python implementation of the trie data structure.',
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    py_modules=['pytrie'],
+    install_requires=['sortedcontainers'],
+    test_suite='tests',
+)
