406755732c7297ae2cb459451decbd327e3fe02e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:28 +0000

    Add package django-composed-configuration/0.7.0/django-composed-configuration-0.7.0.zip

---
+++ packages/django-composed-configuration/django-composed-configuration-0.7.0.zip/django-composed-configuration-0.7.0/composed_configuration/_rest_framework.py
@@ -0,0 +1,36 @@
+from typing import Type
+
+from ._base import ComposedConfiguration, ConfigMixin
+
+
+class RestFrameworkMixin(ConfigMixin):
+    """
+    Configure Django REST Framework.
+
+    This requires the `django-cors-headers` and `drf-yasg2` packages to be installed.
+    """
+
+    @staticmethod
+    def before_binding(configuration: Type[ComposedConfiguration]) -> None:
+        configuration.INSTALLED_APPS += ['rest_framework', 'rest_framework.authtoken', 'drf_yasg2']
+
+    REST_FRAMEWORK = {
+        'DEFAULT_AUTHENTICATION_CLASSES': [
+            'rest_framework.authentication.BasicAuthentication',
+            'rest_framework.authentication.TokenAuthentication',
+        ]
+    }
+
+    SWAGGER_SETTINGS = {
+        'SECURITY_DEFINITIONS': {
+            'Basic': {'type': 'basic'},
+            'Bearer': {'type': 'apiKey', 'name': 'Authorization', 'in': 'header'},
+        }
+    }
+
+    REDOC_SETTINGS = {
+        'SECURITY_DEFINITIONS': {
+            'Basic': {'type': 'basic'},
+            'Bearer': {'type': 'apiKey', 'name': 'Authorization', 'in': 'header'},
+        }
+    }
