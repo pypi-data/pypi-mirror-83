33afec95593f4e71e568134b2ac459e947531226
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:30 +0000

    Add package lw-distributions/0.1/lw_distributions-0.1.tar.gz

---
+++ packages/lw-distributions/lw_distributions-0.1.tar.gz/lw_distributions-0.1/lw_distributions/__init__.py
@@ -0,0 +1,3 @@
+# Create link to Gaussiandistribution.py and Binomialdistribution.py files so import of them directly possible
+from .Gaussiandistribution import Gaussian
+from .Binomialdistribution import Binomial
