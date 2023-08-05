84605ec607c8c004bb516c83bc68fb4d25d2edf3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:05 +0000

    Add package pipelinewise-tap-slack/1.1.0/pipelinewise-tap-slack-1.1.0.tar.gz

---
+++ packages/pipelinewise-tap-slack/pipelinewise-tap-slack-1.1.0.tar.gz/pipelinewise-tap-slack-1.1.0/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+with open('README.md') as f:
+    long_description = f.read()
+
+setup(name='pipelinewise-tap-slack',
+      version='1.1.0',
+      description='Singer.io tap for extracting data from the Slack Web API - PipelineWise compatible',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='dwallace@envoy.com',
+      url='https://github.com/transferwise/pipelinewise-tap-slack',
+      classifiers=[
+          'License :: OSI Approved :: GNU Affero General Public License v3',
+          'Programming Language :: Python :: 3 :: Only'
+      ],
+      py_modules=['tap_slack'],
+      install_requires=[
+          'pipelinewise-singer-python==1.*',
+          'slackclient==2.6.0',
+      ],
+      extras_require={
+          'test': [
+              'pylint',
+              'pytest'
+          ]
+      },
+      python_requires='>=3.6',
+      entry_points='''
+          [console_scripts]
+          tap-slack=tap_slack:main
+      ''',
+      packages=find_packages(),
+      package_data={
+          'tap_slack': [
+              'schemas/*.json'
+          ]
+      })
