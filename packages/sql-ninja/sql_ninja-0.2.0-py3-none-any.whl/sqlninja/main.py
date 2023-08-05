e6c71f62650f3c7c9442ce78a8dcb3b5b6ecead7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:06:56 +0000

    Add package sql-ninja/0.2.0/sql_ninja-0.2.0-py3-none-any.whl

---
+++ packages/sql-ninja/sql_ninja-0.2.0-py3-none-any.whl/sqlninja/main.py
@@ -0,0 +1,24 @@
+import click
+import os
+from sqlninja import engine
+
+@click.command(context_settings=dict(
+    ignore_unknown_options=True,
+    allow_extra_args=True,
+))
+@click.argument('src')
+@click.option('--template_path', default='sql/templates', help='Base directory where SQL templates are located. Defaults to `sql/templates`')
+@click.pass_context
+def cli(ctx, src, template_path):
+
+    # All remaining arguments are passed through to render context
+    context = dict([arg.split('=') for arg in ctx.args if '=' in arg ])
+
+    engine.DEFAULT_TEMPLATE_PATH = template_path
+
+    click.echo(
+        engine.render(
+            name=src,
+            **context
+        )
+    )
