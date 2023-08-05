c054c1690f9b8430a2c88276996327be24ad96c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:10 +0000

    Add package seleniumqt/0.2.2/seleniumqt-0.2.2.tar.gz

---
+++ packages/seleniumqt/seleniumqt-0.2.2.tar.gz/seleniumqt-0.2.2/seleniumqt/__init__.py
@@ -0,0 +1,59 @@
+from seleniumqt.ezUi.test_tool.operate import open_url
+from seleniumqt.ezUi.util import rkd2ks, webDv, operate, verify, character, \
+    ele2ope, db_ip
+from seleniumqt.ezUi.util.case import step2ks
+from seleniumqt.ezUi.util.frame import rpt2htm, step_info
+from seleniumqt.ezUi.util.sys import ini_file
+
+
+def tb1th(d, t):
+    from . import table_down
+    return table_down.run(d, t)
+
+
+def qt_open_url():
+    return open_url
+
+
+def qt_rkd2ks():
+    return rkd2ks
+
+
+def qt_webDv():
+    return webDv
+
+
+def qt_operate():
+    return operate
+
+
+def qt_verify():
+    return verify
+
+
+def qt_character():
+    return character
+
+
+def qt_ele2ope():
+    return ele2ope
+
+
+def qt_db_ip():
+    return db_ip
+
+
+def qt_step_info():
+    return step_info
+
+
+def qt_step2ks():
+    return step2ks
+
+
+def qt_rpt2htm():
+    return rpt2htm
+
+
+def qt_ini_file():
+    return ini_file
