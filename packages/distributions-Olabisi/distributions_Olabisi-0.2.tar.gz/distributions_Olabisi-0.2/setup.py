65abd86504091ad88d62ba777c8c1f8b248c45dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:47 +0000

    Add package distributions-Olabisi/0.2/distributions_Olabisi-0.2.tar.gz

---
+++ packages/distributions-Olabisi/distributions_Olabisi-0.2.tar.gz/distributions_Olabisi-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_Olabisi',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['distributions_Olabisi'],
+      author = 'olabisi',
+      email = 's.olabisi@yahoo.com',
+      zip_safe=False)
