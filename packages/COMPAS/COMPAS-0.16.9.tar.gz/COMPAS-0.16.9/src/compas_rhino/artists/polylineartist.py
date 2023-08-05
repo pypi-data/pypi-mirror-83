6e3f5ece8820908141b9dd2542fa178ea2a60729
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:53 +0000

    Add package COMPAS/0.16.9/COMPAS-0.16.9.tar.gz

---
+++ packages/COMPAS/COMPAS-0.16.9.tar.gz/COMPAS-0.16.9/src/compas_rhino/artists/polylineartist.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from __future__ import division
+
+import compas_rhino
+from compas_rhino.artists._primitiveartist import PrimitiveArtist
+
+
+__all__ = ['PolylineArtist']
+
+
+class PolylineArtist(PrimitiveArtist):
+    """Artist for drawing polylines.
+
+    Parameters
+    ----------
+    primitive : :class:`compas.geometry.Polyline`
+        A COMPAS polyline.
+
+    Notes
+    -----
+    See :class:`compas_rhino.artists.PrimitiveArtist` for all other parameters.
+
+    """
+
+    def draw(self):
+        """Draw the polyline.
+
+        Returns
+        -------
+        list
+            The GUIDs of the created Rhino objects.
+        """
+        polylines = [{'points': map(list, self.primitive.points), 'color': self.color, 'name': self.name}]
+        guids = compas_rhino.draw_polylines(polylines, layer=self.layer, clear=False, redraw=False)
+        self._guids = guids
+        return guids
+
+
+# ==============================================================================
+# Main
+# ==============================================================================
+
+if __name__ == "__main__":
+
+    pass
