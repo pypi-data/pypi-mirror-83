8ba413333649a5e3c5e5ba9f87c002a05c8156b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:47 +0000

    Add package simplisafe-python/9.5.0/simplisafe-python-9.5.0.tar.gz

---
+++ packages/simplisafe-python/simplisafe-python-9.5.0.tar.gz/simplisafe-python-9.5.0/simplipy/sensor/v2.py
@@ -0,0 +1,62 @@
+"""Define a v2 (old) SimpliSafe sensor."""
+from simplipy.entity import Entity, EntityTypes
+from simplipy.errors import SimplipyError
+
+
+class SensorV2(Entity):
+    """A V2 (old) sensor.
+
+    Note that this class shouldn't be instantiated directly; it will be
+    instantiated as appropriate via :meth:`simplipy.API.get_systems`.
+    """
+
+    @property
+    def data(self) -> int:
+        """Return the sensor's current data flag (currently not understood).
+
+        :rtype: ``int``
+        """
+        return self._entity_data["sensorData"]
+
+    @property
+    def error(self) -> bool:
+        """Return the sensor's error status.
+
+        :rtype: ``bool``
+        """
+        return self._entity_data["error"]
+
+    @property
+    def low_battery(self) -> bool:
+        """Return whether the sensor's battery is low.
+
+        :rtype: ``bool``
+        """
+        return self._entity_data.get("battery", "ok") != "ok"
+
+    @property
+    def settings(self) -> bool:
+        """Return the sensor's settings.
+
+        :rtype: ``bool``
+        """
+        return self._entity_data["setting"]
+
+    @property
+    def trigger_instantly(self) -> bool:
+        """Return whether the sensor will trigger instantly.
+
+        :rtype: ``bool``
+        """
+        return self._entity_data["instant"]
+
+    @property
+    def triggered(self) -> bool:
+        """Return whether the sensor has been triggered.
+
+        :rtype: ``bool``
+        """
+        if self.type == EntityTypes.entry:
+            return self._entity_data.get("entryStatus", "closed") == "open"
+
+        raise SimplipyError(f"Cannot determine triggered state for sensor: {self.name}")
