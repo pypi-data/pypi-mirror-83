4c96421c9ae6714bde9d0f9df996e74b52c6f133
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:28 +0000

    Add package pytest-splunk-env/0.1.1b1/pytest-splunk-env-0.1.1b1.tar.gz

---
+++ packages/pytest-splunk-env/pytest-splunk-env-0.1.1b1.tar.gz/pytest-splunk-env-0.1.1b1/pytest_splunk_env/splunk/helmut/splunk/__init__.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2020 Splunk Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+This module contains everything regarding a Splunk installation.
+
+@author: Nicklas Ansman-Giertz
+@contact: U{ngiertz@splunk.com<mailto:ngiertz@splunk.com>}
+@since: 2011-11-23
+"""
+
+__all__ = ["local", "ssh"]
