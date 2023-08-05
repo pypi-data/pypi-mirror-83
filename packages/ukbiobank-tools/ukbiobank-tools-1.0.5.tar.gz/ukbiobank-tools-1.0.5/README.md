92a83fd04cb8e6ef39b4ac5d20c57ce6ca091a3d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:47 +0000

    Add package ukbiobank-tools/1.0.5/ukbiobank-tools-1.0.5.tar.gz

---
+++ packages/ukbiobank-tools/ukbiobank-tools-1.0.5.tar.gz/ukbiobank-tools-1.0.5/README.md
@@ -0,0 +1,19 @@
+# UKBiobank-tools
+
+
+## Requirements
+
+It is highly recommended to install the Anaconda python distribution (Python 3.6) which contains most dependencies (see https://www.anaconda.com/download/).
+
+### About
+
+UKBiobank-tools is a simple application enabling the filtering and pre-processing of ukbiobank datasets.
+
+
+### Viewing documentation 
+
+https://ukbiobank-tools.readthedocs.io/en/latest/
+
+## Contributing
+
+Feel free to contribute via pull requests
---
+++ packages/ukbiobank-tools/ukbiobank-tools-1.0.5.tar.gz/ukbiobank-tools-1.0.5/setup.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+
+def list_requirements():
+    with open('requirements.txt', 'r') as f:
+        reqs = [line.strip() for line in f]
+        return reqs
+
+
+
+
+setup(
+    name='ukbiobank-tools',
+    version='1.0.5',
+    description='Useful tools for working with UK Biobank meta-data',
+    author='Joe Necus',
+    author_email='joenecus@gmail.com',
+    url='https://github.com/jnecus/ukbiobank-tools',
+    license=license,
+	packages=find_packages(),
+    package_data={"": ["data_coding/*"]},
+	install_requires=["pandas"],
+    extras_require = {"docs": [ "sphinx",
+                                "sphinxcontrib-httpdomain",
+                                "sphinx-rtd-theme"]})
