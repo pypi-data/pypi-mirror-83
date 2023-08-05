438fca29b3b1dbb49eb5ebe46f6cb58c65216946
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:05:56 +0000

    Add package trunklucator/1.1.5/trunklucator-1.1.5.tar.gz

---
+++ packages/trunklucator/trunklucator-1.1.5.tar.gz/trunklucator-1.1.5/trunklucator/const/msg.py
@@ -0,0 +1,17 @@
+#Json protocol top level keys
+TYPE = 'type'
+PAYLOAD = 'payload'
+ID = 'msg_id'
+REPLY_ID = 'reply_id'
+#Json protocol type field
+TYPE_ERROR = 'error'
+TYPE_TASK = 'task'
+TYPE_SOLUTION = 'solution'
+TYPE_ACK = 'ack'
+TYPE_UPDATE = 'update'
+TYPE_STOP = 'stop'
+
+#Error reply fields
+ERROR_CODE = 'code'
+ERROR_TITLE = 'title'
+ERROR_DESC = 'description'
---
+++ packages/trunklucator/trunklucator-1.1.5.tar.gz/trunklucator-1.1.5/trunklucator/frontend/html_field/app.js
@@ -0,0 +1,69 @@
+import ReconnectingWebSocket from './lib/reconnecting-websocket.js';
+
+import 'https://unpkg.com/vue-shortkey'
+
+Vue.use(window.VueShortkey, { prevent: ['input', 'textarea'] })
+
+const wsVue = new Vue({
+    el: '#app',
+    delimiters: ['[[' , ']]'],
+    data: {
+        reply: '',
+        data: {
+            x: {},
+            meta: {}
+        },
+        update: '',
+        status: 'not connected'
+    },
+    computed: {
+        buttons: function() {
+            return (this.data.meta || {}).buttons || window._buttons
+        }
+    },
+    created() {
+        const self = this
+            // This code will run on startup
+        self.websocket = null
+
+        const URL = ((location.protocol === "https:") ? "wss://" : "ws://") + location.host + "/trunklucator/v1.0"
+        ReconnectingWebSocket(self, URL)
+
+        //attache keyup event listener
+        /** 
+        window.addEventListener('keyup', function(event) {
+            self.buttons.forEach(function(element) {
+                if (event.keyCode == element[2]) {
+                    self.send(element[1])
+                }
+            })
+        });
+        */
+    },
+
+    methods: {
+        send(decision) {
+            var sol = {
+                "type": "solution",
+                "payload": {
+                    "task_id": this.data["task_id"],
+                    "y": decision
+                },
+                "reply_id": null,
+                "msg_id": "v1"
+            }
+
+            const to_send = JSON.stringify(sol);
+            this.websocket.send(to_send)
+        },
+        setStatus(data) {
+            this.status = data
+        },
+        setUpdate(data) {
+            this.update = data
+        },
+        setTask(task_data) {
+            this.data = task_data
+        }
+    },
+});
