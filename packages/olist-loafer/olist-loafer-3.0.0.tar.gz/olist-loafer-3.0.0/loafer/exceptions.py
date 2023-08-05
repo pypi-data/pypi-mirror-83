78ecc7fe2c9c2943f2d3aba52835e4f3819a656b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:51 +0000

    Add package olist-loafer/3.0.0/olist-loafer-3.0.0.tar.gz

---
+++ packages/olist-loafer/olist-loafer-3.0.0.tar.gz/olist-loafer-3.0.0/loafer/exceptions.py
@@ -0,0 +1,15 @@
+
+class ProviderError(Exception):
+    pass
+
+
+class ConfigurationError(Exception):
+    pass
+
+
+class LoaferException(Exception):
+    pass
+
+
+class DeleteMessage(LoaferException):
+    pass
