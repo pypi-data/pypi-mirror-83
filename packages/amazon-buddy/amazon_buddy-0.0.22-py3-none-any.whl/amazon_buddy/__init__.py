eec7330e7309a32df13a5427a2664e303eeaf0f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:09:33 +0000

    Add package amazon-buddy/0.0.22/amazon_buddy-0.0.22-py3-none-any.whl

---
+++ packages/amazon-buddy/amazon_buddy-0.0.22-py3-none-any.whl/amazon_buddy/__init__.py
@@ -0,0 +1,7 @@
+from .amazon_buddy import AmazonBuddy
+
+from .enums.category import Category
+from .enums.sort_type import SortType
+
+from .models.product import *
+from .models.review import Review
