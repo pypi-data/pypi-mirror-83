d63567c431b15adde6aeec268005ed3296ce6020
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:08:30 +0000

    Add package domain-eventbus/0.1.14/domain-eventbus-0.1.14.tar.gz

---
+++ packages/domain-eventbus/domain-eventbus-0.1.14.tar.gz/domain-eventbus-0.1.14/HISTORY.rst
@@ -0,0 +1,48 @@
+=======
+History
+=======
+
+0.1.3 (2020-10-01)
+------------------
+
+* First release on PyPI.
+
+0.1.6 (2020-10-05)
+------------------
+
+* Introducing AbstractEventHandler
+
+0.1.7 (2020-10-07)
+------------------
+
+* Cleanup: Docs, Labels
+
+0.1.9 (2020-10-07)
+------------------
+
+* Cleanup: Docs, setup.py
+
+0.1.10 (2020-10-09)
+-------------------
+
+* Cleanup: Docs, setup.py
+
+0.1.11 (2020-10-09)
+-------------------
+
+* Added possibility to specify custom __last_modified__ during entity instantiation
+
+0.1.12 (2020-10-21)
+-------------------
+
+* Removed versioning and discard functionality, we don't need them since we don't have event store/sourcing
+
+0.1.13 (2020-10-21)
+-------------------
+
+* Revert 0.1.11, __last_modified__ reverted back to internal only value
+
+0.1.14 (2020-10-21)
+-------------------
+
+* Time. Moved from Decimal to Datetime format
---
+++ packages/domain-eventbus/domain-eventbus-0.1.14.tar.gz/domain-eventbus-0.1.14/tests/test_eventbus.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+from time import sleep
+from typing import List, Optional, Union, Type, Tuple
+from uuid import uuid4
+
+import pytest
+
+from eventbus.application.eventbus import EventBus
+from eventbus.domain.eventbus import AbstractEventHandler
+from eventbus.domain.events import DomainEvent
+from eventbus.domain.whitehead import TEvent, T
+from eventbus.example.entity import Example
+
+
+@pytest.mark.asyncio
+async def test_example_entity():
+    received_events = []
+
+    class SomeEvent(DomainEvent):
+        pass
+
+    class NonExistentEventsTestHandler(AbstractEventHandler):
+        @property
+        def event_type(self) -> Optional[Union[str, Type, Tuple]]:
+            return SomeEvent
+
+        async def handler(self, events: List[TEvent]):
+            received_events.extend(events)
+
+    class AllEventsTestHandler(AbstractEventHandler):
+        @property
+        def event_type(self) -> Union[str, Type, Tuple]:
+            return self.ASTERISK
+
+        async def handler(self, events: List[TEvent]):
+            received_events.extend(events)
+
+    handler1 = AllEventsTestHandler()
+    handler2 = NonExistentEventsTestHandler()
+
+    bus = EventBus(handlers=[
+        handler1, handler2
+    ])
+
+    # Same instance of handler will be skipped
+    bus.subscribe(handler1)
+    # No such events thus skipping as well
+    bus.subscribe(handler2)
+
+    # "Example.Created" has been added to a pending list (Domain Root Entity)
+    example = await Example.__create__(event_bus=bus, first_name="First", last_name="Last", age=56)
+
+    assert example.__created_on__ == example.__last_modified__
+
+    assert len(example.__pending_events__) == 1
+    assert len(received_events) == 0
+
+    for value in [10, 20, 30]:
+        # "ExampleInternal.Created" has been published (Domain Entity)
+        # "Example.ExampleInternalAdded" bas been added to a pending list (Domain Root Entity)
+        await example.add(value)
+
+    assert example.__last_modified__ > example.__created_on__
+
+    # 1x "Example.Created" + 3x "Example.ExampleInternalAdded"
+    assert len(example.__pending_events__) == 4
+
+    # 1x "Example.Created" has been published
+    await example.__save__()
+
+    assert example.summ == 60
+
+    assert len(example.__pending_events__) == 0
+
+    # 3x "ExampleInternal.Created" + 1x "Example.Created" + 3x "ExampleInternal.Created"
+    assert len(received_events) == 7
+
