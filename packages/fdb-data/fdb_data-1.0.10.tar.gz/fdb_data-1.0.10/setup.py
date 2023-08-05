205df03091ad23398af5d284fe97d33a7e9603f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 19:07:10 +0000

    Add package fdb-data/1.0.10/fdb_data-1.0.10.tar.gz

---
+++ packages/fdb-data/fdb_data-1.0.10.tar.gz/fdb_data-1.0.10/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name='fdb_data',
+      version='1.0.1',
+      description='FDB database module',
+      long_description='Work to the FDB database. This is easy database. It save file in xml format.\nFDB-data use bs4 and lxml. It work in win32, linux and other.',
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+      ],
+      keywords='database foton fotonpc fdb data big',
+      url='http://ppbe.ru',
+      author='FotonPC',
+      author_email='foton-pc@inbox.ru',
+      license='MIT',
+      packages=['fdb_data'],
+      install_requires=[
+          'bs4',
+          'lxml',
+          'robin_pl',
+          'evalnumstr',
+      ],
+      include_package_data=True,
+      zip_safe=False)
