630170023b2d9169ca579f53a66a3fa078c56023
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:36 +0000

    Add package dogpile-backend-redis-advanced/0.3.1/dogpile_backend_redis_advanced-0.3.1.tar.gz

---
+++ packages/dogpile-backend-redis-advanced/dogpile_backend_redis_advanced-0.3.1.tar.gz/dogpile_backend_redis_advanced-0.3.1/setup.py
@@ -0,0 +1,90 @@
+import os
+import re
+import sys
+
+from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
+
+
+class PyTest(TestCommand):
+    user_options = [("pytest-args=", "a", "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+v = open(
+    os.path.join(
+        os.path.dirname(__file__), "dogpile_backend_redis_advanced", "__init__.py"
+    )
+)
+VERSION = re.compile(r'.*__version__ = "(.*?)"', re.S).match(v.read()).group(1)
+v.close()
+
+long_description = description = "Advanced Redis plugins for `dogpile.cache`."
+try:
+    README = os.path.join(os.path.dirname(__file__), "README.md")
+    long_description = open(README).read()
+except:
+    pass
+
+install_requires = [
+    "redis",
+    "dogpile.cache<1.0.0",
+]
+tests_require = [
+    "pytest",
+    "pytest-cov",
+    "mock",
+    "msgpack-python",
+    "dogpile.cache<1.0.0",
+    "redis",
+    "six",
+]
+testing_extras = [
+    "flake8",
+    "tox",
+]
+
+
+setup(
+    name="dogpile_backend_redis_advanced",
+    version=VERSION,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+    ],
+    keywords="caching dogpile",
+    author="Jonathan Vanasco",
+    author_email="jonathan@findmeon.com",
+    url="https://github.com/jvanasco/dogpile_backend_redis_advanced",
+    license="BSD",
+    packages=find_packages(".", exclude=["tests*"]),
+    zip_safe=False,
+    install_requires=install_requires,
+    tests_require=tests_require,
+    extras_require={
+        "testing": testing_extras,
+    },
+    cmdclass={"test": PyTest},
+)
