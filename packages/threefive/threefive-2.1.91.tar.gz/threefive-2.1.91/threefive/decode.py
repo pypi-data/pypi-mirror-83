edd1c5caa57647420c2ba7b9b42b6e33f56ace32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:48 +0000

    Add package threefive/2.1.91/threefive-2.1.91.tar.gz

---
+++ packages/threefive/threefive-2.1.91.tar.gz/threefive-2.1.91/threefive/decode.py
@@ -0,0 +1,49 @@
+import sys
+from .cue import Cue
+from .stream import Stream
+
+
+def read_stdin():
+    scte35 = None
+    try:
+        scte35 = Stream(sys.stdin.buffer, show_null=False).decode()
+    except BaseException:
+        scte35 = Cue(sys.stdin.buffer)
+        scte35.show()        
+    return scte35
+
+def read_stuff(stuff):
+    scte35 = None
+    try:
+        scte35 = Cue(stuff)
+        scte35.show()
+    except BaseException:
+        try:
+            with open(stuff, 'rb') as tsdata:
+                scte35 = Stream(tsdata,show_null=False).decode()
+        except BaseException:
+            pass        
+    return scte35
+
+def decode(stuff=None):
+    """
+    All purpose SCTE 35 decoder function
+    
+    usage:
+
+    # for a mpegts video
+
+    import threefive
+    threefive.decode('/path/to/mpegts')
+
+    # for a base64 encoded string
+
+    import threefive
+    Bee64='/DAvAAAAAAAA///wBQb+dGKQoAAZAhdDVUVJSAAAjn+fCAgAAAAALKChijUCAKnMZ1g='
+    threefive.decode(Bee64)  
+    """
+
+    if stuff in [None, sys.stdin.buffer]:
+        return read_stdin()
+    else:
+        return read_stuff(stuff)
