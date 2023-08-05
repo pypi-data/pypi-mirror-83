c9c9a5cb8a228157531b922b5427a5d0fd3e9942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:33 +0000

    Add package a2wsgi/1.2.1/a2wsgi-1.2.1.tar.gz

---
+++ packages/a2wsgi/a2wsgi-1.2.1.tar.gz/a2wsgi-1.2.1/README.md
@@ -0,0 +1,43 @@
+# a2wsgi
+
+Convert WSGI app to ASGI app or ASGI app to WSGI app.
+
+Pure Python. Only depend on the standard library.
+
+## Install
+
+```
+pip install a2wsgi
+```
+
+## How to use
+
+Convert WSGI app to ASGI app:
+
+```python
+from a2wsgi import WSGIMiddleware
+
+ASGI_APP = WSGIMiddleware(WSGI_APP)
+```
+
+Convert ASGI app to WSGI app:
+
+```python
+from a2wsgi import ASGIMiddleware
+
+WSGI_APP = ASGIMiddleware(ASGI_APP)
+```
+
+## Benchmark
+
+Run `pytest ./benchmark.py -s` to compare the performance of `a2wsgi` and `uvicorn.middleware.wsgi.WSGIMiddleware` / `asgiref.wsgi.WsgiToAsgi`.
+
+## Why a2wsgi
+
+### Convert WSGI app to ASGI app
+
+The uvicorn-WSGIMiddleware dealing with large file uploads, it is easy to cause insufficient memory [uvicorn/issue#371](https://github.com/encode/uvicorn/issues/371). a2wsgi uses `asyncio.run_coroutine_threadsafe` to regulate the pace of reading data, thus solving this problem.
+
+### Convert ASGI app to WSGI app
+
+The HTTP trigger of Alibaba Cloud Serverless supports the WSGI interface but not the ASGI interface, which is very useful for deploying starlette/index.py to such services.
---
+++ packages/a2wsgi/a2wsgi-1.2.1.tar.gz/a2wsgi-1.2.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['a2wsgi']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'a2wsgi',
+    'version': '1.2.1',
+    'description': 'Convert WSGI app to ASGI app or ASGI app to WSGI app.',
+    'long_description': '# a2wsgi\n\nConvert WSGI app to ASGI app or ASGI app to WSGI app.\n\nPure Python. Only depend on the standard library.\n\n## Install\n\n```\npip install a2wsgi\n```\n\n## How to use\n\nConvert WSGI app to ASGI app:\n\n```python\nfrom a2wsgi import WSGIMiddleware\n\nASGI_APP = WSGIMiddleware(WSGI_APP)\n```\n\nConvert ASGI app to WSGI app:\n\n```python\nfrom a2wsgi import ASGIMiddleware\n\nWSGI_APP = ASGIMiddleware(ASGI_APP)\n```\n\n## Benchmark\n\nRun `pytest ./benchmark.py -s` to compare the performance of `a2wsgi` and `uvicorn.middleware.wsgi.WSGIMiddleware` / `asgiref.wsgi.WsgiToAsgi`.\n\n## Why a2wsgi\n\n### Convert WSGI app to ASGI app\n\nThe uvicorn-WSGIMiddleware dealing with large file uploads, it is easy to cause insufficient memory [uvicorn/issue#371](https://github.com/encode/uvicorn/issues/371). a2wsgi uses `asyncio.run_coroutine_threadsafe` to regulate the pace of reading data, thus solving this problem.\n\n### Convert ASGI app to WSGI app\n\nThe HTTP trigger of Alibaba Cloud Serverless supports the WSGI interface but not the ASGI interface, which is very useful for deploying starlette/index.py to such services.\n',
+    'author': 'abersheeran',
+    'author_email': 'me@abersheeran.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/abersheeran/a2wsgi',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
