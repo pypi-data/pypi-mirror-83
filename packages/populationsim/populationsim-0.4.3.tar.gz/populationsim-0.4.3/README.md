8917c45edbca0c6312fab03752ce698e08cb1a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:34 +0000

    Add package populationsim/0.4.3/populationsim-0.4.3.tar.gz

---
+++ packages/populationsim/populationsim-0.4.3.tar.gz/populationsim-0.4.3/README.md
@@ -0,0 +1,15 @@
+PopulationSim
+=============
+
+[![Build Status](https://travis-ci.org/activitysim/populationsim.svg?branch=master)](https://travis-ci.org/ActivitySim/populationsim) [![Coverage Status](https://coveralls.io/repos/ActivitySim/populationsim/badge.png?branch=master)](https://coveralls.io/r/ActivitySim/populationsim?branch=master)
+
+
+PopulationSim is an open platform for population synthesis.  It emerged
+from Oregon DOT's desire to build a shared, open, platform that could be
+easily adapted for statewide, regional, and urban transportation planning
+needs.  PopulationSim is implemented in the
+[ActivitySim](https://github.com/activitysim/activitysim) framework.
+
+## Documentation
+
+https://activitysim.github.io/populationsim/
---
+++ packages/populationsim/populationsim-0.4.3.tar.gz/populationsim-0.4.3/setup.py
@@ -0,0 +1,32 @@
+from ez_setup import use_setuptools
+use_setuptools()  # nopep8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='populationsim',
+    version='0.4.3',
+    description='Population Synthesis',
+    author='contributing authors',
+    author_email='ben.stabler@rsginc.com',
+    license='BSD-3',
+    url='https://github.com/ActivitySim/populationsim',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: BSD License'
+    ],
+    packages=find_packages(exclude=['*.tests']),
+    include_package_data=True,
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
+    install_requires=[
+        'activitysim >= 0.9.2',
+        'numpy >= 1.16.1',
+        'pandas >= 0.24.1',
+        'ortools >= 5.1.4045, < 7.5',
+        'future >= 0.16.0'
+    ]
+)
