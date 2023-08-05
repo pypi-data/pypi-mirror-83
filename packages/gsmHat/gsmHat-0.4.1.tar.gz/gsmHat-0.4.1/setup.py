9ef39eb6ed71aab7489772ce9d199c6ee123fbac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:57 +0000

    Add package gsmHat/0.4.1/gsmHat-0.4.1.tar.gz

---
+++ packages/gsmHat/gsmHat-0.4.1.tar.gz/gsmHat-0.4.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools  import setup
+setup(
+  name = 'gsmHat',
+  packages = ['gsmHat'],
+  version = '0.4.1',
+  license='MIT',
+  description = 'Using the Waveshare GSM/GPRS/GNSS Hat for Raspberry Pi with Python',
+  long_description = 'Using the Waveshare GSM/GPRS/GNSS Hat for Raspberry Pi with Python',
+  long_description_content_type="text/markdown",
+  author = 'Tarek Tounsi',
+  author_email = 'software@tounsi.de',
+  url = 'https://github.com/Civlo85/gsmHat',
+  download_url = 'https://github.com/Civlo85/gsmHat/archive/v_04.tar.gz',
+  keywords = ['Waveshare', 'GSM', 'GPS', 'Raspberry', 'Pi'],
+  install_requires=[
+          'serial',
+          'datetime'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
