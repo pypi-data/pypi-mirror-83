a1b454815b1144cbfe1d5d99b0fc6d28859855ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:35 +0000

    Add package gmapFinder/1.0.2/gmapFinder-1.0.2-py3-none-any.whl

---
+++ packages/gmapFinder/gmapFinder-1.0.2-py3-none-any.whl/gmapFinder/__init__.py
@@ -0,0 +1,3 @@
+"""This piece of software was last updated on 21-October-2020 at 09:00 IST"""
+
+from gmapFinder.directions import getDirection, getCurrentLocation
---
+++ packages/gmapFinder/gmapFinder-1.0.2-py3-none-any.whl/gmapFinder/directions.py
@@ -0,0 +1,38 @@
+from geopy.geocoders import Nominatim
+from geopy.distance import great_circle
+import requests
+
+def getCurrentLocation():
+	res = requests.get("https://ipinfo.io/")
+	data = res.json()
+	return data
+
+def getDirection(startingPoint, destinationPoint):
+
+	geolocator = Nominatim(user_agent='gmap')
+	if 'current' in startingPoint:
+		startinglocation = geolocator.reverse(getCurrentLocation()['loc'])
+	else:
+		startinglocation = geolocator.geocode(startingPoint)
+
+	destinationlocation = geolocator.geocode(destinationPoint)
+	startingPoint = startinglocation.address.replace(' ', '+')
+	destinationPoint = destinationlocation.address.replace(' ', '+')
+
+	URL = 'https://www.google.co.in/maps/dir/'+startingPoint+'/'+destinationPoint+'/'
+
+	startinglocationCoordinate = (startinglocation.latitude, startinglocation.longitude)
+	destinationlocationCoordinate = (destinationlocation.latitude, destinationlocation.longitude)
+	total_distance = great_circle(startinglocationCoordinate, destinationlocationCoordinate)
+	
+	result = {
+		"url": URL,
+		"total_distance(km)": total_distance.km,
+		"total_distance(miles)": total_distance.miles,
+		"Starting address": startingPoint,
+		"Starting Co-ordinates": startinglocationCoordinate,
+		"Destination Address": destinationPoint,
+		"Destination Co-ordinates": destinationlocationCoordinate
+	}
+
+	return result
