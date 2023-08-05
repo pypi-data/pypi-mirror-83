15c9248160600e463e74d7fb103eb357b6f17144
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:55 +0000

    Add package micromlgen/1.1.12/micromlgen-1.1.12.tar.gz

---
+++ packages/micromlgen/micromlgen-1.1.12.tar.gz/micromlgen-1.1.12/micromlgen/platforms.py
@@ -0,0 +1,21 @@
+ARDUINO = 'arduino'
+ATTINY = 'attiny'
+ALL = [
+    ARDUINO,
+    ATTINY
+]
+
+ALLOWED_CLASSIFIERS = [
+    'SVC',
+    'LinearSVC',
+    'OneClassSVC',
+    'RVC',
+    'SEFR',
+    'DecisionTree',
+    'RandomForest',
+    'GaussianNB',
+    'LogisticRegression',
+    'PCA',
+    'PrincipalFFT',
+    'LinearRegression'
+]
