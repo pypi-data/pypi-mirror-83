06151f3229e2edff6475b20ff8014f2f383a79c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:34 +0000

    Add package miniutils/1.0.2/miniutils-1.0.2.tar.gz

---
+++ packages/miniutils/miniutils-1.0.2.tar.gz/miniutils-1.0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='miniutils',
+    version='1.0.2',
+    packages=['miniutils'],
+    url='http://miniutils.readthedocs.io/en/latest/',
+    license='MIT',
+    author='scnerd',
+    author_email='scnerd@gmail.com',
+    description='Small Python utilities for adding concise functionality and usability to your code',
+    long_description=open('README.rst').read(),
+    install_requires=[
+        'tqdm',
+        'pycontracts',
+        'coloredlogs',
+    ],
+    download_url='https://github.com/scnerd/miniutils',
+    keywords=['miniutils', 'utilities', 'decorators', 'minimal'],
+    python_requires='>=3',
+)
