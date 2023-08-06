7973c0db13d1dc5b066b51c73ff3c732ac1c71c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:08 +0000

    Add package tonicapp-helpers/1.17.3/tonicapp-helpers-1.17.3.tar.gz

---
+++ packages/tonicapp-helpers/tonicapp-helpers-1.17.3.tar.gz/tonicapp-helpers-1.17.3/helpers/permissions.py
@@ -0,0 +1,38 @@
+from rest_framework import permissions
+
+
+class IsLoggedInAndOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        kwargs = request.parser_context['kwargs']
+        user_id = str(kwargs['user_id']) if 'user_id' in kwargs \
+            else str(kwargs['id'])
+
+        return 'user_id' in request.user and user_id == request.user['user_id']
+
+
+class IsLoggedInAndDoctor(permissions.BasePermission):
+    def has_permission(self, request, view):
+        kwargs = request.parser_context['kwargs']
+        user_id = str(kwargs['user_id']) if 'user_id' in kwargs \
+            else str(kwargs['id'])
+
+        return 'user_id' in request.user \
+            and user_id == request.user['user_id']\
+            and 'role' in request.user \
+            and 'doctor' == request.user['role']
+
+
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return 'is_admin' in request.user and bool(request.user['is_admin'])
+
+
+class IsAdminOrLoggedInAndOwner(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if 'is_admin' in request.user and bool(request.user['is_admin']):
+            return True
+
+        kwargs = request.parser_context['kwargs']
+        user_id = str(kwargs['user_id']) if 'user_id' in kwargs \
+            else str(kwargs['id'])
+        return 'user_id' in request.user and user_id == request.user['user_id']
