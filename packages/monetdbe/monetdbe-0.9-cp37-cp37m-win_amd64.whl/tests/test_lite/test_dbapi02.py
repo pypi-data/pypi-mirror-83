1ebbe283de19fc1543e6c3a43be79c26f78afe31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:02 +0000

    Add package monetdbe/0.9/monetdbe-0.9-cp37-cp37m-win_amd64.whl

---
+++ packages/monetdbe/monetdbe-0.9-cp37-cp37m-win_amd64.whl/tests/test_lite/test_dbapi02.py
@@ -0,0 +1,21 @@
+from monetdbe.monetize import monet_identifier_escape as identifier_escape
+import numpy
+import pytest
+
+
+class TestMultipleResultSets:
+    def test_string_insertion(self, monetdbe_cursor):
+        monetdbe_cursor.execute('CREATE TABLE strings(s STRING)')
+        monetdbe_cursor.executemany('INSERT INTO strings VALUES (%s)', [["'hello\" world\"'"]])
+        monetdbe_cursor.execute('SELECT * FROM strings')
+        result = monetdbe_cursor.fetchall()
+        assert result == [("'hello\" world\"'",)], "Incorrect result returned"
+
+    def test_table_name(self, monetdbe_cursor):
+        sname = "table"
+        tname = 'integerinios'
+        monetdbe_cursor.execute('CREATE SCHEMA %s' % identifier_escape(sname))
+        monetdbe_cursor.create(tname, {'i': numpy.arange(3)}, schema=sname)
+        monetdbe_cursor.execute('SELECT * FROM %s.%s' % (identifier_escape(sname), identifier_escape(tname)))
+        result = monetdbe_cursor.fetchall()
+        assert result == [(0,), (1,), (2,)], "Incorrect result returned"
