9c4889ca91cc1512a752a5e8176a2a157b56a3fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:55 +0000

    Add package opennem/3.0.0b3/opennem-3.0.0b3.tar.gz

---
+++ packages/opennem/opennem-3.0.0b3.tar.gz/opennem-3.0.0b3/opennem/scheduler/__init__.py
@@ -0,0 +1,22 @@
+from huey import RedisHuey, crontab
+
+from opennem.api.exporter import wem_export_all, wem_run_all
+from opennem.exporter.geojson import export_facility_geojson
+from opennem.settings import settings
+
+huey = RedisHuey("opennem.exporter", host=settings.cache_url.host)
+
+
+@huey.periodic_task(crontab(minute="*/5"))
+def schedule_wem_export_task():
+    wem_run_all()
+
+
+@huey.periodic_task(crontab(hour="*/6"))
+def schedule_wem_export_all():
+    wem_export_all()
+
+
+@huey.periodic_task(crontab(minute="*/5"))
+def schedule_export_geojson():
+    export_facility_geojson()
---
+++ packages/opennem/opennem-3.0.0b3.tar.gz/opennem-3.0.0b3/opennem/settings/schema.py
@@ -0,0 +1,35 @@
+from typing import Optional
+
+from pydantic import BaseSettings, PostgresDsn, RedisDsn
+from scrapy.settings import Settings
+from scrapy.utils.project import get_project_settings
+
+
+class OpennemSettings(BaseSettings):
+    env: str = "development"
+    db_url: PostgresDsn = "postgresql://opennem:opennem@127.0.0.1:15433/opennem"
+    cache_url: RedisDsn = "redis://127.0.0.1"
+    sentry_url: Optional[str]
+
+    google_places_api_key: str
+    requests_cache_path: str = ".requests"
+
+    slack_hook_url: Optional[str]
+
+    s3_bucket_path: str = "s3://data.opennem.org.au/"
+
+    interval_default: str = "15m"
+    period_default: str = "7d"
+
+    # @todo overwrite scrapy settings here
+    scrapy: Optional[Settings] = get_project_settings()
+
+    class Config:
+        fields = {
+            "env": {"env": "ENV"},
+            "db_url": {"env": "DATABASE_HOST_URL"},
+            "cache_url": {"env": "REDIS_HOST_URL"},
+            "sentry_url": {"env": "SENTRY_URL"},
+            "slack_hook_url": {"env": "WATCHDOG_SLACK_HOOK"},
+            "s3_bucket_path": {"env": "S3_DATA_BUCKET_PATH"},
+        }
