9057f3255fc2405c1bc7a197551d86568c2c5eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:16 +0000

    Add package pysdl/1.0.5/pysdl-1.0.5.tar.gz

---
+++ packages/pysdl/pysdl-1.0.5.tar.gz/pysdl-1.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pysdl',
+    version='1.0.5',
+    url='',
+    license='',
+    author='NewtonX',
+    author_email='',
+    install_requires=['pythonjsonlogger'],
+    python_requires='>=3.4',
+    test_suite="tests.tests",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    package_dir={'': 'src'},
+    packages=find_packages("src", exclude="tests"),
+    description='A python library that helps Stackdriver consume python logs appropriately built on madzak/python-json-logger',
+)
