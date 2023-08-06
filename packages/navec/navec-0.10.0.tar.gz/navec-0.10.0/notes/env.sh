204c9c110cd48762b7829bdb834ad71beb9e9edd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:17 +0000

    Add package navec/0.10.0/navec-0.10.0.tar.gz

---
+++ packages/navec/navec-0.10.0.tar.gz/navec-0.10.0/notes/env.sh
@@ -0,0 +1,6 @@
+
+export S3_KEY=_S1s7eDSAu_SvbKQz2FD
+export S3_SECRET=J6LW3L_Poas8Deog7vLDFNAtHiRIW-ktu0gUYUqG
+export S3_BUCKET=alexkuk-navec
+export GLOVE_DIR=~/proj/glove/build
+export GLOVE_DIR=~/glove/build
