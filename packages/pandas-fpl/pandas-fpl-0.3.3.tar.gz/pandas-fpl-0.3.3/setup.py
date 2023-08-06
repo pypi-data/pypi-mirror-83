17751b7c0f8a5773ede54a0d6e16b9baf5322c15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:18 +0000

    Add package pandas-fpl/0.3.3/pandas-fpl-0.3.3.tar.gz

---
+++ packages/pandas-fpl/pandas-fpl-0.3.3.tar.gz/pandas-fpl-0.3.3/setup.py
@@ -0,0 +1,29 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+cwd = pathlib.Path(__file__).parent
+
+# The text of the README file
+readme = (cwd / 'README.md').read_text()
+
+# This call to setup() does all the work
+setup(name='pandas-fpl',
+        version='0.3.3',
+        description='Pandas wrapper for Fantasy Premier League API based on the FPL package: https://github.com/amosbastian/fpl',
+        long_description=readme,
+        long_description_content_type='text/markdown',
+        url='https://github.com/177arc/pandas-fpl',
+        author='Marc Maier',
+        author_email='py@177arc.net',
+        license='MIT',
+        classifiers=[
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+        ],
+        packages=['fplpandas'],
+        include_package_data=True,
+        install_requires=['pandas', 'fpl']
+)
