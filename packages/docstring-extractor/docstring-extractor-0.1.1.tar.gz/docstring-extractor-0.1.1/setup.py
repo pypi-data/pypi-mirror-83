b46225de4b1fb94f55ded159dcb0414a1733eb45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:52 +0000

    Add package docstring-extractor/0.1.1/docstring-extractor-0.1.1.tar.gz

---
+++ packages/docstring-extractor/docstring-extractor-0.1.1.tar.gz/docstring-extractor-0.1.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['docstring_extractor']
+setup_kwargs = {
+    'name': 'docstring-extractor',
+    'version': '0.1.1',
+    'description': 'Get Python docstrings from files',
+    'long_description': None,
+    'author': 'Francisco Jimenez Cabrera',
+    'author_email': 'jkfran@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
