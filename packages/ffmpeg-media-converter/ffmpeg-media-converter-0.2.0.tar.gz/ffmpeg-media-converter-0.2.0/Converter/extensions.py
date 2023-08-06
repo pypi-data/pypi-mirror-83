a4f671a415fdd6e4178c0f3b30fa86c4ac3a7d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:35 +0000

    Add package ffmpeg-media-converter/0.2.0/ffmpeg-media-converter-0.2.0.tar.gz

---
+++ packages/ffmpeg-media-converter/ffmpeg-media-converter-0.2.0.tar.gz/ffmpeg-media-converter-0.2.0/Converter/extensions.py
@@ -0,0 +1,3 @@
+image = [".png", ".jpg", ".bmp", ".jpeg", ".jpe", ".gif", ".tif", ".tiff"]
+audio = [".mp3", ".flac", ".m4a", ".ogg", ".oga", ".wma", ".wav"]
+video = [".mp4", ".mov", ".avi", ".flv", ".3gp", ".h264", ".mpeg", ".ogv", ".wmv"]
