9bdb0d1983899a873d93d7652d265062a614cbb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:13 +0000

    Add package zope.app.security/5.1.0/zope.app.security-5.1.0-py2.py3-none-any.whl

---
+++ packages/zope.app.security/zope.app.security-5.1.0-py2.py3-none-any.whl/zope/app/security/browser/tests.py
@@ -0,0 +1,65 @@
+##############################################################################
+#
+# Copyright (c) 2004 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Security Views Tests
+
+"""
+__docformat__ = "reStructuredText"
+import unittest
+import doctest
+
+from zope.app.security.tests.test_doctests import PermissionsLayer
+
+class TestBWCImports(unittest.TestCase):
+
+    def test_principalterms_imports(self):
+          import zope.app.security.browser.principalterms as old
+          import zope.authentication.principal as new
+
+          self.assertIs(old.PrincipalTerms, new.PrincipalTerms)
+          self.assertIs(old.Term, new.PrincipalTerm)
+
+    def test_settings_imports(self):
+        import zope.app.security.settings as old
+        import zope.securitypolicy.settings as new
+
+        self.assertIs(old.Allow, new.Allow)
+
+    def test_protectclass_imports(self):
+        import zope.app.security.protectclass as old
+        import zope.security.checker as new1
+        import zope.security.protectclass as new2
+
+        self.assertIs(old.Checker, new1.Checker)
+        self.assertIs(old.protectName, new2.protectName)
+
+    def test__protections(self):
+        import zope.app.security._protections as new
+
+        new.protect()
+
+def test_suite():
+    def make_doctest(path):
+        test = doctest.DocFileSuite(path)
+        test.layer = PermissionsLayer
+        return test
+
+    search = make_doctest('authutilitysearchview.rst')
+
+    login = make_doctest('loginlogout.rst')
+
+    return unittest.TestSuite((
+        unittest.defaultTestLoader.loadTestsFromName(__name__),
+        search,
+        login
+    ))
---
+++ packages/zope.app.security/zope.app.security-5.1.0-py2.py3-none-any.whl/zope/app/security/principalregistry.py
@@ -0,0 +1,32 @@
+##############################################################################
+#
+# Copyright (c) 2001, 2002 Zope Foundation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Backward-compatibility imports for the global principal registry
+
+"""
+
+# BBB: these were moved to zope.principalregistry
+from zope.principalregistry.principalregistry import (
+    DuplicateLogin,
+    DuplicateId,
+    PrincipalRegistry,
+    principalRegistry,
+    PrincipalBase,
+    Group,
+    Principal,
+    UnauthenticatedPrincipal,
+    fallback_unauthenticated_principal,
+    UnauthenticatedGroup,
+    AuthenticatedGroup,
+    EverybodyGroup,
+    )
