945ed91f06bef586a1c1d992ecf078ae4d311124
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:16 +0000

    Add package dimcli/0.8/dimcli-0.8.tar.gz

---
+++ packages/dimcli/dimcli-0.8.tar.gz/dimcli-0.8/dimcli/shortcuts.py
@@ -0,0 +1,13 @@
+"""
+**DEPRECATED** 
+NOTE This module is deprecated and will be removed in future versions.
+
+Use instead `dimcli.utils`
+"""
+
+print("WARNING: this module is deprecated. Use instead ``from dimcli.utils import *``")
+
+from . import *
+
+from .utils.misc_utils import *
+from .utils.dim_utils import *
