daabb4739109451ce411e21bd905635763d6c221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:19 +0000

    Add package digwebs/0.0.1/digwebs-0.0.1-py3-none-any.whl

---
+++ packages/digwebs/digwebs-0.0.1-py3-none-any.whl/digwebs/router.py
@@ -0,0 +1,203 @@
+#!/usr/bin/env python
+
+__author__ = 'SLZ'
+
+import re, logging, mimetypes, os, types, importlib
+
+from .errors import notfound, badrequest
+
+def _static_file_generator(fpath):
+    BLOCK_SIZE = 8192
+    with open(fpath, 'rb') as f:
+        block = f.read(BLOCK_SIZE)
+        while block:
+            yield block
+            block = f.read(BLOCK_SIZE)
+
+class StaticFileRoute(object):
+    def __init__(self):
+        self.method = 'GET'
+        self.is_static = False
+
+    def match(self, url):
+        if url.startswith('/static/'):
+            return (url[1:], )
+        return None
+
+    def __call__(self, context, *args):
+        fpath = os.path.join(context.application.document_root, args[0])
+        if not os.path.isfile(fpath):
+            raise notfound()
+        fext = os.path.splitext(fpath)[1]
+        context.response.content_type = mimetypes.types_map.get(fext.lower(), 'application/octet-stream')
+        return _static_file_generator(fpath)
+
+class FaviconFileRoute(object):
+
+    def __init__(self):
+        self.method = 'GET'
+        self.is_static = False
+
+    def match(self, url):
+        if url == '/favicon.ico':
+            return ('favicon.ico', )
+        return None
+
+    def __call__(self, context, *args):
+        fpath = os.path.join(context.application.document_root, args[0])
+        if not os.path.isfile(fpath):
+            raise notfound()
+        fext = os.path.splitext(fpath)[1]
+        context.response.content_type = mimetypes.types_map.get(fext.lower(), 'application/octet-stream')
+        return _static_file_generator(fpath)
+
+_re_route = re.compile(r'(\:[a-zA-Z_]\w*)')
+
+def _build_regex(path):
+    r'''
+    Convert route path to regex.
+
+    >>> _build_regex('/path/to/:file')
+    '^\\/path\\/to\\/(?P<file>[^\\/]+)$'
+    >>> _build_regex('/:user/:comments/list')
+    '^\\/(?P<user>[^\\/]+)\\/(?P<comments>[^\\/]+)\\/list$'
+    >>> _build_regex(':id-:pid/:w')
+    '^(?P<id>[^\\/]+)\\-(?P<pid>[^\\/]+)\\/(?P<w>[^\\/]+)$'
+    '''
+    re_list = ['^']
+    var_list = []
+    is_var = False
+    for v in _re_route.split(path):
+        if is_var:
+            var_name = v[1:]
+            var_list.append(var_name)
+            re_list.append(r'(?P<%s>[^\/]+)' % var_name)
+        else:
+            s = ''
+            for ch in v:
+                if ch>='0' and ch<='9':
+                    s = s + ch
+                elif ch>='A' and ch<='Z':
+                    s = s + ch
+                elif ch>='a' and ch<='z':
+                    s = s + ch
+                else:
+                    s = s + '\\' + ch
+            re_list.append(s)
+        is_var = not is_var
+    re_list.append('$')
+    return ''.join(re_list)
+
+class Route(object):
+    '''
+    A Route object is a callable object.
+    '''
+
+    def __init__(self, func):
+        self.path = func.__web_route__
+        self.method = func.__web_method__
+        self.is_static = _re_route.search(self.path) is None
+        if not self.is_static:
+            self.route = re.compile(_build_regex(self.path))
+        self.func = func
+
+    def match(self, url):
+        m = self.route.match(url)
+        if m:
+            return m.groups()
+        return None
+
+    def __call__(self, context, *args):
+        return self.func(*args)
+
+    def __str__(self):
+        if self.is_static:
+            return 'Route(static,%s,path=%s)' % (self.method, self.path)
+        return 'Route(dynamic,%s,path=%s)' % (self.method, self.path)
+
+    __repr__ = __str__
+
+def _load_module(module_name):
+    '''
+    Load module from name as str.
+
+    >>> m = _load_module('xml')
+    >>> m.__name__
+    'xml'
+    >>> m = _load_module('xml.sax')
+    >>> m.__name__
+    'xml.sax'
+    >>> m = _load_module('xml.sax.handler')
+    >>> m.__name__
+    'xml.sax.handler'
+    '''
+    last_dot = module_name.rfind('.')
+    if last_dot==(-1):
+        return __import__(module_name, globals(), locals())
+    from_module = module_name[:last_dot]
+    import_module = module_name[last_dot+1:]
+    m = __import__(from_module, globals(), locals(), [import_module])
+    return getattr(m, import_module)
+
+class Router(object):
+
+    def __init__(
+        self,
+        is_develop_mode):
+        self.static_method_to_route = {'GET':{},'POST':{},'PUT':{},'DELETE':{}}
+        self.dynamic_method_to_route ={'GET':[],'POST':[],'PUT':[],'DELETE':[]}
+        if is_develop_mode:
+            self.dynamic_method_to_route['GET'].append(StaticFileRoute())
+            self.dynamic_method_to_route['GET'].append(FaviconFileRoute())
+        
+        self.func_to_route = {}
+
+    def create_route(self, func):
+        route = Route(func)
+        if route.is_static:
+            self.static_method_to_route[route.method][route.path] = route
+        else:
+            self.dynamic_method_to_route[route.method].append(route)
+        logging.info('Add route: %s' % str(route))
+        return route
+
+    def add_module(self, mod):
+        m = mod if type(mod)==types.ModuleType else _load_module(mod)
+        logging.info('Add module: %s' % m.__name__)
+        for name in dir(m):
+            fn = getattr(m, name)
+            if callable(fn) and hasattr(fn, '__web_route__') and hasattr(fn, '__web_method__'):
+                self.func_to_route[name] = self.create_route(fn)
+
+    def route_to(self, request_method,path_info):
+        fn = self.static_method_to_route[request_method].get(path_info, None)
+        if fn:
+            return (fn, [])
+        for fn in self.dynamic_method_to_route[request_method]:
+            args = fn.match(path_info)
+            if args:
+                return (fn, args)
+        raise notfound()
+
+    def create_controller(self,root_path, controller_folder):
+        controller_modules_path = os.path.join(root_path, controller_folder)
+        importlib.import_module(controller_folder)
+        for f in os.listdir(controller_modules_path):
+            if f.endswith('.py') and f != '__init__.py':
+                import_module = "."+f.replace(".py", "")
+                m = importlib.import_module(import_module,package=controller_folder)
+                #s_m = getattr(m, import_module)
+                self.add_module(m)
+
+        def handle_route(ctx, next):
+            request_method = ctx.request.request_method
+            path_info = ctx.request.path_info
+            f, arg = self.route_to(request_method,path_info)
+            return f(ctx, *arg)
+        return (handle_route,10000)
+    
+    def url_for(self,func_name):
+        r = self.func_to_route.get(func_name,None)
+        if r:
+            return r.path
+        raise notfound()
---
+++ packages/digwebs/digwebs-0.0.1-py3-none-any.whl/digwebs/web.py
@@ -0,0 +1,345 @@
+#!/usr/bin/env python
+'''
+A simple, lightweight, WSGI-compatible web framework.
+'''
+
+__author__ = 'SLZ'
+
+# import build-in modules
+import os
+import sys
+import time
+import datetime
+import threading
+import logging
+import traceback
+import hashlib
+import functools
+import json
+from io import StringIO
+
+# import custom modules
+from .common import Dict
+from .errors import notfound, HttpError, RedirectError
+from .request import Request
+from .response import Response
+from .template import Template, Jinja2TemplateEngine
+from .router import Router
+from .apis import APIError
+
+# thread local object for storing request and response:
+ctx = threading.local()
+
+class digwebs(object):
+    def __init__(
+        self,
+        root_path = None,
+        template_folder = 'views',
+        middlewares_folder= 'middlewares',
+        controller_folder = 'controllers',
+        is_develop_mode = True):
+        '''
+        Init a digwebs.
+
+        Args:
+          root_path: root path.
+        '''
+
+        self.root_path = root_path if root_path else os.path.abspath(os.path.dirname(sys.argv[0]))
+        self.middleware = []
+        self.template_folder = template_folder
+        self.middlewares_folder = middlewares_folder
+        self.controller_folder = controller_folder
+        self.is_develop_mode = is_develop_mode
+        self.template_callbacks = set()
+        self.router = None
+    
+    def init_all(self):
+        if self.template_folder:
+            self._init_template_engine(os.path.join(self.root_path, self.template_folder))
+        
+        self.router = Router(self.is_develop_mode)
+        self.middleware.append(self.router.create_controller(self.root_path,self.controller_folder,))
+        if self.middlewares_folder:
+            self._init_middlewares(os.path.join(self.root_path, self.middlewares_folder))
+
+    def _init_template_engine(self,template_path):
+        def datetime_filter(t):
+            delta = int(time.time() - t)
+            if delta < 60:
+                return u'1分钟前'
+            if delta < 3600:
+                return u'%s分钟前' % (delta // 60)
+            if delta < 86400:
+                return u'%s小时前' % (delta // 3600)
+            if delta < 604800:
+                return u'%s天前' % (delta // 86400)
+            dt = datetime.datetime.fromtimestamp(t)
+            return u'%s年%s月%s日' % (dt.year, dt.month, dt.day)
+
+        self.template_engine = Jinja2TemplateEngine(template_path)
+        self.template_engine.add_filter('datetime', datetime_filter)
+
+    def _init_middlewares(self,middlewares_path):
+        for f in os.listdir(middlewares_path):
+            if f.endswith('.py'):
+                import_module = f.replace(".py", "")
+                m = __import__(self.middlewares_folder, globals(), locals(),
+                               [import_module])
+                s_m = getattr(m, import_module)
+                fn = getattr(s_m, import_module, None)
+                if fn is not None and callable(
+                        fn) and not import_module.endswith('__'):
+                    self.middleware.append(fn())
+
+        def take_second(elem):
+            return elem[1]
+
+        self.middleware.sort(key=take_second)
+
+    def run(self, port=9999, host='127.0.0.1'):
+        from wsgiref.simple_server import make_server
+        logging.info('application (%s) will start at %s:%s...' %
+                     (self.root_path, host, port))
+        server = make_server(host, port, self.get_wsgi_application())
+        server.serve_forever()
+
+    def get_wsgi_application(self):
+        _application = Dict(document_root=self.root_path)
+
+        def fn_route():
+            def route_entry(context, next):
+                def dispatch(i):
+                    fn = self.middleware[i][0]
+                    if i == len(self.middleware):
+                        fn = next
+                    return fn(context, lambda: dispatch(i + 1))
+
+                return dispatch(0)
+
+            return route_entry
+
+        fn_exec = fn_route()
+
+        def wsgi(env, start_response):
+            ctx.application = _application
+            ctx.request = Request(env)
+            response = ctx.response = Response()
+            try:
+                r = fn_exec(ctx, None)
+                if isinstance(r, Template):
+                    tmp = []
+                    for cbf in self.template_callbacks:
+                        r.model.update(cbf())
+                    r.model['ctx'] = ctx
+                    tmp.append(self.template_engine(r.template_name, r.model))
+                    r = tmp
+                if isinstance(r, str):
+                    tmp = []
+                    tmp.append(r.encode('utf-8'))
+                    r = tmp
+                if r is None:
+                    r = []
+                start_response(response.status, response.headers)
+                return r
+            except RedirectError as e:
+                response.set_header('Location', e.location)
+                start_response(e.status, response.headers)
+                return []
+            except HttpError as e:
+                start_response(e.status, response.headers)
+                return ['<html><body><h1>', e.status, '</h1></body></html>']
+            except Exception as e:
+                logging.exception(e)
+                '''
+                if not configs.get('debug',False):
+                    start_response('500 Internal Server Error', [])
+                    return ['<html><body><h1>500 Internal Server Error</h1></body></html>']
+                '''
+                exc_type, exc_value, exc_traceback = sys.exc_info()
+                fp = StringIO()
+                traceback.print_exception(
+                    exc_type, exc_value, exc_traceback, file=fp)
+                stacks = fp.getvalue()
+                fp.close()
+                start_response('500 Internal Server Error', [])
+                return [
+                    r'''<html><body><h1>500 Internal Server Error</h1><div style="font-family:Monaco, Menlo, Consolas, 'Courier New', monospace;"><pre>''',
+                    stacks.replace('<', '&lt;').replace('>', '&gt;'),
+                    '</pre></div></body></html>'
+                ]
+            finally:
+                del ctx.application
+                del ctx.request
+                del ctx.response
+
+        return wsgi
+
+    def register_template_callback(self,cb):
+        self.template_callbacks.add(cb)
+    
+    def unregister_template_callback(self,cb):
+        self.template_callbacks.remove(cb)
+    
+    @property
+    def static_resource_url(self):
+        return self.template_engine.get_globals('static_file_prefix')
+
+    @static_resource_url.setter
+    def static_resource_url(self, value):
+        self.template_engine.set_globals('static_file_prefix',value)
+
+    def get(self, path):
+        '''
+        A @get decorator.
+
+        @get('/:id')
+        def index(id):
+            pass
+
+        >>> @get('/test/:id')
+        ... def test():
+        ...     return 'ok'
+        ...
+        >>> test.__web_route__
+        '/test/:id'
+        >>> test.__web_method__
+        'GET'
+        >>> test()
+        'ok'
+        '''
+
+        def _decorator(func):
+            func.__web_route__ = path
+            func.__web_method__ = 'GET'
+            return func
+
+        return _decorator
+
+    def post(self, path):
+        '''
+        A @post decorator.
+
+        >>> @post('/post/:id')
+        ... def testpost():
+        ...     return '200'
+        ...
+        >>> testpost.__web_route__
+        '/post/:id'
+        >>> testpost.__web_method__
+        'POST'
+        >>> testpost()
+        '200'
+        '''
+
+        def _decorator(func):
+            func.__web_route__ = path
+            func.__web_method__ = 'POST'
+            return func
+
+        return _decorator
+
+    def put(self, path):
+        '''
+        A @put decorator.
+        '''
+
+        def _decorator(func):
+            func.__web_route__ = path
+            func.__web_method__ = 'PUT'
+            return func
+
+        return _decorator
+
+    def delete(self, path):
+        '''
+        A @delete decorator.
+        '''
+
+        def _decorator(func):
+            func.__web_route__ = path
+            func.__web_method__ = 'DELETE'
+            return func
+
+        return _decorator
+
+    def view(self, path):
+        '''
+        A view decorator that render a view by dict.
+
+        >>> @view('test/view.html')
+        ... def hello():
+        ...     return dict(name='Bob')
+        >>> t = hello()
+        >>> isinstance(t, Template)
+        True
+        >>> t.template_name
+        'test/view.html'
+        >>> @view('test/view.html')
+        ... def hello2():
+        ...     return ['a list']
+        >>> t = hello2()
+        Traceback (most recent call last):
+        ...
+        ValueError: Expect return a dict when using @view() decorator.
+        '''
+
+        def _decorator(func):
+            @functools.wraps(func)
+            def _wrapper(*args, **kw):
+                r = func(*args, **kw)
+                if isinstance(r, dict):
+                    logging.info('return Template')
+                    return Template(path, **r)
+                raise ValueError(
+                    'Expect return a dict when using @view() decorator.')
+
+            return _wrapper
+
+        return _decorator
+    
+    def dynamic_view(self, pathfn):
+        def _decorator(func):
+            @functools.wraps(func)
+            def _wrapper(*args, **kw):
+                r = func(*args, **kw)
+                if isinstance(r, dict):
+                    logging.info('return Template')
+                    return Template(pathfn(), **r)
+                raise ValueError('Expect return a dict when using @view() decorator.')
+            return _wrapper
+        return _decorator
+
+    def api(self,func):
+        '''
+        A decorator that makes a function to json api, makes the return value as json.
+
+        @app.route('/api/test')
+        @api
+        def api_test():
+            return dict(result='123', items=[])
+        '''
+        @functools.wraps(func)
+        def _wrapper(*args, **kw):
+            try:
+                r = json.dumps(func(*args, **kw))
+            except APIError as e:
+                r = json.dumps(dict(error=e.error, data=e.data, message=e.message))
+            except Exception as e:
+                logging.exception(e)
+                r = json.dumps(dict(error='internalerror', data=e.__class__.__name__, message=str(e)))
+            ctx.response.content_type = 'application/json'
+            return r
+        return _wrapper
+
+current_app = None
+def get_app(config_ins):
+    global current_app
+    if current_app is None:
+        current_app = digwebs(**config_ins)
+    return current_app
+
+if __name__ == '__main__':
+    sys.path.append('.')
+    import doctest
+    doctest.testmod()
