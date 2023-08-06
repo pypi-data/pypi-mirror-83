438934bb5dee81678d13cb844f06ec3533482a00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:20 +0000

    Add package generalfile/2.1.1/generalfile-2.1.1.tar.gz

---
+++ packages/generalfile/generalfile-2.1.1.tar.gz/generalfile-2.1.1/setup.py
@@ -0,0 +1,38 @@
+
+from setuptools import setup, find_packages
+
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="generalfile",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Rickard "Mandera" Abraham',
+    url="https://github.com/Mandera/generalfile",
+    version="2.1.1",
+    description=(
+        "Easily manage files cross platform."
+    ),
+    packages=find_packages(),
+    install_requires=["generallibrary", "send2trash", "wheel", "appdirs"],
+    extras_require={
+        "full": [],
+        "spreadsheet": ["pandas"]
+    },
+    classifiers=[
+        "Operating System :: Microsoft :: Windows :: Windows 7",
+        "Operating System :: Microsoft :: Windows :: Windows 10",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Desktop Environment :: File Managers",
+    ]
+)
