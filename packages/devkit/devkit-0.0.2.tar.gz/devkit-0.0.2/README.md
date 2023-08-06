45d0dfce671fffa407fb6a30d4d1f5534dcff1ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:57 +0000

    Add package devkit/0.0.2/devkit-0.0.2.tar.gz

---
+++ packages/devkit/devkit-0.0.2.tar.gz/devkit-0.0.2/README.md
@@ -0,0 +1,16 @@
+# DevKit
+
+> This package provides a web interface which helps developers to efficiently manage
+> their development environment on a linux machine.
+
+
+## Requirements
+
+* Python3/pip3
+* Linux
+
+## Installation
+
+```bash
+pip install devkit
+```
---
+++ packages/devkit/devkit-0.0.2.tar.gz/devkit-0.0.2/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="devkit", # Replace with your own username
+    version="0.0.2",
+    author="Sylvan LE DEUNFF",
+    author_email="sledeunf@gmail.com",
+    description="A tool to efficiently manage development environment",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/hoshiyosan/devtools/devkit/devkit-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': ['devkit=devkit.cli:main'],
+    },
+    python_requires='>=3.6',
+
+    # Optional
+    project_urls={
+        'Documentation': 'https://donate.pypi.org',
+        'Source': 'https://gitlab.com/hoshiyosan/devtools/devkit/devkit-python',
+    },
+)
