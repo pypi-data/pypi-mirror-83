b0351f9cb0b2db7cb2d1bb670a18514f71951500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:52 +0000

    Add package mlpiper/2.3.3/mlpiper-2.3.3-py2.py3-none-any.whl

---
+++ packages/mlpiper/mlpiper-2.3.3-py2.py3-none-any.whl/mlpiper/common/constants.py
@@ -0,0 +1,15 @@
+import logging
+
+LOGGER_NAME_PREFIX = "mlpiper"
+
+LOG_LEVELS = {
+    "noset": logging.NOTSET,
+    "debug": logging.DEBUG,
+    "info": logging.INFO,
+    "warn": logging.WARN,
+    "error": logging.ERROR,
+    "critical": logging.CRITICAL,
+}
+
+
+REQUIREMENTS_FILENAME = "requirements.txt"
