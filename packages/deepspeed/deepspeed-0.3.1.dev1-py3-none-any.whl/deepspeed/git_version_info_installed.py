eb581f69c8bc29469d2c053bc2551c00566e5b5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:48 +0000

    Add package deepspeed/0.3.1.dev1/deepspeed-0.3.1.dev1-py3-none-any.whl

---
+++ packages/deepspeed/deepspeed-0.3.1.dev1-py3-none-any.whl/deepspeed/git_version_info_installed.py
@@ -0,0 +1,5 @@
+version='0.3.1.dev1'
+git_hash='8a118c9'
+git_branch='pypi-version-logic'
+installed_ops={'cpu_adam_op': False, 'fused_adam_op': False, 'fused_lamb_op': False, 'sparse_attn_op': False, 'transformer_op': False, 'stochastic_transformer_op': False, 'utils_op': False}
+compatible_ops={'cpu_adam_op': True, 'fused_adam_op': True, 'fused_lamb_op': True, 'sparse_attn_op': True, 'transformer_op': True, 'stochastic_transformer_op': True, 'utils_op': True}
