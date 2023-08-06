2761af84fcfdde7439ac06c3fc3069c42ad4abdf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:27 +0000

    Add package horkos/0.0.8/horkos-0.0.8.tar.gz

---
+++ packages/horkos/horkos-0.0.8.tar.gz/horkos-0.0.8/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name='horkos',
+    version='0.0.8',
+    description='A package for validating incoming data.',
+
+    author='Kevin Schiroo',
+    author_email='kjschiroo@gmail.com',
+    license='MIT',
+    url='https://gitlab.com/kjschiroo/horkos',
+
+    packages=setuptools.find_packages(),
+    entry_points = {
+        'console_scripts': ['horkos=horkos.cmdline:main'],
+    },
+    install_requires=['pyyaml'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.7',
+)
