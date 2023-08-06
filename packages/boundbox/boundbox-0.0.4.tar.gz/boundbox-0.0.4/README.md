a6d7d9993725ff16e9088728cd5b527ac2e8a29a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:46 +0000

    Add package boundbox/0.0.4/boundbox-0.0.4.tar.gz

---
+++ packages/boundbox/boundbox-0.0.4.tar.gz/boundbox-0.0.4/README.md
@@ -0,0 +1,15 @@
+# boundbox
+
+The package is available in pip, https://pypi.org/project/boundbox/
+
+for installation : pip install boundbox
+
+boundbox is a lightweight python library that can help developers with bounding boxes while using an ocr or other image
+processing projects.
+
+The library provides support for converting the results of OCR's like pytesseract or contours directly into
+boundbox objects. Various operations that can be done on the bounding boxes are included in the package and
+developers can use them with ease
+
+Anyone interested to improve it is welcome to make contributions
+
---
+++ packages/boundbox/boundbox-0.0.4.tar.gz/boundbox-0.0.4/boundbox/Point_class.py
@@ -0,0 +1,77 @@
+from math import sqrt
+
+
+class Point:
+
+    """
+
+            .(x,y,z)
+
+    """
+    def __init__(self, x=0, y=0, z=0):
+        self._x = x
+        self._y = y
+        self._z = z
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @property
+    def z(self):
+        return self._z
+
+    @property
+    def sum(self):
+        return self._x + self._y + self._z
+
+    @property
+    def diff_of_x_y(self):
+        return self._x - self._y
+
+    @x.setter
+    def x(self, x_value):
+        if not isinstance(x_value, (int, )):
+            raise TypeError("x value can only be an int not a {}".format(type(x_value)))
+        self._x = x_value
+
+    @y.setter
+    def y(self, y_value):
+        if not isinstance(y_value, (int, )):
+            raise TypeError("y value can only be an int not a {}".format(type(y_value)))
+        self._y = y_value
+
+    @z.setter
+    def z(self, z_value):
+        if not isinstance(z_value, (int, )):
+            raise TypeError("z value can only be an int not a {}".format(type(z_value)))
+        self._z = z_value
+
+    def __repr__(self):
+        if self._z:
+            return "({}, {}, {})".format(self._x, self._y, self._z)
+        else:
+            return "({}, {})".format(self._x, self._y)
+
+    def __str__(self):
+        if self._z:
+            return "({}, {}, {})".format(self._x, self._y, self._z)
+        else:
+            return "({}, {})".format(self._x, self._y)
+
+    def __sub__(self, other):
+        """
+        finds the Euclidean distance between two points
+        :param other:
+        :return: distance
+        """
+        # TODO: include sub for z
+        d_square = (self._x - other.x)**2 + (self._y - other.y)**2
+        return sqrt(d_square)
+
+
+
