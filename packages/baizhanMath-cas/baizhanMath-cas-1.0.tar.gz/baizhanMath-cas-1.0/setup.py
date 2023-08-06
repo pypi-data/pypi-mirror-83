6dda4e67440f723a4d5fac79a2f32f4fdcadca7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:48 +0000

    Add package baizhanMath-cas/1.0/baizhanMath-cas-1.0.tar.gz

---
+++ packages/baizhanMath-cas/baizhanMath-cas-1.0.tar.gz/baizhanMath-cas-1.0/setup.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+from distutils.core import setup
+setup(
+name='baizhanMath-cas', # 对外我们模块的名字
+version='1.0', # 版本号
+description='这是第一个对外发布的模块，用于测试的哦', #描述
+author='chenansheng', # 作者
+author_email='cas822@163.com',
+py_modules=['baizhanMath.demo1','baizhanMath.demo2'] # 要发布的模块
+)
