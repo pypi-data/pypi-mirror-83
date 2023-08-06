6ccf0b136812d8aabd8aab23064843cd39858671
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:11 +0000

    Add package nupy/0.1/nupy-0.1.tar.gz

---
+++ packages/nupy/nupy-0.1.tar.gz/nupy-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+setup(
+    name="nupy",
+    version="0.1",
+    description="A package for numerical calculus",
+    author="Iván Montiel Cardona",
+    author_email="ivan.montiel.c@gmail.com",
+    url="https://github.com/begeistert",
+    packages=['nupy'],
+    scripts=[]
+)
