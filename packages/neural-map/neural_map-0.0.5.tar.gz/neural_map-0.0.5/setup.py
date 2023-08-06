a35427cdb1033fe959b327808c7a54405ab669b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:08 +0000

    Add package neural-map/0.0.5/neural_map-0.0.5.tar.gz

---
+++ packages/neural-map/neural_map-0.0.5.tar.gz/neural_map-0.0.5/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='neural_map',
+    version="0.0.5",
+    author="Franco José Bobadilla",
+    author_email='1709673@ucc.edu.ar',
+    description='NeuralMap is a data analysis tool based on Self-Organizing Maps',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/FrancoBobadilla/NeuralMap',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    license='MIT',
+    keywords=[
+        'self-organizing-maps',
+        'dimensionality-reduction',
+        'machine-learning',
+        'clustering',
+        'hdbscan',
+        'kmeans-clustering',
+        'kmedoids-clustering',
+        'data-analysis',
+        'unsupervised-learning'
+    ],
+    install_requires=[
+        'hdbscan~=0.8.26',
+        'scipy~=1.4.1',
+        'matplotlib~=3.2.1',
+        'scikit_learn_extra~=0.1.0b2',
+        'numpy~=1.18.3',
+        'numba~=0.50.1',
+        'scikit_learn~=0.23.2'
+    ],
+)
