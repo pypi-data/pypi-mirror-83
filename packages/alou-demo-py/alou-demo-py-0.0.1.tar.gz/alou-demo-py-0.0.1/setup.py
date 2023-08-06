e0579c6ea79542de91437b5d3b623f4a4405fa41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:44 +0000

    Add package alou-demo-py/0.0.1/alou-demo-py-0.0.1.tar.gz

---
+++ packages/alou-demo-py/alou-demo-py-0.0.1.tar.gz/alou-demo-py-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as f:
+    long_description = f.read()
+
+setup(
+    name="alou-demo-py",
+    version="0.0.1",
+    author="Example Author",
+    description="This is a demo package. Please use. I'm sure you'll find it riveting.",
+    author_email="author@example.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=["Django"],
+    setup_requires=["setuptools_scm"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Framework :: Django",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
+)
