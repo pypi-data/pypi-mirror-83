8e239ba0b91ceb2a9b631b45cf720445b545d5b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:02 +0000

    Add package NHL-API-Wrapper/0.2/NHL_API_Wrapper-0.2.tar.gz

---
+++ packages/NHL-API-Wrapper/NHL_API_Wrapper-0.2.tar.gz/NHL_API_Wrapper-0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+	'Development Status :: 5 - Production/Stable',
+	'Intended Audience :: Education',
+	'Operating System :: Microsoft :: Windows :: Windows 10',
+	'License :: OSI Approved :: MIT License',
+	'Programming Language :: Python :: 3']
+	
+setup(
+	name='NHL_API_Wrapper',
+	version='0.2',
+	description='A wrapper for the NHL API',
+	long_description=open('README.txt').read(),
+	url='',
+	author='Peter Faulkner',
+	author_email='faulknep@yahoo.com',
+	license='MIT',
+	classifiers=classifiers,
+	keywords=['nhl','nhl api','nhl api wrapper', 'hockey', 'stats'],
+	packages=find_packages(),
+	install_requires=['requests']
+)
