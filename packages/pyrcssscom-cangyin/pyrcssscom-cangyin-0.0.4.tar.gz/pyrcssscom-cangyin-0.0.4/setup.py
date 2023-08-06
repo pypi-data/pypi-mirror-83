bfc5ab0cd9ecc323fded8131a4a76c3d347144a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:50 +0000

    Add package pyrcssscom-cangyin/0.0.4/pyrcssscom-cangyin-0.0.4.tar.gz

---
+++ packages/pyrcssscom-cangyin/pyrcssscom-cangyin-0.0.4.tar.gz/pyrcssscom-cangyin-0.0.4/setup.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+# coding=utf-8
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyrcssscom-cangyin", # Replace with your own username
+    version="0.0.4",
+    author="cangyin",
+    author_email="excangyin@gmail.com",
+    description="simple communication with rcssserver3d server.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(where='.', exclude=[], include=['rcssscom']),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=['coloredlog'],
+)
