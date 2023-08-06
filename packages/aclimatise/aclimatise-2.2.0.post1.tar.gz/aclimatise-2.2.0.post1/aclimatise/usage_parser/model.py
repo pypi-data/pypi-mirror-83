aab013f3bbbaed3d673f0e6e703224d1675e1a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:33 +0000

    Add package aclimatise/2.2.0.post1/aclimatise-2.2.0.post1.tar.gz

---
+++ packages/aclimatise/aclimatise-2.2.0.post1.tar.gz/aclimatise-2.2.0.post1/aclimatise/usage_parser/model.py
@@ -0,0 +1,67 @@
+from typing import List, Optional
+
+from dataclasses import dataclass
+
+from aclimatise import model
+
+
+@dataclass
+class UsageElement:
+    text: str
+    """
+    The name of this element, as defined in the usage section
+    """
+
+    optional: bool = False
+    """
+    Whether or not this element is required
+    """
+
+    variable: bool = False
+    """
+    True if this is a variable, ie you are supposed to replace this text with your own, False if this is a constant
+    that you shouldn't change, e.g. the name of the application
+    """
+
+    # flag: bool = False
+    """
+    True if this is a flag (starts with dashes) and not a regular element
+    """
+
+    repeatable: bool = False
+    """
+    If this flag/argument can be used multiple times
+    """
+
+
+@dataclass
+class UsageInstance:
+    items: List[UsageElement]
+    """
+    The string of elements that make up a valid command invocation
+    """
+
+    description: Optional[str] = None
+    """
+    Description of this invocation
+    """
+
+    @property
+    def positionals(self) -> List["model.Positional"]:
+        """
+        Return all the positional arguments that could be derived from this instance
+        """
+        return [
+            model.Positional(
+                description="", position=i, name=el.text, optional=el.optional
+            )
+            for i, el in enumerate(self.items)
+            if isinstance(el, UsageElement)
+        ]
+
+    @property
+    def flags(self) -> List["model.Flag"]:
+        """
+        Return all the flags that could be derived from this instance
+        """
+        return [el for el in self.items if isinstance(el, model.Flag)]
---
+++ packages/aclimatise/aclimatise-2.2.0.post1.tar.gz/aclimatise-2.2.0.post1/test/test_cli.py
@@ -0,0 +1,93 @@
+import shutil
+import tempfile
+import traceback
+from pathlib import Path
+
+import pyparsing
+import pytest
+from click.testing import CliRunner
+from packaging import version
+
+from aclimatise.cli import main
+
+from .util import validate_cwl, validate_wdl
+
+
+@pytest.fixture()
+def runner():
+    return CliRunner()
+
+
+def cli_worked(result):
+    if result.exit_code == 0:
+        return True
+    else:
+        traceback.print_exception(*result.exc_info)
+        assert result.exit_code == 0
+
+
+def test_pipe_wdl(runner, htseq_help):
+    result = runner.invoke(
+        main, ["pipe", "htseq-count", "--format", "wdl"], input=htseq_help
+    )
+    cli_worked(result)
+    validate_wdl(result.output)
+
+
+def test_pipe_cwl(runner, htseq_help):
+    result = runner.invoke(
+        main, ["pipe", "htseq-count", "--format", "cwl"], input=htseq_help
+    )
+    cli_worked(result)
+    validate_cwl(result.output)
+
+
+@pytest.mark.skipif(
+    not shutil.which("htseq-count"), reason="htseq-count is not installed"
+)
+def test_explore_htseq(runner, caplog):
+    caplog.set_level(100000)
+    with tempfile.TemporaryDirectory() as tempdir:
+        result = runner.invoke(main, ["explore", "htseq-count", "--out-dir", tempdir])
+        cli_worked(result)
+        assert len(list(Path(tempdir).iterdir())) == 3
+
+
+@pytest.mark.skipif(not shutil.which("samtools"), reason="samtools is not installed")
+def test_explore_samtools(runner, caplog):
+    caplog.set_level(100000)
+    with tempfile.TemporaryDirectory() as tempdir:
+        result = runner.invoke(
+            main, ["explore", "samtools", "--help-flag", "--help", "--out-dir", tempdir]
+        )
+        cli_worked(result)
+        assert len(list(Path(tempdir).iterdir())) > 20
+
+
+@pytest.mark.skipif(not shutil.which("samtools"), reason="samtools is not installed")
+def test_explore_samtools_no_subcommands(runner, caplog):
+    caplog.set_level(100000)
+    with tempfile.TemporaryDirectory() as tempdir:
+        result = runner.invoke(
+            main,
+            [
+                "explore",
+                "samtools",
+                "--no-subcommands",
+                "--out-dir",
+                tempdir,
+            ],
+        )
+        cli_worked(result)
+        # Since we aren't looking at subcommands, there should be one file for each format
+        assert len(list(Path(tempdir).iterdir())) >= 3
+
+
+@pytest.mark.skipif(
+    version.parse(pyparsing.__version__) < version.parse("3.0.0a2"),
+    reason="PyParsing 3.0.0+ is not installed",
+)
+def test_grammar(runner):
+    result = runner.invoke(main, ["grammar"])
+    assert result.exit_code == 0
+    assert len(result.output) > 20
