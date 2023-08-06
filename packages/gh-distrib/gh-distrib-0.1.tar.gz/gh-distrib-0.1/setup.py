41c4efd5cc34286eb82ad0d5fca83ef88d5052c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:58 +0000

    Add package gh-distrib/0.1/gh-distrib-0.1.tar.gz

---
+++ packages/gh-distrib/gh-distrib-0.1.tar.gz/gh-distrib-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='gh-distrib',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['gh-distrib'],
+      author='Amro Ghoneim',
+      author_email='amroghoneim@aucegypt.edu',
+      zip_safe=False)
