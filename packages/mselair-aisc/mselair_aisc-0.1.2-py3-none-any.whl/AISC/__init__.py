0b9ab7543397f5cd87f1bca78e8c6e05b04a6fbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:35 +0000

    Add package mselair-aisc/0.1.2/mselair_aisc-0.1.2-py3-none-any.whl

---
+++ packages/mselair-aisc/mselair_aisc-0.1.2-py3-none-any.whl/AISC/__init__.py
@@ -0,0 +1,20 @@
+# Copyright 2020-present, Mayo Clinic Department of Neurology - Laboratory of Bioelectronics Neurophysiology and Engineering
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+__version__ = '0.1.2'
+
+
+
+import os
+# Check windows or linux and sets separator
+if os.name == 'nt': DELIMITER = '\\'
+else: DELIMITER = '/'
+
+
+
+
+
+
