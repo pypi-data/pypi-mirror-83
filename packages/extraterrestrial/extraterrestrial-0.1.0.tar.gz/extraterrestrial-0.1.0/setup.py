4a05d738f40bee115ce20202fa59118bd984a709
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:48 +0000

    Add package extraterrestrial/0.1.0/extraterrestrial-0.1.0.tar.gz

---
+++ packages/extraterrestrial/extraterrestrial-0.1.0.tar.gz/extraterrestrial-0.1.0/setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+# cryptonitex
+
+
+setuptools.setup(
+    name="extraterrestrial",
+    version="0.1.0",
+    author="Shaurya Pratap Singh",
+    author_email="shaurya.p.singh21@gmail.com",
+    packages=setuptools.find_packages()
+)
