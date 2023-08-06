c8a135e50893292bdc4d886b497785f7d6abcd06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:25 +0000

    Add package runez/2.1.5/runez-2.1.5.tar.gz

---
+++ packages/runez/runez-2.1.5.tar.gz/runez-2.1.5/setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+
+setup(
+    name="runez",
+    setup_requires="setupmeta",
+    versioning="dev",
+    author="Zoran Simic zoran@simicweb.com",
+    url="https://github.com/zsimic/runez",
+)
