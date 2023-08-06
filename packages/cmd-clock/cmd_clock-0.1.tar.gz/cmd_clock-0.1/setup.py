ba5ba99b95b261250a3a20fa145bcee6d0ee8e34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:59 +0000

    Add package cmd-clock/0.1/cmd_clock-0.1.tar.gz

---
+++ packages/cmd-clock/cmd_clock-0.1.tar.gz/cmd_clock-0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="cmd_clock", # Replace with your own username
+    version="0.1",
+    author="Allen Sun",
+    author_email="allen.haha@hotmail.com",
+    description="a command-line clock",
+    long_description='',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
