1009f83832e4d84f7fe0cb6838e83639d8314784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:49 +0000

    Add package phishdetect/1.8.2/phishdetect-1.8.2.tar.gz

---
+++ packages/phishdetect/phishdetect-1.8.2.tar.gz/phishdetect-1.8.2/phishdetect/models/alerts.py
@@ -0,0 +1,19 @@
+# This file is part of phishdetect-python:
+# https://github.com/phishdetect/phishdetect-python
+# See the file 'LICENSE' for copying permission.
+
+from .model import Model
+from ..endpoints import API_PATH
+
+class Alerts(Model):
+
+    def fetch(self, limit=0, offset=0):
+        """Fetch all alerts stored by PhishDetect Node.
+        :param limit: Set an integer to use as limit of records to retrieve.
+        :param offset: Set an integer to use as offset of records to retrieve.
+        """
+        params = {
+            "limit": limit,
+            "offset": offset,
+        }
+        return self._phishdetect.get(API_PATH["alerts_fetch"], params=params)
