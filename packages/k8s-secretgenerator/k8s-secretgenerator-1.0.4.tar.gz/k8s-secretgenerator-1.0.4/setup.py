aa543a57a85a8983e095f1a04230adfcdbd8eccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:51 +0000

    Add package k8s-secretgenerator/1.0.4/k8s-secretgenerator-1.0.4.tar.gz

---
+++ packages/k8s-secretgenerator/k8s-secretgenerator-1.0.4.tar.gz/k8s-secretgenerator-1.0.4/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="k8s-secretgenerator",
+    version="1.0.4",
+    packages=setuptools.find_packages(),
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    python_requires=">=3.7",
+    install_requires=["PyYAML"],
+    url='https://github.com/baspenny/k8s_secretgenerator'
+)
