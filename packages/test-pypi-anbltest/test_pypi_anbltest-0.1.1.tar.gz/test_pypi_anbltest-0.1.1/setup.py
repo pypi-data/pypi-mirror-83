fed1ae44a0dc37babe5b87ac52bd974af392f992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:48:41 +0000

    Add package test-pypi-anbltest/0.1.1/test_pypi_anbltest-0.1.1.tar.gz

---
+++ packages/test-pypi-anbltest/test_pypi_anbltest-0.1.1.tar.gz/test_pypi_anbltest-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['test_pypi_anbltest', 'test_pypi_anbltest.test_pypi_anbltest']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['APPLICATION-NAME = entry:main']}
+
+setup_kwargs = {
+    'name': 'test-pypi-anbltest',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Your Name',
+    'author_email': 'you@example.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
