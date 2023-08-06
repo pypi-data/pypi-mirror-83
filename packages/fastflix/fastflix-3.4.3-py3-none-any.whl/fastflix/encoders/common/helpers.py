e91e518c215bcadafd9b40d3325b8d207fd0e368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:23 +0000

    Add package fastflix/3.4.3/fastflix-3.4.3-py3-none-any.whl

---
+++ packages/fastflix/fastflix-3.4.3-py3-none-any.whl/fastflix/encoders/common/helpers.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+import reusables
+
+null = "/dev/null"
+if reusables.win_based:
+    null = "NUL"
+
+
+class Loop:
+    item = "loop"
+
+    def __init__(self, condition, commands, dirs=(), files=(), name="", ensure_paths=()):
+        self.name = name
+        self.condition = condition
+        self.commands = commands
+        self.ensure_paths = ensure_paths
+        self.dirs = dirs
+        self.files = files
+
+
+class Command:
+    item = "command"
+
+    def __init__(self, command, variables, internal, name="", ensure_paths=(), exe=None, shell=False):
+        self.name = name
+        self.command = command
+        self.variables = variables
+        self.internal = internal
+        self.ensure_paths = ensure_paths
+        self.exe = exe
+        self.shell = shell
+
+
+def generate_ffmpeg_start(
+    source,
+    ffmpeg,
+    encoder,
+    video_track,
+    start_time=0,
+    end_time=None,
+    pix_fmt="yuv420p10le",
+    filters=None,
+    max_mux="default",
+    fast_time=True,
+    video_title="",
+    custom_map=False,
+    **_,
+):
+    time_settings = f'{f"-ss {start_time}" if start_time else ""} {f"-to {end_time}" if end_time else ""} '
+    time_one = time_settings if fast_time else ""
+    time_two = time_settings if not fast_time else ""
+    title = f'-metadata title="{video_title}"' if video_title else ""
+
+    return (
+        f'"{ffmpeg}" -y '
+        f" {time_one} "
+        f'-i "{source}" '
+        f" {time_two} "
+        f"{title} "
+        f"{f'-max_muxing_queue_size {max_mux}' if max_mux != 'default' else ''} "
+        f'{f"-map 0:{video_track}" if not filters else ""} '
+        f'{filters if filters else ""} '
+        f"-c:v {encoder} "
+        f"-pix_fmt {pix_fmt} "
+    )
+
+
+def generate_ending(
+    audio,
+    subtitles,
+    cover,
+    output_video=None,
+    copy_chapters=True,
+    remove_metadata=True,
+    null_ending=False,
+    extra="",
+    **_,
+):
+    ending = (
+        f" {'-map_metadata -1' if remove_metadata else ''} "
+        f"{'-map_chapters 0' if copy_chapters else ''} "
+        f"{audio} {subtitles} {cover} {extra} "
+    )
+    if output_video and not null_ending:
+        output_video = output_video.replace("\\", "/")
+        ending += f'"{output_video}"'
+    else:
+        ending += null
+    return ending
+
+
+def generate_filters(
+    video_track,
+    crop=None,
+    scale=None,
+    scale_filter="lanczos",
+    scale_width=None,
+    scale_height=None,
+    disable_hdr=False,
+    rotate=None,
+    vflip=None,
+    hflip=None,
+    burn_in_track=None,
+    custom_filters=None,
+    raw_filters=False,
+    **_,
+):
+
+    filter_list = []
+    if crop:
+        filter_list.append(f"crop={crop}")
+    if scale:
+        filter_list.append(f"scale={scale}:flags={scale_filter}")
+    elif scale_width:
+        filter_list.append(f"scale={scale_width}:-1:flags={scale_filter}")
+    elif scale_height:
+        filter_list.append(f"scale=-1:{scale_height}:flags={scale_filter}")
+    if rotate is not None:
+        if rotate < 3:
+            filter_list.append(f"transpose={rotate}")
+        if rotate == 4:
+            filter_list.append(f"transpose=2,transpose=2")
+    if vflip:
+        filter_list.append("vflip")
+    if hflip:
+        filter_list.append("hflip")
+
+    if disable_hdr:
+        filter_list.append(
+            "zscale=t=linear:npl=100,format=gbrpf32le,zscale=p=bt709,tonemap=tonemap=hable:desat=0,"
+            "zscale=t=bt709:m=bt709:r=tv,format=yuv420p"
+        )
+
+    filters = ",".join(filter_list)
+    if filters and custom_filters:
+        filters = f"{filters},{custom_filters}"
+    elif not filters and custom_filters:
+        filters = custom_filters
+
+    if burn_in_track is not None:
+        if filters:
+            # You have to overlay first for it to work when scaled
+            filter_complex = f"[0:{video_track}][0:{burn_in_track}]overlay[subbed];[subbed]{filters}[v]"
+
+        else:
+            filter_complex = f"[0:{video_track}][0:{burn_in_track}]overlay[v]"
+    elif filters:
+        filter_complex = f"[0:{video_track}]{filters}[v]"
+    else:
+        return None
+    if raw_filters:
+        return filter_complex
+    return f' -filter_complex "{filter_complex}" -map "[v]" '
---
+++ packages/fastflix/fastflix-3.4.3-py3-none-any.whl/fastflix/widgets/video_options.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import logging
+
+from box import Box, BoxList
+from qtpy import QtCore, QtGui, QtWidgets
+
+from fastflix.widgets.panels.audio_panel import AudioList
+from fastflix.widgets.panels.command_panel import CommandList
+from fastflix.widgets.panels.cover_panel import CoverPanel
+from fastflix.widgets.panels.status_panel import StatusPanel
+from fastflix.widgets.panels.subtitle_panel import SubtitleList
+
+logger = logging.getLogger("fastflix")
+
+
+class VideoOptions(QtWidgets.QTabWidget):
+    def __init__(self, parent, available_audio_encoders, log_queue):
+        super().__init__(parent)
+        self.main = parent
+
+        self.selected = 0
+        self.commands = CommandList(self)
+        self.current_plugin = list(self.main.plugins.values())[0]
+        self.current_settings = self.current_plugin.settings_panel(self, self.main)
+
+        self.audio = AudioList(self, available_audio_encoders)
+        self.subtitles = SubtitleList(self)
+        self.status = StatusPanel(self, log_queue)
+        self.attachments = CoverPanel(self)
+        # self.subtitles.hide()
+        self.addTab(self.current_settings, "Quality")
+        self.addTab(self.audio, "Audio")
+        self.addTab(self.subtitles, "Subtitles")
+        self.addTab(self.attachments, "Cover")
+        self.addTab(self.commands, "Command List")
+        self.addTab(self.status, "Encoding Status")
+
+    @property
+    def audio_formats(self):
+        plugin_formats = set(self.current_plugin.audio_formats)
+        if self.main.config.get("use_sane_audio") and self.main.config.get("sane_audio_selection"):
+            return list(plugin_formats & set(self.main.config.sane_audio_selection))
+        return list(plugin_formats)
+
+    def change_conversion(self, conversion):
+        conversion = conversion.strip()
+        self.current_settings.close()
+        self.current_plugin = self.main.plugins[conversion]
+        self.current_settings = self.current_plugin.settings_panel(self, self.main)
+        self.current_settings.show()
+        self.removeTab(0)
+        self.insertTab(0, self.current_settings, "Quality")
+        self.setCurrentIndex(0)
+        self.setTabEnabled(1, getattr(self.current_plugin, "enable_audio", True))
+        self.setTabEnabled(2, getattr(self.current_plugin, "enable_subtitles", True))
+        self.setTabEnabled(3, getattr(self.current_plugin, "enable_attachments", True))
+        self.selected = conversion
+        self.audio.allowed_formats(self.audio_formats)
+        self.current_settings.new_source()
+        self.main.page_update()
+
+    def get_settings(self):
+        settings = Box()
+        settings.update(self.current_settings.get_settings())
+        tracks = 1
+        if getattr(self.current_plugin, "enable_audio", False):
+            audio_settings = self.audio.get_settings()
+            tracks += audio_settings.audio_track_count
+            settings.update(audio_settings)
+        if getattr(self.current_plugin, "enable_subtitles", False):
+            subtitle_settings = self.subtitles.get_settings()
+            tracks += subtitle_settings.subtitle_track_count
+            settings.update(subtitle_settings)
+        if getattr(self.current_plugin, "enable_attachments", False):
+            settings.update(self.attachments.get_settings(out_stream_start_index=tracks))
+        return settings
+
+    def new_source(self):
+        if getattr(self.current_plugin, "enable_audio", False):
+            self.audio.new_source(self.audio_formats, starting_pos=1)
+        if getattr(self.current_plugin, "enable_subtitles", False):
+            self.subtitles.new_source(starting_pos=len(self.audio) + 1)
+        if getattr(self.current_plugin, "enable_attachments", False):
+            self.attachments.new_source(self.main.streams.attachment)
+        self.current_settings.new_source()
+
+    def refresh(self):
+        if getattr(self.current_plugin, "enable_audio", False):
+            self.audio.refresh(starting_pos=1)
+        if getattr(self.current_plugin, "enable_subtitles", False):
+            self.subtitles.refresh(starting_pos=len(self.audio) + 1)
