7f370fe85f00922e5056a620165f28c7bb063b8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:18 +0000

    Add package runtime-type-checker/0.4.0/runtime_type_checker-0.4.0-py3-none-any.whl

---
+++ packages/runtime-type-checker/runtime_type_checker-0.4.0-py3-none-any.whl/runtime_type_checker/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for runtime-type-checker."""
+from ._checkers import *  # noqa: F401,F403
+
+__author__ = """Pierre-Jean Campigotto"""
+__email__ = "pierrejean.campigotto@gmail.com"
+__version__ = "0.4.0"
