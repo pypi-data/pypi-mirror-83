6331bb61865b1ebc79e89e209276379c0eb7314a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:29 +0000

    Add package django-baton/1.12.1/django-baton-1.12.1.tar.gz

---
+++ packages/django-baton/django-baton-1.12.1.tar.gz/django-baton-1.12.1/setup.py
@@ -0,0 +1,53 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+REPO_URL = 'http://github.com/otto-torino/django-baton'
+
+setup(
+    name='django-baton',
+    version='1.12.1',
+    packages=['baton', 'baton.autodiscover', 'baton.templatetags'],
+    include_package_data=True,
+    license='MIT License',
+    description='A cool, modern and responsive django admin application',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    url=REPO_URL,
+    author='abidibo',
+    author_email='abidibo@gmail.com',
+    install_requires=[
+        'google-auth==1.22.1',
+        'google-auth-httplib2==0.0.4',
+        'google-api-python-client==1.5.5',
+    ],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: User Interfaces',
+    ],
+    project_urls={
+        'Documentation': 'https://django-baton.readthedocs.io/en/latest/',
+        'Demo': 'https://django-baton-demo.herokuapp.com/admin',
+        'Source': REPO_URL,
+        'Tracker': REPO_URL + '/issues',
+    },
+)
