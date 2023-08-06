1faa2e8f96d681d753565062e533cde539f3ff1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:45 +0000

    Add package janomecabdic/0.1.1/janomecabdic-0.1.1.tar.gz

---
+++ packages/janomecabdic/janomecabdic-0.1.1.tar.gz/janomecabdic-0.1.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, Extension
+
+try:
+    from Cython.Build import cythonize
+    ext_modules = cythonize([
+        Extension(
+            'janomecabdic.dic',
+            sources=['janomecabdic/dic.pyx'],
+            language='c++',
+        ),
+    ])
+except ImportError:
+    ext_modules = None
+
+
+classifiers = [
+    'Development Status :: 4 - Beta',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3',
+]
+
+setup(
+    name="janomecabdic",
+    version="0.1.1",
+    url='https://github.com/nakagami/janomecabdic/',
+    classifiers=classifiers,
+    license="LGPL2,BSD",
+    keywords=['janome', 'MeCab'],
+    author='Hajime Nakagami',
+    author_email='nakagami@gmail.com',
+    description='MeCab dictionary access library for janome',
+    long_description=open('README.rst').read(),
+    test_suite="tests",
+    ext_modules=ext_modules,
+)
