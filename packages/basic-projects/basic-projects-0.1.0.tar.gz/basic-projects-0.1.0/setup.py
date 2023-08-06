1d8a979edb85749b556d9dd8c67ceef710191b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:30 +0000

    Add package basic-projects/0.1.0/basic-projects-0.1.0.tar.gz

---
+++ packages/basic-projects/basic-projects-0.1.0.tar.gz/basic-projects-0.1.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="basic-projects",
+    version="0.1.0",
+    author="Shaurya Pratap Singh",
+    author_email="shaurya.p.singh21@gmail.com",
+    packages=setuptools.find_packages()
+)
