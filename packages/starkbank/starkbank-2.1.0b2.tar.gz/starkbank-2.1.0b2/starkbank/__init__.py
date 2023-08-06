0e07cf8542673a066cfae06550d79c1152a319da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:12 +0000

    Add package starkbank/2.1.0b2/starkbank-2.1.0b2.tar.gz

---
+++ packages/starkbank/starkbank-2.1.0b2.tar.gz/starkbank-2.1.0b2/starkbank/__init__.py
@@ -0,0 +1,40 @@
+version = "2.1.0b2"
+
+user = None
+language = "en-US"
+timeout = 15
+
+from .user.__project import Project
+
+from . import transaction
+from .transaction.__transaction import Transaction
+
+from . import balance
+from .balance.__balance import Balance
+
+from . import boleto
+from .boleto.__boleto import Boleto
+
+from . import transfer
+from .transfer.__transfer import Transfer
+
+from . import boletopayment
+from .boletopayment.__boletopayment import BoletoPayment
+
+from . import utilitypayment
+from .utilitypayment.__utilitypayment import UtilityPayment
+
+from . import webhook
+from .webhook.__webhook import Webhook
+
+from . import event
+from .event.__event import Event
+
+from . import paymentrequest
+from .paymentrequest.__paymentrequest import PaymentRequest
+
+from . import invoice
+from .invoice.__invoice import Invoice
+
+from . import error
+from . import key
