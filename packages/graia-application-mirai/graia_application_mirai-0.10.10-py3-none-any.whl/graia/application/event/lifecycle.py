49575e6841664eea1745e6963abcae9ebaf5101c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:36 +0000

    Add package graia-application-mirai/0.10.10/graia_application_mirai-0.10.10-py3-none-any.whl

---
+++ packages/graia-application-mirai/graia_application_mirai-0.10.10-py3-none-any.whl/graia/application/event/lifecycle.py
@@ -0,0 +1,42 @@
+from typing import Any
+from graia.broadcast.entities.event import BaseEvent, BaseDispatcher
+from graia.broadcast.interfaces.dispatcher import DispatcherInterface
+
+class ApplicationLaunched(BaseEvent):
+    app: "GraiaMiraiApplication"
+
+    def __init__(self, app) -> None:
+        self.app = app
+
+    class Dispatcher(BaseDispatcher):
+        @staticmethod
+        def catch(interface: "DispatcherInterface"):
+            from .. import GraiaMiraiApplication
+            if interface.annotation is GraiaMiraiApplication:
+                return interface.event.app
+
+class ApplicationLaunchedBlocking(BaseEvent):
+    app: Any
+
+    def __init__(self, app) -> None:
+        self.app = app
+
+    class Dispatcher(BaseDispatcher):
+        @staticmethod
+        def catch(interface: "DispatcherInterface"):
+            from .. import GraiaMiraiApplication
+            if interface.annotation is GraiaMiraiApplication:
+                return interface.event.app
+
+class ApplicationShutdowned(BaseEvent):
+    app: Any
+
+    def __init__(self, app) -> None:
+        self.app = app
+
+    class Dispatcher(BaseDispatcher):
+        @staticmethod
+        def catch(interface: "DispatcherInterface"):
+            from .. import GraiaMiraiApplication
+            if interface.annotation is GraiaMiraiApplication:
+                return interface.event.app
