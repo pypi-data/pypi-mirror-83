867e3ba8caf29e4878c3341b7c8fc71e59695202
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:39 +0000

    Add package google-api-python-client-stubs/0.3.0/google-api-python-client-stubs-0.3.0.tar.gz

---
+++ packages/google-api-python-client-stubs/google-api-python-client-stubs-0.3.0.tar.gz/google-api-python-client-stubs-0.3.0/googleapiclient-stubs/_apis/policytroubleshooter/v1/resources.pyi
@@ -0,0 +1,28 @@
+import typing
+
+import httplib2  # type: ignore
+import typing_extensions
+
+import googleapiclient.discovery
+import googleapiclient.http  # type: ignore
+
+from .schemas import *
+@typing.type_check_only
+class PolicyTroubleshooterResource(googleapiclient.discovery.Resource):
+    @typing.type_check_only
+    class IamResource(googleapiclient.discovery.Resource):
+        def troubleshoot(
+            self,
+            *,
+            body: GoogleCloudPolicytroubleshooterV1TroubleshootIamPolicyRequest = ...,
+            **kwargs: typing.Any
+        ) -> GoogleCloudPolicytroubleshooterV1TroubleshootIamPolicyResponseHttpRequest: ...
+    def iam(self) -> IamResource: ...
+
+@typing.type_check_only
+class GoogleCloudPolicytroubleshooterV1TroubleshootIamPolicyResponseHttpRequest(
+    googleapiclient.http.HttpRequest
+):
+    def execute(
+        self, http: typing.Optional[httplib2.Http] = ..., num_retries: int = ...
+    ) -> GoogleCloudPolicytroubleshooterV1TroubleshootIamPolicyResponse: ...
---
+++ packages/google-api-python-client-stubs/google-api-python-client-stubs-0.3.0.tar.gz/google-api-python-client-stubs-0.3.0/googleapiclient-stubs/_apis/speech/v2beta1/resources.pyi
@@ -0,0 +1,44 @@
+import typing
+
+import httplib2  # type: ignore
+import typing_extensions
+
+import googleapiclient.discovery
+import googleapiclient.http  # type: ignore
+
+from .schemas import *
+@typing.type_check_only
+class SpeechResource(googleapiclient.discovery.Resource):
+    @typing.type_check_only
+    class ProjectsResource(googleapiclient.discovery.Resource):
+        @typing.type_check_only
+        class LocationsResource(googleapiclient.discovery.Resource):
+            @typing.type_check_only
+            class OperationsResource(googleapiclient.discovery.Resource):
+                def get(
+                    self, *, name: str, **kwargs: typing.Any
+                ) -> OperationHttpRequest: ...
+                def list(
+                    self,
+                    *,
+                    name: str,
+                    filter: str = ...,
+                    pageSize: int = ...,
+                    pageToken: str = ...,
+                    **kwargs: typing.Any
+                ) -> ListOperationsResponseHttpRequest: ...
+            def operations(self) -> OperationsResource: ...
+        def locations(self) -> LocationsResource: ...
+    def projects(self) -> ProjectsResource: ...
+
+@typing.type_check_only
+class ListOperationsResponseHttpRequest(googleapiclient.http.HttpRequest):
+    def execute(
+        self, http: typing.Optional[httplib2.Http] = ..., num_retries: int = ...
+    ) -> ListOperationsResponse: ...
+
+@typing.type_check_only
+class OperationHttpRequest(googleapiclient.http.HttpRequest):
+    def execute(
+        self, http: typing.Optional[httplib2.Http] = ..., num_retries: int = ...
+    ) -> Operation: ...
