242367cb088257076fe5a613c3de4efee5c88a49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:30 +0000

    Add package zzu-low/0.4/zzu_low-0.4.tar.gz

---
+++ packages/zzu-low/zzu_low-0.4.tar.gz/zzu_low-0.4/zzu_low/crawlzz.py
@@ -0,0 +1,11 @@
+import requests
+import time
+
+
+def get_create_time(cls):
+    creat_time = int(round(time.time() * 1000))
+    return creat_time
+
+class CrawlAPI(object):
+    def add(self,a,b):
+        return a+b
