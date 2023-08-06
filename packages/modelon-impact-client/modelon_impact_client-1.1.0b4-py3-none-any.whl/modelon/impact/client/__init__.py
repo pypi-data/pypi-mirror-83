f8001f498560ea434960907d502c939d53e4922e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:35 +0000

    Add package modelon-impact-client/1.1.0b4/modelon_impact_client-1.1.0b4-py3-none-any.whl

---
+++ packages/modelon-impact-client/modelon_impact_client-1.1.0b4-py3-none-any.whl/modelon/impact/client/__init__.py
@@ -0,0 +1,6 @@
+from modelon.impact.client.client import Client
+from modelon.impact.client.experiment_definition import (
+    SimpleExperimentDefinition,
+    Range,
+    SimpleExperimentExtension,
+)
