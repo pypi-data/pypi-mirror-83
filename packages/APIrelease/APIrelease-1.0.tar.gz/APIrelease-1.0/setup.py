1d454ce17f2aeb15cdaf51e4109e4caa6865dc19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:20 +0000

    Add package APIrelease/1.0/APIrelease-1.0.tar.gz

---
+++ packages/APIrelease/APIrelease-1.0.tar.gz/APIrelease-1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='APIrelease',
+    version='1.0',
+    description='这是一个实现汉诺塔的模块',
+    author='Drury_Lane',
+    author_email='1721509224@qq.com',
+    py_modules='APIrelease.hanoi_test'
+)
