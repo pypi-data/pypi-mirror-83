78755cde46700e38165a68ef113e23332df38007
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:01 +0000

    Add package xontrib-output-search/0.5.4/xontrib-output-search-0.5.4.tar.gz

---
+++ packages/xontrib-output-search/xontrib-output-search-0.5.4.tar.gz/xontrib-output-search-0.5.4/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+try:
+    with open('README.md', 'r', encoding='utf-8') as fh:
+        long_description = fh.read()
+except (IOError, OSError):
+    long_description = ''
+
+setup(
+    name='xontrib-output-search',
+    version='0.5.4',
+    license='BSD',
+    author='anki-code',
+    author_email='author@example.com',
+    description="Get identifiers, names, paths, URLs and words from the previous command output and use them for the next command in xonsh.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    python_requires='>=3.6',
+    install_requires=['tokenize-output'],
+    packages=['xontrib'],
+    package_dir={'xontrib': 'xontrib'},
+    package_data={'xontrib': ['output_search/*.py']},
+    platforms='any',
+    url='https://github.com/tokenizer/xontrib-output-search',
+    project_urls={
+        "Documentation": "https://github.com/tokenizer/xontrib-output-search/blob/master/README.md",
+        "Code": "https://github.com/tokenizer/xontrib-output-search",
+        "Issue tracker": "https://github.com/tokenizer/xontrib-output-search/issues",
+    },
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        "Programming Language :: Unix Shell",
+        "Topic :: System :: Shells",
+        "Topic :: System :: System Shells",
+        "Topic :: Terminals",
+        "Topic :: System :: Networking",
+        "License :: OSI Approved :: BSD License"
+    ]
+)
---
+++ packages/xontrib-output-search/xontrib-output-search-0.5.4.tar.gz/xontrib-output-search-0.5.4/xontrib/output_search.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env xonsh
+
+import re
+from tokenize_output.tokenize_output import tokenize_output
+
+_key_meta = __xonsh__.env.get('XONTRIB_OUTPUT_SEARCH_KEY_META', 'escape')
+_key = __xonsh__.env.get('XONTRIB_OUTPUT_SEARCH_KEY', 'f')
+_key_binding = __xonsh__.env.get('XONTRIB_OUTPUT_SEARCH_KEY_BINDING', None)
+_add_previous_cmd_to_output = True
+_support_special_chars_in_prefix = True
+
+_output_search_prefix = _key + '__'
+
+def prev_special_char_pos(s, chars=':;+-_~=/\\{[(<>|#"\'^$%&?!.,'):
+    for i in reversed(range(0, len(s))):
+        if s[i] in chars:
+            return i
+    return None
+
+
+__xonsh__.xontrib_output_search_completion = False
+__xonsh__.xontrib_output_search_previous_output = None
+
+def _xontrib_output_search_completer(prefix, line, begidx, endidx, ctx):
+    """
+    Get new arguments from previous command output use Alt+F hotkey or f__ prefix before tab key.
+    """
+    is_output_search_prefix = prefix.startswith(_output_search_prefix)
+    if __xonsh__.xontrib_output_search_completion or is_output_search_prefix:
+        __xonsh__.xontrib_output_search_completion = False
+        current_cmd = {'prefix': prefix, 'line': line, 'begidx': begidx, 'endidx': endidx}
+        prev = __xonsh__.xontrib_output_search_previous_output
+        if 'output' in prev:
+            cmd = prev['cmd']
+            output = prev['output']
+            substring = prefix[len(_output_search_prefix):] if is_output_search_prefix else prefix
+            tokens = tokenize_output(text=output, text_cmd=cmd, substring=substring, current_cmd=current_cmd)
+            if _add_previous_cmd_to_output:
+                tokens = set.union(tokens, tokenize_output(text=cmd, text_cmd=cmd, substring=substring, current_cmd=current_cmd))
+
+            if _support_special_chars_in_prefix and tokens == set() and not is_output_search_prefix:
+                sc_pos = prev_special_char_pos(prefix)
+                if sc_pos is not None:
+                    prefix_after_char = prefix[sc_pos + 1:]
+                    prefix_before_char = prefix[:sc_pos + 1]
+                    if prefix_before_char != _output_search_prefix:
+                        tokens = tokenize_output(text=output, text_cmd=cmd, substring=prefix_after_char, current_cmd=current_cmd)
+                        if _add_previous_cmd_to_output:
+                            tokens = set.union(tokens, tokenize_output(text=cmd, text_cmd=cmd, substring=prefix, current_cmd=current_cmd))
+                        tokens = set([prefix_before_char + t for t in tokens])
+
+            tokens = tokens if tokens else set([prefix])
+            return (tokens, len(prefix))
+
+
+__xonsh__.completers['xontrib_output_search'] = _xontrib_output_search_completer
+__xonsh__.completers.move_to_end('xontrib_output_search', last=False)
+
+_color_regexp = re.compile(r'(\x9B|\x1B\[)[0-?]*[ -/]*[@-~]')
+@events.on_postcommand
+def _save_output(cmd: str, rtn: int, out: str or None, ts: list, **kwargs):
+    if out is not None:
+        out = out.strip()
+        if out:
+            __xonsh__.xontrib_output_search_previous_output = {'output': _color_regexp.sub('', out), 'cmd': cmd}
+
+try:
+    @events.on_ptk_create
+    def outout_keybindings(prompter, history, completer, bindings, **kw):
+        if __xonsh__.env.get('SHELL_TYPE') in ["prompt_toolkit", "prompt_toolkit2"]:
+            handler = bindings.add
+        else:
+            handler = bindings.registry.add_binding
+
+        bind = (_key_binding,) if _key_binding else (_key_meta, _key)
+
+        @bindings.add(*bind)
+        def _(event):
+            if __xonsh__.xontrib_output_search_previous_output is not None:
+                __xonsh__.xontrib_output_search_completion=True
+                event.current_buffer.start_completion(select_first=True)
+
+except Exception as e:
+    print('xontrib-output-search: Cannot set shortcuts')
+    raise
