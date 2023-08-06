36703d073ef8efcb0ebc3451bb686701a407a95b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:29 +0000

    Add package mutuazones/0.1.4/mutuazones-0.1.4.tar.gz

---
+++ packages/mutuazones/mutuazones-0.1.4.tar.gz/mutuazones-0.1.4/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mutuazones",
+    version="0.1.4",
+    author="Carlos López Pérez",
+    author_email="carlos.lopez@mutuatfe.com",
+    description="Add minimal parameter to pandas profiling",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.com/mutuatfe/smartflow/mutuazones.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'pandas',
+        'pandas_profiling',
+        'sqlalchemy'
+    ]
+)
