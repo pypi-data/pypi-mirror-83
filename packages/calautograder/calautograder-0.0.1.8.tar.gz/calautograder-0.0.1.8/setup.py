fa23b3c2b7b1e57e6e952d0d8cfc9d0ba6850ca8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:14 +0000

    Add package calautograder/0.0.1.8/calautograder-0.0.1.8.tar.gz

---
+++ packages/calautograder/calautograder-0.0.1.8.tar.gz/calautograder-0.0.1.8/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['calautograder',
+ 'calautograder.project',
+ 'calautograder.project.freezerestore']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['calautograder = main:funky']}
+
+setup_kwargs = {
+    'name': 'calautograder',
+    'version': '0.0.1.8',
+    'description': 'commandline autograder implimentation',
+    'long_description': None,
+    'author': 'Nick Brown',
+    'author_email': 'bicknrown@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
