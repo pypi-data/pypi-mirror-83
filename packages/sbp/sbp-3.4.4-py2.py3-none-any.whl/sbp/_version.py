fc24b2a07b45e5cadd4c087302c6ade5e6beeb44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:37 +0000

    Add package sbp/3.4.4/sbp-3.4.4-py2.py3-none-any.whl

---
+++ packages/sbp/sbp-3.4.4-py2.py3-none-any.whl/sbp/_version.py
@@ -0,0 +1,7 @@
+# THIS FILE IS GENERATED FROM TRAITS SETUP.PY
+version = '3.4.4'
+full_version = '3.4.4'
+git_revision = 'Unknown'
+is_released = True
+if not is_released:
+    version = full_version
