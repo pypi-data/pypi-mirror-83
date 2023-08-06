ca49410bdf985643703d0e3c56900a57c62300f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:03 +0000

    Add package noterr/1.2.3/noterr-1.2.3.tar.gz

---
+++ packages/noterr/noterr-1.2.3.tar.gz/noterr-1.2.3/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name = 'noterr',
+    version = '1.2.3',
+    packages = ['noterr'],
+    license='MIT',        
+    description = 'A package to keep track of your note inside the terminal',  
+    author = 'Japroz Singh Saini',     
+    long_description = long_description,  
+    long_description_content_type="text/markdown",            
+    author_email = 'sainijaproz@gmail.com',     
+    url = 'https://github.com/Japroz-Saini/',  
+    download_url = 'https://github.com/Japroz-Saini/noterr/archive/v1.1.0.tar.gz',
+    keywords = ['noterr', 'japroz singh saini', 'python', 'pip'],
+    entry_points = {
+        'console_scripts': [
+            'noterr = noterr.__main__:main'
+        ]
+    },
+    install_requires=[
+          'cs50',
+          'colored',
+    ])
