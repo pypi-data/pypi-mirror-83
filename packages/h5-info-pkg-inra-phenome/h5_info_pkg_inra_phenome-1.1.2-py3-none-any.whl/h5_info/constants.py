bd626b99f6fd6593a5bb61c30011d814f51d42a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:37 +0000

    Add package h5-info-pkg-inra-phenome/1.1.2/h5_info_pkg_inra_phenome-1.1.2-py3-none-any.whl

---
+++ packages/h5-info-pkg-inra-phenome/h5_info_pkg_inra_phenome-1.1.2-py3-none-any.whl/h5_info/constants.py
@@ -0,0 +1,11 @@
+import os
+
+END_LINE = os.linesep  # For Reading
+END_LINE_W = '\n'  # For Writing
+
+TYPE_CAMERA = 'camera'
+TYPE_LIDAR = 'lidar'
+TYPE_MULTISPECTRAL = 'multispectral_camera'
+
+DATETIME_FORMAT = '%Y-%m-%d_%H:%M:%S.%f'
+DATETIME_FORMAT_S = '%Y-%m-%d_%H:%M:%S'
