a17a5d833e23f013dbf3ac257910d42c7b9e8d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:19 +0000

    Add package FINQ/1.1.1b0/FINQ-1.1.1b0.tar.gz

---
+++ packages/FINQ/FINQ-1.1.1b0.tar.gz/FINQ-1.1.1b0/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="FINQ",
+    version="1.1.1b",
+    author="FacelessLord",
+    author_email="skyres21@gmail.com",
+    description="Lightweight conveyor data processing python framework",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FacelessLord/FINQ",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
