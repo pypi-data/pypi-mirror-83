0da1391813c56a27dab72b0abb81b088d9ee3f5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:43 +0000

    Add package collapse/0.0.1/collapse-0.0.1.tar.gz

---
+++ packages/collapse/collapse-0.0.1.tar.gz/collapse-0.0.1/README.md
@@ -0,0 +1,9 @@
+# Collapsing Cosmology Research Utilities
+
+
+The `collapse` package contains utilities for computing symbolic and numerical expressions related to the time evolution
+of collapsing cosmologies. 
+
+
+## Symbolic Tools
+The `collapse` package makes use of `sympy` to compute symbolic curvature equations (EFE).
---
+++ packages/collapse/collapse-0.0.1.tar.gz/collapse-0.0.1/setup.py
@@ -0,0 +1,31 @@
+"""Setup file
+"""
+
+import setuptools
+
+import collapse
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(name='collapse',
+                 version=collapse.__version__,
+                 description='Collapse',
+                 long_description=long_description,
+                 long_description_content_type="text/markdown",
+                 url=collapse.__github_url__,
+                 author='James Kennington',
+                 author_email='jwkennington@psu.edu',
+                 license='MIT',
+                 packages=setuptools.find_packages(),
+                 install_requires=[
+                     'matplotlib',
+                     'numpy',
+                     'palettable',
+                     'pytest',
+                     'scipy',
+                     'simpy',
+                 ],
+                 zip_safe=False,
+                 include_package_data=True,
+                 )
