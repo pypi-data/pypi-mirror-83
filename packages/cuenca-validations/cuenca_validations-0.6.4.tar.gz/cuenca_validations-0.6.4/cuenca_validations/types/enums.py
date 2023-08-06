004e6cff7eb42351cf90b98de21476c3efc4b82b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:21 +0000

    Add package cuenca-validations/0.6.4/cuenca_validations-0.6.4.tar.gz

---
+++ packages/cuenca-validations/cuenca_validations-0.6.4.tar.gz/cuenca_validations-0.6.4/cuenca_validations/types/enums.py
@@ -0,0 +1,69 @@
+from enum import Enum
+
+
+class CardNetwork(str, Enum):
+    atm = 'atm'
+    visa = 'visa'
+
+
+class CardStatus(str, Enum):
+    active = 'active'
+    blocked = 'blocked'
+    created = 'created'
+    deactivated = 'deactivated'
+    printing = 'printing'
+
+
+class CardTransactionType(str, Enum):
+    auth = 'auth'
+    capture = 'capture'
+    expiration = 'expiration'
+    refund = 'refund'
+    void = 'void'
+
+
+class CardType(str, Enum):
+    physical = 'physical'
+    virtual = 'virtual'
+
+
+class DepositNetwork(str, Enum):
+    cash = 'cash'
+    internal = 'internal'
+    spei = 'spei'
+
+
+class ServiceProviderCategory(str, Enum):
+    cable = 'cable'
+    credit_card = 'credit_card'
+    electricity = 'electricity'
+    gas = 'gas'
+    internet = 'internet'
+    landline_telephone = 'landline_telephone'
+    mobile_telephone_postpaid = 'mobile_telephone_postpaid'
+    mobile_telephone_prepaid = 'mobile_telephone_prepaid'
+    satelite_television = 'satelite_television'
+    water = 'water'
+
+
+class TransactionStatus(str, Enum):
+    created = 'created'
+    submitted = 'submitted'
+    in_review = 'in_review'
+    succeeded = 'succeeded'
+    failed = 'failed'
+
+
+class TransferNetwork(str, Enum):
+    internal = 'internal'
+    spei = 'spei'
+
+
+class CommissionType(str, Enum):
+    card_request = 'card_request'
+    cash_deposit = 'cash_deposit'
+
+
+class EntryType(str, Enum):
+    credit = 'credit'
+    debit = 'debit'
