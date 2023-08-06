d00d18627e9991a72ecb6f2fa0caf2d0084da467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:06 +0000

    Add package MLOne/0.0.2/MLOne-0.0.2.tar.gz

---
+++ packages/MLOne/MLOne-0.0.2.tar.gz/MLOne-0.0.2/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+setuptools.setup(
+    name="MLOne", # Replace with your own username
+    version="0.0.2",
+    author="Suhas and Dhamodaran",
+    author_email="srisuhas2000@gmail.com",
+    description="A Python package with which users can just drop their dataset and download the best ML model for their dataset",
+    long_description=open('README.md',errors='ignore').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Dhamodaran-Babu/ML-Thunai",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"],
+    install_requires=[
+       "python",
+       "pandas",
+       "numpy",
+       "matplotlib",
+       "imblearn",
+       "sklearn",
+       "mlxtend",
+       "seaborn",
+       "joblib"
+    ]
+   
+)
