6a953e0a129af19605311be6cd77bff57a989639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:11 +0000

    Add package jet-bridge-base/0.9.0/jet_bridge_base-0.9.0-py3-none-any.whl

---
+++ packages/jet-bridge-base/jet_bridge_base-0.9.0-py3-none-any.whl/jet_bridge_base/settings.py
@@ -0,0 +1,36 @@
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+CURRENT_MODULE = sys.modules[__name__]
+
+DEBUG = False
+READ_ONLY = False
+AUTO_OPEN_REGISTER = True
+PROJECT = None
+TOKEN = None
+CORS_HEADERS = True
+BASE_URL = None
+
+WEB_BASE_URL = None
+API_BASE_URL = None
+
+DATABASE_ENGINE = None
+DATABASE_HOST = None
+DATABASE_PORT = None
+DATABASE_USER = None
+DATABASE_PASSWORD = None
+DATABASE_NAME = None
+DATABASE_EXTRA = None
+DATABASE_CONNECTIONS = None
+DATABASE_ONLY = None
+DATABASE_EXCEPT = None
+DATABASE_SCHEMA = None
+
+SSO_APPLICATIONS = {}
+
+ALLOW_ORIGIN = '*'
+
+def set_settings(settings):
+    for key, value in settings.items():
+        setattr(CURRENT_MODULE, key, value)
