313a6e52d663f36700d2e0f0dcf8c9b131a371bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:24 +0000

    Add package edgerun-galileo/0.8.0.dev1/edgerun-galileo-0.8.0.dev1.tar.gz

---
+++ packages/edgerun-galileo/edgerun-galileo-0.8.0.dev1.tar.gz/edgerun-galileo-0.8.0.dev1/setup.py
@@ -0,0 +1,37 @@
+import os
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+if os.path.exists("requirements-dev.txt"):
+    with open("requirements-dev.txt", "r") as fh:
+        tests_require = [line for line in fh.read().split(os.linesep) if line]
+else:
+    tests_require = []
+
+with open("requirements.txt", "r") as fh:
+    install_requires = [line for line in fh.read().split(os.linesep) if line and not line.startswith('git')]
+
+setuptools.setup(
+    name="edgerun-galileo",
+    version="0.8.0.dev1",
+    author="Thomas Rausch",
+    author_email="t.rausch@dsg.tuwien.ac.at",
+    description="Galileo: A framework for distributed load testing experiments",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/edgerun/galileo",
+    packages=setuptools.find_packages(),
+    test_suite="tests",
+    tests_require=tests_require,
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        "console_scripts": ['galileo = galileo.cli.galileo:main']
+    },
+)
