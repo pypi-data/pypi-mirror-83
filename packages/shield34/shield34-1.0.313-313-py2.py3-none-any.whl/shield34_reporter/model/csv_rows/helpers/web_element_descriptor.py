5ba9156d192e2d0a38b23b6f490aa45570564bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:10 +0000

    Add package shield34/1.0.313/shield34-1.0.313-313-py2.py3-none-any.whl

---
+++ packages/shield34/shield34-1.0.313-313-py2.py3-none-any.whl/shield34_reporter/model/csv_rows/helpers/web_element_descriptor.py
@@ -0,0 +1,20 @@
+from shield34_reporter.container.run_report_container import RunReportContainer
+
+
+class WebElementDescriptor():
+    locator = ''
+
+    def __init__(self, web_element):
+        web_element_locator = RunReportContainer.get_current_block_run_holder().get_web_element(web_element.id)
+        self.locator = web_element_locator
+
+    def gen_row_value(self, lst=['locator']):
+        row_value = {}
+
+        for a in lst:
+            row_value[a] = getattr(self, a)
+        return row_value
+
+
+
+
