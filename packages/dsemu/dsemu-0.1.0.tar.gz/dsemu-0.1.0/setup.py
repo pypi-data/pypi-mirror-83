3be7332de7ee3623bbda2ed0bde7bc68ebb6b29e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:29 +0000

    Add package dsemu/0.1.0/dsemu-0.1.0.tar.gz

---
+++ packages/dsemu/dsemu-0.1.0.tar.gz/dsemu-0.1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="dsemu",
+    version="0.1.0",
+    url="https://github.com/fwojciec/dsemu",
+    license="MIT",
+    author="Filip Wojciechowski",
+    author_email="fwojciec@gmail.com",
+    description="A wrapper around the datastore emulator instance for use in tests.",
+    packages=find_packages(exclude=("tests",)),
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
