922162158f9ac7e90612e6d23558fc052c26403f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:53 +0000

    Add package Command-Line-Menu/0.2.2/Command%20Line%20Menu-0.2.2.tar.gz

---
+++ packages/Command-Line-Menu/Command%20Line%20Menu-0.2.2.tar.gz/Command Line Menu-0.2.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Command Line Menu',
+      version='0.2.2',
+      description='A simple module to create a menu for command line programs.',
+      url='https://github.com/KrazyKirby99999/command-line-menu',
+      author='KrazyKirby99999',
+      author_email='krazykirby99999@gmail.com',
+      license='GNU AGPLv3',
+      packages=['command_line_menu'],
+      zip_safe=False)
