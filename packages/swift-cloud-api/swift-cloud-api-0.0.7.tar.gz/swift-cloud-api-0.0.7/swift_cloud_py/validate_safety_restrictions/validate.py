ead27d5cd42d10c6b0a660bfc038930195931152
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:16 +0000

    Add package swift-cloud-api/0.0.7/swift-cloud-api-0.0.7.tar.gz

---
+++ packages/swift-cloud-api/swift-cloud-api-0.0.7.tar.gz/swift-cloud-api-0.0.7/swift_cloud_py/validate_safety_restrictions/validate.py
@@ -0,0 +1,21 @@
+from swift_cloud_py.entities.control_output.fixed_time_schedule import FixedTimeSchedule
+from swift_cloud_py.entities.intersection.intersection import Intersection
+from swift_cloud_py.validate_safety_restrictions.validate_bounds import validate_bounds
+from swift_cloud_py.validate_safety_restrictions.validate_completeness import validate_completeness
+from swift_cloud_py.validate_safety_restrictions.validate_conflicts import validate_conflicts
+from swift_cloud_py.validate_safety_restrictions.validate_other_sg_relations import validate_other_sg_relations
+
+
+def validate_safety_restrictions(intersection: Intersection, fixed_time_schedule: FixedTimeSchedule) -> None:
+    """
+    Check if the fixed-time schedule satisfies the safety restrictions such as bounds on greenyellow times
+    and bounds on red times.
+    intersection: intersection object (this object also contains safety restrictions that a
+    fixed-time schedule should satisfy)
+
+    This method raises a SafetyViolation-exception if the safety restrictions are not satisfied.
+    """
+    validate_bounds(intersection=intersection, fts=fixed_time_schedule)
+    validate_conflicts(intersection=intersection, fts=fixed_time_schedule)
+    validate_other_sg_relations(intersection=intersection, fts=fixed_time_schedule)
+    validate_completeness(intersection=intersection, fts=fixed_time_schedule)
