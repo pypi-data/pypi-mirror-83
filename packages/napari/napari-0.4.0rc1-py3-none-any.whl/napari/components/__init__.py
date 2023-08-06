69a90ceef93afaecfe279c8b67677ba124d4f4b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:13 +0000

    Add package napari/0.4.0rc1/napari-0.4.0rc1-py3-none-any.whl

---
+++ packages/napari/napari-0.4.0rc1-py3-none-any.whl/napari/components/__init__.py
@@ -0,0 +1,19 @@
+"""napari.components provides the public-facing models for widgets
+and other utilities that the user will be able to programmatically interact
+with.
+
+Classes
+-------
+Dims
+    Current indices along each data dimension, together with which dimensions
+    are being displayed, projected, sliced...
+LayerList
+    List of layers currently present in the viewer.
+ViewerModel
+    Data viewer displaying the currently rendered scene and
+    layer-related controls.
+"""
+from .camera import Camera
+from .dims import Dims
+from .layerlist import LayerList
+from .viewer_model import ViewerModel
