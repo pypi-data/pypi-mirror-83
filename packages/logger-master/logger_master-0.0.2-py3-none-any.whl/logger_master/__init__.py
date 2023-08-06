b7bb021d4dc3df48b7600f1401f6bac9aaf1ed01
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:15 +0000

    Add package logger-master/0.0.2/logger_master-0.0.2-py3-none-any.whl

---
+++ packages/logger-master/logger_master-0.0.2-py3-none-any.whl/logger_master/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*
+"""
+      ┏┓       ┏┓
+    ┏━┛┻━━━━━━━┛┻━┓
+    ┃      ☃      ┃
+    ┃  ┳┛     ┗┳  ┃
+    ┃      ┻      ┃
+    ┗━┓         ┏━┛
+      ┗┳        ┗━┓
+       ┃          ┣┓
+       ┃          ┏┛
+       ┗┓┓┏━━━━┳┓┏┛
+        ┃┫┫    ┃┫┫
+        ┗┻┛    ┗┻┛
+    God Bless,Never Bug
+"""
+from logger_master.logger import *
+from logger_master._mongo_handler import *
+from logger_master._redis_handler import *
+from logger_master._fluentd_handler import *
