3f1b636bad54ddd4f869a8fe627d43d5afc68bcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:40 +0000

    Add package num-thai-cnv/1.0.1/num_thai_cnv-1.0.1.tar.gz

---
+++ packages/num-thai-cnv/num_thai_cnv-1.0.1.tar.gz/num_thai_cnv-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding='utf8') as fh:
+    long_description = fh.read()
+
+setup(
+    name="num_thai_cnv",  # Replace with your own username
+    version="1.0.1",
+    author="Natthakhon Laosurasuntorn",
+    author_email="nattlao@gmail.com",
+    description="Thai localization package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    keywords=['num', 'thai'],
+    License='MIT',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
