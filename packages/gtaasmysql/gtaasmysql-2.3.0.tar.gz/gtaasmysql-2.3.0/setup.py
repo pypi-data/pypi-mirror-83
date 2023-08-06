2a624902ae656e9af9e59ef6e3502259b67720fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:55 +0000

    Add package gtaasmysql/2.3.0/gtaasmysql-2.3.0.tar.gz

---
+++ packages/gtaasmysql/gtaasmysql-2.3.0.tar.gz/gtaasmysql-2.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='gtaasmysql',
+    version='2.3.0',
+    py_modules=['gtaasmysql'],
+    author='WangFei',
+    author_email='qinexpire@gmail.com',
+    url='https://blog.csdn.net/andrew_wf',
+    description='Other'
+)
