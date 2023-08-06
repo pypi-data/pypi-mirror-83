10cfa6665292d63af74eb715f3da2794b61b1aab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:24 +0000

    Add package pygoul/0.0.3/pygoul-0.0.3.tar.gz

---
+++ packages/pygoul/pygoul-0.0.3.tar.gz/pygoul-0.0.3/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+  name = 'pygoul',
+    version="0.0.3",
+  description = 'A goul library',
+  author = 'Goulc\'hen',
+  author_email = 'goulmeur@gmail.com',
+  url = 'https://github.com/goulchen/PyGoul',
+  packages=setuptools.find_packages(),
+  classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+  ],
+  python_requires='>=3.6',
+)
