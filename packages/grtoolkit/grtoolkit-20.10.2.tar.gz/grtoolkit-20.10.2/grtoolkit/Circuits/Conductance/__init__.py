8873ec6665326a60538899a14f2701589e015649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:13:55 +0000

    Add package grtoolkit/20.10.2/grtoolkit-20.10.2.tar.gz

---
+++ packages/grtoolkit/grtoolkit-20.10.2.tar.gz/grtoolkit-20.10.2/grtoolkit/Circuits/Conductance/__init__.py
@@ -0,0 +1,16 @@
+
+
+# def Conductance(r):
+#     # Look at functions as objects
+#     # https://medium.com/python-pandemonium/function-as-objects-in-python-d5215e6d1b0d
+#     return 1/r
+
+def __call__(r): #conductance
+    return 1/r 
+
+def ConductanceInSeries(g_list):
+    sumOfInverse = sum([1/g for g in g_list])
+    return 1/sumOfInverse
+
+def ConductanceInParallel(g_list):
+    return sum(g_list)
