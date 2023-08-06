70f07c08652f497831ef78307c5ec6578e0a3c12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:20 +0000

    Add package duckdb-engine/0.1.2/duckdb_engine-0.1.2.tar.gz

---
+++ packages/duckdb-engine/duckdb_engine-0.1.2.tar.gz/duckdb_engine-0.1.2/duckdb_engine/__init__.py
@@ -0,0 +1,98 @@
+from dataclasses import dataclass, field
+from typing import List
+
+import duckdb
+from sqlalchemy.dialects.postgresql import dialect as postgres_dialect
+
+
+class DBAPI:
+    paramstyle = "qmark"
+
+    class Error(Exception):
+        pass
+
+
+@dataclass
+class ConnectionWrapper:
+    c: duckdb.DuckDBPyConnection
+    notices: List[str] = field(default_factory=list)
+
+    def cursor(self):
+        return self
+
+    def __getattr__(self, name):
+        return getattr(self.c, name)
+
+    @property
+    def connection(self):
+        return self
+
+    def close(self):
+        # duckdb doesn't support 'soft closes'
+        pass
+
+    def execute(self, statement, parameters, context):
+        self.c.execute(statement, parameters)
+
+
+def check_existance(connection, function: str, name: str, type_: str) -> bool:
+    try:
+        connection.execute(f"{function}('{name}');")
+    except RuntimeError as e:
+        if e.args[0].startswith(
+            f"Catalog Error: {type_} with name {name} does not exist!"
+        ):
+            return False
+        else:
+            raise
+    else:
+        return True
+
+
+class Dialect(postgres_dialect):
+    _has_events = False
+    identifier_preparer = None
+    # colspecs TODO: remap types to duckdb types
+
+    def connect(self, *args, **kwargs):
+        return ConnectionWrapper(duckdb.connect(*args, **kwargs))
+
+    def on_connect(self):
+        pass
+
+    def ddl_compiler(self, dialect, ddl, **kwargs):
+        # TODO: enforce no `serial` type
+        ddl.include_foreign_key_constraints = {}
+        return postgres_dialect.ddl_compiler(dialect, ddl, **kwargs)
+
+    def do_execute(self, cursor, statement, parameters, context):
+        cursor.execute(statement, parameters, context)
+
+    def has_table(self, connection, table_name, schema=None):
+        return check_existance(connection, "PRAGMA show", table_name, "Table")
+
+    def has_sequence(self, connection, sequence_name, schema=None):
+        return check_existance(connection, "SELECT nextval", sequence_name, "Sequence")
+
+    def has_type(self, connection, type_name, schema=None):
+        return False
+
+    @staticmethod
+    def dbapi():
+        return DBAPI
+
+    def create_connect_args(self, u):
+        return (), {"database": u.__to_string__(hide_password=False).split("///")[1]}
+
+    def initialize(self, connection):
+        pass
+
+    def do_rollback(self, connection):
+        pass
+
+    @classmethod
+    def get_dialect_cls(cls, u):
+        return cls
+
+
+dialect = Dialect
---
+++ packages/duckdb-engine/duckdb_engine-0.1.2.tar.gz/duckdb_engine-0.1.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['duckdb_engine', 'duckdb_engine.tests']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['duckdb==0.2.2.dev655', 'sqlalchemy>=1.3.19,<2.0.0']
+
+entry_points = \
+{'sqlalchemy.dialects': ['duckdb = duckdb_engine']}
+
+setup_kwargs = {
+    'name': 'duckdb-engine',
+    'version': '0.1.2',
+    'description': '',
+    'long_description': '# duckdb_engine\n\nVery very very basic sqlalchemy driver for duckdb\n\nOnce you install this package, you should be able to just use it, as sqlalchemy does a python path search\n\n```python\nfrom sqlalchemy import Column, Integer, Sequence, String, create_engine\nfrom sqlalchemy.ext.declarative import declarative_base\nfrom sqlalchemy.orm.session import Session\n\nBase = declarative_base()\n\n\nclass FakeModel(Base):  # type: ignore\n    __tablename__ = "fake"\n\n    id = Column(Integer, Sequence("fakemodel_id_sequence"), primary_key=True)\n    name = Column(String)\n\n\neng = create_engine("duckdb:///:memory:")\nBase.metadata.create_all(eng)\nsession = Session(bind=eng)\n\nsession.add(FakeModel(name="Frank"))\nsession.commit()\n\nfrank = session.query(FakeModel).one()\n\nassert frank.name == "Frank"\n```\n',
+    'author': 'Elliana',
+    'author_email': 'me@mause.me',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Mause/duckdb_engine',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
