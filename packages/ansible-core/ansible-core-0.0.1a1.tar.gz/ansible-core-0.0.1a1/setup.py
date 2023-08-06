6eada5380f726651d60a6575ce9f61086a2cfdc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:41 +0000

    Add package ansible-core/0.0.1a1/ansible-core-0.0.1a1.tar.gz

---
+++ packages/ansible-core/ansible-core-0.0.1a1.tar.gz/ansible-core-0.0.1a1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+	name="ansible-core",
+	version="0.0.1a1",
+	packages=find_packages("lib"),
+	author="Ansible Project",
+	author_email="info@ansible.com",
+	url="https://github.com/ansible/ansible/"
+)
