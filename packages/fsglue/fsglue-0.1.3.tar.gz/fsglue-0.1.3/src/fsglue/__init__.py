640af81a8b39661ef2b4fdf8c9187f4ba9dd338e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:08 +0000

    Add package fsglue/0.1.3/fsglue-0.1.3.tar.gz

---
+++ packages/fsglue/fsglue-0.1.3.tar.gz/fsglue-0.1.3/src/fsglue/__init__.py
@@ -0,0 +1,27 @@
+# flake8: noqa
+
+__version__ = "0.1.3"
+
+from .client import initialize
+
+from .model import BaseModel
+
+from .property import (
+    PropertySpecialValue,
+    BaseProperty,
+    StringProperty,
+    IntegerProperty,
+    FloatProperty,
+    BooleanProperty,
+    TimestampProperty,
+    JsonProperty,
+    ComputedProperty,
+    ConstantProperty,
+)
+
+from .exceptions import (
+    FsglueException,
+    FsglueProgrammingError,
+    FsglueValidationError,
+    FsglueDocumentNotFound,
+)
