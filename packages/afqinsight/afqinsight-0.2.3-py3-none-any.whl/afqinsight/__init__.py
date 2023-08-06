5212d9e253101b96f00646e48f0f35b82d5187ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:35 +0000

    Add package afqinsight/0.2.3/afqinsight-0.2.3-py3-none-any.whl

---
+++ packages/afqinsight/afqinsight-0.2.3-py3-none-any.whl/afqinsight/__init__.py
@@ -0,0 +1,7 @@
+"""AFQ-Insight is a Python library for statistical learning of tractometry data."""
+from . import datasets  # noqa
+from . import utils  # noqa
+from .datasets import *  # noqa
+from .insight import *  # noqa
+from .transform import *  # noqa
+from ._version import version as __version__  # noqa
