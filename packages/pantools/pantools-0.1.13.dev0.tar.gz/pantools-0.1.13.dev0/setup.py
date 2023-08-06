43675d5ebe8ab1569e3310084ab30b239182966f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:05 +0000

    Add package pantools/0.1.13.dev0/pantools-0.1.13.dev0.tar.gz

---
+++ packages/pantools/pantools-0.1.13.dev0.tar.gz/pantools-0.1.13.dev0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+setup(
+    name='pantools',
+    packages=find_packages(include=['src']),
+    version='0.1.13.dev0',
+    author='Peter Andersson',
+    author_email='peter.andersson@fsh.se',
+    description='PAN Tools library',
+    url="https://github.com/fshsweden/pantools",
+    license='MIT',
+    install_requires=[],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==4.4.1'],
+    test_suite='tests',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+)
