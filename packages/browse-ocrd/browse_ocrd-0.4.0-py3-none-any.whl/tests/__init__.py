b6cfae422f982b1845bc0f6371758e698d188114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:35 +0000

    Add package browse-ocrd/0.4.0/browse_ocrd-0.4.0-py3-none-any.whl

---
+++ packages/browse-ocrd/browse_ocrd-0.4.0-py3-none-any.whl/tests/__init__.py
@@ -0,0 +1,30 @@
+"""
+Fixes warnings about unspecified version contraints a la:
+PyGIWarning: GtkSource was imported without specifying a version first. Use gi.require_version('GtkSource', '4') before import to ensure that the right version gets loaded.
+
+import with:
+
+  import tests
+
+in your Gtk using test file in the first line
+"""
+# pylint: disable=unused-import
+from unittest import TestCase
+import gi
+from pathlib import Path
+from ocrd_utils import initLogging
+
+gi.require_version('Gtk', '3.0')
+gi.require_version('GdkPixbuf', '2.0')
+try:
+    gi.require_version('GtkSource', '4')
+except ValueError:
+    gi.require_version('GtkSource', '3.0')
+
+initLogging()
+
+TEST_BASE_PATH = (Path(__file__).parent).absolute()
+ASSETS_PATH = (TEST_BASE_PATH / 'assets').absolute()
+if not ASSETS_PATH.exists():
+    raise RuntimeError('Assset path {} not found, please run: make tests/assets first')
+
