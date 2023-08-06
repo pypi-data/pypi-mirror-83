9938779de5505ab70060754be3a37bbb79a4f62e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:52 +0000

    Add package pytest-splunk-env/0.1.3a1/pytest-splunk-env-0.1.3a1.tar.gz

---
+++ packages/pytest-splunk-env/pytest-splunk-env-0.1.3a1.tar.gz/pytest-splunk-env-0.1.3a1/pytest_splunk_env/splunk/helmut/exceptions/search.py
@@ -0,0 +1,17 @@
+# SPDX-FileCopyrightText: 2020 Splunk Inc.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+class SearchFailure(RuntimeError):
+    """
+    This exception is raised when a search fails and returns the error through sdk get_message function.
+    """
+
+    def __init__(self, search_message):
+        self.search_message = search_message
+        super(SearchFailure, self).__init__(self._error_message)
+
+    @property
+    def _error_message(self):
+        message = "Search failed with Error: {0}"
+        return message.format(self.search_message)
