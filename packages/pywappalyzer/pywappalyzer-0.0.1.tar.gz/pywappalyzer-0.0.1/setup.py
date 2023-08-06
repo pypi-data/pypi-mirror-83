4d344d76b649d729c7f6642fde96d73f7d2abd57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:34 +0000

    Add package pywappalyzer/0.0.1/pywappalyzer-0.0.1.tar.gz

---
+++ packages/pywappalyzer/pywappalyzer-0.0.1.tar.gz/pywappalyzer-0.0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pywappalyzer",  # Replace with your own username
+    version="0.0.1",
+    author="Kel0",
+    author_email="rozovdima123@gmail.com",
+    description="Easy identify site's IT technologies",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
