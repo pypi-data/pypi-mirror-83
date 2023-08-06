255abc79cbe5f1e868815df34753444f071e07c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:20 +0000

    Add package five.formlib/2.2/five.formlib-2.2-py2.py3-none-any.whl

---
+++ packages/five.formlib/five.formlib-2.2-py2.py3-none-any.whl/five/formlib/formbase.py
@@ -0,0 +1,117 @@
+##############################################################################
+#
+# Copyright (c) 2006 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+"""Five baseclasses for zope.formlib.form
+"""
+import os.path
+
+import zope.formlib
+from zope import interface
+from zope.formlib import interfaces, form
+
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from ZPublisher import HTTPRequest
+from zope.i18nmessageid import MessageFactory
+_ = MessageFactory("zope")
+
+_FORMLIB_DIR = os.path.dirname(zope.formlib.__file__)
+_PAGEFORM_PATH = os.path.join(_FORMLIB_DIR, 'pageform.pt')
+_SUBPAGEFORM_PATH = os.path.join(_FORMLIB_DIR, 'subpageform.pt')
+
+
+class FiveFormlibMixin(object):
+
+    # Overrides the formlib.form.FormBase.template attributes implemented
+    # using NamedTemplates. NamedTemplates using ViewPageTemplateFile (like
+    # formlib does by default) cannot work in Zope2.
+
+    # XXX Maybe we need to have Five-compatible NamedTemplates?
+
+    template = ViewPageTemplateFile(_PAGEFORM_PATH)
+
+    # Overrides formlib.form.FormBase.update. Make sure user input is
+    # decoded first and the page encoding is set before proceeding.
+
+    def update(self):
+        # BBB: for CMFDefault < 2.3 (explicit charset required)
+        self.request.RESPONSE.setHeader(
+            'Content-Type',
+            'text/html; charset=%s' % HTTPRequest.default_encoding
+        )
+        super(FiveFormlibMixin, self).update()
+
+
+class FormBase(FiveFormlibMixin, form.FormBase):
+    pass
+
+
+class EditFormBase(FiveFormlibMixin, form.EditFormBase):
+    pass
+
+
+class DisplayFormBase(FiveFormlibMixin, form.DisplayFormBase):
+    pass
+
+
+class AddFormBase(FiveFormlibMixin, form.AddFormBase):
+    pass
+
+
+@interface.implementer(interfaces.IPageForm)
+class PageForm(FormBase):
+    pass
+
+
+Form = PageForm
+
+
+@interface.implementer(interfaces.IPageForm)
+class PageEditForm(EditFormBase):
+    pass
+
+
+EditForm = PageEditForm
+
+
+@interface.implementer(interfaces.IPageForm)
+class PageDisplayForm(DisplayFormBase):
+    pass
+
+
+DisplayForm = PageDisplayForm
+
+
+@interface.implementer(interfaces.IPageForm)
+class PageAddForm(AddFormBase):
+    pass
+
+
+AddForm = PageAddForm
+
+
+@interface.implementer(interfaces.ISubPageForm)
+class SubPageForm(FormBase):
+
+    template = ViewPageTemplateFile(_SUBPAGEFORM_PATH)
+
+
+@interface.implementer(interfaces.ISubPageForm)
+class SubPageEditForm(EditFormBase):
+
+    template = ViewPageTemplateFile(_SUBPAGEFORM_PATH)
+
+
+@interface.implementer(interfaces.ISubPageForm)
+class SubPageDisplayForm(DisplayFormBase):
+
+    template = ViewPageTemplateFile(_SUBPAGEFORM_PATH)
---
+++ packages/five.formlib/five.formlib-2.2-py2.py3-none-any.whl/five/formlib/tests/test_formlib.py
@@ -0,0 +1,115 @@
+##############################################################################
+#
+# Copyright (c) 2004, 2005 Zope Corporation and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the Zope Public License,
+# Version 2.1 (ZPL).  A copy of the ZPL should accompany this distribution.
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL EXPRESS OR IMPLIED
+# WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND FITNESS
+# FOR A PARTICULAR PURPOSE.
+#
+##############################################################################
+
+import unittest
+from doctest import DocTestSuite
+from Testing.ZopeTestCase import FunctionalDocFileSuite
+from uuid import uuid4
+from Testing.ZopeTestCase.zopedoctest.functional import http
+
+
+def test_get_widgets_for_schema_fields():
+    """
+    Test widget lookup for schema fields
+
+    First, load the configuration files:
+
+      >>> from Zope2.App import zcml
+      >>> import Products.Five
+      >>> zcml.load_config('configure.zcml', Products.Five)
+      >>> import zope.app.form.browser
+      >>> zcml.load_config('configure.zcml', zope.app.form.browser)
+
+    Now for some actual testing...
+
+      >>> from zope.schema import Choice, TextLine
+      >>> salutation = Choice(title=u'Salutation',
+      ...                     values=("Mr.", "Mrs.", "Captain", "Don"))
+      >>> contactname = TextLine(title=u'Name')
+
+      >>> from zope.publisher.browser import TestRequest
+      >>> request = TestRequest()
+      >>> salutation = salutation.bind(request)
+      >>> contactname = contactname.bind(request)
+
+      >>> from zope.component import getMultiAdapter
+      >>> from zope.formlib.interfaces import IInputWidget
+      >>> from zope.formlib.textwidgets import TextWidget
+      >>> from zope.formlib.itemswidgets import DropdownWidget
+
+      >>> view1 = getMultiAdapter((contactname, request), IInputWidget)
+      >>> view1.__class__ == TextWidget
+      True
+
+      >>> view2 = getMultiAdapter((salutation, request), IInputWidget)
+      >>> view2.__class__ == DropdownWidget
+      True
+
+    Clean up:
+
+      >>> from zope.component.testing import tearDown
+      >>> tearDown()
+    """
+
+
+def http_request(url, form_parts=None, body=None, auth=None):
+    """perform HTTP request from given parameters.
+
+    The primary purpose of this auxiliary function is to compute
+    the `Content-Length` header. While Python 2's `cgi` module
+    seems resilient to a wrong `Content-Length`, Python 3's `cgi` fails
+    badly (at least for a too small value).
+
+    If given, *form_parts* must be an iterable yielding pairs *name*,*value*.
+    *body* is then computed as a typical form response from it.
+    Otherwise, if *body* is not `None`, it defines the request content.
+    Otherwise, a `GET` request is created.
+    """
+    boundary = None
+    if form_parts is not None:
+        boundary = str(uuid4())
+        body = "".join(
+            "--" + boundary + "\n" +
+            "Content-Disposition: form-data; name=\"" + name + "\"\n\n" +
+            value + "\n"
+            for (name, value) in form_parts
+            ) + "--" + boundary + "--\n"
+    headers = []
+    headers.append(("POST" if body is not None else "GET")
+                   + " " + url + " HTTP/1.1"
+                   )
+    if auth:
+        headers.append("Authorization: " + auth)
+    headers.append("Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7")
+    if body is not None:
+        if not body.endswith("\n"):
+            body += "\n"
+        headers.append("Content-Length: " + str(len(body)))
+        headers.append(
+            "Content-Type: " +
+            ("application/x-www-form-urlencoded" if boundary is None
+             else "multipart/form-data; boundary=" + boundary)
+                      )
+        body = "\n\n" + body
+    return http("\n".join(headers) + (body or ''))
+
+
+def test_suite():
+    return unittest.TestSuite([
+        DocTestSuite(),
+        FunctionalDocFileSuite('forms.txt', package="five.formlib.tests",
+                               globs=dict(http_request=http_request),
+                               ),
+        FunctionalDocFileSuite('formlib.txt', package='five.formlib.tests'),
+    ])
