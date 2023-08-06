054c432d7725669c7d76300f367b5112eacb37b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:14:25 +0000

    Add package dhnx/0.0.1rc1/dhnx-0.0.1rc1.tar.gz

---
+++ packages/dhnx/dhnx-0.0.1rc1.tar.gz/dhnx-0.0.1rc1/setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import os
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name='dhnx',
+    version='0.0.1rc1',
+    description='Simulation and optimization of district heating and cooling networks',
+    author="oemof developer group",
+    author_email='jann.launer@rl-institut.de, johannes-roeder@uni-bremen.de',
+    long_description=read('README.rst'),
+    packages=['dhnx'],
+    install_requires=[
+        'pandas >= 0.18.0',
+        'matplotlib',
+        'networkx',
+        'pillow',
+        'folium',
+        'addict',
+        'oemof.solph >= 0.4.0',
+        'scipy >= 1.5',
+    ],
+    extras_require={
+        'cartopy': ['cartopy'],
+        'geopandas': ['geopandas'],
+        'osmnx': ['osmnx'],
+    }
+)
