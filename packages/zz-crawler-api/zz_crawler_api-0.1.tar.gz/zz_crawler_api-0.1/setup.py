b59fb93e4c1f0a1228152b40500a89cad0404a71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:34 +0000

    Add package zz-crawler-api/0.1/zz_crawler_api-0.1.tar.gz

---
+++ packages/zz-crawler-api/zz_crawler_api-0.1.tar.gz/zz_crawler_api-0.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r",encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="zz_crawler_api",  # Replace with your own username
+    version="0.1",
+    author="CooperHan",
+    author_email="wy2208293418@163.com",
+    description="A sample Crawler API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TomersHan/AI-X-crawler",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+
+    ],
+    python_requires='>=3.6',
+)
---
+++ packages/zz-crawler-api/zz_crawler_api-0.1.tar.gz/zz_crawler_api-0.1/zz_crawler_api/zz_crawl_api.py
@@ -0,0 +1,30 @@
+import requests
+import time
+
+
+class CrawlAPI(object):
+    VERSION = 0.2
+    @classmethod
+    def wrap_api(cls, json_data):
+        json_data["meta"]["version"] = CrawlAPI.VERSION
+
+    @classmethod
+    def get_task_id(cls):
+        url = 'http://127.0.0.1:3000/get-task-id'
+        task_id = requests.get(url).text
+        return task_id
+
+    @classmethod
+    def get_create_time(cls):
+        creat_time = int(round(time.time() * 1000))
+        return creat_time
+
+    @classmethod
+    def upload(cls, data):
+        url_post = 'http://127.0.0.1:3000/kno-storage-req'
+        input = requests.post(url_post, json=data)
+        if input.status_code == 200:
+            print("插入成功")
+        else:
+            raise Exception("插入失败")
+        return input.status_code
