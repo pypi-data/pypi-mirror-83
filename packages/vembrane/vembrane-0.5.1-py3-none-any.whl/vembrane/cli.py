9ebbf365c1e28deef4a65df808e4fe4ca69b08d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:57 +0000

    Add package vembrane/0.5.1/vembrane-0.5.1-py3-none-any.whl

---
+++ packages/vembrane/vembrane-0.5.1-py3-none-any.whl/vembrane/cli.py
@@ -0,0 +1,20 @@
+import argparse
+
+from .modules import filter, table
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(
+        dest="command", description="valid subcommands", required=True
+    )
+    filter.add_subcommmand(subparsers)
+    table.add_subcommmand(subparsers)
+
+    args = parser.parse_args()
+    if args.command == "filter":
+        filter.execute(args)
+    elif args.command == "table":
+        table.execute(args)
+    else:
+        raise ValueError(f"Unknown subcommand {args.command}")
