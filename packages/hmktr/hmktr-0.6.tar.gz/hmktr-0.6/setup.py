f1eff31247f37102c3f04f5284590dcc3ea93e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:14 +0000

    Add package hmktr/0.6/hmktr-0.6.tar.gz

---
+++ packages/hmktr/hmktr-0.6.tar.gz/hmktr-0.6/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    README = readme_file.read()
+
+setup_args = dict(
+    name="hmktr",
+    version="0.6",
+    description="A python package to handle all traversal problems",
+    long_description_content_type="text/markdown",
+    long_description=README,
+    license="MIT",
+    packages=find_packages(),
+    author="Heramb Mishra",
+    author_email="heramb1008@gmail.com",
+    url="https://github.com/Herambdata/dl-package",
+    include_package_data=True
+)
+
+install_requires=[
+        "numpy>=1.18.5",
+        "pandas>=1.0.5",
+        "scikit-learn>=0.23.1",      
+]
+
+if __name__=='__main__':
+    setup(**setup_args,install_requires=install_requires)
