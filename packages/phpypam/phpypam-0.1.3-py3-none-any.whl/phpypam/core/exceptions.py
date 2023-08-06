97f2ef029d7db3c281e81e2ac9f3751f2cadc4a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:12 +0000

    Add package phpypam/0.1.3/phpypam-0.1.3-py3-none-any.whl

---
+++ packages/phpypam/phpypam-0.1.3-py3-none-any.whl/phpypam/core/exceptions.py
@@ -0,0 +1,42 @@
+# provides the main query methods.
+
+import json
+
+
+class PHPyPAMException(Exception):
+
+    def __init__(self, *args, **kwargs):
+        self._code = kwargs.pop('code', None)
+        self._message = kwargs.pop('message', None)
+
+        _NOT_FOUND_MESSAGES = {
+            'No subnets found',
+            'Address not found',
+        }
+
+        if (self._code == 200 and self._message in _NOT_FOUND_MESSAGES) or self._code == 404:
+            raise PHPyPAMEntityNotFoundException(self._message)
+        elif self._code == 500:
+            if self._message == 'Invalid username or password':
+                raise PHPyPAMInvalidCredentials(self._message)
+        elif self._code == 400:
+            raise PHPyPAMInvalidSyntax(message=self._message)
+
+        # super(PHPyPAMException, self).__init__(*args, **kwargs)
+
+
+class PHPyPAMInvalidCredentials(Exception):
+    def __init__(self, *args, **kwargs):
+        super(PHPyPAMInvalidCredentials, self).__init__(*args, **kwargs)
+
+
+class PHPyPAMEntityNotFoundException(Exception):
+    def __init__(self, *args, **kwargs):
+        super(PHPyPAMEntityNotFoundException, self).__init__(*args, **kwargs)
+
+
+class PHPyPAMInvalidSyntax(Exception):
+    def __init__(self, *args, **kwargs):
+        self._message = kwargs.pop('message', '')
+
+        super(PHPyPAMInvalidSyntax, self).__init__(self._message, *args, **kwargs)
