a147599a6fa1009e91bcaefec09db8ca9c2ec7d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:05 +0000

    Add package test-package-jun/0.0.1/test-package-jun-0.0.1.tar.gz

---
+++ packages/test-package-jun/test-package-jun-0.0.1.tar.gz/test-package-jun-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='test-package-jun',
+    version='0.0.1',
+    description='My private package from private github repo',
+    url='https://github.com/junnlikestea/test-package-jun',
+    author='Jun',
+    author_email='likesgophers@gmail.com',
+    license='unlicense',
+    packages=['test-package-jun'],
+    zip_safe=False
+)
