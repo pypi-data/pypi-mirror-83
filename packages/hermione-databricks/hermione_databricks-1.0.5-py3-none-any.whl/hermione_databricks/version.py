021d6852b4ceffa50b8561387c6eb42a940e8276
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:44 +0000

    Add package hermione-databricks/1.0.5/hermione_databricks-1.0.5-py3-none-any.whl

---
+++ packages/hermione-databricks/hermione_databricks-1.0.5-py3-none-any.whl/hermione_databricks/version.py
@@ -0,0 +1,12 @@
+__version__ = "1.0.5"
+# Version :1
+# Release :0
+# Revision:5
+
+"""
+VERSÃO: Acrescenta-se um numero a VERSÃO se o projeto for uma evolução.
+
+RELEASE: Acrescenta-se um numero a RELEASE se o projeto for uma melhoria (ex: exclusão ou inclusão de novas funcionalidades).
+
+REVISÃO: Acrescenta-se um numero a REVISÃO se o projeto for uma correção de funcionalidades já existentes.
+"""
