9e8da09cec91fe19d0e18b8bbe81166271a0a099
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:58 +0000

    Add package pydevd-odoo/1.1/pydevd-odoo-1.1.tar.gz

---
+++ packages/pydevd-odoo/pydevd-odoo-1.1.tar.gz/pydevd-odoo-1.1/pydevd_plugins/extensions/pydevd_plugin_odoo.py
@@ -0,0 +1,43 @@
+import sys
+from collections import OrderedDict
+
+from _pydevd_bundle.pydevd_extension_api import TypeResolveProvider, StrPresentationProvider
+from _pydevd_bundle.pydevd_resolver import defaultResolver
+
+
+class OdooRecordSetProvider(object):
+    def can_provide(self, type_object, type_name):
+        try:
+            from odoo import models
+            return isinstance(type_object, models.MetaModel)
+        except:
+            return False
+
+    def resolve(self, obj, attr):
+        try:
+            _id = int(attr)
+        except:
+            return getattr(obj, attr)
+        else:
+            return obj[_id]
+
+    def get_dictionary(self, obj):
+        if len(obj) > 1:
+            d = OrderedDict()
+            for idx, r in enumerate(obj):
+                d[str(idx)] = r
+            return d
+        return defaultResolver.get_dictionary(obj)
+
+    def get_str(self, val):
+        s = str(val)
+        if len(val) == 1:
+            name = getattr(val, 'name', None)
+            if name:
+                s += ' ⇨ %s' % name
+        return s
+
+
+if not sys.platform.startswith("java"):
+    TypeResolveProvider.register(OdooRecordSetProvider)
+    StrPresentationProvider.register(OdooRecordSetProvider)
---
+++ packages/pydevd-odoo/pydevd-odoo-1.1.tar.gz/pydevd-odoo-1.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pydevd-odoo',
+    version='1.1',
+    description='PyDev.Debugger plugin for Odoo',
+    url='https://github.com/trinhanhngoc/pydevd-odoo',
+    author='Trinh Anh Ngoc',
+    author_email='atw1990@gmail.com',
+    packages=find_packages(),
+    namespace_packages=['pydevd_plugins.extensions'],
+    license='MIT',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
