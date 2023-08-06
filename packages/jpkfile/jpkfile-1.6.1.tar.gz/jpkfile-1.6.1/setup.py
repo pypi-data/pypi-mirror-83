a3db9549c5c5d9b2eaa71e2b04e951ea90cbb2ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:36 +0000

    Add package jpkfile/1.6.1/jpkfile-1.6.1.tar.gz

---
+++ packages/jpkfile/jpkfile-1.6.1.tar.gz/jpkfile-1.6.1/setup.py
@@ -0,0 +1,28 @@
+"""
+USAGE: 
+   o install in develop mode: navigate to the folder containing this file,
+                              and type 'python setup.py develop --user'.
+                              (ommit '--user' if you want to install for 
+                               all users)                           
+"""
+from setuptools import setup
+
+with open("README.rst", 'r') as fh:
+    long_description = fh.read()
+
+setup(name='jpkfile',
+      version='1.6.1',
+      description='Package to handle reading of files recorded with JPK instruments.',
+      long_description=long_description,
+      long_description_content_type="text/x-rst",
+      url='https://gitlab.gwdg.de/ikuhlem/jpkfile',
+      author='Ilyas Kuhlemann',
+      author_email='ilyasp.ku@gmail.com',
+      license='MIT',
+      entry_points={
+          "console_scripts": [],
+          "gui_scripts": []
+      },
+      install_requires=['numpy'],
+      packages=['jpkfile'],
+      zip_safe=False)
