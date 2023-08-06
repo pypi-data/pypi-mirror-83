b15a19ea005972f8eb29ad5458d61bf043adcb6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:01 +0000

    Add package fwmp3/1.0.1/fwmp3-1.0.1.tar.gz

---
+++ packages/fwmp3/fwmp3-1.0.1.tar.gz/fwmp3-1.0.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['fwmp3']
+setup_kwargs = {
+    'name': 'fwmp3',
+    'version': '1.0.1',
+    'description': 'python 3.8',
+    'long_description': None,
+    'author': 'John Smith',
+    'author_email': 'azarovnick@bk.ru',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'py_modules': modules,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
