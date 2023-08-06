2b7e20df4e1f45d28077383c7399d0f4f338df6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:43 +0000

    Add package prettyqt/0.129.1/PrettyQt-0.129.1-py3-none-any.whl

---
+++ packages/prettyqt/PrettyQt-0.129.1-py3-none-any.whl/prettyqt/multimedia/__init__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+"""Multimedia module."""
+
+from .abstractvideobuffer import AbstractVideoBuffer
+
+# from .abstractplanarvideobuffer import AbstractPlanarVideoBuffer
+from .videoframe import VideoFrame
+from .audioformat import AudioFormat
+from .camerainfo import CameraInfo
+from .mediabindableinterface import MediaBindableInterface
+from .videoencodersettings import VideoEncoderSettings
+from .audioencodersettings import AudioEncoderSettings
+from .mediaobject import MediaObject
+from .camera import Camera
+from .cameraviewfindersettings import CameraViewfinderSettings
+from .mediacontent import MediaContent
+from .mediacontrol import MediaControl
+from .mediaplaylist import MediaPlaylist
+from .mediaplayer import MediaPlayer
+from .mediarecorder import MediaRecorder
+from .audiorecorder import AudioRecorder
+from .soundeffect import SoundEffect
+
+__all__ = [
+    "AudioFormat",
+    "AbstractVideoBuffer",
+    # "AbstractPlanarVideoBuffer",
+    "VideoFrame",
+    "CameraInfo",
+    "CameraViewfinderSettings",
+    "Camera",
+    "MediaBindableInterface",
+    "VideoEncoderSettings",
+    "AudioEncoderSettings",
+    "MediaControl",
+    "MediaContent",
+    "MediaObject",
+    "MediaPlaylist",
+    "MediaPlayer",
+    "MediaRecorder",
+    "AudioRecorder",
+    "SoundEffect",
+]
