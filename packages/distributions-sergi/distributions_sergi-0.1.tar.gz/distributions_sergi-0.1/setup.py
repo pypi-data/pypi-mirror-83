20691e73165b16358fab6875db7766b19a5455b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:49 +0000

    Add package distributions-sergi/0.1/distributions_sergi-0.1.tar.gz

---
+++ packages/distributions-sergi/distributions_sergi-0.1.tar.gz/distributions_sergi-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_sergi',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_sergi'],
+      zip_safe=False)
