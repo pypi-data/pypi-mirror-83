c1ff823a30672d7b35e5b724b4ee6ba3e3030759
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:00 +0000

    Add package scrapli-netconf/2020.10.24/scrapli_netconf-2020.10.24-py3-none-any.whl

---
+++ packages/scrapli-netconf/scrapli_netconf-2020.10.24-py3-none-any.whl/scrapli_netconf/__init__.py
@@ -0,0 +1,5 @@
+"""scrapli_netconf scrapli netconf plugin"""
+from scrapli_netconf.driver import AsyncNetconfScrape, NetconfScrape
+
+__version__ = "2020.10.24"
+__all__ = ("NetconfScrape", "AsyncNetconfScrape")
