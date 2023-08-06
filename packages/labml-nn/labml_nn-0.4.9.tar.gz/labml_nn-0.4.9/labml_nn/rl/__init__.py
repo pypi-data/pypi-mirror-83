d0b08bd0502a488dae52f086f378f1d0ef5c2a40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:17 +0000

    Add package labml-nn/0.4.9/labml_nn-0.4.9.tar.gz

---
+++ packages/labml-nn/labml_nn-0.4.9.tar.gz/labml_nn-0.4.9/labml_nn/rl/__init__.py
@@ -0,0 +1,9 @@
+"""
+# RL Algorithms
+
+* [Proximal Policy Optimization](ppo)
+    * [This is an experiment](ppo/experiment.html) that runs a PPO agent on Atari Breakout.
+* [Generalized advantage estimation](ppo/gae.html)
+
+[This is the implementation for OpenAI game wrapper](game.html) that uses `multiprocessing`.
+"""
