c167807e8335bdf3b6c1d2c56f873f02a9693ba1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:28 +0000

    Add package gamla/33/gamla-33-py3-none-any.whl

---
+++ packages/gamla/gamla-33-py3-none-any.whl/gamla/functional_generic.py
@@ -0,0 +1,417 @@
+import asyncio
+import functools
+import inspect
+import itertools
+from typing import Any, Callable, Iterable, Text, Tuple, Type, TypeVar, Union
+
+import toolz
+from toolz import curried
+from toolz.curried import operator
+
+from gamla import currying, data, functional
+
+
+def compose_left(*funcs):
+    return compose(*reversed(funcs))
+
+
+_any_is_async = toolz.compose(any, curried.map(asyncio.iscoroutinefunction))
+
+
+async def to_awaitable(value):
+    if inspect.isawaitable(value):
+        return await value
+    return value
+
+
+def _acompose(*funcs):
+    @functools.wraps(toolz.last(funcs))
+    async def async_composed(*args, **kwargs):
+        for f in reversed(funcs):
+            args = [await to_awaitable(f(*args, **kwargs))]
+            kwargs = {}
+        return toolz.first(args)
+
+    return async_composed
+
+
+def compose(*funcs):
+    if _any_is_async(funcs):
+        composed = _acompose(*funcs)
+    else:
+
+        @functools.wraps(toolz.last(funcs))
+        def composed(*args, **kwargs):
+            for f in reversed(funcs):
+                args = [f(*args, **kwargs)]
+                kwargs = {}
+            return toolz.first(args)
+
+    # TODO(uri): Far from a perfect id, but should work most of the time.
+    # Improve by having higher order functions create meaningful names (e.g. `map`).
+    # Copying `toolz` convention.
+    composed.__name__ = "_of_".join(map(lambda x: x.__name__, funcs))
+    return composed
+
+
+def _make_amap(f):
+    async def amap_inner(it):
+        return await asyncio.gather(*curried.map(f, it))
+
+    return amap_inner
+
+
+def gamla_map(*args):
+    if len(args) == 2:
+        f, it = args
+        if asyncio.iscoroutinefunction(f):
+            return _make_amap(f)(it)
+        return curried.map(f, it)
+    [f] = args
+    if asyncio.iscoroutinefunction(f):
+        return _make_amap(f)
+    return curried.map(f)
+
+
+map = gamla_map
+
+
+@currying.curry
+def after(f1, f2):
+    return compose(f1, f2)
+
+
+@currying.curry
+def before(f1, f2):
+    return compose_left(f1, f2)
+
+
+def lazyjuxt(*funcs):
+    """Reverts to eager implementation if any of `funcs` is async."""
+    if _any_is_async(funcs):
+
+        async def lazyjuxt_inner(*args, **kwargs):
+            return await toolz.pipe(
+                funcs,
+                curried.map(
+                    compose_left(functional.apply(*args, **kwargs), to_awaitable),
+                ),
+                functional.star(asyncio.gather),
+            )
+
+        return lazyjuxt_inner
+    return compose_left(functional.apply, curried.map, functional.apply(funcs))
+
+
+juxt = compose(after(tuple), lazyjuxt)
+alljuxt = compose(after(all), lazyjuxt)
+anyjuxt = compose(after(any), lazyjuxt)
+juxtcat = compose(after(toolz.concat), lazyjuxt)
+
+
+def ternary(condition, f_true, f_false):
+    if _any_is_async([condition, f_true, f_false]):
+
+        async def ternary_inner_async(*args, **kwargs):
+            return (
+                await to_awaitable(f_true(*args, **kwargs))
+                if await to_awaitable(condition(*args, **kwargs))
+                else await to_awaitable(f_false(*args, **kwargs))
+            )
+
+        return ternary_inner_async
+
+    def ternary_inner(*args, **kwargs):
+        return (
+            f_true(*args, **kwargs)
+            if condition(*args, **kwargs)
+            else f_false(*args, **kwargs)
+        )
+
+    return ternary_inner
+
+
+curried_ternary = ternary
+
+
+def first(*funcs, exception_type: Type[Exception]):
+    if _any_is_async([*funcs]):
+
+        async def inner_async(*args, **kwargs):
+            for func in funcs:
+                try:
+                    return await to_awaitable(func(*args, **kwargs))
+                except exception_type:
+                    pass
+            raise exception_type
+
+        return inner_async
+
+    def inner(*args, **kwargs):
+        for func in funcs:
+            try:
+                return func(*args, **kwargs)
+            except exception_type:
+                pass
+        raise exception_type
+
+    return inner
+
+
+def pipe(val, *funcs):
+    return compose_left(*funcs)(val)
+
+
+def _compose_over_binary_curried(composer):
+    def composition_over_binary_curried(*args):
+        f = composer(args[0])
+        if len(args) == 2:
+            return f(args[1])
+        return f
+
+    return composition_over_binary_curried
+
+
+allmap = _compose_over_binary_curried(compose(after(all), gamla_map))
+anymap = _compose_over_binary_curried(compose(after(any), gamla_map))
+
+
+itemmap = _compose_over_binary_curried(
+    compose(after(dict), before(dict.items), gamla_map),
+)
+keymap = _compose_over_binary_curried(
+    compose(itemmap, lambda f: juxt(f, toolz.second), before(toolz.first)),
+)
+
+valmap = _compose_over_binary_curried(
+    compose(itemmap, lambda f: juxt(toolz.first, f), before(toolz.second)),
+)
+
+
+pair_with = _compose_over_binary_curried(lambda f: juxt(f, toolz.identity))
+pair_right = _compose_over_binary_curried(lambda f: juxt(toolz.identity, f))
+
+filter = _compose_over_binary_curried(
+    compose(
+        after(compose(curried.map(toolz.second), curried.filter(toolz.first))),
+        gamla_map,
+        pair_with,
+    ),
+)
+
+
+_first_truthy_index = compose_left(
+    enumerate,
+    curried.filter(toolz.second),
+    curried.map(toolz.first),
+    toolz.excepts(StopIteration, toolz.first),
+)
+
+
+class NoConditionMatched(Exception):
+    pass
+
+
+def _case(predicates: Tuple[Callable, ...], mappers: Tuple[Callable, ...]):
+    """Case with functions.
+
+    Handles async iff one of the predicates or one of the mappers is async.
+    Raises `NoConditionMatched` if no condition matched.
+    """
+    predicates = (*predicates, functional.just(True))
+    mappers = (
+        *mappers,
+        compose_left(NoConditionMatched, functional.just_raise),
+    )
+    return compose_left(
+        pair_right(
+            compose_left(
+                lazyjuxt(*predicates),
+                _first_truthy_index,
+                mappers.__getitem__,
+                to_awaitable if _any_is_async(mappers) else toolz.identity,
+            ),
+        ),
+        functional.star(
+            lambda value, transformer: functional.apply(value)(transformer),
+        ),
+    )
+
+
+def case(predicates_and_mappers: Tuple[Tuple[Callable, Callable], ...]):
+    predicates = tuple(map(toolz.first, predicates_and_mappers))
+    mappers = tuple(map(toolz.second, predicates_and_mappers))
+    return _case(predicates, mappers)
+
+
+case_dict = compose_left(dict.items, tuple, case)
+
+
+async def _await_dict(value):
+    if isinstance(value, dict) or isinstance(value, data.frozendict):
+        return await pipe(
+            value,
+            # In case input is a `frozendict`.
+            dict,
+            valmap(_await_dict),
+        )
+    if isinstance(value, Iterable):
+        return await pipe(value, gamla_map(_await_dict), type(value))
+    return await to_awaitable(value)
+
+
+def map_dict(nonterminal_mapper: Callable, terminal_mapper: Callable):
+    def map_dict_inner(value):
+        if isinstance(value, dict) or isinstance(value, data.frozendict):
+            return toolz.pipe(
+                value,
+                dict,  # In case input is a `frozendict`.
+                curried.valmap(map_dict(nonterminal_mapper, terminal_mapper)),
+                nonterminal_mapper,
+            )
+        if isinstance(value, Iterable) and not isinstance(value, str):
+            return toolz.pipe(
+                value,
+                curried.map(map_dict(nonterminal_mapper, terminal_mapper)),
+                type(value),  # Keep the same format as input.
+                nonterminal_mapper,
+            )
+        return terminal_mapper(value)
+
+    if _any_is_async([nonterminal_mapper, terminal_mapper]):
+        return compose_left(map_dict_inner, _await_dict)
+
+    return map_dict_inner
+
+
+def _iterdict(d):
+    results = []
+    map_dict(toolz.identity, results.append)(d)
+    return results
+
+
+_has_coroutines = compose_left(_iterdict, _any_is_async)
+
+
+def apply_spec(spec):
+    """
+    >>> spec = {"len": len, "sum": sum}
+    >>> apply_spec(spec)([1,2,3,4,5])
+    {'len': 5, 'sum': 15}
+
+    Notes:
+    - The dictionary can be nested.
+    - Returned function will be async iff any leaf is an async function.
+    """
+    if _has_coroutines(spec):
+
+        async def apply_spec_async(*args, **kwargs):
+            return await map_dict(
+                toolz.identity,
+                compose_left(functional.apply(*args, **kwargs), to_awaitable),
+            )(spec)
+
+        return apply_spec_async
+    return compose_left(
+        functional.apply,
+        lambda applier: map_dict(toolz.identity, applier),
+        functional.apply(spec),
+    )
+
+
+# Stacks functions on top of each other, so will run pairwise on the input.
+# Similar to juxt, only zips with the incoming iterable.
+stack = compose_left(
+    enumerate,
+    map(functional.star(lambda i, f: compose(f, curried.nth(i)))),
+    functional.star(juxt),
+)
+
+
+def bifurcate(*funcs):
+    """Serially runs each function on tee'd copies of `input_generator`."""
+    return compose_left(iter, lambda it: itertools.tee(it, len(funcs)), stack(funcs))
+
+
+average = toolz.compose_left(
+    bifurcate(sum, toolz.count),
+    toolz.excepts(ZeroDivisionError, functional.star(operator.truediv), lambda _: 0),
+)
+
+
+def value_to_dict(key: Text):
+    return compose_left(
+        functional.wrap_tuple,
+        functional.prefix(key),
+        functional.wrap_tuple,
+        dict,
+    )
+
+
+_R = TypeVar("_R")
+_E = TypeVar("_E")
+
+
+def reduce_curried(
+    reducer: Callable[[_R, _E], _R],
+    initial_value: _R,
+) -> Callable[[Iterable[_E]], _R]:
+    if asyncio.iscoroutinefunction(reducer):
+
+        async def reduce_async(elements):
+            state = initial_value
+            for element in elements:
+                state = await reducer(state, element)
+            return state
+
+        return reduce_async
+
+    def reduce(elements):
+        state = initial_value
+        for element in elements:
+            state = reducer(state, element)
+        return state
+
+    return reduce
+
+
+@currying.curry
+def excepts(
+    exception: Union[Tuple[Exception, ...], Exception],
+    handler: Callable[[Exception], Any],
+    function: Callable,
+):
+    if asyncio.iscoroutinefunction(function):
+
+        @functools.wraps(function)
+        async def excepts(*args, **kwargs):
+            try:
+                return await function(*args, **kwargs)
+            except exception as error:
+                return handler(error)
+
+        return excepts
+
+    return toolz.excepts(exception, function, handler)
+
+
+find = _compose_over_binary_curried(
+    compose(
+        after(excepts(StopIteration, functional.just(None), toolz.first)),
+        filter,
+    ),
+)
+
+find_index = _compose_over_binary_curried(
+    compose(
+        after(
+            compose_left(
+                functional.star(find),
+                ternary(toolz.identity, toolz.first, functional.just(-1)),
+            ),
+        ),
+        currying.curry(
+            lambda pred, seq: (compose_left(toolz.second, pred), enumerate(seq)),
+        ),
+    ),
+)
---
+++ packages/gamla/gamla-33-py3-none-any.whl/gamla/graph.py
@@ -0,0 +1,180 @@
+import itertools
+from typing import Any, Callable, Dict, FrozenSet, Iterable, Set, Text, Tuple
+
+import toolz
+from toolz import curried
+from toolz.curried import operator
+
+from gamla import currying, functional, functional_generic
+
+
+@currying.curry
+def graph_traverse(
+    source: Any,
+    get_neighbors: Callable,
+    key: Callable = toolz.identity,
+) -> Iterable:
+    yield from graph_traverse_many([source], get_neighbors=get_neighbors, key=key)
+
+
+@currying.curry
+def graph_traverse_many(
+    sources: Any,
+    get_neighbors: Callable,
+    key: Callable = toolz.identity,
+) -> Iterable:
+    """BFS over a graph, yielding unique nodes.
+    Note: `get_neighbors` must return elements without duplicates."""
+    seen_set: Set = set()
+    remember = toolz.compose_left(key, seen_set.add)
+    should_traverse = toolz.compose_left(key, toolz.complement(seen_set.__contains__))
+    yield from general_graph_traverse_many(
+        sources,
+        get_neighbors,
+        remember,
+        should_traverse,
+    )
+
+
+@currying.curry
+def general_graph_traverse_many(
+    sources: Any,
+    get_neighbors: Callable,
+    remember: Callable,
+    should_traverse: Callable,
+) -> Iterable:
+    """BFS over a graph, yielding unique nodes.
+    Note: `get_neighbors` must return elements without duplicates."""
+
+    queue = [*sources]
+    for element in queue:
+        remember(element)
+
+    while queue:
+        current = queue.pop()
+        yield current
+        for node in get_neighbors(current):
+            if should_traverse(node):
+                remember(node)
+                queue = [node] + queue
+
+
+def traverse_graph_by_radius(
+    source: Any,
+    get_neighbors: Callable,
+    radius: int,
+) -> Iterable:
+    """Like `graph_traverse`, but does not traverse farther from given `radius`."""
+
+    def get_neighbors_limiting_radius(
+        current_and_distance: Tuple[Text, int],
+    ) -> Iterable[Tuple[Text, int]]:
+        current, distance = current_and_distance
+        if distance < radius:
+            yield from map(
+                lambda neighbor: (neighbor, distance + 1),
+                get_neighbors(current),
+            )
+
+    return map(
+        toolz.first,
+        graph_traverse(source=(source, 0), get_neighbors=get_neighbors_limiting_radius),
+    )
+
+
+edges_to_graph = toolz.compose(
+    curried.valmap(toolz.compose(frozenset, curried.map(toolz.second))),
+    curried.groupby(toolz.first),
+)
+
+graph_to_edges = toolz.compose_left(
+    curried.keymap(lambda x: (x,)),
+    dict.items,
+    curried.mapcat(functional.star(itertools.product)),
+)
+
+reverse_graph = toolz.compose_left(
+    graph_to_edges,
+    curried.map(toolz.compose_left(reversed, tuple)),
+    edges_to_graph,
+)
+
+cliques_to_graph = toolz.compose_left(
+    curried.mapcat(lambda clique: itertools.permutations(clique, r=2)),
+    edges_to_graph,
+)
+
+
+def get_connectivity_components(graph: Dict) -> Iterable[FrozenSet]:
+    """Graph is assumed to undirected, so each edge must appear both ways."""
+    nodes_left = frozenset(graph)
+    while nodes_left:
+        result = frozenset(
+            graph_traverse(
+                source=toolz.first(nodes_left),
+                get_neighbors=toolz.compose(
+                    curried.filter(operator.contains(nodes_left)),
+                    graph.get,
+                ),
+            ),
+        )
+        yield result
+        nodes_left -= result
+
+
+@currying.curry
+def groupby_many(f, it):
+    """Return a mapping `{y: {x s.t. y in f(x)}}, where x in it. `
+
+    Parameters:
+    f (Callable): Key function (given object in collection outputs tuple of keys).
+    it (Iterable): Collection.
+
+    Returns:
+    Dict[Text, Any]: Dictionary where key has been computed by the `f` key function.
+
+    >>> names = ['alice', 'bob', 'charlie', 'dan', 'edith', 'frank']
+    >>> groupby_many(lambda name: (name[0], name[-1]), names)
+    {'a': frozenset({'alice'}),
+     'e': frozenset({'alice', 'charlie', 'edith'}),
+     'b': frozenset({'bob'}),
+     'c': frozenset({'charlie'}),
+     'd': frozenset({'dan'}),
+     'n': frozenset({'dan'}),
+     'h': frozenset({'edith'}),
+     'f': frozenset({'frank'}),
+     'k': frozenset({'frank'})}
+    """
+    return toolz.pipe(
+        it,
+        curried.mapcat(
+            toolz.compose_left(
+                lambda element: (f(element), [element]),
+                functional.star(itertools.product),
+            ),
+        ),
+        edges_to_graph,
+    )
+
+
+@currying.curry
+def _has_cycle(sourced, get_neighbors, visited, node):
+    if node in sourced:
+        return True
+    if node in visited:
+        return False
+    visited.add(node)
+    return toolz.pipe(
+        node,
+        get_neighbors,
+        functional_generic.anymap(_has_cycle(sourced | {node}, get_neighbors, visited)),
+    )
+
+
+def has_cycle(graph):
+    return toolz.pipe(
+        graph,
+        dict.keys,
+        curried.map(_has_cycle(frozenset(), curried.get(seq=graph, default=()), set())),
+        any,
+    )
