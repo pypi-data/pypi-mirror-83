911e87c08cec2497c55169f8fb28788e0dd1f4e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:32 +0000

    Add package imgflippy/0.0.3/imgflippy-0.0.3.tar.gz

---
+++ packages/imgflippy/imgflippy-0.0.3.tar.gz/imgflippy-0.0.3/setup.py
@@ -0,0 +1,32 @@
+import os
+from setuptools import find_packages, setup
+
+this_dir = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_dir, 'README.md'), 'r') as fh:
+    long_description = fh.read()
+
+setup(
+    name='imgflippy',
+    version='0.0.3',
+    author='Caleb Bell',
+    author_email='caleb@theceebee.com',
+    description='An open source Python wrapper for the imgflip RESTful API (https://api.imgflip.com).',
+    keywords='imgflip meme API',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/theceebee/pyimgflip',
+    packages=[p for p in find_packages() if p != 'tests'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ],
+    install_requires=[
+        'requests',
+        'six'
+    ],
+    python_requires='>=2.7'
+)
