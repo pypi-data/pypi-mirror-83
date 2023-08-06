e2b3f1cf7da2105427875528de5ac5f30911bdb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:19 +0000

    Add package duckietown-docker-utils-daffy/6.0.16/duckietown-docker-utils-daffy-6.0.16.tar.gz

---
+++ packages/duckietown-docker-utils-daffy/duckietown-docker-utils-daffy-6.0.16.tar.gz/duckietown-docker-utils-daffy-6.0.16/src/duckietown_docker_utils/constants.py
@@ -0,0 +1,11 @@
+DT1_TOKEN_CONFIG_KEY = "token_dt1"
+CONFIG_DOCKER_USERNAME = "docker_username"
+CONFIG_DUCKIETOWN_VERSION = "duckietown_version"
+
+CONFIG_DOCKER_PASSWORD = "docker_password"
+
+IMPORTANT_ENVS = {
+    "AIDO_REGISTRY": "docker.io",
+    "PIP_INDEX_URL": "https://pypi.org/simple",
+    "DTSERVER": "https://challenges.duckietown.org/v4",
+}
