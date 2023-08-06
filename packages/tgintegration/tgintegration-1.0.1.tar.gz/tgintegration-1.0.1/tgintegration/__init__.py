c902962778c600f868a7a3075bc34cab59049a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:50 +0000

    Add package tgintegration/1.0.1/tgintegration-1.0.1.tar.gz

---
+++ packages/tgintegration/tgintegration-1.0.1.tar.gz/tgintegration-1.0.1/tgintegration/__init__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+The root package of `tgintegration`.
+"""
+from tgintegration.botcontroller import BotController
+from tgintegration.containers import InlineKeyboard
+from tgintegration.containers import InlineResult
+from tgintegration.containers import InlineResultContainer
+from tgintegration.containers import InvalidResponseError
+from tgintegration.containers import ReplyKeyboard
+from tgintegration.containers import Response
+
+
+__author__ = """Joscha Götzer"""
+__email__ = "joscha.goetzer@gmail.com"
+__version__ = "0.4.0"
+
+__all__ = [
+    "Response",
+    "BotController",
+    "InlineResult",
+    "InlineResultContainer",
+    "InvalidResponseError",
+    "InlineKeyboard",
+    "ReplyKeyboard",
+]
