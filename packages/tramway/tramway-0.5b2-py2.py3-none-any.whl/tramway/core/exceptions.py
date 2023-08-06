fa079f3874479d0373da2e6e38e49db79d8f3568
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:53 +0000

    Add package tramway/0.5b2/tramway-0.5b2-py2.py3-none-any.whl

---
+++ packages/tramway/tramway-0.5b2-py2.py3-none-any.whl/tramway/core/exceptions.py
@@ -0,0 +1,31 @@
+
+try:
+    from .lazy import PermissionError
+except ImportError:
+    pass
+
+class NaNWarning(RuntimeWarning):
+    pass
+
+class EfficiencyWarning(RuntimeWarning):
+    pass
+
+class MissingSupportWarning(UserWarning):
+    pass
+
+class IOWarning(Warning):
+    pass
+
+class FileNotFoundWarning(IOWarning):
+    pass
+
+class MultipleArgumentError(ValueError):
+    def __str__(self):
+        assert self.args and self.args[1:] and all( isinstance(a, str) for a in self.args )
+        args = [ "'{}'".format(a) for a in self.args ]
+        s = ' or '.join((', '.join(args[:-1]), args[-1]))
+        return 'please define either ' + s
+
+class MisplacedAttributeWarning(UserWarning):
+    pass
+
