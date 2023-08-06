c0a0961cf043c1d8933442b3bdf8e986578bb989
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:11 +0000

    Add package list-reserve/0.1.1/list_reserve-0.1.1.tar.gz

---
+++ packages/list-reserve/list_reserve-0.1.1.tar.gz/list_reserve-0.1.1/setup.py
@@ -0,0 +1,21 @@
+import os
+from setuptools import setup, Extension
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
+
+setup(name='list_reserve',
+      version='0.1.1',
+      description='Python builtin list memory allocation library',
+      long_description=read('README.md'),
+      long_description_content_type="text/markdown",
+      ext_modules=[
+        Extension('list_reserve', ['src/list_reserve.c'])],
+      url='https://github.com/ChanTsune/list-reserve',
+      author='ChanTsune',
+      author_email='yshegou@gmail.com',
+      license='MIT',
+      keywords='list extension memory reserve capacity',
+      )
