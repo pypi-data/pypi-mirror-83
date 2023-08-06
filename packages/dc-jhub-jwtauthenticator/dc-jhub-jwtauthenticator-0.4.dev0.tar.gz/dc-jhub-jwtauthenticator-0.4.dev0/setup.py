78ea0af6a57c6a4458c2f480b01e01b9db9ba289
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:13 +0000

    Add package dc-jhub-jwtauthenticator/0.4.dev0/dc-jhub-jwtauthenticator-0.4.dev0.tar.gz

---
+++ packages/dc-jhub-jwtauthenticator/dc-jhub-jwtauthenticator-0.4.dev0.tar.gz/dc-jhub-jwtauthenticator-0.4.dev0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='dc-jhub-jwtauthenticator',
+    version='0.4-dev',
+    description='JSONWebToken Authenticator for JupyterHub',
+    tests_require = [
+    'unittest2',
+    ],
+    test_suite = 'unittest2.collector',
+    packages=['dc_jwtauthenticator'],
+    install_requires=[
+        'jupyterhub',
+        'python-jose',
+    ]
+)
