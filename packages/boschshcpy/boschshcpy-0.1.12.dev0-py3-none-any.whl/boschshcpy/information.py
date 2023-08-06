4d24a59a91546fe92e055f7e9dc15b7e0b6cd076
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:51 +0000

    Add package boschshcpy/0.1.12.dev0/boschshcpy-0.1.12.dev0-py3-none-any.whl

---
+++ packages/boschshcpy/boschshcpy-0.1.12.dev0-py3-none-any.whl/boschshcpy/information.py
@@ -0,0 +1,36 @@
+from enum import Enum
+from getmac import get_mac_address
+
+class SHCInformation:
+    class UpdateState(Enum):
+        NO_UPDATE_AVAILABLE = "NO_UPDATE_AVAILABLE"
+        DOWNLOADING = "DOWNLOADING"
+        UPDATE_IN_PROGRESS = "UPDATE_IN_PROGRESS"
+        UPDATE_AVAILABLE = "UPDATE_AVAILABLE"
+
+    def __init__(self, api, raw_information):
+        self._api = api
+        self._raw_information = raw_information
+
+    @property
+    def version(self):
+        return self._raw_information["version"]
+
+    @property
+    def updateState(self) -> UpdateState:
+        return self.UpdateState(self._raw_information["updateState"])
+
+    @property
+    def connectivityVersion(self):
+        return self._raw_information["connectivityVersion"]
+
+    @property
+    def macAddress(self):
+        return get_mac_address(ip=self._api._controller_ip)
+
+    def summary(self):
+        print(f"Information:")
+        print(f"  SW Version         : {self.version}")
+        print(f"  State              : {self.updateState}")
+        print(f"  connectivityVersion: {self.connectivityVersion}")
+        print(f"  mac address        : {self.macAddress}")
