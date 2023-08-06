40cdb0ea2c1ea6632845785d23f4363f984d38c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:04 +0000

    Add package gaia-sdk/2.2.0/gaia-sdk-2.2.0.tar.gz

---
+++ packages/gaia-sdk/gaia-sdk-2.2.0.tar.gz/gaia-sdk-2.2.0/gaia_sdk/graphql/GaiaClientBuilder.py
@@ -0,0 +1,21 @@
+from gaia_sdk.api.client_options import ClientOptions
+from gaia_sdk.http.HttpTransporter import HttpTransporter
+from gaia_sdk.graphql.GaiaClient import GaiaClient
+
+#Class generated from template src/main/resources/template/python/ClientBuilderTemplate.vm
+
+class GaiaClientBuilder(object):
+
+    @staticmethod
+    def http(url):
+        return GaiaClientBuilder(url)
+
+    def __init__(self, url):
+        self.url = url
+
+    def with_credentials(self, credentials):
+        self.credentials = credentials
+        return self
+
+    def build(self):
+        return GaiaClient(HttpTransporter(self.url), ClientOptions(self.credentials))
