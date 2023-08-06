69ca8d9c88afd29f0951faabf2ae68ef7476158f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:38 +0000

    Add package xontrib-cmd-durations/0.2.2/xontrib-cmd-durations-0.2.2.tar.gz

---
+++ packages/xontrib-cmd-durations/xontrib-cmd-durations-0.2.2.tar.gz/xontrib-cmd-durations-0.2.2/README.md
@@ -0,0 +1,42 @@
+<p align="center">
+Send notification once long running command is finished. Add duration PROMP_FIELD.
+</p>
+
+<p align="center">
+If you like the idea click ⭐ on the repo and stay tuned.
+</p>
+
+## Installation
+
+To install use pip:
+
+``` bash
+xpip install xontrib-cmd-durations
+# or: xpip install -U git+https://github.com/jnoortheen/xontrib-cmd-durations
+```
+
+## Usage
+
+``` bash
+xontrib load cmd_done
+```
+
+## Usage
+
+* makes `long_cmd_duration` available to the `$PROMP_FIELD `
+* if the command is taking more than `$LONG_DURATION` seconds
+  + it is `long_cmd_duration` returns the duration in human readable way
+  + a desktop notification is sent if the terminal is not focused.
+    - **Note**: Currently the focusing part requires `xdotool` to be installed.
+
+        So the notification part will not work in Windows/OSX. PRs welcome on that.
+
+``` bash
+$RIGHT_PROMPT = '{long_cmd_duration:⌛{}}{user:{{BOLD_RED}}🤖{}}{hostname:{{BOLD_#FA8072}}🖥{}}'
+```
+
+![](./images/2020-10-26-10-59-38.png)
+
+## Credits
+
+This package was created with [xontrib cookiecutter template](https://github.com/jnoortheen/xontrib-cookiecutter).
---
+++ packages/xontrib-cmd-durations/xontrib-cmd-durations-0.2.2.tar.gz/xontrib-cmd-durations-0.2.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['xontrib', 'xontrib.notifypy', 'xontrib.notifypy.os_notifiers']
+
+package_data = \
+{'': ['*'],
+ 'xontrib.notifypy.os_notifiers': ['binaries/Notificator.app/Contents/*',
+                                   'binaries/Notificator.app/Contents/MacOS/*',
+                                   'binaries/Notificator.app/Contents/Resources/*',
+                                   'binaries/Notificator.app/Contents/Resources/Scripts/*']}
+
+install_requires = \
+['xonsh>=0.9.20']
+
+setup_kwargs = {
+    'name': 'xontrib-cmd-durations',
+    'version': '0.2.2',
+    'description': 'Send notification once long running command is finished. Add duration PROMP_FIELD.',
+    'long_description': '<p align="center">\nSend notification once long running command is finished. Add duration PROMP_FIELD.\n</p>\n\n<p align="center">\nIf you like the idea click ⭐ on the repo and stay tuned.\n</p>\n\n## Installation\n\nTo install use pip:\n\n``` bash\nxpip install xontrib-cmd-durations\n# or: xpip install -U git+https://github.com/jnoortheen/xontrib-cmd-durations\n```\n\n## Usage\n\n``` bash\nxontrib load cmd_done\n```\n\n## Usage\n\n* makes `long_cmd_duration` available to the `$PROMP_FIELD `\n* if the command is taking more than `$LONG_DURATION` seconds\n  + it is `long_cmd_duration` returns the duration in human readable way\n  + a desktop notification is sent if the terminal is not focused.\n    - **Note**: Currently the focusing part requires `xdotool` to be installed.\n\n        So the notification part will not work in Windows/OSX. PRs welcome on that.\n\n``` bash\n$RIGHT_PROMPT = \'{long_cmd_duration:⌛{}}{user:{{BOLD_RED}}🤖{}}{hostname:{{BOLD_#FA8072}}🖥{}}\'\n```\n\n![](./images/2020-10-26-10-59-38.png)\n\n## Credits\n\nThis package was created with [xontrib cookiecutter template](https://github.com/jnoortheen/xontrib-cookiecutter).\n',
+    'author': 'Noortheen Raja J',
+    'author_email': 'jnoortheen@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/jnoortheen/xontrib-cmd-durations',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6',
+}
+
+
+setup(**setup_kwargs)
