86de8e9076f3dc4872eb29789ea904b9a542af61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:22 +0000

    Add package wheelsbyhasan/0.0.8/wheelsbyhasan-0.0.8.tar.gz

---
+++ packages/wheelsbyhasan/wheelsbyhasan-0.0.8.tar.gz/wheelsbyhasan-0.0.8/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='wheelsbyhasan',
+    version='0.0.8',
+    packages=setuptools.find_packages(),
+    author='Hasan Abu-Rayyan',
+    author_email='hasanaburayyan21@gmail.com',
+    description='Cool First Wheel',
+    python_requires='>=3.6'
+)
