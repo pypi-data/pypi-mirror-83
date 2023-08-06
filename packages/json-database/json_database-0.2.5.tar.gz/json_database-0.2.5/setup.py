1db9bb8521cadc429631c4ba8339134dd21c806d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:04 +0000

    Add package json-database/0.2.5/json_database-0.2.5.tar.gz

---
+++ packages/json-database/json_database-0.2.5.tar.gz/json_database-0.2.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='json_database',
+    version='0.2.5',
+    packages=['json_database'],
+    url='https://github.com/OpenJarbas/json_database',
+    license='MIT',
+    author='jarbasAI',
+    author_email='jarbasai@mailfence.com',
+    install_requires=["xdg"],
+    description='searchable json database with persistence'
+)
