6d827a417fabf49d1d83f3741789036b05d4108b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:24 +0000

    Add package grebble-flow/0.0.3.6/grebble_flow-0.0.3.6-py3-none-any.whl

---
+++ packages/grebble-flow/grebble_flow-0.0.3.6-py3-none-any.whl/example/simple/processor.py
@@ -0,0 +1,9 @@
+from greble_flow.processors.base import BaseFlowProcessor
+
+
+class SimpleProcessor(BaseFlowProcessor):
+    name = "simple-flow-processor"
+
+    def execute(self, *args, **kwargs):
+        for i in range(0, 100):
+            yield {"test": i}
