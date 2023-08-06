2af7a33b7d3c245ead187ccc2addd0e83a6167a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:28 +0000

    Add package raachem/0.0.8.post1/raachem-0.0.8.post1.tar.gz

---
+++ packages/raachem/raachem-0.0.8.post1.tar.gz/raachem-0.0.8.post1/raachem/file_creator/deploy_scripts.py
@@ -0,0 +1,25 @@
+import os
+from raachem.util.gen_purp import file_weeder, preferences
+
+def deploy():
+	scripts_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),"scripts")
+	while True:
+		options = file_weeder([".py"], scripts_dir)
+		options = [a for a in options if a != "__init__.py"]
+		options.insert(0, "To cancel")
+		print("Which script do you want to paste in the current folder?")
+		for idx, i in enumerate(options):
+			print("{:>3} - {}".format(idx, i))
+		option = input()
+		if option in [str(a) for a in range(len(options))]:
+			break
+	if option == "0":
+		return
+	else:
+		with open(os.path.join(scripts_dir,options[int(option)])) as file:
+			script = file.readlines()
+		script = [a.replace("sub_s_name",preferences.sub_s_name) for a in script]
+		script = [a.replace(".gjf", preferences.gauss_ext) for a in script]
+		with open((os.path.join(os.getcwd(), options[int(option)])), "w", newline="\n") as file_b:
+			for line in script:
+				file_b.write(str(line))
---
+++ packages/raachem/raachem-0.0.8.post1.tar.gz/raachem-0.0.8.post1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="raachem",
+    version="0.0.8.post1",
+    author="Ricardo Almir Angnes",
+    author_email="ricardo_almir@hotmail.com",
+    description="Tools I use for computational chemistry",
+    long_description=long_description,
+	license="MIT",
+    url="https://github.com/ricalmang/raachem",
+	keywords = ['chemistry'],
+	install_requires = ["numpy","xlwt"],
+    packages=setuptools.find_packages(),
+	package_data = {"":["*.GAUSSIAN","*.ORCA","*.gbs"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
