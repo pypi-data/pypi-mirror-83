b53dced6adead11fbcc84399f5fe9d6e5d53f613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:50 +0000

    Add package rpc-gateway/0.4.1/rpc_gateway-0.4.1.tar.gz

---
+++ packages/rpc-gateway/rpc_gateway-0.4.1.tar.gz/rpc_gateway-0.4.1/rpc_gateway/utils.py
@@ -0,0 +1,11 @@
+from typing import Optional, Awaitable, Any
+import asyncio
+
+
+def await_sync(awaitable: Awaitable, loop: Optional[asyncio.AbstractEventLoop] = None, timeout: Optional[float] = None) -> Any:
+    loop = loop or asyncio.get_event_loop()
+    if loop.is_running():
+        future = asyncio.run_coroutine_threadsafe(awaitable, loop or asyncio.get_event_loop())
+        return future.result(timeout=timeout)
+    else:
+        return loop.run_until_complete(awaitable)
