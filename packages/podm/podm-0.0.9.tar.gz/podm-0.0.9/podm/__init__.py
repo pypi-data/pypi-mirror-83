f7c4d9e0567a7f3a97c534bb70da685f0f2ea8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:25 +0000

    Add package podm/0.0.9/podm-0.0.9.tar.gz

---
+++ packages/podm/podm-0.0.9.tar.gz/podm-0.0.9/podm/__init__.py
@@ -0,0 +1,7 @@
+# vim:ts=4:sw=4:expandtab
+__author__ = "Carlos Descalzi"
+
+from .jsonobject import BaseJsonObject, JsonObject
+from .meta import Handler, Property, ArrayOf, MapOf
+from .processor import Processor
+from .properties import PropertyHandler, RichPropertyHandler
