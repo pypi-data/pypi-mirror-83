275069b8c2856fa6a34b8f7e67d0b10dfef70e89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:59 +0000

    Add package scikit-surgerysurfacematch/0.4.5/scikit-surgerysurfacematch-0.4.5.tar.gz

---
+++ packages/scikit-surgerysurfacematch/scikit-surgerysurfacematch-0.4.5.tar.gz/scikit-surgerysurfacematch-0.4.5/sksurgerysurfacematch/algorithms/reconstructor_with_rectified_images.py
@@ -0,0 +1,193 @@
+# -*- coding: utf-8 -*-
+
+""" Base class for surface reconstruction on already rectified images. """
+
+import numpy as np
+import cv2
+
+import sksurgerysurfacematch.interfaces.stereo_reconstructor as sr
+
+
+class StereoReconstructorWithRectifiedImages(sr.StereoReconstructor):
+    """
+    Base class for those stereo reconstruction methods that work specifically
+    from rectified images. This class handles rectification and
+    the necessary coordinate transformations. Note: The client calls
+    the reconstruct() method which requires undistorted images,
+    which are NOT already rectified. It's THIS class that does the
+    rectification for you, and calls through to the _compute_disparity()
+    method that derived classes must implement.
+    """
+    def __init__(self,
+                 lower_disparity_multiplier=2.0,
+                 upper_disparity_multiplier=2.0,
+                 alpha: float = 0):
+        """
+        Constructor creates some member variables, so this class
+        becomes statefull. You call reconstruct() once, and then
+        you can call extract multiple times with different masks
+        to pull out different subsets of data.
+
+        :param lower_disparity_multiplier: min=median - (this * std).
+        :param upper_disparity_multiplier: max=median + (this * std).
+        :param alpha: opencv alpha parameter for StereoRectify
+        """
+        super().__init__()
+        self.disparity = None
+        self.points = None
+        self.rgb_image = None
+        self.r_1 = None
+        self.lower_disparity_multiplier = lower_disparity_multiplier
+        self.upper_disparity_multiplier = upper_disparity_multiplier
+
+        self.left_rectified = None
+        self.right_rectified = None
+        self.left_mask = None
+        self.alpha = alpha
+
+    # pylint:disable=too-many-arguments
+    def reconstruct(self,
+                    left_image: np.ndarray,
+                    left_camera_matrix: np.ndarray,
+                    right_image: np.ndarray,
+                    right_camera_matrix: np.ndarray,
+                    left_to_right_rmat: np.ndarray,
+                    left_to_right_tvec: np.ndarray,
+                    left_mask: np.ndarray = None,
+                    ):
+        """
+        Implementation of stereo surface reconstruction that takes
+        undistorted images, rectifies them, asks derived classes
+        to compute a disparity map on the rectified images, and
+        then sorts out extracting points and their colours.
+
+        Camera parameters are those obtained from OpenCV.
+
+        :param left_image: undistorted left image, BGR
+        :param left_camera_matrix: [3x3] camera matrix
+        :param right_image: undistorted right image, BGR
+        :param right_camera_matrix: [3x3] camera matrix
+        :param left_to_right_rmat: [3x3] rotation matrix
+        :param left_to_right_tvec: [3x1] translation vector
+        :param left_mask: mask image, single channel, same size as left_image
+        :return: [Nx6] point cloud where the 6 columns
+        are x, y, z in left camera space, followed by r, g, b colours.
+        """
+        # pylint:disable=too-many-locals
+        (width, height) = (left_image.shape[1], left_image.shape[0])
+
+        self.r_1, r_2, p_1, p_2, q_mat, _, _ = \
+            cv2.stereoRectify(left_camera_matrix,
+                              None,
+                              right_camera_matrix,
+                              None,
+                              (width, height),
+                              left_to_right_rmat,
+                              left_to_right_tvec,
+                              alpha=self.alpha
+                              )
+
+        undistort_rectify_map_l_x, undistort_rectify_map_l_y = \
+            cv2.initUndistortRectifyMap(left_camera_matrix,
+                                        None,
+                                        self.r_1, p_1,
+                                        (width, height), cv2.CV_32FC1)
+
+        undistort_rectify_map_r_x, undistort_rectify_map_r_y = \
+            cv2.initUndistortRectifyMap(right_camera_matrix,
+                                        None,
+                                        r_2, p_2,
+                                        (width, height), cv2.CV_32FC1)
+
+        self.left_rectified = \
+            cv2.remap(left_image, undistort_rectify_map_l_x,
+                      undistort_rectify_map_l_y, cv2.INTER_LINEAR)
+
+        self.right_rectified = \
+            cv2.remap(right_image, undistort_rectify_map_r_x,
+                      undistort_rectify_map_r_y, cv2.INTER_LINEAR)
+
+        # Need to remap the mask if we have one
+        self.left_mask = left_mask
+        if left_mask is not None:
+
+            self.left_mask = \
+                cv2.remap(left_mask, undistort_rectify_map_l_x,
+                          undistort_rectify_map_l_y, cv2.INTER_NEAREST)
+
+        self.disparity = self._compute_disparity(self.left_rectified,
+                                                 self.right_rectified)
+
+        self.points = cv2.reprojectImageTo3D(self.disparity, q_mat)
+        self.rgb_image = cv2.cvtColor(self.left_rectified, cv2.COLOR_BGR2RGB)
+
+        # Calls method below to extract data.
+        return self.extract(left_mask)
+
+    def extract(self, left_mask: np.ndarray):
+        """
+        Extracts the actual point cloud. This is a separate method,
+        so that you can reconstruct once using reconstruct(), and then
+        call this extract method with multiple masks, without incurring
+        the cost of multiple calls to the reconstruction algorithm, which
+        may be expensive.
+        :param left_mask: mask image, single channel, same size as left_image
+        :return: [Nx6] point cloud where the 6 columns
+        are x, y, z in left camera space, followed by r, g, b colours.
+        """
+
+        median_disp = np.median(self.disparity)
+        std_dev_disp = np.std(self.disparity)
+        lower_bound = (median_disp - (self.lower_disparity_multiplier
+                                      * std_dev_disp))
+        upper_bound = (median_disp + (self.upper_disparity_multiplier
+                                      * std_dev_disp))
+        mask = np.logical_and(self.disparity > lower_bound,
+                              self.disparity < upper_bound)
+
+        print("Disparity, min=" + str(self.disparity.min())
+              + ", max=" + str(self.disparity.max())
+              + ", med=" + str(np.median(self.disparity))
+              + ", std=" + str(np.std(self.disparity))
+              + ", lower=" + str(lower_bound)
+              + ", upper=" + str(upper_bound)
+              )
+
+        if left_mask is not None:
+
+            if left_mask.dtype != bool:
+                left_mask = left_mask > 0
+
+            mask = np.logical_and(mask, left_mask)
+
+        out_points = self.points[mask]
+        out_colors = self.rgb_image[mask]
+
+        non_zero = np.count_nonzero(mask)
+        result = np.zeros((non_zero, 6))
+
+        result[:, 0:3] = out_points
+        result[:, 3:6] = out_colors
+
+        # Convert from first (left) camera rectified to left camera unrectified
+        result[:, 0:3] = np.transpose(
+            np.matmul(np.linalg.inv(self.r_1), np.transpose(result[:, 0:3])))
+
+        return result
+
+    def _compute_disparity(self, left_rectified_image, right_rectified_image):
+        """
+        Derived classes implement this to compute a disparity map from
+        pre-rectified images. But clients still call the reconstruct() method.
+
+        The returned disparity map, must be equivalent to what OpenCV
+        returns from other stereo reconstructors like the SGBM reconstructor.
+        That is an image, same size as left and right rectified images,
+        of type float32, where each pixel value represents left-to-right
+        disparity.
+
+        :param left_rectified_image: undistorted, rectified image, BGR
+        :param right_rectified_image: undistorted, rectified image, BGR
+        :return: disparity map
+        """
+        raise NotImplementedError("Derived classes should implement this.")
---
+++ packages/scikit-surgerysurfacematch/scikit-surgerysurfacematch-0.4.5.tar.gz/scikit-surgerysurfacematch-0.4.5/sksurgerysurfacematch/pipelines/register_cloud_to_stereo_mosaic.py
@@ -0,0 +1,271 @@
+# -*- coding: utf-8 -*-
+
+""" Pipeline to register 3D point cloud to mosaic'ed surface reconstruction. """
+
+import copy
+import numpy as np
+import cv2
+import sksurgerypclpython as pclp
+import sksurgeryopencvpython as cvcpp
+import sksurgerycore.algorithms.procrustes as proc
+import sksurgerysurfacematch.utils.registration_utils as ru
+import sksurgerysurfacematch.interfaces.video_segmentor as vs
+import sksurgerysurfacematch.algorithms.reconstructor_with_rectified_images \
+    as sr
+import sksurgerysurfacematch.interfaces.rigid_registration as rr
+
+
+# pylint:disable=too-many-instance-attributes, invalid-name, too-many-locals
+# pylint:disable=unsubscriptable-object, too-many-arguments,too-many-branches
+
+
+class Register3DToMosaicedStereoVideo:
+    """
+    Class to register a point cloud to a series of surfaces
+    derived from stereo video, and stitched together.
+    """
+    def __init__(
+            self,
+            video_segmentor: vs.VideoSegmentor,
+            surface_reconstructor: sr.StereoReconstructorWithRectifiedImages,
+            rigid_registration: rr.RigidRegistration,
+            left_camera_matrix: np.ndarray,
+            right_camera_matrix: np.ndarray,
+            left_to_right_rmat: np.ndarray,
+            left_to_right_tvec: np.ndarray,
+            min_number_of_keypoints: int = 25,
+            max_fre_threshold=2,
+            left_mask: np.ndarray = None,
+            z_range: list = None,
+            radius_removal: list = None,
+            voxel_reduction: list = None):
+        """
+        Uses Dependency Injection for each pluggable component.
+
+        :param video_segmentor: Optional class to pre-segment the video.
+        :param surface_reconstructor: Mandatory class to do reconstruction.
+        :param rigid_registration: Mandatory class to perform rigid alignment.
+        :param left_camera_matrix: [3x3] camera matrix.
+        :param right_camera_matrix: [3x3] camera matrix.
+        :param left_to_right_rmat: [3x3] left-to-right rotation matrix.
+        :param left_to_right_tvec: [1x3] left-to-right translation vector.
+        :param min_number_of_keypoints: Number of keypoints to use for matching.
+        :param max_fre_threshold: maximum FRE when stitching frames together.
+        :param left_mask: a static mask to apply to stereo reconstruction.
+        :param z_range: [min range, max range] to limit reconstructed points.
+        :param radius_removal: [radius, number] to reject points with too few
+        neighbours
+        :param voxel_reduction: [vx, vy, vz] parameters for PCL
+        Voxel Grid reduction.
+        """
+        self.video_segmentor = video_segmentor
+        self.surface_reconstructor = surface_reconstructor
+        self.rigid_registration = rigid_registration
+        self.left_camera_matrix = left_camera_matrix
+        self.right_camera_matrix = right_camera_matrix
+        self.left_to_right_rmat = left_to_right_rmat
+        self.left_to_right_tvec = left_to_right_tvec
+        self.min_number_of_keypoints = min_number_of_keypoints
+        self.max_fre_threshold = max_fre_threshold
+        self.left_static_mask = left_mask
+        self.z_range = z_range
+        self.radius_removal = radius_removal
+        self.voxel_reduction = voxel_reduction
+        self.previous_keypoints = None
+        self.previous_descriptors = None
+        self.previous_good_l2r_matches = None
+        self.previous_good_l2r_matched_descriptors = None
+        self.previous_triangulated_key_points = None
+        self.previous_recon = None
+
+    def reset(self):
+        """
+        Reset's internal data members, so that you can start accumulating
+        data again.
+        """
+        self.previous_keypoints = None
+        self.previous_descriptors = None
+        self.previous_good_l2r_matches = None
+        self.previous_good_l2r_matched_descriptors = None
+        self.previous_triangulated_key_points = None
+        self.previous_recon = None
+
+    def grab(self,
+             left_image: np.ndarray,
+             right_image: np.ndarray):
+        """
+        Call this repeatedly to grab a surface and use ORM key points to
+        match previous reconstruction to the current frame.
+
+        :param left_image: undistorted, BGR image
+        :param right_image: undistorted, BGR image
+        """
+        left_mask = np.ones((left_image.shape[0],
+                             left_image.shape[1]), dtype=np.uint8) * 255
+
+        if self.video_segmentor is not None:
+            left_mask = self.video_segmentor.segment(left_image)
+            left_mask = 255 * (left_mask > 0)
+
+        if self.left_static_mask is not None:
+            left_mask = np.bitwise_and(left_mask, self.left_static_mask)
+
+        orb = cv2.ORB_create()
+
+        current_left_key_points, current_left_descriptors = \
+            orb.detectAndCompute(left_image, None)
+
+        current_right_key_points, current_right_descriptors = \
+            orb.detectAndCompute(right_image, None)
+
+        index_params = dict(algorithm=6,
+                            table_number=6,
+                            key_size=12,
+                            multi_probe_level=1)  # 6=FLANN_INDEX_LSH
+        search_params = dict(checks=50)
+
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+        left_to_right_matches = flann.knnMatch(
+            current_left_descriptors,
+            current_right_descriptors, k=2)
+
+        # Keep good matches, based on Lowe's ratio test.
+        good_l2r_matches = []
+        for m, n in left_to_right_matches:
+            if m.distance < 0.7 * n.distance:
+                good_l2r_matches.append(m)
+
+        if len(good_l2r_matches) > self.min_number_of_keypoints:
+
+            left_descriptors = np.zeros((len(good_l2r_matches),
+                                         len(current_left_descriptors[0])),
+                                        dtype=np.uint8)
+            for i, m in enumerate(good_l2r_matches):
+                left_descriptors[i, :] = current_left_descriptors[m.queryIdx]
+
+            left_pts = np.float32([current_left_key_points[m.queryIdx].pt
+                                   for m in good_l2r_matches])
+            right_pts = np.float32([current_right_key_points[m.trainIdx].pt
+                                    for m in good_l2r_matches])
+            paired_pts = np.zeros((left_pts.shape[0], 4))
+            paired_pts[:, 0:2] = left_pts
+            paired_pts[:, 2:4] = right_pts
+            triangulated_l2r_key_points = \
+                cvcpp.triangulate_points_using_hartley(paired_pts,
+                                                       self.left_camera_matrix,
+                                                       self.right_camera_matrix,
+                                                       self.left_to_right_rmat,
+                                                       self.left_to_right_tvec)
+
+            full_reconstruction = \
+                self.surface_reconstructor.reconstruct(left_image,
+                                                       self.left_camera_matrix,
+                                                       right_image,
+                                                       self.right_camera_matrix,
+                                                       self.left_to_right_rmat,
+                                                       self.left_to_right_tvec,
+                                                       left_mask
+                                                       )
+
+            full_reconstruction = full_reconstruction[:, 0:3]
+
+            if self.z_range is not None:
+                full_reconstruction = \
+                    pclp.pass_through_filter(full_reconstruction,
+                                             'z',
+                                             self.z_range[0],
+                                             self.z_range[1],
+                                             True)
+
+            if self.radius_removal is not None:
+                full_reconstruction = \
+                    pclp.radius_removal_filter(full_reconstruction,
+                                               self.radius_removal[0],
+                                               self.radius_removal[1])
+
+            # Match to previous frame
+            if self.previous_keypoints is not None and \
+                    self.previous_descriptors is not None:
+
+                previous_to_current = flann.knnMatch(
+                    self.previous_good_l2r_matched_descriptors,
+                    left_descriptors,
+                    k=2)
+
+                # Keep good matches, based on Lowe's ratio test.
+                good_prev_to_current_matches = []
+                for i, id_pair in enumerate(previous_to_current):
+                    if id_pair is not None and len(id_pair) == 2:
+                        m, n = id_pair
+                        if m.distance < 0.7 * n.distance:
+                            good_prev_to_current_matches.append(m)
+
+                if len(good_prev_to_current_matches) > \
+                        self.min_number_of_keypoints:
+
+                    prev_3d_pts = np.float32(
+                        [self.previous_triangulated_key_points[m.queryIdx]
+                         for m in good_prev_to_current_matches])
+                    current_3d_pts = np.float32(
+                        [triangulated_l2r_key_points[m.trainIdx]
+                         for m in good_prev_to_current_matches])
+
+                    # Compute rigid body transform. Maybe use RANSAC?
+                    rmat, tvec, fre = proc.orthogonal_procrustes(current_3d_pts,
+                                                                 prev_3d_pts)
+
+                    if fre < self.max_fre_threshold:
+                        # Transform previous point cloud to current
+                        transformed_point_cloud = \
+                            np.transpose(np.matmul(rmat,
+                                                   np.transpose(
+                                                       self.previous_recon))
+                                         + tvec)
+
+                        # Combine and simplify? or just combine?
+                        full_reconstruction = \
+                            np.vstack((transformed_point_cloud,
+                                       full_reconstruction))
+
+            # Save current iteration, such that next iteration it will
+            # be called the 'previous' iteration, for tracking purposes.
+            self.previous_keypoints = current_left_key_points
+            self.previous_descriptors = current_left_descriptors
+            self.previous_good_l2r_matches = good_l2r_matches
+            self.previous_good_l2r_matched_descriptors = left_descriptors
+            self.previous_triangulated_key_points = triangulated_l2r_key_points
+            self.previous_recon = full_reconstruction
+
+    def register(self,
+                 point_cloud: np.ndarray,
+                 initial_transform: np.ndarray = None
+                 ):
+        """
+        Registers a point cloud to the internal mosaicc'ed reconstruction.
+
+        :param point_cloud: [Nx3] points, each row, x,y,z, e.g. from CT/MR.
+        :param initial_transform: [4x4] of initial rigid transform.
+        :return: residual, [4x4] transform, of point_cloud to left camera space,
+        and [Mx6] reconstructed point cloud, as [x, y, z, r, g, b] rows.
+        """
+        if self.previous_recon is None:
+            raise ValueError("No reconstruction has been performed")
+
+        recon_points = self.previous_recon
+
+        if self.voxel_reduction is not None:
+            recon_points = \
+                pclp.down_sample_points(
+                    recon_points,
+                    self.voxel_reduction[0],
+                    self.voxel_reduction[1],
+                    self.voxel_reduction[2])
+
+        residual, transform = ru.do_rigid_registration(recon_points,
+                                                       point_cloud,
+                                                       self.rigid_registration,
+                                                       initial_transform)
+
+        # Don't return a pointer to internal self.previous_recon.
+        return residual, transform, copy.deepcopy(recon_points)
