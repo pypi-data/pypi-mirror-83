528ccfa2d317c4d8d60647edb796737e9b834151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:48 +0000

    Add package guillotina-elasticsearch/3.3.26/guillotina_elasticsearch-3.3.26.tar.gz

---
+++ packages/guillotina-elasticsearch/guillotina_elasticsearch-3.3.26.tar.gz/guillotina_elasticsearch-3.3.26/setup.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+
+test_requires = [
+    'pytest>=3.6',
+    'docker',
+    'backoff',
+    'psycopg2-binary',
+    'pytest-asyncio',
+    'coverage',
+    'pytest-cov',
+    'pytest-aiohttp',
+    'pytest-rerunfailures',
+    'pytest-docker-fixtures>=1.3.0'
+]
+
+
+setup(
+    name='guillotina_elasticsearch',
+    description='elasticsearch catalog support for guillotina',
+    keywords='search async guillotina elasticsearch',
+    author='Ramon Navarro Bosch & Nathan Van Gheem',
+    author_email='ramon@plone.org',
+    version=open('VERSION').read().strip(),
+    long_description=(open('README.rst').read() + '\n' +
+                      open('CHANGELOG.rst').read()),
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    url='https://pypi.python.org/pypi/guillotina_elasticsearch',
+    license='GPL version 3',
+    setup_requires=[
+        'pytest-runner',
+    ],
+    zip_safe=True,
+    include_package_data=True,
+    package_data={"": ["*.txt", "*.rst"], "guillotina_elasticsearch": ["py.typed"]},
+    packages=find_packages(exclude=['ez_setup']),
+    install_requires=[
+        'guillotina>=4.6.0',
+        'mypy_extensions',
+        'aioelasticsearch<0.6.0',
+        'ujson',
+        'lru-dict',
+        'backoff',
+        'asyncpg'
+    ],
+    tests_require=test_requires,
+    extras_require={
+        'test': test_requires
+    }
+)
