793f21f4692279bdd130da6d3b1a2d9742c8bbd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:50 +0000

    Add package textanalyzer/0.0.2/textanalyzer-0.0.2.tar.gz

---
+++ packages/textanalyzer/textanalyzer-0.0.2.tar.gz/textanalyzer-0.0.2/src/textanalyzer/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.0.2"
+# lac tokenizer를 활용한 중국어 키워드 추출 전용 tokenizer 개발
+
+
+# basic utils
+from .data_utils import Doc, Token
+from .process_utils import Tokenization, TokenCandidateGeneration
+
+# lac-tokenizer based chinese keyphrase extractor
+from .tokenization_zh import LACTokenization
