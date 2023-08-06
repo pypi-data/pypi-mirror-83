dcc86d7a1e4b180466d5512b0b9cac3591f4a717
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:10 +0000

    Add package libraw-cffi/0.19.0/libraw_cffi-0.19.0-cp38-cp38-manylinux2010_x86_64.whl

---
+++ packages/libraw-cffi/libraw_cffi-0.19.0-cp38-cp38-manylinux2010_x86_64.whl/libraw_cffi/_api.py
@@ -0,0 +1,127 @@
+from contextlib import closing
+from functools import wraps
+import mmap
+import os
+
+import attr
+
+from _raw import ffi, lib
+
+
+class LibRawError(Exception):
+    def __init__(self, message, code=None):
+        super(LibRawError, self).__init__(message, code)
+        self._message = message
+        self._code = code
+
+    def __str__(self):
+        return self._message
+
+    @classmethod
+    def from_code(cls, code):
+        message = ffi.string(lib.libraw_strerror(code))
+        return cls(message=message.decode(), code=code)
+
+
+def version():
+    return ffi.string(lib.libraw_version())
+
+
+def version_info():
+    number = lib.libraw_versionNumber()
+    patch = number & 0xFF
+    number >>= 8
+    minor = number & 0xFF
+    number >>= 8
+    major = number & 0xFF
+    return major, minor, patch
+
+
+def _data():
+    data = lib.libraw_init(0)
+    if data == ffi.NULL:
+        raise LibRawError(message="Null pointer returned by libraw_init")
+    return ffi.gc(data, lib.libraw_close)
+
+
+@attr.s
+class Raw(object):
+
+    data = attr.ib(factory=_data)
+
+    @classmethod
+    def from_file(cls, file):
+        raw = cls()
+        mmapped = mmap.mmap(file.fileno(), 0, mmap.MAP_PRIVATE, mmap.PROT_READ)
+        with closing(mmapped):
+            raw.open_buffer(ffi.from_buffer(mmapped), mmapped.size())
+        return raw
+
+    @classmethod
+    def from_path(cls, path):
+        raw = cls()
+        raw.open_file(os.fsencode(path))
+        return raw
+
+    def dcraw_make_mem_image(self):
+        errcode = ffi.new("int *")
+        processed_image = lib.libraw_dcraw_make_mem_image(self.data, errcode)
+        if processed_image == ffi.NULL:
+            raise LibRawError.from_code(errcode[0])
+        return _ProcessedImage(contents=processed_image)
+
+    def dcraw_make_mem_thumb(self):
+        errcode = ffi.new("int *")
+        processed_image = lib.libraw_dcraw_make_mem_thumb(self.data, errcode)
+        if processed_image == ffi.NULL:
+            raise LibRawError.from_code(errcode[0])
+        return _ProcessedImage(contents=processed_image)
+
+
+@attr.s
+class _ProcessedImage(object):
+
+    contents = attr.ib()
+
+    @property
+    def size(self):
+        return self.contents.width, self.contents.height
+
+    def buffer(self):
+        return ffi.buffer(self.contents.data, self.contents.data_size)
+
+
+def _wrap(fn):
+    """
+    Wrap a LibRaw function to pass the data struct and check for errors.
+
+    For functions not taking the data struct, returns the function as-is.
+    """
+    signature = ffi.typeof(fn)
+    if signature.args and signature.args[0].cname == "libraw_data_t *":
+        if signature.result.cname == "int":
+            # Implement the behavior specified in
+            # https://www.libraw.org/docs/API-notes.html#errors
+            def wrapper(self, *args, **kwargs):
+                errorcode = fn(self.data, *args, **kwargs)
+                if errorcode < 0:
+                    raise LibRawError.from_code(errorcode)
+                elif errorcode > 0:
+                    raise OSError(errorcode, os.strerror(errorcode))
+        else:
+            def wrapper(self, *args, **kwargs):
+                return fn(self.data, *args, **kwargs)
+        return wraps(fn)(wrapper)
+    else:
+        return fn
+
+
+prefix = "libraw_"
+prefix_length = len(prefix)
+for name in dir(lib):
+    fn = getattr(lib, name)
+    if name.startswith(prefix):
+        name = name[prefix_length:]
+        if hasattr(Raw, name):
+            continue
+        setattr(Raw, name, _wrap(fn))
---
+++ packages/libraw-cffi/libraw_cffi-0.19.0-cp38-cp38-manylinux2010_x86_64.whl/libraw_cffi/tests/test_samples.py
@@ -0,0 +1,256 @@
+"""
+A reimplementation of the examples in the samples directory.
+"""
+from datetime import datetime
+from unittest import TestCase
+
+from _raw import ffi
+from libraw_cffi.tests import RADIOHEAD, RADIOHEAD_SIZE
+import libraw_cffi
+
+
+class TestSamples(TestCase):
+    def test_raw_identify(self):
+        raw = libraw_cffi.Raw.from_path(RADIOHEAD)
+        data = raw.data
+
+        self.assertEqual(
+            (
+                ffi.string(data.idata.make),
+                ffi.string(data.idata.model),
+                data.sizes.width,
+                data.sizes.height,
+            ),
+            (b"Sony", b"ILCE-7RM3") + RADIOHEAD_SIZE,
+        )
+
+        raw.adjust_sizes_info_only()
+
+        self.assertEqual(
+            (
+                # Whee.. this looks like a naive datetime stored in the ARW
+                datetime.fromtimestamp(data.other.timestamp),
+                data.lens.makernotes.CamID,
+                ffi.string(data.shootinginfo.BodySerial),
+                ffi.string(data.shootinginfo.InternalBodySerial),
+                ffi.string(data.other.artist),
+                data.idata.dng_version,
+            ),
+            (
+                datetime(
+                    month=7, day=11, year=2018, hour=1, minute=18, second=47,
+                ),
+                0x16a,
+                b"",
+                b"30ff0000e608",
+                b"",
+                0,
+            ),
+        )
+
+        self.assertEqual(
+            (
+                data.lens.MinFocal,
+                data.lens.MaxFocal,
+                data.lens.MaxAp4MinFocal,
+                data.lens.MaxAp4MaxFocal,
+                data.lens.EXIF_MaxAp,
+                data.lens.FocalLengthIn35mmFormat,
+                ffi.string(data.lens.LensMake),
+                ffi.string(data.lens.Lens),
+            ),
+            (
+                55.0,
+                210.0,
+                4.5,
+                6.300000190734863,
+                5.595918655395508,
+                193,
+                b"",
+                b"E 55-210mm F4.5-6.3 OSS",
+            ),
+        )
+
+        self.assertEqual(
+            (
+                data.shootinginfo.DriveMode,
+                data.shootinginfo.FocusMode,
+                data.shootinginfo.MeteringMode,
+                data.shootinginfo.AFPoint,
+                data.shootinginfo.ExposureMode,
+                data.shootinginfo.ImageStabilization,
+            ),
+            (0, 3, 0, -1, 7, 1),
+        )
+
+        self.assertEqual(
+            (
+                ffi.string(data.lens.makernotes.body),
+                data.lens.makernotes.CameraFormat,
+                data.lens.makernotes.CameraMount,
+                data.lens.makernotes.LensID,
+                ffi.string(data.lens.makernotes.Lens),
+                data.lens.makernotes.LensFormat,
+                data.lens.makernotes.LensMount,
+                data.lens.makernotes.FocalType,
+            ),
+            (b"", 2, 37, 32786, b"", 1, 37, 0),
+        )
+
+        self.assertEqual(
+            (
+                ffi.string(data.lens.makernotes.LensFeatures_pre),
+                ffi.string(data.lens.makernotes.LensFeatures_suf),
+                data.lens.makernotes.MinFocal,
+                data.lens.makernotes.MaxFocal,
+                data.lens.makernotes.MaxAp4MinFocal,
+                data.lens.makernotes.MaxAp4MaxFocal,
+                data.lens.makernotes.MinAp4MinFocal,
+                data.lens.makernotes.MinAp4MaxFocal,
+                data.lens.makernotes.MaxAp,
+                data.lens.makernotes.MinAp,
+                data.lens.makernotes.CurFocal,
+                data.lens.makernotes.CurAp,
+                data.lens.makernotes.MaxAp4CurFocal,
+                data.lens.makernotes.MinAp4CurFocal,
+            ),
+            (
+                b"",
+                b"",
+                55.0,
+                210.0,
+                4.5,
+                6.300000190734863,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+                0.0,
+            ),
+        )
+
+        self.assertEqual(
+            (
+                data.sizes.pixel_aspect,
+                data.thumbnail.twidth,
+                data.thumbnail.theight,
+                data.sizes.raw_width,
+                data.sizes.raw_height,
+                data.sizes.iwidth,
+                data.sizes.iheight,
+            ),
+            (1.0, 1616, 1080, 5216, 3464, 5184, 3464),
+        )
+
+        self.assertEqual(
+            (
+                data.sizes.raw_inset_crop.cwidth,
+                data.sizes.raw_inset_crop.cheight,
+                data.sizes.raw_inset_crop.cleft,
+                data.sizes.raw_inset_crop.ctop,
+            ),
+            (5168, 3448, 8, 8),
+        )
+        self.assertEqual(
+            (
+                data.idata.colors,
+                ffi.string(data.idata.cdesc),
+                list(data.color.linear_max),
+            ), (
+                3,
+                b"RGBG",
+                [15360] * 4,
+            ),
+        )
+
+        self.assertEqual(
+            (
+                list(data.color.cam_mul),
+                [list(coeffs) for coeffs in data.color.WB_Coeffs if coeffs[0]],
+                [
+                    list(data.color.rgb_cam[i])
+                    for i in range(data.idata.colors)
+                ],
+                [
+                    list(data.color.cam_xyz[i])
+                    for i in range(data.idata.colors)
+                ],
+                [
+                    list(data.color.ccm[i])
+                    for i in range(data.idata.colors)
+                ],
+                [data.color.pre_mul[i] for i in range(data.idata.colors)],
+            ), (
+                [2528.0, 1024.0, 1564.0, 1024.0], [
+                    [2452, 1024, 1640, 1024],
+                    [1492, 1024, 3016, 1024],
+                    [2696, 1024, 1460, 1024],
+                    [2660, 1024, 1500, 1024],
+                    [2944, 1024, 1348, 1024],
+                    [2656, 1024, 1588, 1024],
+                    [2364, 1024, 1724, 1024],
+                    [2220, 1024, 2344, 1024],
+                    [1760, 1024, 2892, 1024],
+                    [1492, 1024, 3016, 1024],
+                    [2364, 1024, 1724, 1024],
+                    [1672, 1024, 2600, 1024],
+                    [2528, 1024, 1564, 1024],
+                ], [
+                    [
+                        1.7365375757217407,
+                        -0.5611968040466309,
+                        -0.17534084618091583,
+                        0.0,
+                    ], [
+                        -0.15308751165866852,
+                        1.5583630800247192,
+                        -0.4052755832672119,
+                        0.0,
+                    ], [
+                        0.01987767033278942,
+                        -0.4041007161140442,
+                        1.3842231035232544,
+                        0.0,
+                    ],
+                ], [
+                    [
+                        0.6639999747276306,
+                        -0.18469999730587006,
+                        -0.0502999983727932,
+                    ], [
+                        -0.5238000154495239,
+                        1.3009999990463257,
+                        0.24740000069141388,
+                    ], [
+                        -0.09929999709129333,
+                        0.1673000007867813,
+                        0.6527000069618225,
+                    ],
+                ], [
+                    [
+                        1.1270772218704224,
+                        -0.2003910094499588,
+                        0.07331378012895584,
+                        0.0,
+                    ], [
+                        0.013671875,
+                        1.10546875,
+                        -0.119140625,
+                        0.0,
+                    ], [
+                        0.03125,
+                        -0.0556640625,
+                        1.0244140625,
+                        0.0,
+                    ],
+                ],
+                [2.553339719772339, 0.932383120059967, 1.2761651277542114],
+            ),
+        )
+        self.assertEqual(
+            (data.color.profile, ffi.string(data.color.model2)),
+            (ffi.NULL, b""),
+        )
