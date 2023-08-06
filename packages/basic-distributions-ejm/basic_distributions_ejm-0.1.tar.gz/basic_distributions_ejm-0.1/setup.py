c6810e122825ca48c9a466c463f1c8443340ae1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:21 +0000

    Add package basic-distributions-ejm/0.1/basic_distributions_ejm-0.1.tar.gz

---
+++ packages/basic-distributions-ejm/basic_distributions_ejm-0.1.tar.gz/basic_distributions_ejm-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='basic_distributions_ejm',
+      version='0.1',
+      description='Gaussian/Binomial distributions',
+      packages=['basic_distributions_ejm'],
+      author="Erik Mason",
+      author_email='ejmdpg@gmail.com',
+      zip_safe=False)
