8c6a4c6ef3fec3432784fa4e4093f5c944db490e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:14:29 +0000

    Add package ner-anonymizer/0.1.5/ner-anonymizer-0.1.5.tar.gz

---
+++ packages/ner-anonymizer/ner-anonymizer-0.1.5.tar.gz/ner-anonymizer-0.1.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ner-anonymizer",
+    version="0.1.5",
+    author="Kelvin Tay",
+    author_email="btkelvin@gmail.com",
+    description="Anonymizes pandas dataset and provides a hash dictionary to de-anonymize",
+    url="https://github.com/kelvnt/ner-anonymizer",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    license="MIT license",
+    install_requires=[
+        "transformers>=3.0.0",
+        "torch>=1.5.0",
+        "torchvision>=0.6.0",
+        "pandas>=1.0.0"
+    ],
+    python_requires=">=3.6.0"
+)
