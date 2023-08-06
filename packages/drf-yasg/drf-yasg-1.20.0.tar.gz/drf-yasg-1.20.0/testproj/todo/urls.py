bc97f68d99ec95658cb2847ec842c1a53dd7dae3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:26 +0000

    Add package drf-yasg/1.20.0/drf-yasg-1.20.0.tar.gz

---
+++ packages/drf-yasg/drf-yasg-1.20.0.tar.gz/drf-yasg-1.20.0/testproj/todo/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from rest_framework import routers
+
+from todo import views
+
+router = routers.DefaultRouter()
+router.register(r'', views.TodoViewSet)
+router.register(r'another', views.TodoAnotherViewSet)
+router.register(r'yetanother', views.TodoYetAnotherViewSet)
+router.register(r'tree', views.TodoTreeView)
+router.register(r'recursive', views.TodoRecursiveView)
+router.register(r'harvest', views.HarvestViewSet)
+
+urlpatterns = router.urls
+
+urlpatterns += [
+    path(r'<int:todo_id>/yetanothers/<int:yetanother_id>/',
+         views.NestedTodoView.as_view(), ),
+]
