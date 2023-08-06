025d6e31a8d837faed0bad3c96b7f0ad5c564a22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:14 +0000

    Add package molgemtools/0.0.10/molgemtools-0.0.10-py3-none-any.whl

---
+++ packages/molgemtools/molgemtools-0.0.10-py3-none-any.whl/molgemtools/__init__.py
@@ -0,0 +1,8 @@
+"""Tools for working with molecular geometry data.
+"""
+
+
+__author__ = "Attila Dékány"
+__copyright__ = "2020, Attila Dékány"
+__contact__ = "dekanyattilaadam@gmail.com"
+__version__ = "0.0.10"
