d5877d1782cae8e5a408fc1e54689767b2312f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:05 +0000

    Add package probability-mk/0.1/probability_mk-0.1.tar.gz

---
+++ packages/probability-mk/probability_mk-0.1.tar.gz/probability_mk-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_mk',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['probability_mk'],
+      author='Mikel Alberdi',
+      author_email='mikelalberdi131@gmail.com',
+      zip_safe=False)
