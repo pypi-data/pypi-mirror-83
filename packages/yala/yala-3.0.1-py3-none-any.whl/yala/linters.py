4b10c8d55c16e90ee351879dd53699ddb8419f56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:14 +0000

    Add package yala/3.0.1/yala-3.0.1-py3-none-any.whl

---
+++ packages/yala/yala-3.0.1-py3-none-any.whl/yala/linters.py
@@ -0,0 +1,177 @@
+"""Module for linters."""
+# The less we need to code, the better!
+# pylint: disable=too-few-public-methods
+import re
+
+from .base import Linter, LinterOutput
+
+
+class Flake8(Linter):
+    """Parser for flake8."""
+
+    name = 'flake8'
+
+    def parse(self, stdout_lines, stderr_lines):
+        """Parse linter stdout and stderr lines."""
+        pattern = re.compile(r'''
+                             ^(?P<path>.+?)
+                             :(?P<line_nr>\d+?)
+                             :(?P<col>\d+?)
+                             :\ (?P<msg>.+)$''', re.VERBOSE)
+        return self._parse_by_pattern(stdout_lines, pattern), stderr_lines
+
+
+class Isort(Linter):
+    """Isort parser."""
+
+    name = 'isort'
+
+    def parse(self, stdout_lines, stderr_lines):
+        """Parse linter stdout and stderr lines."""
+        # E.g. "ERROR: /my/path/main.py Imports are incorrectly sorted."
+        pattern = re.compile(r'''
+                             ^.+?
+                             :\ (?P<full_path>.+\.py)
+                             \ (?P<msg>.+)$''', re.VERBOSE)
+        return self._parse_by_pattern(stderr_lines, pattern), stdout_lines
+
+    def _create_output_from_match(self, match_result):
+        """As isort outputs full path, we change it to relative path."""
+        full_path = match_result['full_path']
+        path = self._get_relative_path(full_path)
+        return LinterOutput(self.name, path, match_result['msg'])
+
+
+class Pycodestyle(Linter):
+    """Pycodestyle parser."""
+
+    name = 'pycodestyle'
+
+    def parse(self, stdout_lines, stderr_lines):
+        """Parse linter stdout and stderr lines."""
+        pattern = re.compile(r'''
+                             ^(?P<path>.+?)
+                             :(?P<line_nr>\d+?)
+                             :(?P<col>\d+?)
+                             :\ (?P<msg>.+)$''', re.VERBOSE)
+        return self._parse_by_pattern(stdout_lines, pattern), stderr_lines
+
+
+class Mypy(Linter):
+    """Mypy parser."""
+
+    name = 'mypy'
+
+    def parse(self, stdout_lines, stderr_lines):
+        """Parse linter stdout and stderr lines."""
+        pattern = re.compile(r'''
+                             ^(?P<path>.+?)
+                             :(?P<line_nr>\d+?)
+                             :\ (?P<msg>.+)$''', re.VERBOSE)
+        return self._parse_by_pattern(stdout_lines, pattern), stderr_lines
+
+
+class Pydocstyle(Linter):
+    """Pydocstyle parser."""
+
+    name = 'pydocstyle'
+
+    def parse(self, stdout_lines, stderr_lines):
+        """Parse linter stdout and stderr lines."""
+        return self._parse(stdout_lines), stderr_lines
+
+    def _parse(self, lines):
+        """Get :class:`base.LinterOutput` parameters using regex.
+
+        There are 2 lines for each pydocstyle result:
+            1. Filename and line number;
+            2. Message for the problem found.
+        """
+        patterns = [re.compile(r'^(.+?):(\d+)'),
+                    re.compile(r'^\s+(.+)$')]
+        for i, line in enumerate(lines):
+            if i % 2 == 0:
+                path, line_nr = patterns[0].match(line).groups()
+            else:
+                msg = patterns[1].match(line).group(1)
+                yield LinterOutput(self.name, path, msg, line_nr)
+
+
+class Pyflakes(Linter):
+    """Pyflakes parser."""
+
+    name = 'pyflakes'
+
+    def parse(self, stdout_lines, stderr_lines):
+        """Parse linter stdout and stderr lines."""
+        pattern = re.compile(r'''
+                             ^(?P<path>.+?)
+                             :(?P<line_nr>\d+?)
+                             :(?P<col>\d+?)?
+                             \ (?P<msg>.+)$''', re.VERBOSE)
+        return self._parse_by_pattern(stdout_lines, pattern), stderr_lines
+
+
+class Pylint(Linter):
+    """Pylint parser."""
+
+    name = 'pylint'
+
+    def parse(self, stdout_lines, stderr_lines):
+        """Parse linter stdout and stderr lines."""
+        pattern = re.compile(r'''
+                             .*?^(?P<path>[^\n]+?)
+                             :(?P<msg>.+)
+                             :(?P<line_nr>\d+?)
+                             :(?P<col>\d+?)$''', re.X | re.M | re.S)
+        return self._parse_by_pattern(stdout_lines, pattern), stderr_lines
+
+
+class RadonCC(Linter):
+    """Parser for radon ciclomatic complexity."""
+
+    name = 'radon cc'
+
+    def parse(self, stdout_lines, stderr_lines):
+        """Parse linter stdout and stderr lines."""
+        return self._parse(stdout_lines), stderr_lines
+
+    def _parse(self, lines):
+        """Get :class:`base.LinterOutput` parameters using regex.
+
+        The output has one line with the file path followed by others with
+        one problem per line.
+        """
+        # E.g. 'relative/path/to/file.py'
+        pattern_path = re.compile(r'^(\S.*$)')
+        # E.g. '    C 19:0 RadonCC - A'
+        pattern_result = re.compile(r'\s+\w (\d+):(\d+) (.+)$')
+        path = None
+        for line in lines:
+            match = pattern_path.match(line)
+            if match:
+                # We have the file path. Skip the remainder of the loop
+                path = line
+                continue
+            match = pattern_result.match(line)
+            if match:
+                # Output found for the file stored in ``path``.
+                line_nr, col, msg = match.groups()
+                yield LinterOutput(self.name, path, msg, line_nr, col)
+
+
+class RadonMI(Linter):
+    """Parser for radon maintainability index."""
+
+    name = 'radon mi'
+
+    def parse(self, stdout_lines, stderr_lines):
+        """Parse linter stdout and stderr lines."""
+        pattern = re.compile(r'''
+                             ^(?P<path>.+)
+                             \ -\ (?P<msg>[A-F])$''', re.VERBOSE)
+        return self._parse_by_pattern(stdout_lines, pattern), stderr_lines
+
+
+#: dict: All Linter subclasses indexed by class name
+LINTERS = {cls.name: cls for cls in Linter.__subclasses__()}
---
+++ packages/yala/yala-3.0.1-py3-none-any.whl/yala/main.py
@@ -0,0 +1,165 @@
+"""Run linters on files and directories and sort results.
+
+Usage:
+  yala <path>...
+  yala --dump-config
+  yala --version
+  yala -h | --help
+
+Options:
+  --dump-config  Show all detected configurations
+  --version  Show yala and linters' versions.
+  -h --help  Show this help.
+
+"""
+import logging
+import shlex
+import subprocess
+import sys
+from itertools import chain
+from multiprocessing import Pool
+from typing import List
+
+from docopt import docopt
+
+from . import __version__
+from .config import Config
+from .linters import LINTERS
+
+LOG = logging.getLogger(__name__)
+
+
+class LinterRunner:
+    """Run linter and process results."""
+
+    config = None
+    targets = []  # type: List[str]
+
+    def __init__(self, linter_class):
+        """Set linter class and its configuration."""
+        linter_class.config = self.config.get_linter_config(linter_class.name)
+        self._linter = linter_class()
+
+    @classmethod
+    def run(cls, linter_cfg_tgts):
+        """Run a linter and return the results."""
+        linter_class, cls.config, cls.targets = linter_cfg_tgts
+        runner = cls(linter_class)
+        return runner.get_results()
+
+    def get_results(self):
+        """Run the linter, parse, and return result list.
+
+        If a linter specified by the user is not found, return an error message
+        as result.
+        """
+        try:
+            stdout, stderr = self._lint()
+            # Can't return a generator from a subprocess
+            return list(stdout), self._format_stderr(stderr)
+        except FileNotFoundError as exception:
+            # Error if the linter was not found but was chosen by the user
+            if self._linter.name in self.config.user_linters:
+                error_msg = 'Could not find {}. Did you install it? ' \
+                    'Got exception: {}'.format(self._linter.name, exception)
+                return [], [error_msg]
+            # If the linter was not chosen by the user, do nothing
+            return [], []
+
+    def _get_command(self):
+        """Return command with options and targets, ready for execution."""
+        targets = ' '.join(self.targets)
+        cmd_str = self._linter.command_with_options + ' ' + targets
+        cmd_shlex = shlex.split(cmd_str)
+        return list(cmd_shlex)
+
+    def _lint(self):
+        """Run linter in a subprocess."""
+        command = self._get_command()
+        process = subprocess.run(command, stdout=subprocess.PIPE,  # nosec
+                                 stderr=subprocess.PIPE, check=False)
+        LOG.info('Finished %s', self._linter.name)
+        stdout, stderr = self._get_output_lines(process)
+        return self._linter.parse(stdout, stderr)
+
+    @staticmethod
+    def _get_output_lines(process):
+        return [(line for line in output.decode('utf-8').splitlines() if line)
+                for output in (process.stdout, process.stderr)]
+
+    def _format_stderr(self, lines):
+        return ['[{}] {}'.format(self._linter.name, line) for line in lines]
+
+
+class Main:
+    """Parse all linters and aggregate results."""
+
+    # We only need the ``run`` method.
+    # pylint: disable=too-few-public-methods
+
+    def __init__(self, config=None, all_linters=None):
+        """Initialize the only Config object and assign it to other classes.
+
+        Args:
+            config (Config): Config object.
+            all_linters (dict): Names and classes of all available linters.
+
+        """
+        self._classes = all_linters or LINTERS
+        self._config = config or Config(self._classes)
+        LinterRunner.config = self._config
+
+    def lint(self, targets):
+        """Run linters in parallel and sort all results.
+
+        Args:
+            targets (list): List of files and folders to lint.
+
+        """
+        LinterRunner.targets = targets
+        linters = self._config.get_linter_classes()
+        with Pool() as pool:
+            linter_cfg_tgts = ((linter, self._config, targets)
+                               for linter in linters)
+            linters_out_err = pool.map(LinterRunner.run, linter_cfg_tgts)
+        stdouts, stderrs = zip(*linters_out_err)
+        return (sorted(chain.from_iterable(stdouts)),
+                chain.from_iterable(stderrs))
+
+    def run_from_cli(self, args):
+        """Read arguments, run and print results.
+
+        Args:
+            args (dict): Arguments parsed by docopt.
+
+        """
+        if args['--dump-config']:
+            self._config.print_config()
+        else:
+            stdout, stderr = self.lint(args['<path>'])
+            self.print_results(stdout, stderr)
+
+    @classmethod
+    def print_results(cls, stdout, stderr):
+        """Print linter results and exits with an error if there's any."""
+        for line in stderr:
+            print(line, file=sys.stderr)
+        if stdout:
+            if stderr:  # blank line to separate stdout from stderr
+                print(file=sys.stderr)
+            cls._print_stdout(stdout)
+        else:
+            print(':) No issues found.')
+
+    @staticmethod
+    def _print_stdout(stdout):
+        for line in stdout:
+            print(line)
+        issue = 'issues' if len(stdout) > 1 else 'issue'
+        sys.exit('\n:( {} {} found.'.format(len(stdout), issue))
+
+
+def main():
+    """Entry point for the console script."""
+    args = docopt(__doc__, version=__version__)
+    Main().run_from_cli(args)
