f028a50338f032e512aa223025e7a72a7c3d97a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:35 +0000

    Add package EnviroMS/4.1.1/EnviroMS-4.1.1.tar.gz

---
+++ packages/EnviroMS/EnviroMS-4.1.1.tar.gz/EnviroMS-4.1.1/setup.py
@@ -0,0 +1,36 @@
+import pathlib, os, sys
+
+import setuptools
+from setuptools import Command, setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="EnviroMS",
+    version="4.1.1",
+    description="Search and Assign Molecular Formulas for Complex Mixtures of Small Molecules",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EMSL-Computing/EnviroMS",
+    author="Corilo, Yuri",
+    author_email="corilo@pnnl.gov",
+    license="GNU Affero General Public License v3.0",
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+
+    install_requires=['Click', 'tabulate', 'CoreMS'],
+    entry_points= '''
+
+            [console_scripts]
+            enviroMS=enviroMS.cli:cli
+            ''',
+)
+    
