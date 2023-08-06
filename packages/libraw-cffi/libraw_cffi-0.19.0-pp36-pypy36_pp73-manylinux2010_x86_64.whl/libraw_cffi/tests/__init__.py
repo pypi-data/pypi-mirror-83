8316eaf355fbf5cae73e2683ca1038ef5f6c0985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:11 +0000

    Add package libraw-cffi/0.19.0/libraw_cffi-0.19.0-pp36-pypy36_pp73-manylinux2010_x86_64.whl

---
+++ packages/libraw-cffi/libraw_cffi-0.19.0-pp36-pypy36_pp73-manylinux2010_x86_64.whl/libraw_cffi/tests/__init__.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import os
+
+
+if "RADIOHEAD" in os.environ:
+    RADIOHEAD = Path(os.environ["RADIOHEAD"])
+else:
+    RADIOHEAD = Path(__file__).parent / "_DSC2164.ARW"
+    if not RADIOHEAD.exists():
+        raise RuntimeError(
+            "Radiohead exists, I promise, but the test suite can't find them. "
+            "Run the suite from alongsie a git checkout, or set the RADIOHEAD "
+            "environment variable to the path of the _DSC2164.ARW file."
+        )
+RADIOHEAD_SIZE = 5184, 3464
