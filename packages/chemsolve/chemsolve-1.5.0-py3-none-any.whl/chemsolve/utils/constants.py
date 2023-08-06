a6836889f28dc0a0263c6d8fb80c3dbb66a926bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:44 +0000

    Add package chemsolve/1.5.0/chemsolve-1.5.0-py3-none-any.whl

---
+++ packages/chemsolve/chemsolve-1.5.0-py3-none-any.whl/chemsolve/utils/constants.py
@@ -0,0 +1,37 @@
+'''
+Any prevalent constants in chemistry.
+'''
+
+# BASIC
+
+# Avogadro's Constant
+AVOGADRO = 6.02214e23
+# Elementary Charge
+COULOMB = 1.60e-19
+# Atomic Mass Unit
+M = 1.66e-27
+# Speed of Light
+C = 2.997925e8
+
+# ELECTROCHEMISTRY
+
+# Faraday's Constant
+F = 96485.33289
+
+# GASES
+
+# Molar Gas Constant
+R = 8.3144598
+# Boltzmann's Constant
+kB = 1.380649e-23
+
+# QUANTUM CHEMISTRY
+
+# Planck Constant
+h = 6.62607e-34
+# Rydberg Constant
+rH = 2.17987e-18
+
+# CONVENIENCE
+REDOX = "redox"
+BASEREDOX = "nredox"
