36ce8b6652e4484a3936452776c41dfc2545979f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:08:46 +0000

    Add package stkml/1.0.2/stkml-1.0.2-py3-none-any.whl

---
+++ packages/stkml/stkml-1.0.2-py3-none-any.whl/Stkml/__init__.py
@@ -0,0 +1,6 @@
+#Copyright STACKEO - INRIA 2020
+#!/usr/bin/python
+import os
+MODULE_DIR = os.path.realpath(os.path.join(__file__, '..'))
+STACKML_URL_SCHEMA = 'https://stkml.stackeo.io/_static/stackml.schema.json'
+__version__ = '1.0.2'
