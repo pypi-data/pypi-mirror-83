b4bb625cbc9bf435cd7d39c6745997899d690c7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:38 +0000

    Add package niti-distributions/0.1/niti_distributions-0.1.tar.gz

---
+++ packages/niti-distributions/niti_distributions-0.1.tar.gz/niti_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='niti_distributions',
+      version='0.1',
+      description='Gaussian & Binomial distributions',
+      packages=['niti_distributions'],
+      zip_safe=False)
