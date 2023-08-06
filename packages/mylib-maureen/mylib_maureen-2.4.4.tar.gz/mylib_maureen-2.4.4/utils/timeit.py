ca5958fe34cc1d6a91873cde054034e64d49bf48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:47 +0000

    Add package mylib-maureen/2.4.4/mylib_maureen-2.4.4.tar.gz

---
+++ packages/mylib-maureen/mylib_maureen-2.4.4.tar.gz/mylib_maureen-2.4.4/utils/timeit.py
@@ -0,0 +1,36 @@
+# -*- coding:utf-8 _*-  
+"""
+@author: Maureen Hsu
+@file: timeit.py 
+@time: 2020/06/24
+"""
+
+# python packages
+import time
+# 3rd-party packages
+from loguru import logger
+
+
+# self-defined packages
+
+class Timer:
+    def __init__(self, name=None):
+        self.name = name
+        self.start_time = time.time()
+        self.end_time = None
+
+    def start(self):
+        self.start_time = time.time()
+
+    def stop(self):
+        self.end_time = time.time()
+        logger.debug(
+            f"{self.name + ' ' if self.name is not None else ''} Time elapse {self.end_time - self.start_time}s")
+
+
+def time_function(func):
+    def wrap(*args, **kwargs):
+        timer = Timer(f"{func.__name__}")
+        func(*args, **kwargs)
+        timer.stop()
+    return wrap
