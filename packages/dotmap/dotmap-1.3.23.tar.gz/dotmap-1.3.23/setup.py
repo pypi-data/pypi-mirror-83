161b1e863cf2b6e9e64f618dc896f480208d7bca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:27 +0000

    Add package dotmap/1.3.23/dotmap-1.3.23.tar.gz

---
+++ packages/dotmap/dotmap-1.3.23.tar.gz/dotmap-1.3.23/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+	version = '1.3.23',
+    name='dotmap',
+    packages=['dotmap'],  # this must be the same as the name above
+    description='ordered, dynamically-expandable dot-access dictionary',
+    author='Chris Redford',
+    author_email='credford@gmail.com',
+    url='https://github.com/drgrib/dotmap',  # use the URL to the github repo
+    download_url='https://github.com/drgrib/dotmap/tarball/1.0',
+    keywords=['dict', 'dot', 'map', 'order', 'ordered',
+              'ordereddict', 'access', 'dynamic'],  # arbitrary keywords
+    classifiers=[],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
