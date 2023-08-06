f996c3da3f199f564979123868986fd95820bf92
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:49 +0000

    Add package logdecorator/2.2/logdecorator-2.2.tar.gz

---
+++ packages/logdecorator/logdecorator-2.2.tar.gz/logdecorator-2.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, "README.rst"), encoding="utf-8") as f:
+    README = f.read()
+
+
+setup(
+    name='logdecorator',
+    packages=['logdecorator'],
+    version='2.2',
+    description='Move logging code out of your business logic with decorators',
+    long_description=README,
+    long_description_content_type='text/x-rst',
+    author='Jakob Rößler',
+    author_email='roessler@sighalt.de',
+    url='https://github.com/sighalt/logdecorator',
+    keywords=['logging', 'decorators', 'clean code'],
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: System :: Logging",
+    ],
+)
