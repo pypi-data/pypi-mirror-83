58d90e16fd10482536438e1addd80b5be227c57b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:08 +0000

    Add package django-cast/0.1.30/django-cast-0.1.30.tar.gz

---
+++ packages/django-cast/django-cast-0.1.30.tar.gz/django-cast-0.1.30/cast/api/urls.py
@@ -0,0 +1,52 @@
+from django.conf.urls import url
+
+from rest_framework.schemas import get_schema_view
+
+from . import views
+
+app_name = "api"
+schema_view = get_schema_view(title="Cast API")
+
+urlpatterns = [
+    url(r"^schema/$", schema_view),
+    url(r"^$", views.api_root, name="root"),
+    # image
+    url(r"^images/?$", views.ImageListView.as_view(), name="image_list"),
+    url(
+        r"^images/(?P<pk>\d+)/?$", views.ImageDetailView.as_view(), name="image_detail"
+    ),
+    url(
+        regex=r"^upload_image/$",
+        view=views.ImageCreateView.as_view(),
+        name="upload_image",
+    ),
+    # gallery
+    url(r"^gallery/?$", views.GalleryListView.as_view(), name="gallery_list"),
+    url(
+        r"^gallery/(?P<pk>\d+)/?$",
+        views.GalleryDetailView.as_view(),
+        name="gallery_detail",
+    ),
+    # video
+    url(r"^videos/?$", views.VideoListView.as_view(), name="video_list"),
+    url(
+        r"^videos/(?P<pk>\d+)/?$", views.VideoDetailView.as_view(), name="video_detail"
+    ),
+    url(
+        regex=r"^upload_video/$",
+        view=views.VideoCreateView.as_view(),
+        name="upload_video",
+    ),
+    # audio
+    url(r"^audio/?$", views.AudioListView.as_view(), name="audio_list"),
+    url(
+        r"^audios/(?P<pk>\d+)/?$", views.AudioDetailView.as_view(), name="audio_detail"
+    ),
+    url(
+        r"^audios/podlove/(?P<pk>\d+)/?$",
+        views.AudioPodloveDetailView.as_view(),
+        name="audio_podlove_detail",
+    ),
+    # request
+    url(r"^request/?$", views.RequestListView.as_view(), name="request_list"),
+]
