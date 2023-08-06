403a7aa75e35390e63c523e095a535832e15b003
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:12 +0000

    Add package platonic-sqs/0.1.2/platonic-sqs-0.1.2.tar.gz

---
+++ packages/platonic-sqs/platonic-sqs-0.1.2.tar.gz/platonic-sqs-0.1.2/platonic/sqs/queue/__init__.py
@@ -0,0 +1,8 @@
+from platonic.sqs.queue.errors import (
+    SQSMessageDoesNotExist,
+    SQSQueueDoesNotExist,
+)
+from platonic.sqs.queue.input import SQSInputQueue
+from platonic.sqs.queue.message import SQSMessage
+from platonic.sqs.queue.output import SQSOutputQueue
+from platonic.sqs.queue.types import InternalType, ValueType
