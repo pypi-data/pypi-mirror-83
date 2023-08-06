a184e6d14d50e77183ac2762e27c57c1772fc65d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:40 +0000

    Add package adapt-parser/0.3.7/adapt-parser-0.3.7.tar.gz

---
+++ packages/adapt-parser/adapt-parser-0.3.7.tar.gz/adapt-parser-0.3.7/setup.py
@@ -0,0 +1,52 @@
+# Copyright 2017 Mycroft AI Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+__author__ = 'seanfitz'
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="adapt-parser",
+    version="0.3.7",
+    author="Sean Fitzgerald",
+    author_email="sean@fitzgeralds.me",
+    description=("A text-to-intent parsing framework."),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license=("Apache License 2.0"),
+    keywords="natural language processing",
+    url="https://github.com/MycroftAI/adapt",
+    packages=["adapt", "adapt.tools", "adapt.tools.text"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Text Processing :: Linguistic',
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+
+    install_requires=[
+        "pyee==8.1.0",
+        "six>=1.10.0"
+    ]
+)
