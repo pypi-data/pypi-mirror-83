d255fefb5129554c77d171f1b231ee16a1e0f5a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:51 +0000

    Add package neuro-extras/20.10.23a5/neuro-extras-20.10.23a5.tar.gz

---
+++ packages/neuro-extras/neuro-extras-20.10.23a5.tar.gz/neuro-extras-20.10.23a5/setup.py
@@ -0,0 +1,37 @@
+import re
+
+from setuptools import find_packages, setup
+
+
+with open("neuro_extras/version.py") as f:
+    txt = f.read()
+    try:
+        version = re.findall(r'^__version__ = "([^"]+)"\r?$', txt, re.M)[0]
+    except IndexError:
+        raise RuntimeError("Unable to determine version.")
+
+
+install_requires = [
+    "neuromation>=20.9.24",
+    "yarl==1.5.1",
+    "multidict==4.7.6",
+    "click",
+    "toml",
+    "pyyaml",
+    'dataclasses>=0.5; python_version<"3.7"',
+]
+
+setup(
+    name="neuro-extras",
+    version=version,
+    python_requires=">=3.6.0",
+    url="https://github.com/neuromation/neuro-extras",
+    packages=find_packages(),
+    install_requires=install_requires,
+    entry_points={
+        "console_scripts": ["neuro-extras=neuro_extras:main"],
+        "neuro_api": ["neuro-extras=neuro_extras:setup_plugin"],
+    },
+    zip_safe=False,
+    include_package_data=True,
+)
