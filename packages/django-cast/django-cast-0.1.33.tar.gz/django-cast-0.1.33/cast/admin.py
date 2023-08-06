b1ebee06b3695f6fa36af48fde2ad4c6145862a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:15:20 +0000

    Add package django-cast/0.1.33/django-cast-0.1.33.tar.gz

---
+++ packages/django-cast/django-cast-0.1.33.tar.gz/django-cast-0.1.33/cast/admin.py
@@ -0,0 +1,103 @@
+import logging
+
+from django.contrib import admin
+from watson.admin import SearchAdmin
+
+from .models import (
+    Blog,
+    Post,
+    File,
+    Image,
+    Video,
+    Audio,
+    Gallery,
+    ItunesArtWork,
+    ChapterMark,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class AdminUserMixin:
+    def get_changeform_initial_data(self, request):
+        return {"user": request.user, "author": request.user}
+
+
+class BlogModelAdmin(AdminUserMixin, admin.ModelAdmin):
+    list_display = ("title", "owner")
+
+
+admin.site.register(Blog, BlogModelAdmin)
+
+
+class PostModelAdmin(AdminUserMixin, SearchAdmin):
+    list_display = ("title", "owner", "blog")
+    search_fields = ("title",)
+
+    class Media:
+        js = ("js/cast/ckeditor_fix.js",)
+
+
+admin.site.register(Post, PostModelAdmin)
+
+
+class ImageModelAdmin(AdminUserMixin, admin.ModelAdmin):
+    list_display = ("pk", "original", "user")
+    fields = ("user", "original")
+
+
+admin.site.register(Image, ImageModelAdmin)
+
+
+class ItunesArtWorkModelAdmin(AdminUserMixin, admin.ModelAdmin):
+    list_display = ("pk", "original")
+    fields = ("original",)
+
+
+admin.site.register(ItunesArtWork, ItunesArtWorkModelAdmin)
+
+
+class FileModelAdmin(AdminUserMixin, admin.ModelAdmin):
+    list_display = ("original", "user")
+    fields = ("user", "original")
+
+
+admin.site.register(File, FileModelAdmin)
+
+
+class AudioAdmin(AdminUserMixin, admin.ModelAdmin):
+    list_display = ("user", "title", "subtitle", "m4a", "mp3", "oga", "opus")
+    fields = ("user", "title", "subtitle", "m4a", "mp3", "oga", "opus")
+
+
+admin.site.register(Audio, AudioAdmin)
+
+
+class ChapterMarkModelAdmin(AdminUserMixin, admin.ModelAdmin):
+    list_display = ("start", "title", "link", "image", "audio")
+
+
+admin.site.register(ChapterMark, ChapterMarkModelAdmin)
+
+
+class VideoModelAdmin(AdminUserMixin, admin.ModelAdmin):
+    list_display = ("pk", "user")
+
+    def save_model(self, request, obj, form, change):
+        logger.info("poster: {}".format(obj.poster))
+        logger.info("form: {}".format(form.cleaned_data))
+        if change and not form.cleaned_data["poster"]:
+            logger.info("poster was cleared")
+            obj.calc_poster = False
+        super().save_model(request, obj, form, change)
+
+
+admin.site.register(Video, VideoModelAdmin)
+
+
+class GalleryModelAdmin(AdminUserMixin, admin.ModelAdmin):
+    list_display = ("pk",)
+    fields = ("user", "images")
+
+
+admin.site.register(Gallery, GalleryModelAdmin)
