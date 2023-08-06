4cd2a1981e0fe65c13afb84d34ad5c3086eab425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:09 +0000

    Add package dlight.py/0.0.3/dlight.py-0.0.3.tar.gz

---
+++ packages/dlight.py/dlight.py-0.0.3.tar.gz/dlight.py-0.0.3/README.md
@@ -0,0 +1,1 @@
+This is as stated. https://dlight-py.gitbook.io/dlight-py/ for docs, examples, and how to get started. 
---
+++ packages/dlight.py/dlight.py-0.0.3.tar.gz/dlight.py-0.0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Developers',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+
+setup(
+  name='dlight.py',
+  version='0.0.3',
+  description='Python api wrapper for daylight/DLIGHT',
+  long_description="For docs, setup, and examples, go to https://dlight-py.gitbook.io/dlight-py/.",
+  url='https://dlight-py.gitbook.io/dlight-py/',
+  author='Andromeda',
+  author_email='and@rjbot.xyz',
+  license='MIT',
+  classifiers=classifiers,
+  keywords='dlight',
+  packages=find_packages(),
+  install_requires=['aiohttp']
+)
