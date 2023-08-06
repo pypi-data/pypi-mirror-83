03373302a26fb20231f71a00d038a1743b63dd42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:27 +0000

    Add package stackl-cli/0.2.5/stackl-cli-0.2.5.tar.gz

---
+++ packages/stackl-cli/stackl-cli-0.2.5.tar.gz/stackl-cli-0.2.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+__version__ = "0.2.5"
+
+setup(
+    name='stackl-cli',
+    version=__version__,
+    py_modules=['stackl', 'commands', 'context'],
+    packages=find_packages(),
+    install_requires=[
+        f'stackl-client=={__version__}', 'pyYAML==5.3', 'Click==7.0',
+        'mergedeep==1.3.0', 'tabulate==0.8.6', 'glom==19.10.0'
+    ],
+    entry_points='''
+        [console_scripts]
+        stackl=stackl:cli
+    ''',
+)
