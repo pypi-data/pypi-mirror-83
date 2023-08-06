d1a379f73861a37b5285c0682566c6b19eb203f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:50 +0000

    Add package squaredown/1.0.0a12/squaredown-1.0.0a12-py2.py3-none-any.whl

---
+++ packages/squaredown/squaredown-1.0.0a12-py2.py3-none-any.whl/squaredown/__init__.py
@@ -0,0 +1,9 @@
+"""A set of functions to retrieve and save Square data into MongoDB.
+"""
+from squaredown.catalog import Catalog
+from squaredown.config import Config
+from squaredown.connector import Connector
+from squaredown.orders import Orders
+
+
+__version__ = '1.0.0-alpha.12'
