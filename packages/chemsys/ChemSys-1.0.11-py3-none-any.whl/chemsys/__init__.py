09b5cc56a5171ed55bcddfd82dbfdbadbf60c56b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:21 +0000

    Add package chemsys/1.0.11/ChemSys-1.0.11-py3-none-any.whl

---
+++ packages/chemsys/ChemSys-1.0.11-py3-none-any.whl/chemsys/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 21 18:48:07 2020
+
+Github: https://github.com/tjczec01
+
+@author: Travis J Czechorski
+
+E-mail: tjczec01@gmail.com
+
+"""
+
+from chemsys import *
