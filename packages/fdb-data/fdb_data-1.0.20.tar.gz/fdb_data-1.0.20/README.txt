2e1a80e4b518c79dce057ca86ed68dd97a4e1873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:53 +0000

    Add package fdb-data/1.0.20/fdb_data-1.0.20.tar.gz

---
+++ packages/fdb-data/fdb_data-1.0.20.tar.gz/fdb_data-1.0.20/README.txt
@@ -0,0 +1,4 @@
+pip install bs4
+pip install lxml
+
+This Module ro work with Foton DataBase -> FDB
---
+++ packages/fdb-data/fdb_data-1.0.20.tar.gz/fdb_data-1.0.20/fdb_data/errors.py
@@ -0,0 +1,14 @@
+# FDB Errors
+class FdbTypeValueError(Exception):
+    def __init__(self,*argv,**kwargv):
+        pass
+class FdbAddDataError(Exception):
+    def __init__(self,*argv,**kwargv):
+        pass
+class FdbNoKeyError(Exception):
+    def __init__(self,*argv,**kwargv):
+        pass
+class FdbCodeError(Exception):
+    def __init__(self,*argv,**kwargv):
+        pass
+    
