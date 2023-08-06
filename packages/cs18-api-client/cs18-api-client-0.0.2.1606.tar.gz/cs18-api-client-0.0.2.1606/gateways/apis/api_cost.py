fbb47da5d9390cd2a4f32f693d25dd441f0e5f9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:06 +0000

    Add package cs18-api-client/0.0.2.1606/cs18-api-client-0.0.2.1606.tar.gz

---
+++ packages/cs18-api-client/cs18-api-client-0.0.2.1606.tar.gz/cs18-api-client-0.0.2.1606/gateways/apis/api_cost.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+from gateways.apis.api_base_class import ApiBase
+
+
+class ApiCost(ApiBase):
+    def breakdown(self, space_name: str, frm: datetime, until: datetime, criteria: str = 'blueprint'):
+        return self.build_route(
+            "spaces/{space_name}/cost/breakdown?criteria={criteria}&from={frm}&to={until}".format(**locals()))
+
+    def usage(self, space_name: str, date: datetime, timezone: str = ''):
+        return self.build_route("spaces/{space_name}/cost/usage?date={date}&timezone={timezone}".format(**locals()))
+
+    def forecast(self, space_name: str, start_date: str, end_date: str):
+        return self.build_route("spaces/{space_name}/cost/cloud_accounts?start_date={start_date}&end_date={end_date}".format(**locals()))
