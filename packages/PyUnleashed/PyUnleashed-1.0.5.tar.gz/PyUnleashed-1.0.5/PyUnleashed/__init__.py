468995a5cb6d7dcfd2985bc1d8e377400f13517e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:08 +0000

    Add package PyUnleashed/1.0.5/PyUnleashed-1.0.5.tar.gz

---
+++ packages/PyUnleashed/PyUnleashed-1.0.5.tar.gz/PyUnleashed-1.0.5/PyUnleashed/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""
+PyUnleashed
+~~~~~~~~~~~~~~~
+A Python wrapper for Unleashed Software API.
+:copyright: (c) 2020 John Horton.
+:license: MIT, see LICENSE for details.
+"""
+
+__title__ = "PyUnleashed"
+__version__ = "1.0.5"
+__author__ = "John Horton"
+__license__ = "MIT"
+
+from .PyUnleashed import API
