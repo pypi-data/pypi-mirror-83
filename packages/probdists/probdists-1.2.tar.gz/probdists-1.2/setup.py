49d5acb46f602bef50eb5f9a7d93df58d3e4c974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:34 +0000

    Add package probdists/1.2/probdists-1.2.tar.gz

---
+++ packages/probdists/probdists-1.2.tar.gz/probdists-1.2/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'probdists',
+    version = '1.2',
+    author = 'Ayush Modi',
+    author_email = 'hot9cups@yahoo.in',
+    description = 'Python Package for modelling Gaussian and Binomial distributions',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hot9cups/probdists",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    zip_safe=False
+)
