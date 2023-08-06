5910f968b43853cebf493f8f588d4239525d68fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:31 +0000

    Add package enerdata/0.24.0rc1/enerdata-0.24.0rc1.tar.gz

---
+++ packages/enerdata/enerdata-0.24.0rc1.tar.gz/enerdata-0.24.0rc1/setup.py
@@ -0,0 +1,22 @@
+import sys
+from setuptools import setup, find_packages
+
+PACKAGES_DATA = {'enerdata': ['profiles/data/*.xlsx', 'profiles/data/*.csv']}
+
+INSTALL_REQUIRES = ['pytz', 'workalendar<8.0.0']
+
+if sys.version_info < (2, 7):
+    INSTALL_REQUIRES += ['backport_collections']
+
+setup(
+    name='enerdata',
+    version='0.24.0-rc.1',
+    packages=find_packages(),
+    url='http://code.gisce.net',
+    license='MIT',
+    author='GISCE-TI, S.L.',
+    author_email='devel@gisce.net',
+    install_requires=INSTALL_REQUIRES,
+    package_data=PACKAGES_DATA,
+    description='A set of energy models'
+)
