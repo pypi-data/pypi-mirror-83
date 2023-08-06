b70eee540429c8f02c3151c5ed85fc8c19630656
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:01 +0000

    Add package funny-package/0.1/funny_package-0.1.tar.gz

---
+++ packages/funny-package/funny_package-0.1.tar.gz/funny_package-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funny_package',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/pawarrchetan/funny_package',
+      author='pawarrchetan',
+      author_email='pawarrchetan@gmail.com',
+      license='MIT',
+      packages=['funny_package'],
+      zip_safe=False)
