826d6697936a6cb0712d6e1ac3422a1498dcfd7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:21 +0000

    Add package ssaw/0.2/ssaw-0.2.tar.gz

---
+++ packages/ssaw/ssaw-0.2.tar.gz/ssaw-0.2/ssaw/__init__.py
@@ -0,0 +1,14 @@
+from . import designer
+from .__about__ import __version__
+from .assignments import AssignmentsApi
+from .export import ExportApi
+from .headquarters import Client
+from .interviews import InterviewsApi
+from .maps import MapsApi
+from .questionnaires import QuestionnairesApi
+from .settings import SettingsApi
+from .users import UsersApi
+
+
+__all__ = ["designer", "__version__", "AssignmentsApi", "ExportApi",
+           "Client", "InterviewsApi", "MapsApi", "QuestionnairesApi", "SettingsApi", "UsersApi", ]
