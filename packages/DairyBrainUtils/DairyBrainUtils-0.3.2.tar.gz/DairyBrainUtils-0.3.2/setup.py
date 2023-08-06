5fad5719bb46fcd982c7ecd28b1f267118b92400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:55 +0000

    Add package DairyBrainUtils/0.3.2/DairyBrainUtils-0.3.2.tar.gz

---
+++ packages/DairyBrainUtils/DairyBrainUtils-0.3.2.tar.gz/DairyBrainUtils-0.3.2/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="DairyBrainUtils",
+    version="0.3.2",
+    author="Rui Pan",
+    author_email="rpan33@wisc.edu",
+    description="A set of functions that interacts with a database. It contains some basic functionalities along with some other Dairy-Brain-specific functionalities.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DairyBrain/AgDH_DairyBrainUtils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
