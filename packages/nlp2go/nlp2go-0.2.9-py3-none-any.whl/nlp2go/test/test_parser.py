076ade88f312ce114597959279323c5898061c84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:11 +0000

    Add package nlp2go/0.2.9/nlp2go-0.2.9-py3-none-any.whl

---
+++ packages/nlp2go/nlp2go-0.2.9-py3-none-any.whl/nlp2go/test/test_parser.py
@@ -0,0 +1,13 @@
+import importlib
+import os
+import unittest
+from nlp2go.parser import *
+
+class TestDataset(unittest.TestCase):
+
+    def testType(self):
+        parser = Parser()
+        parser.inputAutoParser()
+
+parser = Parser()
+parser.inputAutoParser()
