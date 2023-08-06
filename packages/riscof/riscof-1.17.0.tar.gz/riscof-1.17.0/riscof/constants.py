dabb54681ffb4fc443efa3937a712193a11ba1f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:09 +0000

    Add package riscof/1.17.0/riscof-1.17.0.tar.gz

---
+++ packages/riscof/riscof-1.17.0.tar.gz/riscof-1.17.0/riscof/constants.py
@@ -0,0 +1,24 @@
+import os
+
+root = os.path.abspath(os.path.dirname(__file__))
+
+suite = os.path.join(root,"suite/")
+framework_db = os.path.join(root, "framework/database.yaml")
+cwd = os.getcwd()
+work_dir = os.path.join(cwd, "riscof_work/")
+html_template = os.path.join(root, 'Templates/report.html')
+css = os.path.join(root, 'Templates/style.css')
+env = os.path.join(root, suite + "env/")
+config_temp = '''[RISCOF]
+ReferencePlugin={0}
+ReferencePluginPath={1}
+DUTPlugin={2}
+DUTPluginPath={3}
+
+[{2}]
+pluginpath={3}
+ispec={3}/{2}_isa.yaml
+pspec={3}/{2}_platform.yaml
+'''
+
+
---
+++ packages/riscof/riscof-1.17.0.tar.gz/riscof-1.17.0/setup.py
@@ -0,0 +1,55 @@
+import os
+import codecs
+import pip
+from setuptools import setup, find_packages
+
+import riscof
+
+# Base directory of package
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+def read_requires():
+    with open(os.path.join(here, "riscof/requirements.txt"),"r") as reqfile:
+        return reqfile.read().splitlines()
+
+
+#Long Description
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+setup(name="riscof",
+      version=riscof.__version__,
+      description="Risc-V Compliance Framework",
+      long_description=long_description,
+      classifiers=[
+          "Programming Language :: Python :: 3.6",
+          "License :: OSI Approved :: BSD License",
+          "Development Status :: 4 - Beta"
+      ],
+      url='https://gitlab.com/incoresemi/riscof',
+      author='InCore Semiconductors Pvt. Ltd.',
+      author_email='neelgala@incoresemi.com',
+      license='BSD-3-Clause',
+      packages=find_packages(),
+      package_dir={'riscof': 'riscof/'},
+      package_data={
+          'riscof': [
+              'requirements.txt',
+              'suite/env/*', 'suite/rv32i_m/I/*.S', 'suite/rv32i_m/M/*.S',
+              'suite/rv32i_m/C/*.S', 'suite/rv32i_m/privilege/*.S',
+              'suite/rv32i_m/Zicsr/*.S', 'suite/rv32i_m/Zifencei/*.S',
+              'suite/rv64i_m/I/*.S',
+              'framework/database.yaml', 'Templates/report.html',
+              'Templates/style.css', 'Templates/setup/*',
+              'Templates/setup/model/env/*', 'Templates/setup/model/*'
+          ]
+      },
+      install_requires=read_requires(),
+      python_requires=">=3.6.0",
+      entry_points={
+          "console_scripts": ["riscof=riscof.main:execute"],
+      })
