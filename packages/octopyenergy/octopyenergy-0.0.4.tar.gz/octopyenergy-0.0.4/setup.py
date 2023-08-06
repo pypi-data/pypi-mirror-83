80921fe54bb61ac7155d288dcde3e00bb93ba6d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:06 +0000

    Add package octopyenergy/0.0.4/octopyenergy-0.0.4.tar.gz

---
+++ packages/octopyenergy/octopyenergy-0.0.4.tar.gz/octopyenergy-0.0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="octopyenergy",
+    version="0.0.4",
+    author="Ayrton Bourn",
+    author_email="ayrtonbourn@outlook.com",
+    description="Python client for the Octopus energy API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/AyrtonB/Octopy-Energy", 
+    packages=setuptools.find_packages(),
+    package_data={'octopyenergy': ['end_points.json']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+)
