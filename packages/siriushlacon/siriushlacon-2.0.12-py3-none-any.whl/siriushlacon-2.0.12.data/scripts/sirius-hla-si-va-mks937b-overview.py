d37cad3aa6fb45215301f43d1549a861bdc04689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:14 +0000

    Add package siriushlacon/2.0.12/siriushlacon-2.0.12-py3-none-any.whl

---
+++ packages/siriushlacon/siriushlacon-2.0.12-py3-none-any.whl/siriushlacon-2.0.12.data/scripts/sirius-hla-si-va-mks937b-overview.py
@@ -0,0 +1,10 @@
+#!python
+import os
+import subprocess
+from siriushlacon.mks937b.consts import MKS_OVERVIEW
+
+
+os.environ["PYDM_DEFAULT_PROTOCOL"] = "ca://"
+
+macros = '\'{"device": "MKS", "TYPE": "SI", "TITLE": "MKS 937b - SI"}\''
+subprocess.Popen("pydm --hide-nav-bar -m " + macros + " " + MKS_OVERVIEW, shell=True)
