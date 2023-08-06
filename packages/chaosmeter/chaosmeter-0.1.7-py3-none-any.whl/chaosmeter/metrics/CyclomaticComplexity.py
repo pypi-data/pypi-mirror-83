62570a4c2f191ebb93c51191181e12907bce5502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:06:53 +0000

    Add package chaosmeter/0.1.7/chaosmeter-0.1.7-py3-none-any.whl

---
+++ packages/chaosmeter/chaosmeter-0.1.7-py3-none-any.whl/chaosmeter/metrics/CyclomaticComplexity.py
@@ -0,0 +1,17 @@
+from littledarwin.JavaParse import JavaParse
+from antlr4 import *
+
+from chaosmeter.metrics import Metric
+
+
+class CyclomaticComplexity(Metric):
+    instantiable = True
+    name = "Cyclomatic Complexity"
+    abbreviation = "CC"
+
+    def __init__(self, javaParseInstance: JavaParse):
+        super().__init__(javaParseInstance)
+        self.defaultValue = 1
+
+    def calculate(self, tree: RuleContext, sourceCode: str = ""):
+        return self.javaParseInstance.getCyclomaticComplexityAllMethods(tree)
