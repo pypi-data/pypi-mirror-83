4c8d547964549e2bb86d2e10a157b3becf43a626
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:42 +0000

    Add package probability-distros/0.1/probability_distros-0.1.tar.gz

---
+++ packages/probability-distros/probability_distros-0.1.tar.gz/probability_distros-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='probability_distros',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['probability_distros'],
+      author='Uzodinma Ikwuakor',
+      author_email='uzo.ikwuakor@gmail.com',
+      zip_safe=False)
