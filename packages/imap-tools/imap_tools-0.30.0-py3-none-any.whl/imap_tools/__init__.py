8f1bd592b0dd1f52a017251a127a5b1109cea24a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:37 +0000

    Add package imap-tools/0.30.0/imap_tools-0.30.0-py3-none-any.whl

---
+++ packages/imap-tools/imap_tools-0.30.0-py3-none-any.whl/imap_tools/__init__.py
@@ -0,0 +1,7 @@
+from .query import Q, AND, OR, NOT, Header, A, O, N, H
+from .mailbox import BaseMailBox, MailBox, MailBoxUnencrypted
+from .message import MailMessage, Attachment, MailMessageFlags
+from .folder import MailBoxFolderManager, MailBoxFolderStatusOptions
+from .errors import *
+
+__version__ = '0.30.0'
