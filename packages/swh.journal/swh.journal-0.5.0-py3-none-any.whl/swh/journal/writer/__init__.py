8c5c69fcb51a2a36556b41c334e723af17c19444
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:45 +0000

    Add package swh.journal/0.5.0/swh.journal-0.5.0-py3-none-any.whl

---
+++ packages/swh.journal/swh.journal-0.5.0-py3-none-any.whl/swh/journal/writer/__init__.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2019 The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import warnings
+
+
+def get_journal_writer(cls, **kwargs):
+    if "args" in kwargs:
+        warnings.warn(
+            'Explicit "args" key is deprecated, use keys directly instead.',
+            DeprecationWarning,
+        )
+        kwargs = kwargs["args"]
+
+    if cls == "inmemory":  # FIXME: Remove inmemory in due time
+        warnings.warn(
+            "cls = 'inmemory' is deprecated, use 'memory' instead", DeprecationWarning
+        )
+        cls = "memory"
+    if cls == "memory":
+        from .inmemory import InMemoryJournalWriter as JournalWriter
+    elif cls == "kafka":
+        from .kafka import KafkaJournalWriter as JournalWriter
+    else:
+        raise ValueError("Unknown journal writer class `%s`" % cls)
+
+    return JournalWriter(**kwargs)
---
+++ packages/swh.journal/swh.journal-0.5.0-py3-none-any.whl/swh/journal/writer/inmemory.py
@@ -0,0 +1,38 @@
+# Copyright (C) 2019 The Software Heritage developers
+# See the AUTHORS file at the top-level directory of this distribution
+# License: GNU General Public License version 3, or any later version
+# See top-level LICENSE file for more information
+
+import logging
+from multiprocessing import Manager
+from typing import List
+
+from swh.journal.serializers import ModelObject
+from swh.model.model import BaseModel
+
+logger = logging.getLogger(__name__)
+
+
+class InMemoryJournalWriter:
+    def __init__(self):
+        # Share the list of objects across processes, for RemoteAPI tests.
+        self.manager = Manager()
+        self.objects = self.manager.list()
+        self.privileged_objects = self.manager.list()
+
+    def write_addition(
+        self, object_type: str, object_: ModelObject, privileged: bool = False
+    ) -> None:
+        assert isinstance(object_, BaseModel)
+        if privileged:
+            self.privileged_objects.append((object_type, object_))
+        else:
+            self.objects.append((object_type, object_))
+
+    write_update = write_addition
+
+    def write_additions(
+        self, object_type: str, objects: List[ModelObject], privileged: bool = False
+    ) -> None:
+        for object_ in objects:
+            self.write_addition(object_type, object_, privileged)
