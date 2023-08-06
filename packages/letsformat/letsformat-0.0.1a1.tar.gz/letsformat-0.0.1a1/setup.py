81356add033a340568a5b6e16521131fe51c17d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:50 +0000

    Add package letsformat/0.0.1a1/letsformat-0.0.1a1.tar.gz

---
+++ packages/letsformat/letsformat-0.0.1a1.tar.gz/letsformat-0.0.1a1/setup.py
@@ -0,0 +1,29 @@
+
+from setuptools import setup, find_packages
+from letsformat.core.version import get_version
+
+VERSION = get_version()
+
+f = open('README.md', 'r')
+LONG_DESCRIPTION = f.read()
+f.close()
+
+setup(
+    name='letsformat',
+    version=VERSION,
+    description='Simple File Format Converter',
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type='text/markdown',
+    author='Matthew Holden',
+    author_email='matthewholden01@gmail.com',
+    url='https://github.com/matthewholden01/letsformat',
+    license='unlicensed',
+    packages=find_packages(exclude=['ez_setup', 'tests*']),
+    package_data={'letsformat': ['templates/*']},
+    include_package_data=True,
+    entry_points="""
+        [console_scripts]
+        letsformat = letsformat.main:main
+    """,
+    install_requires=['exifread']
+)
