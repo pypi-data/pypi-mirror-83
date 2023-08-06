effeb8245759593f48f216084d353fab94818167
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:02 +0000

    Add package htmhelloworld/0.0.1/htmhelloworld-0.0.1.tar.gz

---
+++ packages/htmhelloworld/htmhelloworld-0.0.1.tar.gz/htmhelloworld-0.0.1/README.md
@@ -0,0 +1,4 @@
+'''
+# Hello World
+
+This is a toy package demonstrating how to publish a python module to PyPI
---
+++ packages/htmhelloworld/htmhelloworld-0.0.1.tar.gz/htmhelloworld-0.0.1/src/helloworld.py
@@ -0,0 +1,9 @@
+
+def say_hello(name = None):
+	if name is None:
+		return "Hello, World!"
+	else:
+		return f"Hello, {name}!"
+
+def add_num(num1, num2):
+	return num1+num2
