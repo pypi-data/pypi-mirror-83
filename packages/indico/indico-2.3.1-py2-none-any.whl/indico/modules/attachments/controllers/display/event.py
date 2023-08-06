4dce93980f7132b758f8c7134ccb8eb0b6b255cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:46 +0000

    Add package indico/2.3.1/indico-2.3.1-py2-none-any.whl

---
+++ packages/indico/indico-2.3.1-py2-none-any.whl/indico/modules/attachments/controllers/display/event.py
@@ -0,0 +1,55 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2020 CERN
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see the
+# LICENSE file for more details.
+
+from __future__ import unicode_literals
+
+from flask import redirect, request, session
+from werkzeug.exceptions import Forbidden
+
+from indico.modules.attachments.controllers.display.base import DownloadAttachmentMixin
+from indico.modules.attachments.controllers.event_package import AttachmentPackageMixin
+from indico.modules.attachments.controllers.util import SpecificFolderMixin
+from indico.modules.attachments.views import (WPEventFolderDisplay, WPPackageEventAttachmentsDisplay,
+                                              WPPackageEventAttachmentsDisplayConference)
+from indico.modules.events.controllers.base import RHDisplayEventBase
+from indico.modules.events.models.events import EventType
+
+
+class RHDownloadEventAttachment(DownloadAttachmentMixin, RHDisplayEventBase):
+    def _process_args(self):
+        RHDisplayEventBase._process_args(self)
+        DownloadAttachmentMixin._process_args(self)
+
+    def _check_access(self):
+        RHDisplayEventBase._check_access(self)
+        DownloadAttachmentMixin._check_access(self)
+
+
+class RHListEventAttachmentFolder(SpecificFolderMixin, RHDisplayEventBase):
+    def _process_args(self):
+        RHDisplayEventBase._process_args(self)
+        SpecificFolderMixin._process_args(self)
+
+    def _check_access(self):
+        RHDisplayEventBase._check_access(self)
+        if not self.folder.can_access(session.user):
+            raise Forbidden
+
+    def _process(self):
+        if request.args.get('redirect_if_single') == '1' and len(self.folder.attachments) == 1:
+            return redirect(self.folder.attachments[0].download_url)
+
+        return WPEventFolderDisplay.render_template('folder.html', self.event, folder=self.folder)
+
+
+class RHPackageEventAttachmentsDisplay(AttachmentPackageMixin, RHDisplayEventBase):
+    @property
+    def wp(self):
+        if self.event.type_ == EventType.conference:
+            return WPPackageEventAttachmentsDisplayConference
+        else:
+            return WPPackageEventAttachmentsDisplay
---
+++ packages/indico/indico-2.3.1-py2-none-any.whl/indico/modules/events/persons/placeholders.py
@@ -0,0 +1,73 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2020 CERN
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see the
+# LICENSE file for more details.
+
+from __future__ import unicode_literals
+
+from markupsafe import Markup
+
+from indico.modules.auth.util import url_for_register
+from indico.util.i18n import _
+from indico.util.placeholders import Placeholder
+
+
+# XXX: `person` may be either an `EventPerson` or a `User`
+
+
+class FirstNamePlaceholder(Placeholder):
+    name = 'first_name'
+    description = _("First name of the person")
+
+    @classmethod
+    def render(cls, person, event, **kwargs):
+        return person.first_name
+
+
+class LastNamePlaceholder(Placeholder):
+    name = 'last_name'
+    description = _("Last name of the person")
+
+    @classmethod
+    def render(cls, person, event, **kwargs):
+        return person.last_name
+
+
+class EmailPlaceholder(Placeholder):
+    name = 'email'
+    description = _("Email of the person")
+
+    @classmethod
+    def render(cls, person, event, **kwargs):
+        return person.email
+
+
+class EventTitlePlaceholder(Placeholder):
+    name = 'event_title'
+    description = _("The title of the event")
+
+    @classmethod
+    def render(cls, person, event, **kwargs):
+        return event.title
+
+
+class EventLinkPlaceholder(Placeholder):
+    name = 'event_link'
+    description = _("Link to the event")
+
+    @classmethod
+    def render(cls, person, event, **kwargs):
+        return Markup('<a href="{url}" title="{title}">{url}</a>').format(url=event.short_external_url,
+                                                                          title=event.title)
+
+
+class RegisterLinkPlaceholder(Placeholder):
+    name = 'register_link'
+    description = _("The link for the registration page")
+
+    @classmethod
+    def render(cls, person, event, **kwargs):
+        url = url_for_register(event.url, email=person.email)
+        return Markup('<a href="{url}">{url}</a>').format(url=url)
