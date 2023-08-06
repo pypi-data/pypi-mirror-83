5e7f20e3336a62fbd72f8f6e6ca425625109a574
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:56 +0000

    Add package notekeras/0.6.11/notekeras-0.6.11.tar.gz

---
+++ packages/notekeras/notekeras-0.6.11.tar.gz/notekeras-0.6.11/setup.py
@@ -0,0 +1,23 @@
+import sys
+from os import path
+
+from setuptools import setup, find_packages
+
+from notebuild.tool import get_version
+
+version_path = path.join(path.abspath(path.dirname(__file__)), 'script/__version__.md')
+
+version = get_version(sys.argv, version_path, step=16)
+
+install_requires = ['tensorflow', 'numpy', 'scipy', 'opencv-python', 'pillow', 'tqdm']
+
+setup(name='notekeras',
+      version=version,
+      description='notekeras',
+      author='niuliangtao',
+      author_email='1007530194@qq.com',
+      url='https://github.com/1007530194',
+
+      packages=find_packages(),
+      install_requires=install_requires
+      )
