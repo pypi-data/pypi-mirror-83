043fa802ceded80ad25c4b32046d19d01a4a745b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:49:35 +0000

    Add package chemsys/1.0.2/ChemSys-1.0.2.tar.gz

---
+++ packages/chemsys/ChemSys-1.0.2.tar.gz/ChemSys-1.0.2/ChemSys/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 21 18:48:07 2020
+
+Github: https://github.com/tjczec01
+
+@author: Travis J Czechorski
+
+E-mail: tjczec01@gmail.com
+
+"""
+
+from ChemSys import * 
+import pkg_resources
+from pkgutil import extend_path
+pkg_resources.declare_namespace(__name__)
