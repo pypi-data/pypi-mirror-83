1f9250825c6d3388a50c96663b32c37a1c675eb4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:01 +0000

    Add package insta-scrape/0.7.1/insta-scrape-0.7.1.tar.gz

---
+++ packages/insta-scrape/insta-scrape-0.7.1.tar.gz/insta-scrape-0.7.1/instascrape/scrapers/post.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+import datetime
+from typing import List
+
+from instascrape.core._mappings import _PostMapping
+from instascrape.core._static_scraper import _StaticHtmlScraper
+from instascrape.scrapers.json_tools import parse_json_from_mapping
+
+
+class Post(_StaticHtmlScraper):
+    """
+    Scraper for an Instagram post page
+
+    Methods
+    -------
+    from_shortcode(shortcode: str) -> Post
+        Factory method that returns a Post object from a shortcode
+    """
+
+    _Mapping = _PostMapping
+
+    def load(self, keys: List[str] = [], exclude: List[str] = []):
+        super().load(keys=keys)
+        self.upload_date = datetime.datetime.fromtimestamp(self.upload_date)
+        self.tagged_users = self._parse_tagged_users(self.json_dict)
+
+    def to_json(self, fp: str):
+        # have to convert to serializable format
+        self.upload_date = datetime.datetime.timestamp(self.upload_date)
+        super().to_json(fp=fp)
+        self.upload_date = datetime.datetime.fromtimestamp(self.upload_date)
+
+    def to_csv(self, fp: str):
+        # have to convert to serializable format
+        self.upload_date = datetime.datetime.timestamp(self.upload_date)
+        super().to_csv(fp=fp)
+        self.upload_date = datetime.datetime.fromtimestamp(self.upload_date)
+
+    @classmethod
+    def load_from_mapping(self, json_dict, map_dict):
+        data_dict = parse_json_from_mapping(json_dict, map_dict)
+        post = Post.from_shortcode(data_dict["shortcode"])
+        for key, val in data_dict.items():
+            setattr(post, key, val)
+        # TODO: Bad encapsulation, figure a better way of handling timestamp
+        post.upload_date = datetime.datetime.fromtimestamp(post.upload_date)
+        return post
+
+    def _parse_tagged_users(self, json_dict) -> List[str]:
+        """Parse the tagged users from JSON dict containing the tagged users"""
+        tagged_arr = json_dict['entry_data']['PostPage'][0]['graphql']['shortcode_media']['edge_media_to_tagged_user']['edges']
+        return [node['node']['user']['username'] for node in tagged_arr]
+
+
+    @classmethod
+    def from_shortcode(cls, shortcode: str) -> Post:
+        url = f"https://www.instagram.com/p/{shortcode}/"
+        return cls(url, name=shortcode)
