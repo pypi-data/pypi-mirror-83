b4668e40241512984a39db3ecbf760217a6a7c76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:52 +0000

    Add package mock-firestore/0.7.2/mock-firestore-0.7.2.tar.gz

---
+++ packages/mock-firestore/mock-firestore-0.7.2.tar.gz/mock-firestore-0.7.2/mockfirestore/document.py
@@ -0,0 +1,78 @@
+from copy import deepcopy
+from functools import reduce
+import operator
+from typing import List, Dict, Any
+from mockfirestore import NotFound
+from mockfirestore._helpers import Timestamp, Document, Store, get_by_path, set_by_path, delete_by_path
+
+
+class DocumentSnapshot:
+    def __init__(self, reference: 'DocumentReference', data: Document) -> None:
+        self.reference = reference
+        self._doc = deepcopy(data)
+
+    @property
+    def id(self):
+        return self.reference.id
+
+    @property
+    def exists(self) -> bool:
+        return self._doc != {}
+
+    def to_dict(self) -> Document:
+        return self._doc
+
+    @property
+    def create_time(self) -> Timestamp:
+        timestamp = Timestamp.from_now()
+        return timestamp
+
+    def get(self, field_path: str) -> Any:
+        if not self.exists:
+            return None
+        else:
+            return reduce(operator.getitem, field_path.split('.'), self._doc)
+
+    def _get_by_field_path(self, field_path: str) -> Any:
+        try:
+            return self.get(field_path)
+        except KeyError:
+            return None
+
+
+class DocumentReference:
+    def __init__(self, data: Store, path: List[str],
+                 parent: 'CollectionReference') -> None:
+        self._data = data
+        self._path = path
+        self.parent = parent
+
+    @property
+    def id(self):
+        return self._path[-1]
+
+    def get(self) -> DocumentSnapshot:
+        return DocumentSnapshot(self, get_by_path(self._data, self._path))
+
+    def delete(self):
+        delete_by_path(self._data, self._path)
+
+    def set(self, data: Dict, merge=False):
+        if merge:
+            self.update(deepcopy(data))
+        else:
+            set_by_path(self._data, self._path, deepcopy(data))
+
+    def update(self, data: Dict[str, Any]):
+        document = get_by_path(self._data, self._path)
+        if document == {}:
+            raise NotFound('No document to update: {}'.format(self._path))
+        document.update(deepcopy(data))
+
+    def collection(self, name) -> 'CollectionReference':
+        from mockfirestore.collection import CollectionReference
+        document = get_by_path(self._data, self._path)
+        new_path = self._path + [name]
+        if name not in document:
+            set_by_path(self._data, new_path, {})
+        return CollectionReference(self._data, new_path, parent=self)
---
+++ packages/mock-firestore/mock-firestore-0.7.2.tar.gz/mock-firestore-0.7.2/tests/test_document_snapshot.py
@@ -0,0 +1,90 @@
+from unittest import TestCase
+
+from mockfirestore import MockFirestore
+
+
+class TestDocumentSnapshot(TestCase):
+    def test_documentSnapshot_toDict(self):
+        fs = MockFirestore()
+        fs._data = {'foo': {
+            'first': {'id': 1}
+        }}
+        doc = fs.collection('foo').document('first').get()
+        self.assertEqual({'id': 1}, doc.to_dict())
+
+    def test_documentSnapshot_toDict_isolation(self):
+        fs = MockFirestore()
+        fs._data = {'foo': {
+            'first': {'id': 1}
+        }}
+        doc_dict = fs.collection('foo').document('first').get().to_dict()
+        fs._data['foo']['first']['id'] = 2
+        self.assertEqual({'id': 1}, doc_dict)
+
+    def test_documentSnapshot_exists(self):
+        fs = MockFirestore()
+        fs._data = {'foo': {
+            'first': {'id': 1}
+        }}
+        doc = fs.collection('foo').document('first').get()
+        self.assertTrue(doc.exists)
+
+    def test_documentSnapshot_exists_documentDoesNotExist(self):
+        fs = MockFirestore()
+        fs._data = {'foo': {
+            'first': {'id': 1}
+        }}
+        doc = fs.collection('foo').document('second').get()
+        self.assertFalse(doc.exists)
+
+    def test_documentSnapshot_reference(self):
+        fs = MockFirestore()
+        fs._data = {'foo': {
+            'first': {'id': 1}
+        }}
+        doc_ref = fs.collection('foo').document('second')
+        doc_snapshot = doc_ref.get()
+        self.assertIs(doc_ref, doc_snapshot.reference)
+
+    def test_documentSnapshot_get_by_existing_field_path(self):
+        fs = MockFirestore()
+        fs._data = {'foo': {
+            'first': {'id': 1, 'contact': {
+                'email': 'email@test.com'
+            }}
+        }}
+        doc = fs.collection('foo').document('first').get()
+        self.assertEqual(doc.get('contact.email'), 'email@test.com')
+
+    def test_documentSnapshot_get_by_non_existing_field_path(self):
+        fs = MockFirestore()
+        fs._data = {'foo': {
+            'first': {'id': 1, 'contact': {
+                'email': 'email@test.com'
+            }}
+        }}
+        doc = fs.collection('foo').document('first').get()
+        with self.assertRaises(KeyError):
+            doc.get('contact.phone')
+
+    def test_documentSnapshot_get_in_an_non_existing_document(self):
+        fs = MockFirestore()
+        fs._data = {'foo': {
+            'first': {'id': 1, 'contact': {
+                'email': 'email@test.com'
+            }}
+        }}
+        doc = fs.collection('foo').document('second').get()
+        self.assertIsNone(doc.get('contact.email'))
+
+    def test_documentSnapshot_get_returns_a_copy_of_the_data_stored(self):
+        fs = MockFirestore()
+        fs._data = {'foo': {
+            'first': {'id': 1, 'contact': {
+                'email': 'email@test.com'
+            }}
+        }}
+        doc = fs.collection('foo').document('first').get()
+        self.assertIsNot(
+            doc.get('contact'),fs._data['foo']['first']['contact']
+        )
