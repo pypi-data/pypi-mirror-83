e68e2efefd9a4b42df1cdb446f97b6562ff9e264
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:50:44 +0000

    Add package thorunimore/1.0.12/thorunimore-1.0.12-py3-none-any.whl

---
+++ packages/thorunimore/thorunimore-1.0.12-py3-none-any.whl/thorunimore/deeplinking.py
@@ -0,0 +1,19 @@
+import itsdangerous
+from royalnet.typing import *
+
+
+class DeepLinking:
+    """A helper class to pass secure information between Telegram and Flask via Telegram Deep Linking."""
+
+    def __init__(self, secret_key: str, namespace: str = "t"):
+        self.serializer = itsdangerous.URLSafeSerializer(secret_key=secret_key, salt=namespace)
+
+    def encode(self, value: Any) -> str:
+        signed = self.serializer.dumps(value)
+        signed = signed.replace("_", "__").replace(".", "_d")
+        return signed
+
+    def decode(self, signed: str) -> Any:
+        signed = signed.replace("_d", ".").replace("__", "_")
+        value = self.serializer.loads(signed)
+        return value
