3dd9e21268f09f4cef11dff6a6dbddf62eebbd85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:26 +0000

    Add package fcc-distributions/0.1/fcc_distributions-0.1.tar.gz

---
+++ packages/fcc-distributions/fcc_distributions-0.1.tar.gz/fcc_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='fcc_distributions',
+      version='0.1',
+      description='Gaussian  and binomial distributions',
+      packages=['fcc_distributions'],
+      author='Francisco Chavez',
+      author_email='something@else.com',
+      zip_safe=False)
