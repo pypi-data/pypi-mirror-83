d9512c1a208fc30617e4fd790d869a7b9fcbd1db
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:13 +0000

    Add package distribs-probability/0.1.1/distribs_probability-0.1.1.tar.gz

---
+++ packages/distribs-probability/distribs_probability-0.1.1.tar.gz/distribs_probability-0.1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distribs_probability',
+      version='0.1.1',
+      description='All probability distributions',
+      packages=['distribs_probability'],
+      zip_safe=False)
