520200f86e600b45fdb25e2756f95184d31c3597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:47 +0000

    Add package compressedlogger/0.3.3/compressedlogger-0.3.3-py2.py3-none-any.whl

---
+++ packages/compressedlogger/compressedlogger-0.3.3-py2.py3-none-any.whl/compressedlogger-0.3.3.dist-info/AUTHORS.rst
@@ -0,0 +1,13 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Kai Friedrich
+
+Contributors
+------------
+
+None yet. Why not be the first?
---
+++ packages/compressedlogger/compressedlogger-0.3.3-py2.py3-none-any.whl/compressedlogger/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for CompressedLogger."""
+
+__author__ = """Kai Friedrich"""
+__email__ = 'kai.friedrich@teleste.com'
+__version__ = '0.1.0'
+from .compressedlogger import CompressedLogger
