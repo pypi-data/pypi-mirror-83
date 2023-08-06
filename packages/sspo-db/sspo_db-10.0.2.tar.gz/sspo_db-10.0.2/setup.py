bac82c4e9d2de5650e940f070f59d81dbe365db1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:23 +0000

    Add package sspo-db/10.0.2/sspo_db-10.0.2.tar.gz

---
+++ packages/sspo-db/sspo_db-10.0.2.tar.gz/sspo_db-10.0.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+setup(
+    name='sspo_db',  # Required
+    version="10.0.2",  # Required
+    author="Paulo Sergio dos Santo Junior",
+    author_email="paulossjuniort@gmail.com",
+    description="A lib to access SSPO DB",
+ 
+    packages=find_packages(),
+    
+    install_requires=[
+        'SQLAlchemy', 'SQLAlchemy-Utils', 'factory-boy', 'SQLAlchemy-serializer'
+    ],
+
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+    setup_requires=['wheel'],
+    
+)
---
+++ packages/sspo-db/sspo_db-10.0.2.tar.gz/sspo_db-10.0.2/sspo_db/model/organization/__init__.py
@@ -0,0 +1,2 @@
+from sspo_db.model.process.models import ScrumProject
+from sspo_db.model.core.models import Configuration
