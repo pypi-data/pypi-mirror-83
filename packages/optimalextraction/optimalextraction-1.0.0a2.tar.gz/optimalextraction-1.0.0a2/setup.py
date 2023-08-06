951c2d808bd7a43b52673e55ff342003ac02f1cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:40 +0000

    Add package optimalextraction/1.0.0a2/optimalextraction-1.0.0a2.tar.gz

---
+++ packages/optimalextraction/optimalextraction-1.0.0a2.tar.gz/optimalextraction-1.0.0a2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="optimalextraction",
+    version="1.0.0a2",
+    author="Kornpob Bhirombhakdi",
+    author_email="kbhirombhakdi@stsci.edu",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/bkornpob/optimalextraction",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+        ,"License :: OSI Approved :: MIT License"
+        ,"Operating System :: OS Independent"
+    ],
+    python_requires='>=3.'
+)
