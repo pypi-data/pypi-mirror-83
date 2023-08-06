e4bac134fe249cdb9594ce130eec6ce549db3f27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:39 +0000

    Add package google-api-python-client-stubs/0.3.0/google_api_python_client_stubs-0.3.0-py2.py3-none-any.whl

---
+++ packages/google-api-python-client-stubs/google_api_python_client_stubs-0.3.0-py2.py3-none-any.whl/googleapiclient-stubs/_apis/webfonts/v1/resources.pyi
@@ -0,0 +1,28 @@
+import typing
+
+import httplib2  # type: ignore
+import typing_extensions
+
+import googleapiclient.discovery
+import googleapiclient.http  # type: ignore
+
+from .schemas import *
+@typing.type_check_only
+class WebfontsResource(googleapiclient.discovery.Resource):
+    @typing.type_check_only
+    class WebfontsResource(googleapiclient.discovery.Resource):
+        def list(
+            self,
+            *,
+            sort: typing_extensions.Literal[
+                "SORT_UNDEFINED", "ALPHA", "DATE", "POPULARITY", "STYLE", "TRENDING"
+            ] = ...,
+            **kwargs: typing.Any
+        ) -> WebfontListHttpRequest: ...
+    def webfonts(self) -> WebfontsResource: ...
+
+@typing.type_check_only
+class WebfontListHttpRequest(googleapiclient.http.HttpRequest):
+    def execute(
+        self, http: typing.Optional[httplib2.Http] = ..., num_retries: int = ...
+    ) -> WebfontList: ...
