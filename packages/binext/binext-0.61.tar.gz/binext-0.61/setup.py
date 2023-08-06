2787739e22eebdb2d1513f65d028e89c6c471b93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:29 +0000

    Add package binext/0.61/binext-0.61.tar.gz

---
+++ packages/binext/binext-0.61.tar.gz/binext-0.61/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="binext",
+    version="0.61",
+    author="John M. Shea",
+    author_email="jshea@ece.ufl.edu",
+    description="Opinionated module for working with binary Galois extension fields",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jmshea/coding/tree/main/binext",
+    packages=['binext'],
+    install_requires=[
+          'numpy',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"
+    ]
+)
