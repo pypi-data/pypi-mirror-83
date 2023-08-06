2924dd519d6c58e1c824bc9305148152736cd449
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:09:08 +0000

    Add package exchange-interface/0.0.1/exchange_interface-0.0.1.tar.gz

---
+++ packages/exchange-interface/exchange_interface-0.0.1.tar.gz/exchange_interface-0.0.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+from os import path
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+packages = ['exchange_interface']
+
+setup(
+    name="exchange_interface",
+
+    version="0.0.1",
+    # 0.0.1 - added error message to Disconnected ConnectionStatus
+
+    packages=packages,
+    install_requires=[
+        'calendar_base',
+        'requests',
+    ],
+
+    author="Grant miller",
+    author_email="grant@grant-miller.com",
+    description="An interface to Microsoft Exchange Web Services (EWS). Allows the user to manipulate the outlook calendar events.",
+    long_description=long_description,
+    license="PSF",
+    keywords="grant miller microsoft office365 ews",
+    url="https://github.com/GrantGMiller/exchange_interface",  # project home page, if any
+    project_urls={
+        "Source Code": "https://github.com/GrantGMiller/exchange_interface",
+    }
+
+)
