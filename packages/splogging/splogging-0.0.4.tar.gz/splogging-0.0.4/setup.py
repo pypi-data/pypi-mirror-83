f70f738d8f19f15aee2b3f2dd76efc71420b64a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:20 +0000

    Add package splogging/0.0.4/splogging-0.0.4.tar.gz

---
+++ packages/splogging/splogging-0.0.4.tar.gz/splogging-0.0.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name="splogging",
+    version="0.0.4",
+    author="Stevie Py",
+    author_email="st3v13py@gmail.com",
+    description="Ease the creation of a logger",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/steviepy/splogging",
+    packages=setuptools.find_packages(),
+    package_data={"splogging": ["py.typed"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
