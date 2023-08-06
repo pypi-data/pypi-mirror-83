90e9085d05027b04fd2d03a6ba833088b86715ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:47 +0000

    Add package spotlight/2.1.0/spotlight-2.1.0.tar.gz

---
+++ packages/spotlight/spotlight-2.1.0.tar.gz/spotlight-2.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from collections import OrderedDict
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="spotlight",
+    version="2.1.0",
+    author="Michiel Doesburg",
+    author_email="michiel@moddix.com",
+    description="Laravel style data validation for Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="spotlight validation validate",
+    url="https://github.com/mdoesburg/spotlight",
+    project_urls=OrderedDict(
+        (
+            ("Documentation", "https://github.com/mdoesburg/spotlight"),
+            ("Code", "https://github.com/mdoesburg/spotlight"),
+        )
+    ),
+    license="MIT",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    install_requires=[],
+)
