7f308030dbb2310694dcfc160a77cec9c6de8285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:18 +0000

    Add package underthesea/1.2.0a2/underthesea-1.2.0a2.tar.gz

---
+++ packages/underthesea/underthesea-1.2.0a2.tar.gz/underthesea-1.2.0a2/underthesea/models.py
@@ -0,0 +1,30 @@
+REPO = {
+    "TC_GENERAL": {
+        "cache_dir": "models",
+        "type": "Classification",
+        "license": "Open",
+        "year": "2019",
+        "model_path": "tc_general"
+    },
+    "TC_BANK": {
+        "cache_dir": "models",
+        "type": "Classification",
+        "license": "Open",
+        "year": "2019",
+        "model_path": "tc_bank"
+    },
+    "SA_GENERAL": {
+        "cache_dir": "models",
+        "type": "Sentiment",
+        "license": "Open",
+        "year": "2019",
+        "model_path": "sa_general"
+    },
+    "SA_BANK": {
+        "cache_dir": "models",
+        "type": "Sentiment",
+        "license": "Open",
+        "year": "2019",
+        "model_path": "sa_bank"
+    },
+}
