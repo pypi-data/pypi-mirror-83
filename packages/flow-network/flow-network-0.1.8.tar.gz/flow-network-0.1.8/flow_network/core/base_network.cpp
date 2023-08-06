88e82f29d7f235fa2e7753aea6b2e13c9f8f1a62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:23 +0000

    Add package flow-network/0.1.8/flow-network-0.1.8.tar.gz

---
+++ packages/flow-network/flow-network-0.1.8.tar.gz/flow-network-0.1.8/flow_network/core/base_network.cpp
@@ -0,0 +1,10 @@
+#include "base_network.h"
+
+namespace flow_network {
+    BaseNetwork::BaseNetwork(int n) : dist(new int[n]), n(n), graph(n) {}
+
+    BaseNetwork::~BaseNetwork() {
+        delete[] dist;
+    }
+}
+
