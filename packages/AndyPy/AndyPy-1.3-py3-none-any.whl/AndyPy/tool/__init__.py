64de8e8f257f9b175ef70818bac3af3d7967180b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:01 +0000

    Add package AndyPy/1.3/AndyPy-1.3-py3-none-any.whl

---
+++ packages/AndyPy/AndyPy-1.3-py3-none-any.whl/AndyPy/tool/__init__.py
@@ -0,0 +1,7 @@
+print('AndyPy.tool模块导入成功！---Andy')
+
+import AndyPy.tool.cv
+import AndyPy.tool.excel2csv
+import AndyPy.tool.pics2pdf
+
+__all__ = ["cv","excel2csv","pics2pdf"]
---
+++ packages/AndyPy/AndyPy-1.3-py3-none-any.whl/AndyPy/tool/cv.py
@@ -0,0 +1,21 @@
+import cv2
+from matplotlib import pyplot as plt
+
+__all__ = ["imshow","subplot"]
+
+def imshow(img,title='',show=True):
+    '''显示图片 @param img obj of cv2.read(file).'''
+    plt.rcParams['font.sans-serif'] = ['SimHei']
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    plt.xticks([])
+    plt.yticks([])
+    plt.title(title)
+    plt.imshow(img_rgb)
+    if show: plt.show()
+    
+def subplot(M=11,imgs=[],titles=[]):
+    '''显示多张图片，M为显示图片的矩阵'''
+    for i,img in enumerate(imgs):
+        plt.subplot(M*10+i+1)
+        imshow(img,titles[i],show=False) if i<len(titles) else imshow(img,show=False)
+    plt.show()
