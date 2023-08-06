a9583ab8e84c85157b14808f236e410728105f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:32 +0000

    Add package tap-amplitude/1.1.0/tap-amplitude-1.1.0.tar.gz

---
+++ packages/tap-amplitude/tap-amplitude-1.1.0.tar.gz/tap-amplitude-1.1.0/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='tap-amplitude',
+      version='1.1.0',
+      description='Singer.io tap for extracting data from Amplitude via Snowflake',
+      author='Stitch',
+      url='https://singer.io',
+      classifiers=['Programming Language :: Python :: 3 :: Only'],
+      py_modules=['tap_amplitude'],
+      install_requires=[
+          'snowflake-connector-python==1.6.3',
+          # https://github.com/snowflakedb/snowflake-connector-python/pull/208
+          'asn1crypto<1.0.0',
+          'attrs==16.3.0',
+          'pendulum==1.2.0',
+          'pytz==2018.4',
+          'singer-python==5.1.5',
+          'backoff==1.3.2',
+          'nose==1.3.7'
+      ],
+      entry_points='''
+          [console_scripts]
+          tap-amplitude=tap_amplitude:main
+      ''',
+      packages=['tap_amplitude', 'tap_amplitude.sync_strategies'],
+)
