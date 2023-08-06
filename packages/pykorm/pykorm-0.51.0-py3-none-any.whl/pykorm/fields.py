ac6eecb431b1f077d2887a1583388b43a78eba05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:27 +0000

    Add package pykorm/0.51.0/pykorm-0.51.0-py3-none-any.whl

---
+++ packages/pykorm/pykorm-0.51.0-py3-none-any.whl/pykorm/fields.py
@@ -0,0 +1,73 @@
+from typing import Dict
+
+import dpath.util
+
+
+class DataField:
+    _root_dict_key = []
+    path: str
+    default: object
+    readonly: bool = False
+    required: bool = False
+    allow_empty: bool = False
+
+    def __init__(self, path: str, default=None, required: bool = False,
+                 readonly: bool = False, allow_empty: bool = False):
+        if type(path) == str:
+            self.path = [path]
+        elif type(path) == list:
+            self.path = path
+        else:
+            raise Exception('param path must be a string or list')
+
+        self.default = default
+        self.required = required
+        self.readonly = readonly
+        self.allow_empty = allow_empty
+
+    def __repr__(self) -> str:
+        cls_name = self.__class__.__name__
+        return f'<pykorm/{cls_name}: {self.path}>'
+
+    @property
+    def fullpath(self):
+        return self._root_dict_key + self.path
+
+    @staticmethod
+    def preprocessor(x):
+        return x
+
+    @staticmethod
+    def postprocessor(x):
+        return x
+
+    def to_dict(self, value: str) -> Dict:
+        result = {}
+        if value is None and self.default is not None:
+            value = self.default
+        if value is None and (not self.allow_empty):
+            return {}
+        if value is not None:
+            value = self.preprocessor(value)
+
+        return dpath.util.new(result, self.fullpath, value)
+
+    def get_data(self, k8s_dict: Dict):
+        data = dpath.util.get(k8s_dict, self.fullpath, default=self.default)
+        return self.postprocessor(data) if data is not None else data
+
+
+class Spec(DataField):
+    _root_dict_key = ['spec']
+
+
+class Metadata(DataField):
+    _root_dict_key = ['metadata']
+
+
+class MetadataAnnotation(DataField):
+    _root_dict_key = ['metadata', 'annotations']
+
+
+class Status(DataField):
+    _root_dict_key = 'status'
