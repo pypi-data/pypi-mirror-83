74bb9dc1f69c56c9a620d68fec3a4866d8b1be7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:29 +0000

    Add package customner/0.0.2/customner-0.0.2.tar.gz

---
+++ packages/customner/customner-0.0.2.tar.gz/customner-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(
+      name='customner',
+      version='0.0.2',
+      description='Custom Name Entity Recognition Data Preparation Library for Medical Dataset',
+      py_modules=["customner"],
+      package_dir={'':'Package'},
+      )
+
