c2aff8a653b13673a65b028e5918956b2a48ee09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:10 +0000

    Add package nni/1.9/nni-1.9-py3-none-manylinux1_x86_64.whl

---
+++ packages/nni/nni-1.9-py3-none-manylinux1_x86_64.whl/nni-1.9.data/data/nni/static/precache-manifest.c53f8439a517ade7f38b59db9aa1ca06.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8b2caae008927219dc87f383b39f7c2",
+    "url": "/editor.worker.js"
+  },
+  {
+    "revision": "8356d56d3f05da1bfb5938f8d90f5ade",
+    "url": "/index.html"
+  },
+  {
+    "revision": "742e3ac827e9d006ecbfe39f281fc8a6",
+    "url": "/json.worker.js"
+  },
+  {
+    "revision": "431ea4569a6dd72aa88a",
+    "url": "/static/css/3.cd69d823.chunk.css"
+  },
+  {
+    "revision": "c22b9dc9ed9978224a00",
+    "url": "/static/css/4.5c750b80.chunk.css"
+  },
+  {
+    "revision": "e95831f42b45fea606cd",
+    "url": "/static/css/5.ab143417.chunk.css"
+  },
+  {
+    "revision": "f8a57817ac5cfe6ff780",
+    "url": "/static/css/6.e8de4b18.chunk.css"
+  },
+  {
+    "revision": "78b8c54cd398c5314ad8",
+    "url": "/static/css/main.0660afb8.chunk.css"
+  },
+  {
+    "revision": "c37c56fc9adb2c63d7e6",
+    "url": "/static/js/0.2beb8eb4.chunk.js"
+  },
+  {
+    "revision": "431ea4569a6dd72aa88a",
+    "url": "/static/js/3.8ea55e32.chunk.js"
+  },
+  {
+    "revision": "c22b9dc9ed9978224a00",
+    "url": "/static/js/4.9e050c69.chunk.js"
+  },
+  {
+    "revision": "e95831f42b45fea606cd",
+    "url": "/static/js/5.fa494792.chunk.js"
+  },
+  {
+    "revision": "f8a57817ac5cfe6ff780",
+    "url": "/static/js/6.3be2147b.chunk.js"
+  },
+  {
+    "revision": "6f0a727d51b99da6e652",
+    "url": "/static/js/7.b565ae55.chunk.js"
+  },
+  {
+    "revision": "78b8c54cd398c5314ad8",
+    "url": "/static/js/main.bde03aab.chunk.js"
+  },
+  {
+    "revision": "32822d7e52cefe5a0fbe",
+    "url": "/static/js/runtime~main.e9eea11f.js"
+  },
+  {
+    "revision": "35bb8d560db5205616671eab8c4d0303",
+    "url": "/static/media/codicon.35bb8d56.ttf"
+  },
+  {
+    "revision": "e42a4d82a3e57fa49b196233949bc686",
+    "url": "/static/media/loading.e42a4d82.gif"
+  }
+]);
