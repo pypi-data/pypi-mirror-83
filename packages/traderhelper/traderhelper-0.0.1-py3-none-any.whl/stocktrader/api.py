c72c0655472b0d68149361710130d605059a63f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:18:10 +0000

    Add package traderhelper/0.0.1/traderhelper-0.0.1-py3-none-any.whl

---
+++ packages/traderhelper/traderhelper-0.0.1-py3-none-any.whl/stocktrader/api.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__time__ = '2020/10/26 0:40'
+
+class StockTrader(object):
+    def __init__(self):
+        print("欢迎使用交易助手 stocktrader, 默认您已经知晓并同意以下免责条款，如果不同意请立即停止使用")
