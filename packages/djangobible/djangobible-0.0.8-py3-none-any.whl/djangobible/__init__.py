65a44735aa851e5f30c92e571c0a7ceeafa055e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:00 +0000

    Add package djangobible/0.0.8/djangobible-0.0.8-py3-none-any.whl

---
+++ packages/djangobible/djangobible-0.0.8-py3-none-any.whl/djangobible/__init__.py
@@ -0,0 +1,9 @@
+"""
+django-bible python library.
+
+django-bible lets you easily associate Django models with Bible Scripture references and search accordingly.
+"""
+
+__version__ = "0.0.8"
+
+from pythonbible import *
