eb7deaa1cf97bb393a00a74e2949414e259f7002
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:13:11 +0000

    Add package d3m-automl-rpc/0.0.0/d3m-automl-rpc-0.0.0.tar.gz

---
+++ packages/d3m-automl-rpc/d3m-automl-rpc-0.0.0.tar.gz/d3m-automl-rpc-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='d3m-automl-rpc',
+    version='0.0.0',
+    description='GRPC protocol for the TA3-TA2 communication API.',
+    author='DARPA D3M Program',
+    py_modules=['d3m_automl_rpc'],
+    url='https://gitlab.com/datadrivendiscovery/ta3ta2-api',
+    license='Apache-2.0',
+    classifiers=[
+    ],
+)
