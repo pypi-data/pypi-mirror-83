cd69551505de9af6d5c02c2b273bfe1e84926ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:52 +0000

    Add package celery-redbeat/2.0.0/celery-redbeat-2.0.0.tar.gz

---
+++ packages/celery-redbeat/celery-redbeat-2.0.0.tar.gz/celery-redbeat-2.0.0/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name="celery-redbeat",
+    description="A Celery Beat Scheduler using Redis for persistent storage",
+    long_description=long_description,
+    version="2.0.0",
+    url="https://github.com/sibson/redbeat",
+    license="Apache License, Version 2.0",
+    author="Marc Sibson",
+    author_email="sibson+redbeat@gmail.com",
+    keywords="python celery beat redis".split(),
+    packages=["redbeat"],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Topic :: System :: Distributed Computing',
+        'Topic :: Software Development :: Object Brokering',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=['redis>=3.2', 'celery>=4.2', 'python-dateutil', 'tenacity'],
+    tests_require=['pytest'],
+)
