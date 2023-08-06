8a64b26c6adea777b6daad5580a861dd89923770
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:14 +0000

    Add package palett/0.0.7/palett-0.0.7-py3-none-any.whl

---
+++ packages/palett/palett-0.0.7-py3-none-any.whl/palett/projector/colorant.py
@@ -0,0 +1,12 @@
+from palett.presets import PLANET
+from palett.projector.projector import to_projector
+from palett.projector.utils.bound_to_leap import bound_to_leap
+from palett.projector.utils.preset_to_flat import preset_to_flat
+from intype import is_numeric
+
+
+def colorant(bound, preset=PLANET, effects=[]):
+    leap = bound_to_leap(bound)
+    default_dye = preset_to_flat(preset)
+    projector = to_projector(leap, preset, effects)
+    return lambda x: projector(x) if is_numeric(x) else default_dye
