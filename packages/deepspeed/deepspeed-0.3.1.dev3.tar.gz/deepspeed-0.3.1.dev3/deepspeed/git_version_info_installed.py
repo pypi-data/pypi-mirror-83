7da0f94586a1a380d6bef3555f0e46afcdb639fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:25 +0000

    Add package deepspeed/0.3.1.dev3/deepspeed-0.3.1.dev3.tar.gz

---
+++ packages/deepspeed/deepspeed-0.3.1.dev3.tar.gz/deepspeed-0.3.1.dev3/deepspeed/git_version_info_installed.py
@@ -0,0 +1,5 @@
+version='0.3.1.dev3'
+git_hash='85ff374'
+git_branch='jeffra/jit_extensions'
+installed_ops={'cpu_adam': False, 'fused_adam': False, 'fused_lamb': False, 'sparse_attn': False, 'transformer': False, 'stochastic_transformer': False, 'utils': False}
+compatible_ops={'cpu_adam': True, 'fused_adam': True, 'fused_lamb': True, 'sparse_attn': False, 'transformer': True, 'stochastic_transformer': True, 'utils': True}
