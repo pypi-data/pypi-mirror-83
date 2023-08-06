83781fe3bf788f992f15ebb8d931b2d65ddd3d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:22 +0000

    Add package probabilistic-automata/0.4.1/probabilistic_automata-0.4.1.tar.gz

---
+++ packages/probabilistic-automata/probabilistic_automata-0.4.1.tar.gz/probabilistic_automata-0.4.1/probabilistic_automata/pa.py
@@ -0,0 +1,245 @@
+"""Code for modeling a probablistic automaton."""
+from __future__ import annotations
+
+import random
+from collections import defaultdict
+from itertools import product
+from typing import Callable, Mapping, Set, Union
+
+import attr
+import funcy as fn
+from dfa import DFA
+from dfa.dfa import Alphabet, Letter, State
+
+from probabilistic_automata.distributions import (
+    Action, EnvDist, Distribution, ProductDistribution,
+    prod_dist, uniform
+)
+
+
+def prod_alphabet(left, right) -> Alphabet:
+    return frozenset(product(left, right))
+
+
+def _dict2dist(env_dist) -> EnvDist:
+    @fn.memoize
+    def env_dist2(state, action):
+        dist = env_dist(state, action)
+        if isinstance(dist, Distribution.__args__):
+            return dist
+        return dist
+
+    return env_dist2
+
+
+@attr.s(frozen=True, auto_attribs=True)
+class PDFA:
+    """A DFA over a product alphabet where the first value
+    is non-deterministic and the second value is set according
+    to a state indexed stationary distribution.
+    """
+    dfa: DFA = attr.ib()
+    env_dist: EnvDist = attr.ib(converter=_dict2dist)
+
+    @dfa.validator
+    def _check_product_lang(self, _, dfa):
+        assert all(isinstance(i, tuple) and len(i) == 2 for i in dfa.inputs)
+
+    @property
+    def env_inputs(self):
+        """Accesses the set of environment inputs."""
+        return set(fn.pluck(1, self.dfa.inputs))
+
+    @property
+    def inputs(self):
+        """Accesses the set of (non-environment) inputs."""
+        return set(fn.pluck(0, self.dfa.inputs))
+
+    @property
+    def outputs(self):
+        """Accesses the set of possible outputs."""
+        return self.dfa.outputs
+
+    @property
+    def start(self):
+        """Accesses the start state."""
+        return self.dfa.start
+
+    def states(self):
+        """Computes the set of reachable states from start."""
+        return self.dfa.states()
+
+    def _label(self, state):
+        return self.dfa.label((), start=state)
+
+    def run(self, *, start=None, seed=None, label=False):
+        """Co-routine interface for simulating runs of the automaton.
+
+        - Users can send system actions (elements of self.inputs).
+        - Co-routine yields the current state.
+
+        If label is True, then state labels are returned instead
+        of states.
+
+        Example:
+        =======
+
+        machine: PDFA = ..
+        my_input: Action = ..            # Element of machine.inputs.
+
+        sim = machine.run()              # Start co-routine.
+
+        state1 = sim.send(my_input)
+        state2 = sim.send(my_input)
+        """
+        if seed is not None:
+            random.seed(seed)
+
+        labeler = self.dfa._label if label else lambda x: x
+
+        machine = self.dfa.run(start=start)
+        state = machine.send(None)
+
+        while True:
+            sys_action = yield labeler(state)
+            env_action = self.env_dist(state, sys_action).sample()
+            state = machine.send((sys_action, env_action))
+
+    trace = DFA.trace
+    transition = DFA.transition
+    transduce = DFA.transduce
+    label = DFA.label
+
+    @fn.memoize
+    def support(self, state, action) -> Set[State]:
+        """Returns the set of reachable states given (state, action)."""
+        return set(self.transition_probs(state, action).keys())
+
+    def _probs(self, start, action):
+        for e, p in self.env_dist(start, action).items():
+            end = self.dfa._transition(start, (action, e))
+            yield (end, p)
+
+    def transition_probs(self, state, action) -> Mapping[State, float]:
+        """Returns distribution over states given (state, action)"""
+        probs = defaultdict(lambda: 0)
+        for end, prob in self._probs(state, action):
+            probs[end] += prob
+        return probs
+
+    def prob(self, start: State, end: State, action: Action) -> float:
+        """
+        Returns the probability of transitioning from start to end
+        given action.
+        """
+        return sum(p for s, p in self._probs(start, action) if s == end)
+
+    def __or__(self, other: PDFA) -> PDFA:
+        def transition(composite_state, composite_action):
+            state_l, state_r = composite_state
+            (input_l, input_r), (env_l, env_r) = composite_action
+
+            state2_l = self.dfa._transition(state_l, (input_l, env_l))
+            state2_r = other.dfa._transition(state_r, (input_r, env_r))
+            return (state2_l, state2_r)
+
+        return pdfa(
+            start=(self.start, other.start),
+            label=lambda s: (self.dfa._label(s[0]), other.dfa._label(s[1])),
+            transition=transition,
+            env_dist=prod_dist(self.env_dist, other.env_dist),
+            inputs=prod_alphabet(self.inputs, other.inputs),
+            env_inputs=prod_alphabet(self.env_inputs, other.env_inputs),
+            outputs=prod_alphabet(self.outputs, other.outputs),
+        )
+
+    def __rshift__(self, other: PDFA) -> PDFA:
+        def transition(composite_state, composite_action):
+            state_l, state_r = composite_state
+            input_l, (env_l, env_r) = composite_action
+            input_r = self.dfa._label(state_l)
+
+            state2_l = self.dfa._transition(state_l, (input_l, env_l))
+            state2_r = other.dfa._transition(state_r, (input_r, env_r))
+
+            return (state2_l, state2_r)
+
+        def env_dist(composite_state, input_l):
+            state_l, state_r = composite_state
+            input_r = self.dfa._label(state_l)
+
+            return ProductDistribution(
+                left=self.env_dist(state_l, input_l),
+                right=other.env_dist(state_r, input_r),
+            )
+
+        return pdfa(
+            start=(self.start, other.start),
+            label=lambda s: other.dfa._label(s[1]),
+            transition=transition,
+            env_dist=env_dist,
+            inputs=self.inputs,
+            env_inputs=prod_alphabet(self.env_inputs, other.env_inputs),
+            outputs=other.outputs,
+        )
+
+    def __lshift__(self, other: PDFA) -> PDFA:
+        return other >> self
+
+
+def pdfa(
+        start: State,
+        label: Callable[[State], Letter],
+        transition: Callable[[State, Action], State],
+        env_dist: EnvDist = None,
+        inputs: Alphabet = None,
+        env_inputs: Alphabet = None,
+        outputs: Alphabet = None
+) -> PDFA:
+    """Main entrypoint for construction a Probablistic Automaton."""
+
+    if outputs is None:
+        outputs = {True, False}
+    if env_inputs is None and env_dist is None:
+        env_inputs = {None}
+        env_dist = uniform(env_inputs)
+
+    inputs = None if inputs is None else set(product(inputs, env_inputs))
+
+    return PDFA(
+        env_dist=env_dist,
+        dfa=DFA(
+            start=start, label=label,
+            inputs=inputs, outputs=outputs,
+            transition=transition,
+        ),
+    )
+
+
+def lift(dyn: Union[DFA, PDFA]) -> PDFA:
+    """Lifts a DFA into a deterministic PDFA."""
+    if isinstance(dyn, PDFA):
+        return dyn
+
+    return PDFA(
+        dfa=attr.evolve(
+            dyn,
+            inputs=prod_alphabet(dyn.inputs, {None}),
+            transition=lambda s, c: dyn._transition(s, c[0]),
+        ),
+        env_dist=uniform({None}),
+    )
+
+
+def randomize(dyn: DFA) -> PDFA:
+    """Lifts a DFA into a PDFA where original inputs are applied
+    uniformly at random.
+    """
+    return PDFA(
+        dfa=attr.evolve(
+            dyn,
+            inputs=prod_alphabet({None}, dyn.inputs),
+            transition=lambda s, c: dyn._transition(s, c[1]),
+        ),
+        env_dist=uniform(dyn.inputs),
+    )
---
+++ packages/probabilistic-automata/probabilistic_automata-0.4.1.tar.gz/probabilistic_automata-0.4.1/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['probabilistic_automata']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['attrs>=20.0.0,<21.0.0',
+ 'dfa>=2.0.2,<3.0.0',
+ 'funcy>=1.14,<2.0',
+ 'lenses>=0.5.0,<0.6.0']
+
+setup_kwargs = {
+    'name': 'probabilistic-automata',
+    'version': '0.4.1',
+    'description': 'Python library for manipulating probabilistic automata.',
+    'long_description': '# Probabilistic Automata\n\n[![Build Status](https://cloud.drone.io/api/badges/mvcisback/probabilistic_automata/status.svg)](https://cloud.drone.io/mvcisback/probabilistic_automata)\n[![Docs](https://img.shields.io/badge/API-link-color)](https://mvcisback.github.io/probabilistic_automata)\n[![codecov](https://codecov.io/gh/mvcisback/probabilistic_automata/branch/master/graph/badge.svg)](https://codecov.io/gh/mvcisback/probabilistic_automata)\n[![PyPI version](https://badge.fury.io/py/probabilistic-automata.svg)](https://badge.fury.io/py/probabilistic-automata)\n[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)\n\nPython library for manipulating Probabilistic Automata. This library\nbuilds upon the [`dfa`](https://github.com/mvcisback/dfa) package.\n\n<!-- markdown-toc start - Don\'t edit this section. Run M-x markdown-toc-generate-toc again -->\n**Table of Contents**\n\n- [Probabilistic Automata](#probabilistic-automata)\n- [Installation](#installation)\n- [Usage](#usage)\n    - [Dict <-> PDFA](#dict---pdfa)\n    - [DFA to PDFA](#dfa-to-pdfa)\n    - [Composition](#composition)\n\n<!-- markdown-toc end -->\n\n\n\n# Installation\n\nIf you just need to use `probabilistic_automata`, you can just run:\n\n`$ pip install probabilistic_automata`\n\nFor developers, note that this project uses the\n[poetry](https://poetry.eustace.io/) python package/dependency\nmanagement tool. Please familarize yourself with it and then\nrun:\n\n`$ poetry install`\n\n# Usage\n\nThe `probabilistic_automata` library centers around the `PDFA` object\nwhich models a finite probabilistic transition system, e.g., a Markov\nDecision Process, as a `DFA` or Moore Machine over a product alphabet\nover the system\'s actions and the environment\'s stochastic action.\n\n```python\nimport probabilistic_automata as PA\n\ndef transition(state, composite_action):\n    sys_action, env_action = composite_action\n    return (state + sys_action + env_action) % 2\n\ndef env_dist(state, sys_action):\n    """Based on state and the system action, what are the probabilities \n    of the environment\'s action."""\n\n    return {0: 1/2, 1: 1/2}  # Always a coin flip.\n\nnoisy_parity = PA.pdfa(\n    start=0,\n    label=bool,\n    inputs={0, 1},\n    env_inputs={0, 1},\n    outputs={0, 1},\n    transition=transition,\n    env_dist=env_dist,   # Equivalently, PA.uniform({0, 1}).\n)\n```\n\nThe support and transition probabilities can easily calculated:\n\n```python\nassert noisy_parity.support(0, 0) == {0, 1}\nassert noisy_parity.transition_probs(0, 0) == {0: 1/2, 1: 1/2}\nassert noisy_parity.prob(start=0, action=0, end=0) == 1/2\n```\n\n## Dict <-> PDFA\n\nNote that `pdfa` provides helper functions for going from a dictionary\nbased representation of a probabilistic transition system to a `PDFA`\nobject and back.\n\n```python\nimport probabilistic_automata as PA\n\nmapping = {\n    "s1": (True, {\n        \'a\': {\'s1\': 0.5, \'s2\': 0.5},\n    }),\n    "s2": (False, {\n        \'a\': {\'s1\': 1},\n    }),\n}\n\nstart = "s1"\npdfa = PA.dict2pdfa(mapping=mapping, start=start)\nassert pdfa.inputs == {\'a\'}\n\nmapping2, start2 = PA.pdfa2dict(pdfa)\nassert start == start2\nassert mapping2 == mapping\n```\n\n\n## DFA to PDFA\n\nThe `probabilistic_automata` library has two convenience methods for\ntransforming a Deterministic Finite Automaton (`dfa.DFA`) into a\n`PDFA`.\n\n- The `lift` function simply creates a `PDFA` whose transitions are\n  deterministic and match the original `dfa.DFA`.\n\n```python\nimport probabilistic_automata as PA\nfrom dfa import DFA\n\nparity = DFA(\n    start=0,\n    inputs={0, 1},\n    label=bool,\n    transition=lambda s, c: (s + c) & 1,\n)\n\nparity_pdfa = lift(parity)\n\nassert pdfa.inputs == parity.inputs\nassert pdfa.env_inputs == {None}\n```\n\n- The `randomize` function takes a `DFA` and returns a `PDFA` modeling\n  the actions of the `DFA` being selected uniformly at random.\n\n```\nnoisy_parity = PA.randomize(parity)\n\nassert noisy_parity.inputs == {None}\nassert noisy_parity.env_inputs == noisy_parity.inputs\n```\n\n## Composition\n\nLike their deterministic variants `PDFA` objects can be combined in\ntwo ways:\n\n1. (Synchronous) Cascading Composition: Feed outputs of one `PDFA` into another.\n\n```python\nmachine = noisy_parity >> noisy_parity\n\nassert machine.inputs == noisy_parity.inputs\nassert machine.outputs == noisy_parity.outputs\nassert machine.start == (0, 0)\n\nassert machine.support((0, 0), 0) == {(0, 0), (0, 1), (1, 0), (1, 1)}\n```\n\n2. (Synchronous) Parallel Composition: Run two `PDFA`s in parallel.\n\n```python\nmachine = noisy_parity | noisy_parity\n\nassert machine.inputs.left == noisy_parity.inputs\nassert machine.inputs.right == noisy_parity.inputs\n\nassert machine.outputs.left == noisy_parity.outputs\nassert machine.outputs.right == noisy_parity.outputs\n\nassert machine.env_inputs.left == noisy_parity.env_inputs\nassert machine.env_inputs.right == noisy_parity.env_inputs\n\nassert machine.start == (0, 0)\nassert machine.support((0, 0), (0, 0)) == {(0, 0), (0, 1), (1, 0), (1, 1)}\n```\n\n**Note** Parallel composition results in a `PDFA` with\n`dfa.ProductAlphabet` input and output alphabets.\n',
+    'author': 'Marcell Vazquez-Chanlatte',
+    'author_email': 'mvc@linux.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/mvcisback/probabilistic_automata',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
