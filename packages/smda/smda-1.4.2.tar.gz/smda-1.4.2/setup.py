c560d60735de3343bd5b2dc1bbfec9bbc4c2c905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:53 +0000

    Add package smda/1.4.2/smda-1.4.2.tar.gz

---
+++ packages/smda/smda-1.4.2.tar.gz/smda-1.4.2/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='smda',
+    # note to self: always change this in config as well.
+    version='1.4.2',
+    description='A recursive disassmbler optimized for CFG recovery from memory dumps. Based on capstone.',
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    author='Daniel Plohmann',
+    author_email='daniel.plohmann@mailbox.org',
+    url='https://github.com/danielplohmann/smda',
+    license="BSD 2-Clause",
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        'capstone',
+        'lief'
+    ],
+    data_files=[
+        ('', ['LICENSE']),
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Topic :: Security",
+        "Topic :: Software Development :: Disassemblers",
+    ],
+)
---
+++ packages/smda/smda-1.4.2.tar.gz/smda-1.4.2/smda/common/SmdaInstruction.py
@@ -0,0 +1,60 @@
+class SmdaInstruction:
+
+    smda_function = None
+    offset = None
+    bytes = None
+    mnemonic = None
+    operands = None
+
+    def __init__(self, ins_list=None, smda_function=None):
+        self.smda_function = smda_function
+        if ins_list is not None:
+            self.offset = ins_list[0]
+            self.bytes = ins_list[1]
+            self.mnemonic = ins_list[2]
+            self.operands = ins_list[3]
+
+    def getDetailed(self):
+        capstone = self.smda_function.smda_report.getCapstone()
+        with_details = [i for i in capstone.disasm(bytes.fromhex(self.bytes), self.offset)]
+        assert len(with_details) == 1
+        return with_details[0]
+
+    def getMnemonicGroup(self, escaper):
+        if escaper:
+            return escaper.escapeMnemonic(self.mnemonic)
+        return self.bytes
+
+    def getEscapedOperands(self, escaper):
+        if escaper:
+            return escaper.escapeOperands(self)
+        return self.bytes
+
+    def getMaskedOperands(self, escaper):
+        if escaper:
+            return escaper.escapeOperands(self, offsets_only=True)
+        return self.bytes
+
+    def getEscapedBinary(self, escaper, lower_addr=None, upper_addr=None):
+        if escaper:
+            return escaper.escapeBinary(self, lower_addr=lower_addr, upper_addr=upper_addr)
+        return self.bytes
+
+    @classmethod
+    def fromDict(cls, instruction_dict, smda_function=None):
+        smda_instruction = cls(None)
+        smda_instruction.smda_function = smda_function
+        smda_instruction.offset = instruction_dict[0]
+        smda_instruction.bytes = instruction_dict[1]
+        smda_instruction.mnemonic = instruction_dict[2]
+        smda_instruction.operands = instruction_dict[3]
+        return smda_instruction
+
+    def toDict(self):
+        return [self.offset, self.bytes, self.mnemonic, self.operands]
+
+    def __int__(self):
+        return self.offset
+
+    def __str__(self):
+        return "0x{:08x}: ({:>14s}) - {} {}".format(self.offset, self.bytes, self.mnemonic, self.operands)
