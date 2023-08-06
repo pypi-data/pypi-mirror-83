1e491467beeba0a003425fdbab82f24f8e002e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:01 +0000

    Add package grtoolkit/20.10.5/grtoolkit-20.10.5.tar.gz

---
+++ packages/grtoolkit/grtoolkit-20.10.5.tar.gz/grtoolkit-20.10.5/grtoolkit/Circuits/Conductance/__init__.py
@@ -0,0 +1,18 @@
+
+import sys
+
+def InSeries(g_list):
+    sumOfInverse = sum([1/g for g in g_list])
+    return 1/sumOfInverse
+
+def InParallel(g_list):
+    return sum(g_list)
+
+#callable function for the module:
+class MyModule(sys.modules[__name__].__class__):
+    # Replaces
+    # def Conductance(r):
+    #     return 1/r
+    def __call__(self, r): #conductance
+        return 1/r 
+sys.modules[__name__].__class__ = MyModule
