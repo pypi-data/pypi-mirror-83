1d4ce341cadd99a77e988e4ead13ec8f8b7c12fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:42 +0000

    Add package openstates/5.2.0/openstates-5.2.0.tar.gz

---
+++ packages/openstates/openstates-5.2.0.tar.gz/openstates-5.2.0/openstates/test_settings.py
@@ -0,0 +1,23 @@
+import os
+import dj_database_url
+
+# django settings for tests
+SECRET_KEY = "test"
+INSTALLED_APPS = (
+    "django.contrib.contenttypes",
+    "openstates.data",
+    "openstates.reports",
+)
+MIDDLEWARE_CLASSES = ()
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "test",
+        "USER": "test",
+        "PASSWORD": "test",
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+    }
+}
+DATABASE_URL = os.environ.get("OVERRIDE_DATABASE_URL")
+if DATABASE_URL:
+    DATABASES = {"default": dj_database_url.parse(DATABASE_URL)}
