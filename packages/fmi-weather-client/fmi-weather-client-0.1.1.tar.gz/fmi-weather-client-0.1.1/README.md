ddc4513d3077064a0dfb39d32b159a576c836afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:39 +0000

    Add package fmi-weather-client/0.1.1/fmi-weather-client-0.1.1.tar.gz

---
+++ packages/fmi-weather-client/fmi-weather-client-0.1.1.tar.gz/fmi-weather-client-0.1.1/README.md
@@ -0,0 +1,118 @@
+![Test](https://github.com/saaste/fmi-weather-client/workflows/tests/badge.svg?branch=master)
+![Last commit](https://img.shields.io/github/last-commit/saaste/fmi-weather-client)
+![Latest version in GitHub](https://img.shields.io/github/v/release/saaste/fmi-weather-client?include_prereleases)
+![Latest version in PyPi](https://img.shields.io/pypi/v/fmi-weather-client)
+
+# Finnish Meteorological Institute Weather
+Library for fetching weather information from
+[Finnish Meteorological Institute (FMI)](https://en.ilmatieteenlaitos.fi/open-data). 
+
+## How to use
+
+Working example can be found in [example.py](example.py).
+
+### Install
+
+```
+$ pip install fmi-weather-client 
+```
+
+### Get weather and forecasts
+You can get the weather using the following functions:
+- `weather_by_place_name(place_name)`
+- `weather_by_coordinates(latitude, longitude)`
+
+Example:
+```python
+import fmi_weather_client as fmi
+from fmi_weather_client.errors import ClientError, ServerError
+
+try:
+    weather = fmi.weather_by_place_name("Jäppilä, Pieksämäki")
+    if weather is not None:
+        print(f"Temperature in {weather.place} is {weather.data.temperature}")
+except ClientError as err:
+    print(f"Client error with status {err.status_code}: {err.message}")
+except ServerError as err:
+    print(f"Server error with status {err.status_code}: {err.body}")
+```
+
+You can get the forecasts using the following functions:
+- `forecast_by_place_name(place_name, [timestep_hours=24])`
+- `forecast_by_coordinates(latitude, longitude, [timestep_hours=24])`
+
+Example:
+```python
+import fmi_weather_client as fmi
+from fmi_weather_client.errors import ClientError, ServerError
+
+try:
+    forecast = fmi.forecast_by_coordinates(60.170998, 24.941325)
+    for weather_data in forecast.forecasts:
+        print(f"Temperature at {weather_data.time}: {weather_data.temperature}")
+except ClientError as err:
+    print(f"Client error with status {err.status_code}: {err.message}")
+except ServerError as err:
+    print(f"Server error with status {err.status_code}: {err.body}")
+
+```
+
+All functions have asynchronous versions available with `async_` prefix.
+
+### Errors
+
+##### ClientError
+Happens if FMI service returns `400-499`. This can happens for example if:
+- Provided coordinates are invalid
+- Provided place is not recognized
+- Weather data is not available
+
+Error object contains status code and human-readable error message from FMI service.
+
+##### ServerError
+Happens if FMI service returns any other error.
+
+Error object contains status code and raw response body from FMI service
+
+
+### Weather data
+FMI provides the following commonly used information:
+- Temperature (°C)
+- Pressure (hPa)
+- Humidity (%)
+- Wind direction (°)
+- Wind speed (m/s)
+- Wind gust (m/s)
+- Dew point (°)
+- Cloud coverage (%)
+- Precipitation intensity (mm/h)
+- Symbol [Documentation in Finnish](https://www.ilmatieteenlaitos.fi/latauspalvelun-pikaohje)
+
+
+There are also other information available. Check [models.py](fmi_weather_client/models.py) and FMI documentation for
+more info.
+
+## Development
+
+### Setup
+Create and activate a virtual environment
+```
+$ python -m venv venv
+$ source venv/bin/activate
+```
+
+Install required packages
+```
+$ make setup
+```
+
+Deactivate virtual environment when you are done
+```
+$ deactivate
+```
+
+### Run tests
+This will run unit tests and code quality checks
+```
+$ make test
+```
---
+++ packages/fmi-weather-client/fmi-weather-client-0.1.1.tar.gz/fmi-weather-client-0.1.1/test/test_init.py
@@ -0,0 +1,128 @@
+import unittest
+from unittest import mock
+
+import asyncio
+
+import fmi_weather_client
+import test.test_data as test_data
+from fmi_weather_client.errors import ClientError, ServerError
+
+
+class FMIWeatherTest(unittest.TestCase):
+
+    # HAPPY CASES
+    @mock.patch('requests.get', side_effect=test_data.mock_place_forecast_response)
+    def test_get_weather_by_place(self, mock_get):
+        weather = fmi_weather_client.weather_by_place_name('Iisalmi')
+        self.assert_name_weather(weather)
+
+    @mock.patch('requests.get', side_effect=test_data.mock_place_forecast_response)
+    def test_async_get_weather_by_place(self, mock_get):
+        loop = asyncio.get_event_loop()
+        weather = loop.run_until_complete(fmi_weather_client.async_weather_by_place_name('Iisalmi'))
+        self.assert_name_weather(weather)
+
+    @mock.patch('requests.get', side_effect=test_data.mock_coordinate_forecast_response)
+    def test_get_weather_by_coordinates(self, mock_get):
+        weather = fmi_weather_client.weather_by_coordinates(63.14343, 27.31317)
+        self.assert_coordinate_weather(weather)
+
+    @mock.patch('requests.get', side_effect=test_data.mock_coordinate_forecast_response)
+    def test_async_get_weather_by_coordinates(self, mock_get):
+        loop = asyncio.get_event_loop()
+        weather = loop.run_until_complete(fmi_weather_client.async_weather_by_coordinates(63.14343, 27.31317))
+        self.assert_coordinate_weather(weather)
+
+    @mock.patch('requests.get', side_effect=test_data.mock_place_forecast_response)
+    def test_get_forecast_by_place_name(self, mock_get):
+        forecast = fmi_weather_client.forecast_by_place_name('Iisalmi')
+        self.assert_name_forecast(forecast)
+
+    @mock.patch('requests.get', side_effect=test_data.mock_place_forecast_response)
+    def test_async_get_forecast_by_place_name(self, mock_get):
+        loop = asyncio.get_event_loop()
+        forecast = loop.run_until_complete(fmi_weather_client.async_forecast_by_place_name('Iisalmi'))
+        self.assert_name_forecast(forecast)
+
+    @mock.patch('requests.get', side_effect=test_data.mock_coordinate_forecast_response)
+    def test_get_forecast_by_coordinates(self, mock_get):
+        forecast = fmi_weather_client.forecast_by_coordinates(29.742731, 67.583988)
+        self.assert_coordinate_forecast(forecast)
+
+    @mock.patch('requests.get', side_effect=test_data.mock_coordinate_forecast_response)
+    def test_async_get_forecast_by_coordinates(self, mock_get):
+        loop = asyncio.get_event_loop()
+        forecast = loop.run_until_complete(fmi_weather_client.async_forecast_by_coordinates(29.742731, 67.583988))
+        self.assert_coordinate_forecast(forecast)
+
+    # CORNER CASES
+    @mock.patch('requests.get', side_effect=test_data.mock_nan_response)
+    def test_nil_weather_response(self, mock_get):
+        weather_coord = fmi_weather_client.weather_by_coordinates(25.46816, 65.01236)
+        weather_name = fmi_weather_client.weather_by_place_name('Oulu')
+        self.assertIsNone(weather_coord)
+        self.assertIsNone(weather_name)
+
+    @mock.patch('requests.get', side_effect=test_data.mock_nan_response)
+    def test_nil_forecast_response(self, mock_get):
+        forecast_coord = fmi_weather_client.forecast_by_coordinates(25.46816, 65.01236, 24)
+        forecast_name = fmi_weather_client.forecast_by_place_name('Oulu', 24)
+        self.assertIsNotNone(forecast_coord)
+        self.assertIsNotNone(forecast_name)
+        self.assertEqual(forecast_coord.forecasts, [])
+        self.assertEqual(forecast_name.forecasts, [])
+
+    # ERROR CASES
+    @mock.patch('requests.get', side_effect=test_data.mock_no_location_exception_response)
+    def test_no_location_exception_response(self, mock_get):
+        with self.assertRaises(ClientError):
+            fmi_weather_client.weather_by_coordinates(27.31317, 63.14343)
+
+    @mock.patch('requests.get', side_effect=test_data.mock_invalid_lat_lon_exception_response)
+    def test_invalid_lat_lon_exception_response(self, mock_get):
+        with self.assertRaises(ClientError):
+            fmi_weather_client.weather_by_coordinates(27.31317, 63.14343)
+
+    @mock.patch('requests.get', side_effect=test_data.mock_no_data_available_exception_response)
+    def test_no_data_available_exception_response(self, mock_get):
+        with self.assertRaises(ClientError):
+            fmi_weather_client.weather_by_coordinates(27.31317, 63.14343)
+
+    @mock.patch('requests.get', side_effect=test_data.mock_server_error_response)
+    def test_server_error_response(self, mock_get):
+        with self.assertRaises(ServerError):
+            fmi_weather_client.weather_by_coordinates(27.31317, 63.14343)
+
+    def assert_name_weather(self, weather):
+        self.assertEqual(weather.place, 'Iisalmi')
+        self.assertEqual(weather.data.time.timestamp(), 1582027200.0)
+        self.assertEqual(weather.data.temperature.value, 2.34)
+        self.assertEqual(weather.data.wind_speed.value, 3.52)
+
+    def assert_coordinate_weather(self, weather):
+        self.assertEqual(weather.place, 'Sauoiva')
+        self.assertEqual(weather.data.time.timestamp(), 1582027200.0)
+        self.assertEqual(weather.data.temperature.value, 0.37)
+        self.assertEqual(weather.data.wind_speed.value, 1.35)
+
+    def assert_name_forecast(self, forecast):
+        self.assertEqual(forecast.place, 'Iisalmi')
+        self.assertEqual(forecast.lat, 27.19067)
+        self.assertEqual(forecast.lon, 63.55915)
+        self.assertEqual(len(forecast.forecasts), 5)
+        self.assertEqual(forecast.forecasts[0].temperature.value, 1.21)
+        self.assertEqual(forecast.forecasts[1].pressure.value, 974.67)
+        self.assertEqual(forecast.forecasts[4].humidity.value, 96.31)
+
+    def assert_coordinate_forecast(self, forecast):
+        self.assertEqual(forecast.place, 'Sauoiva')
+        self.assertEqual(forecast.lat, 29.74273)
+        self.assertEqual(forecast.lon, 67.58399)
+        self.assertEqual(len(forecast.forecasts), 5)
+        self.assertEqual(forecast.forecasts[0].temperature.value, -2.26)
+        self.assertEqual(forecast.forecasts[1].pressure.value, 968.58)
+        self.assertEqual(forecast.forecasts[4].humidity.value, 97.0)
+
+
+if __name__ == '__main__':
+    unittest.main()
