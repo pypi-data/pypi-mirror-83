95334e2d441467ed1c6395ee0bb877eb8761aec5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:58 +0000

    Add package django-terra-geocrud/1.0.0/django-terra-geocrud-1.0.0.tar.gz

---
+++ packages/django-terra-geocrud/django-terra-geocrud-1.0.0.tar.gz/django-terra-geocrud-1.0.0/terra_geocrud/apps.py
@@ -0,0 +1,36 @@
+from django.apps import AppConfig
+from django.conf import settings
+from django.urls import reverse_lazy
+from django.utils.translation import gettext_lazy as _
+from sorl.thumbnail.conf import settings as thumbnail_settings
+from terra_accounts.permissions_mixins import PermissionRegistrationMixin
+
+
+class TerraCrudConfig(PermissionRegistrationMixin, AppConfig):
+    name = 'terra_geocrud'
+    verbose_name = _('Geographic Editor Config')
+    permissions = (
+        ("can_manage_views", _("GEOCRUD: Can create / edit / delete views / groups and associated layers.")),
+        ("can_view_feature", _("GEOCRUD: Can read feature detail.")),
+        ("can_add_feature", _("GEOCRUD: Can create feature")),
+        ("can_change_feature", _("GEOCRUD: Can change feature")),
+        ("can_delete_feature", _("GEOCRUD: Can delete feature")),
+    )
+
+    def ready(self):
+        if settings.AUTH_USER_MODEL == 'terra_accounts.TerraUser':
+            # sync functionnal perms if using terra accounts auth user model
+            super().ready()
+            # in terra-admin context, we assume to render module url
+            terra_settings = getattr(settings, 'TERRA_APPLIANCE_SETTINGS', {})
+            modules = terra_settings.get('modules', {})
+            modules.update({
+                'CRUD': {
+                    "settings": reverse_lazy('settings'),
+                }
+            })
+            terra_settings.update({'modules': modules})
+            setattr(settings, 'TERRA_APPLIANCE_SETTINGS', terra_settings)
+        # Thumbnails settings
+        setattr(thumbnail_settings, 'THUMBNAIL_FORMAT', 'PNG')  # force PNG for thumbnail, to keep transparency
+        setattr(thumbnail_settings, 'THUMBNAIL_UPSCALE', False)  # never upscale if image size < thumbnail size
