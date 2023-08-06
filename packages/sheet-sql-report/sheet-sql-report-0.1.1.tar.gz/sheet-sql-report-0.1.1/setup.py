e5489a281760f749b7bc9530755a51666df1a865
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:00 +0000

    Add package sheet-sql-report/0.1.1/sheet-sql-report-0.1.1.tar.gz

---
+++ packages/sheet-sql-report/sheet-sql-report-0.1.1.tar.gz/sheet-sql-report-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="sheet-sql-report",
+    version="0.1.1",
+    author="Jack Pegler",
+    author_email="jackpegler@gmail.com",
+    description="Python package to help users connect with google sheet and SQL data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jackpegler/sheet-sql-report",
+    packages=['sheet_sql_report'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3.0',
+    zip_safe=False
+)
