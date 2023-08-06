08684152459eccc575c4089db566e34792298813
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:55 +0000

    Add package dgllife/0.2.6/dgllife-0.2.6.tar.gz

---
+++ packages/dgllife/dgllife-0.2.6.tar.gz/dgllife-0.2.6/dgllife/model/model_zoo/__init__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Collection of model architectures
+
+# Molecular property prediction
+from .attentivefp_predictor import *
+from .gat_predictor import *
+from .gcn_predictor import *
+from .gin_predictor import *
+from .mgcn_predictor import *
+from .mlp_predictor import *
+from .mpnn_predictor import *
+from .schnet_predictor import *
+from .weave_predictor import *
+from .gnn_ogb_predictor import *
+
+# Generative models
+try:
+    # DGMG requires RDKit support
+    from .dgmg import *
+    from .jtnn import *
+except ImportError:
+    pass
+
+# Reaction prediction
+from .wln_reaction_center import *
+from .wln_reaction_ranking import *
+
+# Protein-Ligand Binding
+from .acnn import *
+
+# Link prediction
+from .hadamard_link_predictor import *
