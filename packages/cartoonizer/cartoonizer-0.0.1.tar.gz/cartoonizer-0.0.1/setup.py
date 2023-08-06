fa1ef1cc96f204d8996090a1a860b89f79969600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:09 +0000

    Add package cartoonizer/0.0.1/cartoonizer-0.0.1.tar.gz

---
+++ packages/cartoonizer/cartoonizer-0.0.1.tar.gz/cartoonizer-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+      name='cartoonizer',
+      version='0.0.1',
+      description='Convert your DP Image into Cartoon form',
+      py_modules=["cartoonizer"],
+      package_dir={'':'cartoonizer'},
+)
