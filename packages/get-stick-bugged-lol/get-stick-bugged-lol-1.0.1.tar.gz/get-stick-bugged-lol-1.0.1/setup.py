0c68dba0444da2e0186bb70abe9b2cd3a950410a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:21 +0000

    Add package get-stick-bugged-lol/1.0.1/get-stick-bugged-lol-1.0.1.tar.gz

---
+++ packages/get-stick-bugged-lol/get-stick-bugged-lol-1.0.1.tar.gz/get-stick-bugged-lol-1.0.1/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='get-stick-bugged-lol',
+    version='1.0.1',
+    author='n0spaces',
+    description="'Get stick bugged' video generator",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/n0spaces/get_stick_bugged_lol',
+    packages=setuptools.find_packages(),
+    package_data={'gsbl': ['media/*.*']},
+    entry_points={'console_scripts': ['gsbl=gsbl.__main__:main']},
+    install_requires=['pylsd-nova>=1.2.0', 'numpy', 'Pillow', 'moviepy'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
