123ed2f10a16f623fc7c37d4144a62c3660fc41b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:40 +0000

    Add package PEQNP/5.2.1/PEQNP-5.2.1.tar.gz

---
+++ packages/PEQNP/PEQNP-5.2.1.tar.gz/PEQNP-5.2.1/setup.py
@@ -0,0 +1,33 @@
+import sys
+import setuptools
+from distutils.core import setup, Extension
+
+ext_modules = [
+    Extension("slime",
+              language="c++",
+              sources=['src/SLIME.cc', 'src/SimpSolver.cc', 'src/Solver.cc'],
+              include_dirs=['.', 'include'],
+              extra_compile_args=['-std=c++11'],
+              ),
+    Extension("pixie",
+              language="c++",
+              sources=['src/pixie.cc'],
+              include_dirs=['.', 'include'],
+              extra_compile_args=['-std=c++11'],
+              ),
+]
+if 'no-solver' in sys.argv:
+    del ext_modules[:]
+    if 'no-solver' in sys.argv:
+        sys.argv.remove('no-solver')
+setup(
+    name='PEQNP',
+    version='5.2.1',
+    packages=['peqnp', 'peqnp.cnf', 'peqnp.sdk'],
+    url='http://www.peqnp.com',
+    license='copyright (c) 2012-2020 Oscar Riveros. All rights reserved.',
+    author='Oscar Riveros',
+    author_email='contact@peqnp.science',
+    description='PEQNP Mathematical Solver from http://www.peqnp.com',
+    ext_modules=ext_modules,
+)
