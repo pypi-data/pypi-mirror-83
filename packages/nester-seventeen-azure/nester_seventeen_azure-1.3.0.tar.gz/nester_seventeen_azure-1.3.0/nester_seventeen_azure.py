a28e1ff7ea0c20da6adedb195e262dad698e0125
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:21 +0000

    Add package nester-seventeen-azure/1.3.0/nester_seventeen_azure-1.3.0.tar.gz

---
+++ packages/nester-seventeen-azure/nester_seventeen_azure-1.3.0.tar.gz/nester_seventeen_azure-1.3.0/nester_seventeen_azure.py
@@ -0,0 +1,9 @@
+"""这是nester.py模块,它的作用是打印列表，其中可能包含也可能不包含嵌套列表"""
+def print_lol(the_list, indent = False, level = 0):
+    """the_list是一个位置参数，可以是任何Python列表"""
+    for each_item in the_list:
+        if isinstance(each_item, list):
+            print_lol(each_item, indent, level + 1)
+        else:
+            print(each_item)
+            
---
+++ packages/nester-seventeen-azure/nester_seventeen_azure-1.3.0.tar.gz/nester_seventeen_azure-1.3.0/setup.py
@@ -0,0 +1,12 @@
+#从Pyhon发布工具导入“setup”函数
+from distutils.core import setup
+
+setup(
+        name            = 'nester_seventeen_azure',
+        version         = '1.3.0',
+        py_modules      = ['nester_seventeen_azure'],
+        author          = 'tb',
+        author_eamil    = 'bo_869423_t@foxmail.com',
+        url             = 'http://www.headfirstlabs.com',
+        description     = '我的第二个上传项目(更新后版本)',
+      )
