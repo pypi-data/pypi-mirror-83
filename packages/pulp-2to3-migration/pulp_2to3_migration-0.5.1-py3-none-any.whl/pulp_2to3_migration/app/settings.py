6f764a03cf3c1be493046c531548a72709679601
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:12:50 +0000

    Add package pulp-2to3-migration/0.5.1/pulp_2to3_migration-0.5.1-py3-none-any.whl

---
+++ packages/pulp-2to3-migration/pulp_2to3_migration-0.5.1-py3-none-any.whl/pulp_2to3_migration/app/settings.py
@@ -0,0 +1,12 @@
+PULP2_MONGODB = {
+    'name': 'pulp_database',
+    'seeds': 'localhost:27017',
+    'username': '',
+    'password': '',
+    'replica_set': '',
+    'ssl': False,
+    'ssl_keyfile': '',
+    'ssl_certfile': '',
+    'verify_ssl': True,
+    'ca_path': '/etc/pki/tls/certs/ca-bundle.crt',
+}
