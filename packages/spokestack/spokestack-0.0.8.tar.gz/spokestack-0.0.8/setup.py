22fbcc432c3780a26b90f711335d9d78da1ee913
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:43 +0000

    Add package spokestack/0.0.8/spokestack-0.0.8.tar.gz

---
+++ packages/spokestack/spokestack-0.0.8.tar.gz/spokestack-0.0.8/setup.py
@@ -0,0 +1,32 @@
+import setuptools  # type: ignore
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="spokestack",
+    version="0.0.8",
+    author="Spokestack",
+    author_email="support@spokestack.io",
+    description="Spokestack Library for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/spokestack/spokestack-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+    install_requires=[
+        "pyaudio",
+        "webrtcvad",
+        "numpy",
+        "websocket_client",
+        "tokenizers",
+        "requests",
+        "streamp3",
+    ],
+)
