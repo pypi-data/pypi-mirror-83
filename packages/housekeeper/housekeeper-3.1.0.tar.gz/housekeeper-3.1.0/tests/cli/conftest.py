8db7fbe66b3733eb7c3a4a319b3951f6fbc5768d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:06 +0000

    Add package housekeeper/3.1.0/housekeeper-3.1.0.tar.gz

---
+++ packages/housekeeper/housekeeper-3.1.0.tar.gz/housekeeper-3.1.0/tests/cli/conftest.py
@@ -0,0 +1,44 @@
+"""Fixtures for CLI tests"""
+
+import pytest
+from click.testing import CliRunner
+
+from housekeeper.store import Store
+
+
+@pytest.yield_fixture(scope="function", name="store")
+def fixture_store(project_dir, db_uri):
+    """Override the store fixture to get a controlled db path"""
+    _store = Store(uri=db_uri, root=str(project_dir))
+    _store.create_all()
+    yield _store
+    _store.drop_all()
+
+
+@pytest.fixture(scope="function", name="cli_runner")
+def fixture_cli_runner():
+    """Return a cli runner for testing Click"""
+    runner = CliRunner()
+    return runner
+
+
+@pytest.fixture(scope="function", name="base_context")
+def fixture_base_context(db_uri, project_dir, store):
+    """Return a context with initialized database"""
+    _ctx = {
+        "database": db_uri,
+        "root": project_dir,
+        "store": store,
+    }
+    return _ctx
+
+
+@pytest.fixture(scope="function", name="populated_context")
+def fixture_populated_context(db_uri, project_dir, populated_store):
+    """Return a context with initialized database with some data"""
+    _ctx = {
+        "database": db_uri,
+        "root": project_dir,
+        "store": populated_store,
+    }
+    return _ctx
