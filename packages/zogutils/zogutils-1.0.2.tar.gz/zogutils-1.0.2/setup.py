ad3244a79a5857b696245d81a1e9f8c0061e5244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:40 +0000

    Add package zogutils/1.0.2/zogutils-1.0.2.tar.gz

---
+++ packages/zogutils/zogutils-1.0.2.tar.gz/zogutils-1.0.2/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['zogutils', 'zogutils.middlewares']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['fastapi==0.61.1',
+ 'piccolo-api==0.11.0',
+ 'sentry-sdk==0.19.0',
+ 'starlette-prometheus==0.7.0']
+
+setup_kwargs = {
+    'name': 'zogutils',
+    'version': '1.0.2',
+    'description': "General utilities to be use in my base-fastapi template. Why ZOG? It's sound like joke and look like zoo.",
+    'long_description': None,
+    'author': 'Hoang Manh Tien',
+    'author_email': 'tienhm.0202@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '==3.7.7',
+}
+
+
+setup(**setup_kwargs)
