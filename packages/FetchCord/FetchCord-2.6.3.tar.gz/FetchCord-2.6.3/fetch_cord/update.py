8942ffffabf3947bc882317ffc7102a7fcfd53f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:39 +0000

    Add package FetchCord/2.6.3/FetchCord-2.6.3.tar.gz

---
+++ packages/FetchCord/FetchCord-2.6.3.tar.gz/FetchCord-2.6.3/fetch_cord/update.py
@@ -0,0 +1,13 @@
+import urllib.request
+import sys
+import os
+
+
+def update():
+    print("Updating database...")
+    url = "https://raw.githubusercontent.com/MrPotatoBobx/FetchCord/master/fetch_cord/ressources/infos.json"
+    urllib.request.urlretrieve(url, os.path.dirname(__file__) + "/ressources/infos.json")
+    sys.exit(0)
+
+
+
