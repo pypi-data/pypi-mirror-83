bd1599482627b743ee03a13a7254a37a0e4377a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:05 +0000

    Add package insta-scrape/0.11.0/insta-scrape-0.11.0.tar.gz

---
+++ packages/insta-scrape/insta-scrape-0.11.0.tar.gz/insta-scrape-0.11.0/instascrape/scrapers/__init__.py
@@ -0,0 +1,6 @@
+from instascrape.scrapers.hashtag import *
+from instascrape.scrapers.json_tools import *
+from instascrape.scrapers.login import *
+from instascrape.scrapers.post import *
+from instascrape.scrapers.profile import *
+from instascrape.scrapers.comment import *
