ee1e016f2f19c26e9eea1f4a9f74d1b657428fb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:09:25 +0000

    Add package browse-ocrd/0.4.1/browse_ocrd-0.4.1-py3-none-any.whl

---
+++ packages/browse-ocrd/browse_ocrd-0.4.1-py3-none-any.whl/tests/view/test_registry.py
@@ -0,0 +1,23 @@
+import tests
+import unittest
+from ocrd_browser.view import ViewImages, ViewRegistry
+
+
+class ViewManagerTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.vm = ViewRegistry({'images': (ViewImages, 'Images', 'Displays Images')})
+
+    def test_get_view_options(self):
+        expected = {'images': 'Images'}
+        actual = self.vm.get_view_options()
+        self.assertEqual(expected, actual)
+
+    def test_get_view(self):
+        expected = ViewImages
+        actual = self.vm.get_view('images')
+        self.assertEqual(expected, actual)
+
+
+if __name__ == '__main__':
+    unittest.main()
