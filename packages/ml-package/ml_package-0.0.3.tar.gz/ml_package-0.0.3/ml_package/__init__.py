e19c347a10025781f2f05fef9feeb8aac3eb74df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:00 +0000

    Add package ml-package/0.0.3/ml_package-0.0.3.tar.gz

---
+++ packages/ml-package/ml_package-0.0.3.tar.gz/ml_package-0.0.3/ml_package/__init__.py
@@ -0,0 +1,10 @@
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+# python -m twine upload dist/*
+# python setup.py sdist
+
+import pandas as pd
+k = pd.DataFrame()
+print(k)
