1e7979668b43cf214ed0eb2f5a550cc08227610c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:03 +0000

    Add package tagenalgo/1.0.1/tagenalgo-1.0.1.tar.gz

---
+++ packages/tagenalgo/tagenalgo-1.0.1.tar.gz/tagenalgo-1.0.1/src/tagenalgo/__init__.py
@@ -0,0 +1,10 @@
+
+"""Evolution programming in python with talib and gplearn inspired API
+``tagenalgo`` is a parameter optimization algorithm for trading strategy.
+"""
+
+from __future__ import absolute_import
+from .hello import *
+
+__version__ = '1.0.1'
+__license__ = 'BSD'
