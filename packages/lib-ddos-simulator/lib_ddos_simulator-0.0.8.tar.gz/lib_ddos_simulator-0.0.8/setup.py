b9d753d39318262f16ff144772f2e0ffd33e3278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:24:39 +0000

    Add package lib-ddos-simulator/0.0.8/lib_ddos_simulator-0.0.8.tar.gz

---
+++ packages/lib-ddos-simulator/lib_ddos_simulator-0.0.8.tar.gz/lib_ddos_simulator-0.0.8/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='lib_ddos_simulator',
+    packages=find_packages(),
+    include_package_data=True,
+    version='0.0.8',
+    author='Justin Furuness and Anna Gorbenko',
+    author_email='jfuruness@gmail.com, agorbenko97@gmail.com',
+    url='https://github.com/jfuruness/lib_ddos_simulator.git',
+    download_url='https://github.com/jfuruness/lib_ddos.git',
+    description="DDOS simulator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords=['Furuness', 'Gorbenko', 'DDOS', 'DOS', 'Simulation',
+              'Sieve', 'Protag', 'KPO', 'DOSE',
+              'Distributed Denial of Service',
+              'Denial of Service'],
+    install_requires=[
+        'flasgger',
+        'flask',
+        'matplotlib',
+        'tikzplotlib',
+        'wheel',
+        'setuptools',
+        'tqdm',
+        'pytest',
+        'pathos'
+    ],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3'],
+    entry_points={
+        'console_scripts': ['lib_ddos_simulator = lib_ddos_simulator.__main__:main']},
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+)
