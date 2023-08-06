d80bdbfb0014b65f4a59ce06577bd6570d1d7ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:09 +0000

    Add package pyautoeios/0.0.1/pyautoeios-0.0.1-py3-none-any.whl

---
+++ packages/pyautoeios/pyautoeios-0.0.1-py3-none-any.whl/pyautoeios/__init__.py
@@ -0,0 +1,8 @@
+from pyautogui import *
+# import pyautoeios.eios as eios
+# import pyautoeios.hex_keycodes as hexcodes
+from pyautoeios import _pyautogui_remoteinput
+platformModule = _pyautogui_remoteinput
+
+__AUTHOR__ = "brett.moan@gmail.com"
+__VERSION__ = "0.0.1"
