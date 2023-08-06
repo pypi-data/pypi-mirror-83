f7cde31a3ca0c26a5662f94672edf4b20980140c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:34 +0000

    Add package example-dsl-pkg/1.1/example_dsl_pkg-1.1-py3-none-any.whl

---
+++ packages/example-dsl-pkg/example_dsl_pkg-1.1-py3-none-any.whl/example_dsl_pkg/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) 2020 XiaoMi Inc. All Rights Reserved.
+# Authors: dushulin <dushulin@xiaomi.com>
+# datetime:2020/10/29 3:46 下午
+
+name = "example_pkg"
---
+++ packages/example-dsl-pkg/example_dsl_pkg-1.1-py3-none-any.whl/example_dsl_pkg/testMyPythonPKG.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+# Copyright (c) 2020 XiaoMi Inc. All Rights Reserved.
+# Authors: dushulin <dushulin@xiaomi.com>
+# datetime:2020/10/29 4:39 下午
+
+import example_dsl_pkg
+print(example_dsl_pkg.name)
