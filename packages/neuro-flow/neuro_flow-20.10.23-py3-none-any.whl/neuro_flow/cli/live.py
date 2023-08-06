65d9f69b4f46f5ff867f2cc47fa64b4179aee4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:00 +0000

    Add package neuro-flow/20.10.23/neuro_flow-20.10.23-py3-none-any.whl

---
+++ packages/neuro-flow/neuro_flow-20.10.23-py3-none-any.whl/neuro_flow/cli/live.py
@@ -0,0 +1,104 @@
+import click
+from typing import List, Optional, Tuple
+
+from neuro_flow.cli.click_types import LIVE_JOB, LIVE_JOB_OR_ALL, SUFFIX_AFTER_LIVE_JOB
+from neuro_flow.cli.utils import argument, option, wrap_async
+from neuro_flow.live_runner import LiveRunner
+
+from .root import Root
+
+
+@click.command()
+@wrap_async()
+async def ps(
+    root: Root,
+) -> None:
+    """List all jobs"""
+    async with LiveRunner(root.config_dir, root.console) as runner:
+        await runner.ps()
+
+
+@click.command()
+@option("-s", "--suffix", help="Optional suffix for multi-jobs")
+@click.option(
+    "--param", type=(str, str), multiple=True, help="Set params of the batch config"
+)
+@argument("job-id", type=LIVE_JOB)
+@argument("args", nargs=-1)
+@wrap_async()
+async def run(
+    root: Root,
+    job_id: str,
+    suffix: Optional[str],
+    args: Optional[Tuple[str]],
+    param: List[Tuple[str, str]],
+) -> None:
+    """Run a job.
+
+    RUN job JOB-ID or ATTACH to it if the job is already running
+
+    For multi-jobs an explicit job suffix can be used with explicit job arguments.
+    """
+    if args:
+        root.console.print(
+            "[yellow]args are deprecated, use --param instead",
+        )
+    async with LiveRunner(root.config_dir, root.console) as runner:
+        await runner.run(job_id, suffix, args, {key: value for key, value in param})
+
+
+@click.command()
+@argument("job-id", type=LIVE_JOB)
+@argument("suffix", required=False, type=SUFFIX_AFTER_LIVE_JOB)
+@wrap_async()
+async def logs(
+    root: Root,
+    job_id: str,
+    suffix: Optional[str],
+) -> None:
+    """Print logs.
+
+    Display logs for JOB-ID
+    """
+    async with LiveRunner(root.config_dir, root.console) as runner:
+        await runner.logs(job_id, suffix)
+
+
+@click.command()
+@argument("job-id", type=LIVE_JOB)
+@argument("suffix", required=False, type=SUFFIX_AFTER_LIVE_JOB)
+@wrap_async()
+async def status(
+    root: Root,
+    job_id: str,
+    suffix: Optional[str],
+) -> None:
+    """Show job status.
+
+    Print status for JOB-ID
+    """
+    async with LiveRunner(root.config_dir, root.console) as runner:
+        await runner.status(job_id, suffix)
+
+
+@click.command()
+@argument("job-id", type=LIVE_JOB_OR_ALL)
+@argument("suffix", required=False, type=SUFFIX_AFTER_LIVE_JOB)
+@wrap_async()
+async def kill(
+    root: Root,
+    job_id: str,
+    suffix: Optional[str],
+) -> None:
+    """Kill a job.
+
+    Kill JOB-ID, use `kill ALL` for killing all jobs."""
+    async with LiveRunner(root.config_dir, root.console) as runner:
+        if job_id != "ALL":
+            await runner.kill(job_id, suffix)
+        else:
+            if suffix is not None:
+                raise click.BadArgumentUsage(
+                    "Suffix is not supported when killing ALL jobs"
+                )
+            await runner.kill_all()
