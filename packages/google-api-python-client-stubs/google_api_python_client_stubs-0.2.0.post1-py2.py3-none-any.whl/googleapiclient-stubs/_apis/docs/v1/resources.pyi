eee88a437f65888afc9db44f1f2e06493818daf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:31 +0000

    Add package google-api-python-client-stubs/0.2.0.post1/google_api_python_client_stubs-0.2.0.post1-py2.py3-none-any.whl

---
+++ packages/google-api-python-client-stubs/google_api_python_client_stubs-0.2.0.post1-py2.py3-none-any.whl/googleapiclient-stubs/_apis/docs/v1/resources.pyi
@@ -0,0 +1,48 @@
+import typing
+
+import httplib2  # type: ignore
+import typing_extensions
+
+import googleapiclient.discovery
+import googleapiclient.http  # type: ignore
+
+from .schemas import *
+@typing.type_check_only
+class DocsResource(googleapiclient.discovery.Resource):
+    @typing.type_check_only
+    class DocumentsResource(googleapiclient.discovery.Resource):
+        def batchUpdate(
+            self,
+            *,
+            documentId: str,
+            body: BatchUpdateDocumentRequest = ...,
+            **kwargs: typing.Any
+        ) -> BatchUpdateDocumentResponseHttpRequest: ...
+        def create(
+            self, *, body: Document = ..., **kwargs: typing.Any
+        ) -> DocumentHttpRequest: ...
+        def get(
+            self,
+            *,
+            documentId: str,
+            suggestionsViewMode: typing_extensions.Literal[
+                "DEFAULT_FOR_CURRENT_ACCESS",
+                "SUGGESTIONS_INLINE",
+                "PREVIEW_SUGGESTIONS_ACCEPTED",
+                "PREVIEW_WITHOUT_SUGGESTIONS",
+            ] = ...,
+            **kwargs: typing.Any
+        ) -> DocumentHttpRequest: ...
+    def documents(self) -> DocumentsResource: ...
+
+@typing.type_check_only
+class BatchUpdateDocumentResponseHttpRequest(googleapiclient.http.HttpRequest):
+    def execute(
+        self, http: typing.Optional[httplib2.Http] = ..., num_retries: int = ...
+    ) -> BatchUpdateDocumentResponse: ...
+
+@typing.type_check_only
+class DocumentHttpRequest(googleapiclient.http.HttpRequest):
+    def execute(
+        self, http: typing.Optional[httplib2.Http] = ..., num_retries: int = ...
+    ) -> Document: ...
---
+++ packages/google-api-python-client-stubs/google_api_python_client_stubs-0.2.0.post1-py2.py3-none-any.whl/googleapiclient-stubs/_apis/localservices/v1/resources.pyi
@@ -0,0 +1,61 @@
+import typing
+
+import httplib2  # type: ignore
+import typing_extensions
+
+import googleapiclient.discovery
+import googleapiclient.http  # type: ignore
+
+from .schemas import *
+@typing.type_check_only
+class LocalservicesResource(googleapiclient.discovery.Resource):
+    @typing.type_check_only
+    class AccountReportsResource(googleapiclient.discovery.Resource):
+        def search(
+            self,
+            *,
+            endDate_day: int = ...,
+            endDate_month: int = ...,
+            endDate_year: int = ...,
+            pageSize: int = ...,
+            pageToken: str = ...,
+            query: str = ...,
+            startDate_day: int = ...,
+            startDate_month: int = ...,
+            startDate_year: int = ...,
+            **kwargs: typing.Any
+        ) -> GoogleAdsHomeservicesLocalservicesV1SearchAccountReportsResponseHttpRequest: ...
+    @typing.type_check_only
+    class DetailedLeadReportsResource(googleapiclient.discovery.Resource):
+        def search(
+            self,
+            *,
+            endDate_day: int = ...,
+            endDate_month: int = ...,
+            endDate_year: int = ...,
+            pageSize: int = ...,
+            pageToken: str = ...,
+            query: str = ...,
+            startDate_day: int = ...,
+            startDate_month: int = ...,
+            startDate_year: int = ...,
+            **kwargs: typing.Any
+        ) -> GoogleAdsHomeservicesLocalservicesV1SearchDetailedLeadReportsResponseHttpRequest: ...
+    def accountReports(self) -> AccountReportsResource: ...
+    def detailedLeadReports(self) -> DetailedLeadReportsResource: ...
+
+@typing.type_check_only
+class GoogleAdsHomeservicesLocalservicesV1SearchAccountReportsResponseHttpRequest(
+    googleapiclient.http.HttpRequest
+):
+    def execute(
+        self, http: typing.Optional[httplib2.Http] = ..., num_retries: int = ...
+    ) -> GoogleAdsHomeservicesLocalservicesV1SearchAccountReportsResponse: ...
+
+@typing.type_check_only
+class GoogleAdsHomeservicesLocalservicesV1SearchDetailedLeadReportsResponseHttpRequest(
+    googleapiclient.http.HttpRequest
+):
+    def execute(
+        self, http: typing.Optional[httplib2.Http] = ..., num_retries: int = ...
+    ) -> GoogleAdsHomeservicesLocalservicesV1SearchDetailedLeadReportsResponse: ...
