10584691fa77bf0dbc6a4e8eeed94d39667791e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:51:15 +0000

    Add package aireport/0.1.10/aireport-0.1.10.tar.gz

---
+++ packages/aireport/aireport-0.1.10.tar.gz/aireport-0.1.10/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aireport', 'aireport.tools']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['aihelper>=0.2.5,<0.3.0',
+ 'black>=19.10b0,<20.0',
+ 'numpy',
+ 'pandas>=1.0.5,<2.0.0']
+
+setup_kwargs = {
+    'name': 'aireport',
+    'version': '0.1.10',
+    'description': '',
+    'long_description': None,
+    'author': 'None',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
