75e28cd13215e58120f194ccafe6f11be739bb6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:10 +0000

    Add package gwdama/0.4.0/gwdama-0.4.0.tar.gz

---
+++ packages/gwdama/gwdama-0.4.0.tar.gz/gwdama-0.4.0/gwdama/__init__.py
@@ -0,0 +1,7 @@
+"""A python package to stor and manipulate gravitational-wava data
+in a conveninet framework.
+Part of its classes and methods have been taken from GwPy, by D.McLeod
+"""
+
+# set package metadata
+__version__ = "0.4.0"
