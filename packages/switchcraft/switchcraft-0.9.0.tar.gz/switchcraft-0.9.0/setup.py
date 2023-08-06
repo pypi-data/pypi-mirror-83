111525afec9db68d8a35edda37ed483cdbd1d580
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:17:36 +0000

    Add package switchcraft/0.9.0/switchcraft-0.9.0.tar.gz

---
+++ packages/switchcraft/switchcraft-0.9.0.tar.gz/switchcraft-0.9.0/setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['switchcraft',
+ 'switchcraft.clients',
+ 'switchcraft.conversion',
+ 'switchcraft.conversion.arnparse']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['boto3>=1.16.3,<2.0.0', 'pydantic>=1.6.1,<2.0.0']
+
+setup_kwargs = {
+    'name': 'switchcraft',
+    'version': '0.9.0',
+    'description': 'Client wrappers and helpful utilities to solve common coding challenges in AWS',
+    'long_description': '',
+    'author': 'WWPS ProServe',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
---
+++ packages/switchcraft/switchcraft-0.9.0.tar.gz/switchcraft-0.9.0/switchcraft/clients/__init__.py
@@ -0,0 +1,7 @@
+"""Clients Package."""
+
+from .alb import AlbClient
+from .config import ConfigClient, Evaluation, ComplianceType
+from .ec2 import EC2Client
+from .sns import SNSClient
+from .dynamodb import DynamoDBClient
