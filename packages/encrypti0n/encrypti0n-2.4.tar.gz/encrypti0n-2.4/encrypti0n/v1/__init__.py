376ad38694475c1f0cbccef683765f3077d2e024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:20:48 +0000

    Add package encrypti0n/2.4/encrypti0n-2.4.tar.gz

---
+++ packages/encrypti0n/encrypti0n-2.4.tar.gz/encrypti0n-2.4/encrypti0n/v1/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# imports.
+from encrypti0n.v1.classes.config import *
+from encrypti0n.v1.classes import utils
+from encrypti0n.v1.classes.encryption import Encryption
