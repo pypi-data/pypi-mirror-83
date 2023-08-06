fa57c62f6e521b4a43b2e04dd684bad17e317c5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:26 +0000

    Add package pip-plus-plus/0.0.7/pip_plus_plus-0.0.7-py3-none-any.whl

---
+++ packages/pip-plus-plus/pip_plus_plus-0.0.7-py3-none-any.whl/pipplusplus/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("pipplusplus/README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pip-plus-plus",
+    version="0.0.7",
+    author="Idan Cohen",
+    author_email="idan57@gmail.com",
+    description="Pip++",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/idan57/pip-plus-plus",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'flask',
+
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
