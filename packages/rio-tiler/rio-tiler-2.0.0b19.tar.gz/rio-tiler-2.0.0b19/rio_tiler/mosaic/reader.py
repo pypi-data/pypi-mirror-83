99791f139eefd6c788b137ab674b208d656ddd27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:53 +0000

    Add package rio-tiler/2.0.0b19/rio-tiler-2.0.0b19.tar.gz

---
+++ packages/rio-tiler/rio-tiler-2.0.0b19.tar.gz/rio-tiler-2.0.0b19/rio_tiler/mosaic/reader.py
@@ -0,0 +1,97 @@
+"""rio_tiler.mosaic: create tile from multiple assets."""
+
+from inspect import isclass
+from typing import Any, Callable, List, Optional, Sequence, Tuple, Type, Union, cast
+
+import numpy
+
+from ..constants import MAX_THREADS
+from ..errors import InvalidMosaicMethod, TileOutsideBounds
+from ..tasks import create_tasks, filter_tasks
+from ..utils import _chunks
+from .methods.base import MosaicMethodBase
+from .methods.defaults import FirstMethod
+
+
+def mosaic_reader(
+    assets: Sequence[str],
+    reader: Callable,
+    *args: Any,
+    pixel_selection: Union[Type[MosaicMethodBase], MosaicMethodBase] = FirstMethod,
+    chunk_size: Optional[int] = None,
+    threads: int = MAX_THREADS,
+    allowed_exceptions: Tuple = (TileOutsideBounds,),
+    **kwargs,
+) -> Tuple[Tuple[numpy.ndarray, numpy.ndarray], Sequence[str]]:
+    """
+    Merge multiple assets.
+
+    Attributes
+    ----------
+    assets: list or tuple
+        List of tiler compatible asset.
+    reader: callable
+        reader function. The function MUST take asset, *args, **kwargs as arguments,
+        and MUST return a tuple with tile data and mask
+        e.g:
+        def reader(asset: str, *args, **kwargs) -> Tuple[numpy.ndarray, numpy.ndarray]:
+            with COGReader(asset) as cog:
+                return cog.tile(*args, **kwargs)
+
+        def reader(asset: str, *args, **kwargs) -> Tuple[numpy.ndarray, numpy.ndarray]:
+            with COGReader(asset) as cog:
+                return cog.preview(*args, **kwargs)
+    args: Any
+        additional argument to forward to the reader function.
+    pixel_selection: MosaicMethod, optional
+        Instance of MosaicMethodBase class.
+        default: "rio_tiler.mosaic.methods.defaults.FirstMethod".
+    chunk_size: int, optional
+        Control the number of asset to process per loop (default = threads).
+    threads: int, optional
+        Number of threads to use. If <= 1, runs single threaded without an event
+        loop. By default reads from the MAX_THREADS environment variable, and if
+        not found defaults to multiprocessing.cpu_count() * 5.
+    allowed_exceptions: Tuple, optional
+        List of exceptions which will be ignored. Default is set to `(TileOutsideBounds,)`.
+        Note: `TileOutsideBounds` is likely to be raised and should be included in the allowed_exceptions.
+    kwargs: dict, optional
+        tiler specific options.
+
+    Returns
+    -------
+    (tile, mask), assets_used : tuple of ndarray, sequence of str
+        Return (tile, mask) data and list of assets used.
+
+    """
+    if isclass(pixel_selection):
+        pixel_selection = cast(Type[MosaicMethodBase], pixel_selection)
+
+        if issubclass(pixel_selection, MosaicMethodBase):
+            pixel_selection = pixel_selection()
+
+    if not isinstance(pixel_selection, MosaicMethodBase):
+        raise InvalidMosaicMethod(
+            "Mosaic filling algorithm should be an instance of "
+            "'rio_tiler.mosaic.methods.base.MosaicMethodBase'"
+        )
+
+    if not chunk_size:
+        chunk_size = threads if threads > 1 else len(assets)
+
+    assets_used: List[str] = []
+
+    for chunks in _chunks(assets, chunk_size):
+        tasks = create_tasks(reader, chunks, threads, *args, **kwargs)
+        for (t, m), asset in filter_tasks(
+            tasks, allowed_exceptions=allowed_exceptions,
+        ):
+            assets_used.append(asset)
+            t = numpy.ma.array(t)
+            t.mask = m == 0
+
+            pixel_selection.feed(t)
+            if pixel_selection.is_done:
+                return pixel_selection.data, assets_used
+
+    return pixel_selection.data, assets_used
---
+++ packages/rio-tiler/rio-tiler-2.0.0b19.tar.gz/rio-tiler-2.0.0b19/setup.py
@@ -0,0 +1,59 @@
+"""Setup for rio-tiler."""
+
+from setuptools import find_packages, setup
+
+with open("README.md") as f:
+    readme = f.read()
+
+# Runtime requirements.
+inst_reqs = [
+    "attrs",
+    "boto3",
+    "numexpr",
+    "numpy",
+    "mercantile",
+    "morecantile>=1.1,<2.0",
+    "rasterio>=1.1.7",
+    "requests",
+]
+
+extra_reqs = {
+    "test": ["pytest", "pytest-benchmark", "pytest-cov", "rio-cogeo"],
+    "dev": [
+        "pytest",
+        "pytest-benchmark",
+        "pytest-cov",
+        "pytest-asyncio",
+        "rio-cogeo",
+        "pre-commit",
+    ],
+    "docs": ["nbconvert", "mkdocs", "mkdocs-material", "pygments", "mkdocs-jupyter"],
+}
+
+setup(
+    name="rio-tiler",
+    version="2.0.0b19",
+    python_requires=">=3.5",
+    description="Rasterio plugin to read mercator tiles from Cloud Optimized GeoTIFF.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: GIS",
+    ],
+    keywords="COG cogeo raster aws map tiles gdal rasterio",
+    author="Vincent Sarago",
+    author_email="vincent@developmentseed.org",
+    url="https://github.com/cogeotiff/rio-tiler",
+    license="BSD",
+    packages=find_packages(exclude=["ez_setup", "examples", "tests"]),
+    include_package_data=True,
+    package_data={"rio_tiler": ["cmap_data/*.npy"]},
+    zip_safe=False,
+    install_requires=inst_reqs,
+    extras_require=extra_reqs,
+)
