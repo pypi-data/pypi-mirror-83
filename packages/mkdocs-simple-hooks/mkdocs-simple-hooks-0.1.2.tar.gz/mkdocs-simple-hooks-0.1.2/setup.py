5205414f054cae3eec0f85a83701a35c25be69c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:01 +0000

    Add package mkdocs-simple-hooks/0.1.2/mkdocs-simple-hooks-0.1.2.tar.gz

---
+++ packages/mkdocs-simple-hooks/mkdocs-simple-hooks-0.1.2.tar.gz/mkdocs-simple-hooks-0.1.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="mkdocs-simple-hooks",
+    version="0.1.2",
+    author="Andrzej Klajnert",
+    author_email="python@aklajnert.pl",
+    description="Define your own hooks for mkdocs, without having to create a new package.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aklajnert/mkdocs-simple-hooks",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["mkdocs>=1"],
+    extras_require={"test": ["pytest>=4.0", "pytest-cov"]},
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={
+        "mkdocs.plugins": [
+            "mkdocs-simple-hooks = mkdocs_simple_hooks:SimpleHooksPlugin"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Documentation",
+    ],
+)
