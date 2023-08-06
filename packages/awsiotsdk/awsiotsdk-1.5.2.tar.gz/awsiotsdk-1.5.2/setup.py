180c1604fa1de24b07e0289be6c1ab325159e094
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:10:03 +0000

    Add package awsiotsdk/1.5.2/awsiotsdk-1.5.2.tar.gz

---
+++ packages/awsiotsdk/awsiotsdk-1.5.2.tar.gz/awsiotsdk-1.5.2/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0.
+
+from setuptools import setup
+
+setup(
+    name='awsiotsdk',
+    version='1.5.2',
+    description='AWS IoT SDK based on the AWS Common Runtime',
+    author='AWS SDK Common Runtime Team',
+    url='https://github.com/aws/aws-iot-device-sdk-python-v2',
+    packages=['awsiot'],
+    install_requires=[
+        'awscrt==0.9.10',
+    ],
+    python_requires='>=3.5',
+)
