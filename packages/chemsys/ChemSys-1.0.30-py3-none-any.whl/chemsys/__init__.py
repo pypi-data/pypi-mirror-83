f13941c00139c796dcc2b66b831ea377a7170b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:00 +0000

    Add package chemsys/1.0.30/ChemSys-1.0.30-py3-none-any.whl

---
+++ packages/chemsys/ChemSys-1.0.30-py3-none-any.whl/chemsys/__init__.py
@@ -0,0 +1,14 @@
+from . import chemsys
+from . import chemsystest
+
+from .chemsys import *
+from .chemsystest import *
+
+__all__ = ["chemsys", "chemsystest"]
+
+
+__all__.extend(chemsys.__all__)
+
+__all__.extend(chemsystest.__all__)
+
+from chemsys import *
