dff731985313cfa5ab7d6c620e7ba07b98745bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:55 +0000

    Add package sentence-spliter/1.0.6/sentence_spliter-1.0.6.tar.gz

---
+++ packages/sentence-spliter/sentence_spliter-1.0.6.tar.gz/sentence_spliter-1.0.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sentence_spliter",
+    version="1.0.6",
+    author="bohuai jiang",
+    author_email="highjump000@hotmail.com",
+    description="This is a sentence cutting tool that supports long sentence segmentation and short sentence merging.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    license="MIT",
+    # package_dir={"":"sentence_spliter"},
+    packages=['sentence_spliter'],
+
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+    ],
+    REQUIRES_PYTHON='>=3.6.0',
+    install_requires=['attrdict==2.0.1',
+                      'certifi==2018.8.24',
+                      'six==1.15.0'
+                      ]
+)
