cd400420203a829d46c5387b1e69d1569b36cbe5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:08:32 +0000

    Add package ows-language-model/0.1.1/ows_language_model-0.1.1-py3-none-any.whl

---
+++ packages/ows-language-model/ows_language_model-0.1.1-py3-none-any.whl/ows_language_model/model.py
@@ -0,0 +1,11 @@
+from ows_language_model.config import config
+
+# TODO: define model here
+
+# defines all callbacks, scheduling and builds model here
+
+model = None
+
+if __name__ == '__main__':
+    # could print summary
+    pass
