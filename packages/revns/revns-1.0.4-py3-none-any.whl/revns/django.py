6c7153f96a7101f5ee45a576050ec3cf19f20eab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:14 +0000

    Add package revns/1.0.4/revns-1.0.4-py3-none-any.whl

---
+++ packages/revns/revns-1.0.4-py3-none-any.whl/revns/django.py
@@ -0,0 +1,35 @@
+from django.conf import settings
+from revns import BaseNotification as PureBaseNotification, MobileNotification as PureMobileNotification, \
+    EmailNotification as PureEmailNotification, TemplatedEmailNotification as PureTemplatedNotification, \
+    SmsNotification as PureSmsNotification
+
+
+class BaseNotification(PureBaseNotification):
+    def __init__(self, *args, **kwargs):
+        kwargs['api_key'] = settings.CLIENT_SECRET
+        kwargs['client_id'] = settings.CLIENT_ID
+        stage = getattr(settings, 'STAGE')
+        if stage in ['prod', 'PROD']:
+            kwargs['stage'] = 'PROD'
+        elif stage in ['stg', 'STG']:
+            kwargs['stage'] = 'STG'
+        else:
+            kwargs['stage'] = 'DEV'
+        super().__init__(*args, **kwargs)
+
+
+class MobileNotification(BaseNotification, PureMobileNotification):
+    @property
+    def public_topic(self):
+        return f'{self.client_id}-public-topic'
+
+
+class EmailNotification(BaseNotification, PureEmailNotification):
+    pass
+
+
+class TemplatedNotification(BaseNotification, PureTemplatedNotification):
+    pass
+
+class SmsNotification(BaseNotification, PureSmsNotification):
+    pass
