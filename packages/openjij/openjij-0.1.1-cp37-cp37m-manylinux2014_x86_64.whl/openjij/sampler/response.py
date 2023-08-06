19ba6be0243d328628b4a6279e70c22fca068164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:03 +0000

    Add package openjij/0.1.1/openjij-0.1.1-cp37-cp37m-manylinux2014_x86_64.whl

---
+++ packages/openjij/openjij-0.1.1-cp37-cp37m-manylinux2014_x86_64.whl/openjij/sampler/response.py
@@ -0,0 +1,40 @@
+# Copyright 2019 Jij Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+| OpenJij response class.
+| The usage is almost the same as :class:`dimod.SampleSet`.
+"""
+
+import numpy as np
+import dimod
+import openjij
+
+
+class Response(dimod.SampleSet):
+    @property
+    def states(self):
+        return self.record.sample
+
+    @property
+    def indices(self):
+        return list(self.variables)
+
+    @property
+    def energies(self):
+        return self.record.energy
+
+    @property
+    def min_samples(self):
+        return self.first.sample
+
