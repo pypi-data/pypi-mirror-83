671f73144107e01f7bbe4c08c935cc9deebd8e20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:10:29 +0000

    Add package kmerexplor/0.5.2/kmerexplor-0.5.2-py3-none-any.whl

---
+++ packages/kmerexplor/kmerexplor-0.5.2-py3-none-any.whl/kmerexplor/info.py
@@ -0,0 +1,8 @@
+# info.py
+
+APPNAME = "KmerExploR"
+SHORTDESC = "KmerExploR provides information on RNA-sequencing datasets."
+LICENCE = "GPL3"
+VERSION = "0.5.2"
+AUTHOR = "Benoit Guibert"
+AUTHOR_EMAIL = "benoit.guibert@free.fr"
