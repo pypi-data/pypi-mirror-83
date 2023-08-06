d868bb4822c7ef31a4bb1e2adbb31ee064715ab3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:26 +0000

    Add package MLecon/0.0.5/MLecon-0.0.5.tar.gz

---
+++ packages/MLecon/MLecon-0.0.5.tar.gz/MLecon-0.0.5/MLecon/Regression/__init__.py
@@ -0,0 +1,5 @@
+#__all__ = ['Regression_kernel' , 'Regression_Sieves' , 'Residuals']
+
+from Regression.Regression_kernel import regression_kernel
+from Regression.Regression_sieves import Sieves
+from Regression.Residuals import residuals
