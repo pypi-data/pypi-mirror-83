a178e199ec2055eb124a84d9482d7afecd4d5539
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:51 +0000

    Add package volkszaehler/0.2.0/volkszaehler-0.2.0.tar.gz

---
+++ packages/volkszaehler/volkszaehler-0.2.0.tar.gz/volkszaehler-0.2.0/CHANGES.rst
@@ -0,0 +1,27 @@
+Changes
+=======
+
+20201026 - 0.2.0
+----------------
+
+- Add support for HTTPS
+
+20200626 - 0.1.3
+----------------
+
+- Update URLs
+
+20180825 - 0.1.2
+----------------
+
+- Fix setup.py
+
+20180825 - 0.1.1
+----------------
+
+- Add support for the tuples
+- Update README
+
+20180814 - 0.1.0
+----------------
+- Initial release
---
+++ packages/volkszaehler/volkszaehler-0.2.0.tar.gz/volkszaehler-0.2.0/volkszaehler/__init__.py
@@ -0,0 +1,47 @@
+"""Wrapper for interacting with the Volkszahler API."""
+import asyncio
+import logging
+
+import aiohttp
+import async_timeout
+
+from . import exceptions
+
+_LOGGER = logging.getLogger(__name__)
+_RESOURCE = "{schema}://{host}:{port}/middleware.php/data/{uuid}.json"
+
+
+class Volkszaehler(object):
+    """A class for handling the data retrieval."""
+
+    def __init__(self, loop, session, uuid, host="localhost", port=80, tls=False):
+        """Initialize the connection to the API."""
+        self._loop = loop
+        self._session = session
+        if tls:
+            self.url = _RESOURCE.format(schema="https", host=host, port=port, uuid=uuid)
+        else:
+            self.url = _RESOURCE.format(schema="http", host=host, port=port, uuid=uuid)
+        self.data = {}
+        self.average = self.max = self.min = self.consumption = None
+        self.tuples = []
+
+    async def get_data(self):
+        """Retrieve the data."""
+        try:
+            with async_timeout.timeout(5, loop=self._loop):
+                response = await self._session.get(self.url)
+
+            _LOGGER.debug("Response from Volkszaehler API: %s", response.status)
+            self.data = await response.json()
+            _LOGGER.debug(self.data)
+        except (asyncio.TimeoutError, aiohttp.ClientError):
+            _LOGGER.error("Can not load data from Volkszaehler API")
+            self.data = None
+            raise exceptions.VolkszaehlerApiConnectionError()
+
+        self.average = self.data["data"]["average"]
+        self.max = self.data["data"]["max"][1]
+        self.min = self.data["data"]["min"][1]
+        self.consumption = self.data["data"]["consumption"]
+        self.tuples = self.data["data"]["tuples"]
