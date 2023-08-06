dec73ab4c9d44ebd331ce708ee98a157739d3350
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:56 +0000

    Add package inst341data/0.0.7/inst341data-0.0.7.tar.gz

---
+++ packages/inst341data/inst341data-0.0.7.tar.gz/inst341data-0.0.7/README.md
@@ -0,0 +1,3 @@
+A little helper function to easily download data from GitHub into a Jupyter
+Notebook running on Colab. In the event I can't get all my students into the
+Google Drive share.
---
+++ packages/inst341data/inst341data-0.0.7.tar.gz/inst341data-0.0.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = 'inst341data',
+    version = '0.0.7',
+    author = 'Ed Summers',
+    author_email = 'ehs@pobox.com',
+    url = 'https://github.com/edsu/inst341data',
+    py_modules = ['inst341data',],
+    description = 'install data in jupyter notebook',
+    install_requires = [],
+)
