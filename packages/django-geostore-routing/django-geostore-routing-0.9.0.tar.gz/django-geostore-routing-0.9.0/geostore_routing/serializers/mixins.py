15a4540b2addc403f7fdb158aa3d49fa74acf425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:38 +0000

    Add package django-geostore-routing/0.9.0/django-geostore-routing-0.9.0.tar.gz

---
+++ packages/django-geostore-routing/django-geostore-routing-0.9.0.tar.gz/django-geostore-routing-0.9.0/geostore_routing/serializers/mixins.py
@@ -0,0 +1,9 @@
+from django.urls import reverse
+from rest_framework import serializers
+
+
+class RoutingLayerSerializer:
+    routing_url = serializers.SerializerMethodField()
+
+    def get_routing_url(self, obj):
+        return reverse('layer-route', args=[obj.pk, ])
