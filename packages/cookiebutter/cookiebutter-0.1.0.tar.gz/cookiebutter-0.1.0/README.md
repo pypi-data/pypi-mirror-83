7ddbf1fca1552c24bbc892589a7baf4aa76ea65f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:42 +0000

    Add package cookiebutter/0.1.0/cookiebutter-0.1.0.tar.gz

---
+++ packages/cookiebutter/cookiebutter-0.1.0.tar.gz/cookiebutter-0.1.0/README.md
@@ -0,0 +1,9 @@
+# cookiebutter 🍪🧈
+
+[![PyPI](https://img.shields.io/pypi/v/cookiebutter)](https://pypi.org/project/cookiebutter/)
+[![PyPI - Python Version](https://img.shields.io/pypi/pyversions/cookiebutter)](https://pypi.org/project/cookiebutter/)
+[![PyPI License](https://img.shields.io/pypi/l/cookiebutter)](https://pypi.org/project/cookiebutter/)
+[![Code Style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black/)
+
+Cookiecutter, but all buttered up.  
+Use cookiecutter with yaml files, or even with python files.
---
+++ packages/cookiebutter/cookiebutter-0.1.0.tar.gz/cookiebutter-0.1.0/cookiebutter/__init__.py
@@ -0,0 +1,28 @@
+import json
+import subprocess
+import sys
+from pathlib import Path
+
+import yaml
+
+
+def wrap_cookiecutter():
+    yaml_names = ('cookiecutter.yml', 'cookiecutter.yaml')
+    for yaml_name in yaml_names:
+        if Path(yaml_name).exists():
+            yaml_to_json(yaml_name)
+            break
+
+    try:
+        subprocess.run(['cookiecutter', *sys.argv[1:]])
+    finally:
+        Path('cookiecutter.json').unlink()
+
+
+def yaml_to_json(name: str):
+    with open(name) as f:
+        yaml_data = yaml.safe_load(f)
+        json_data = json.dumps(yaml_data)
+
+    with open('cookiecutter.json', 'w') as f:
+        f.write(json_data)
