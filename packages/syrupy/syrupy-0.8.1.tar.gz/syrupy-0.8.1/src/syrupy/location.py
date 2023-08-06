10964674f7357d80a70cd28088b04a82cf02344e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:46 +0000

    Add package syrupy/0.8.1/syrupy-0.8.1.tar.gz

---
+++ packages/syrupy/syrupy-0.8.1.tar.gz/syrupy-0.8.1/src/syrupy/location.py
@@ -0,0 +1,57 @@
+from pathlib import Path
+from typing import (
+    Any,
+    Iterator,
+    Optional,
+)
+
+
+class TestLocation:
+    def __init__(self, node: Any):
+        self._node = node
+        self.filepath = self._node.fspath
+        self.modulename = self._node.obj.__module__
+        self.methodname = self._node.obj.__name__
+        self.nodename = getattr(self._node, "name", None)
+        self.testname = self.nodename or self.methodname
+
+    @property
+    def classname(self) -> Optional[str]:
+        _, __, qualname = self._node.location
+        return ".".join(list(self.__valid_ids(qualname))[:-1]) or None
+
+    @property
+    def filename(self) -> str:
+        return Path(self.filepath).stem
+
+    @property
+    def snapshot_name(self) -> str:
+        if self.classname is not None:
+            return f"{self.classname}.{self.testname}"
+        return str(self.testname)
+
+    def __valid_id(self, name: str) -> str:
+        valid_id = ""
+        for char in name:
+            new_valid_id = f"{valid_id}{char}"
+            if not new_valid_id.isidentifier():
+                break
+            valid_id = new_valid_id
+        return valid_id
+
+    def __valid_ids(self, name: str) -> Iterator[str]:
+        for n in name.split("."):
+            valid_id = self.__valid_id(n)
+            if valid_id:
+                yield valid_id
+            if valid_id != n:
+                break
+
+    def __parse(self, name: str) -> str:
+        return ".".join(self.__valid_ids(name))
+
+    def matches_snapshot_name(self, snapshot_name: str) -> bool:
+        return self.__parse(self.snapshot_name) == self.__parse(snapshot_name)
+
+    def matches_snapshot_location(self, snapshot_location: str) -> bool:
+        return self.filename in snapshot_location
