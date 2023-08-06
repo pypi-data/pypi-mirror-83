79333957c401d3eb8da325e41dc5e69daf03b730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:26:57 +0000

    Add package fintoc/0.2.1/fintoc-0.2.1-py3-none-any.whl

---
+++ packages/fintoc/fintoc-0.2.1-py3-none-any.whl/fintoc/__init__.py
@@ -0,0 +1,13 @@
+"""
+__init__.py
+===========
+
+Let's initialize this machine.
+"""
+
+__version__ = "0.2.1"
+
+import locale
+from fintoc.client import Client
+
+locale.setlocale(locale.LC_ALL, "")
