aa2d13e29e4a75b6038691d29978baf2a1a352f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:09:07 +0000

    Add package flow-network/0.1.7/flow-network-0.1.7.tar.gz

---
+++ packages/flow-network/flow-network-0.1.7.tar.gz/flow-network-0.1.7/flow_network/core/base_network.h
@@ -0,0 +1,18 @@
+#ifndef FLOW_NETWORK_BASE_NETWORK_H
+#define FLOW_NETWORK_BASE_NETWORK_H
+
+#include "graph.h"
+
+namespace flow_network {
+
+    struct BaseNetwork {
+        int *dist, n;
+        Graph graph;
+
+        explicit BaseNetwork(int n);
+
+        ~BaseNetwork();
+    };
+}
+
+#endif //FLOW_NETWORK_BASE_NETWORK_H
