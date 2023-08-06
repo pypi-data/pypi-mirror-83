9d661540131c20bc87de528ddb0d2e0f584f2beb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:31 +0000

    Add package tomodachi/0.19.1/tomodachi-0.19.1.tar.gz

---
+++ packages/tomodachi/tomodachi-0.19.1.tar.gz/tomodachi-0.19.1/tomodachi/discovery/__init__.py
@@ -0,0 +1,4 @@
+from tomodachi.discovery.aws_sns_registration import AWSSNSRegistration
+from tomodachi.discovery.dummy_registry import DummyRegistry
+
+__all__ = ["DummyRegistry", "AWSSNSRegistration"]
