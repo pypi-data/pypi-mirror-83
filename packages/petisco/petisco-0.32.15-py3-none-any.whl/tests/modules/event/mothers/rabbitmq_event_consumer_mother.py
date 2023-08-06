a95522e64cbb26ce7fa987bf4e3f4af57263515b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:09 +0000

    Add package petisco/0.32.15/petisco-0.32.15-py3-none-any.whl

---
+++ packages/petisco/petisco-0.32.15-py3-none-any.whl/tests/modules/event/mothers/rabbitmq_event_consumer_mother.py
@@ -0,0 +1,52 @@
+from petisco import RabbitMqConnector
+from petisco.event.consumer.infrastructure.rabbitmq_event_consumer import (
+    RabbitMqEventConsumer,
+)
+from tests.modules.event.mothers.defaults import (
+    DEFAULT_ORGANIZATION,
+    DEFAULT_SERVICE,
+    DEFAULT_MAX_RETRIES,
+    DEFAULT_VERBOSE,
+)
+
+
+class RabbitMqEventConsumerMother:
+    @staticmethod
+    def default(connector: RabbitMqConnector = None):
+        connector = RabbitMqConnector() if not connector else connector
+        return RabbitMqEventConsumer(
+            connector,
+            DEFAULT_ORGANIZATION,
+            DEFAULT_SERVICE,
+            DEFAULT_MAX_RETRIES,
+            DEFAULT_VERBOSE,
+        )
+
+    @staticmethod
+    def with_service(service: str, connector: RabbitMqConnector = None):
+        connector = RabbitMqConnector() if not connector else connector
+        return RabbitMqEventConsumer(
+            connector,
+            DEFAULT_ORGANIZATION,
+            service,
+            DEFAULT_MAX_RETRIES,
+            DEFAULT_VERBOSE,
+        )
+
+    @staticmethod
+    def with_max_retries(max_retries: int, connector: RabbitMqConnector = None):
+        connector = RabbitMqConnector() if not connector else connector
+        return RabbitMqEventConsumer(
+            connector,
+            DEFAULT_ORGANIZATION,
+            DEFAULT_SERVICE,
+            max_retries,
+            DEFAULT_VERBOSE,
+        )
+
+    @staticmethod
+    def without_retry(connector: RabbitMqConnector = None):
+        connector = RabbitMqConnector() if not connector else connector
+        return RabbitMqEventConsumer(
+            connector, DEFAULT_ORGANIZATION, DEFAULT_SERVICE, 0, DEFAULT_VERBOSE
+        )
