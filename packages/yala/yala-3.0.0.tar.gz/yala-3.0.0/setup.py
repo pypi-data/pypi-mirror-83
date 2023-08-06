8f20bf88ed4bd0c51b03a680405beb4be6d28c32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:26 +0000

    Add package yala/3.0.0/yala-3.0.0.tar.gz

---
+++ packages/yala/yala-3.0.0.tar.gz/yala-3.0.0/setup.py
@@ -0,0 +1,77 @@
+"""A setuptools based setup module."""
+from os import path
+
+from setuptools import setup
+
+from yala import __version__
+
+HERE = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(HERE, 'README.rst')) as f:
+    LONG_DESC = f.read()
+
+setup(
+    name='yala',
+    version=__version__,
+    description='Yet Another Linter Aggregator',
+    long_description=LONG_DESC,
+    url='https://github.com/cemsbr/yala',
+    author='Carlos Eduardo Moreira dos Santos',
+    author_email='cems@cemshost.com.br',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing',
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    keywords='linter check quality',
+    packages=['yala'],
+    install_requires=[
+        'docopt',
+        'isort>=5',  # deprecated --recursive flag
+        'pycodestyle',
+        'pylint',
+    ],
+    extras_require={
+        'all': [
+            'mypy',
+            'pydocstyle',
+            'pyflakes',
+            'radon',
+        ],
+        'flake8': ['flake8', 'flake8-polyfill'],
+        'mypy': ['mypy'],
+        'pydocstyle': ['pydocstyle'],
+        'pyflakes': ['pyflakes'],
+        'pylint': ['pylint'],
+        'radon': ['radon'],
+        'dev': [
+            'bandit',
+            'coverage',
+            'eradicate',
+            'pipenv',
+            'rstcheck',
+            'safety',
+            'tox',
+        ]
+    },
+    package_data={
+        'yala': ['setup.cfg', 'logging.ini'],
+    },
+    entry_points={
+        'console_scripts': [
+            'yala=yala.main:main',
+        ],
+    },
+    test_suite='tests',
+)
---
+++ packages/yala/yala-3.0.0.tar.gz/yala-3.0.0/yala/__init__.py
@@ -0,0 +1,8 @@
+"""Root logger configuration."""
+import logging.config
+from pathlib import Path
+
+__version__ = '3.0.0'
+
+CONFIG_PATH = Path(__file__).parent / 'logging.ini'
+logging.config.fileConfig(str(CONFIG_PATH), disable_existing_loggers=False)
