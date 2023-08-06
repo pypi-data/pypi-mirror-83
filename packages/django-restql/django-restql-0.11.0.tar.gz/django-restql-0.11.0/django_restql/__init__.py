c4f4e2c9872e4f95ffa1688b83dbeaf4e9f9818b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:46 +0000

    Add package django-restql/0.11.0/django-restql-0.11.0.tar.gz

---
+++ packages/django-restql/django-restql-0.11.0.tar.gz/django-restql-0.11.0/django_restql/__init__.py
@@ -0,0 +1,11 @@
+__title__ = 'Django RESTQL'
+__description__ = 'Turn your API made with Django REST Framework(DRF) into a GraphQL like API.'
+__url__ = 'https://django-restql.yezyilomo.com'
+__version__ = '0.11.0'
+__author__ = 'Yezy Ilomo'
+__author_email__ = 'yezileliilomo@hotmail.com'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2020 Yezy Ilomo'
+
+# Version synonym
+VERSION = __version__
