bc717747aab9f8a9a84d75a541fba95985634043
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:07 +0000

    Add package watchmaker/0.21.3/watchmaker-0.21.3.tar.gz

---
+++ packages/watchmaker/watchmaker-0.21.3.tar.gz/watchmaker-0.21.3/ci/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export VIRTUAL_ENV_DIR=venv
+
+virtualenv $VIRTUAL_ENV_DIR
+source $VIRTUAL_ENV_DIR/bin/activate
+
+pip install -r requirements/pip.txt
+echo "-----------------------------------------------------------------------"
+python --version
+pip --version
+echo "-----------------------------------------------------------------------"
+pip install -r requirements/build.txt
+pip install --editable .
+
+# creates standalone
+gravitybee --src-dir src --sha file --with-latest --extra-data static --extra-data ../vendor/pypa/get-pip/2.6 --extra-pkgs boto3 --extra-modules boto3
