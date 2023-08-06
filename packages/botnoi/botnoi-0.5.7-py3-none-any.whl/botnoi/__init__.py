48e986089b1170efbd564357eb102333cd696645
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:57 +0000

    Add package botnoi/0.5.7/botnoi-0.5.7-py3-none-any.whl

---
+++ packages/botnoi/botnoi-0.5.7-py3-none-any.whl/botnoi/__init__.py
@@ -0,0 +1,15 @@
+from . import openapi
+from . import scrape
+from . import cv
+from . import nlp
+from . import line
+from . import chatbot
+
+__all__ = [
+    'openapi',
+    'scrape',
+    'cv',
+    'nlp',
+    'line',
+    'chatbot'
+]
