5b98589524d037803099b3d2f6ec8e3a8f0e8060
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:43:52 +0000

    Add package azureml-widgets/1.17.0/azureml_widgets-1.17.0-py3-none-any.whl

---
+++ packages/azureml-widgets/azureml_widgets-1.17.0-py3-none-any.whl/azureml/widgets/_utils.py
@@ -0,0 +1,40 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+
+from azureml.widgets._constants import (WIDGET_MAXIMUM_REFRESH_SLEEP_TIME,
+                                        WIDGET_STARTING_REFRESH_SLEEP_TIME,
+                                        AUTOML_WIDGET_MAXIMUM_REFRESH_SLEEP_TIME,
+                                        AUTOML_WIDGET_STARTING_REFRESH_SLEEP_TIME,
+                                        MULTIRUN_WIDGET_MAXIMUM_REFRESH_SLEEP_TIME,
+                                        MULTIRUN_WIDGET_STARTING_REFRESH_SLEEP_TIME,
+                                        WIDGET_REFRESH_MAXIMUM_SLEEP_TIME_DATABRICKS,
+                                        WIDGET_STARTING_REFRESH_SLEEP_TIME_DATABRICKS)
+
+
+def _update_widget_refresh_sleep_time(alpha=None):
+    if alpha is None:
+        return WIDGET_STARTING_REFRESH_SLEEP_TIME
+    return (1 - alpha) * WIDGET_STARTING_REFRESH_SLEEP_TIME + alpha * WIDGET_MAXIMUM_REFRESH_SLEEP_TIME
+
+
+def _update_automl_widget_refresh_sleep_time(alpha=None):
+    if alpha is None:
+        return AUTOML_WIDGET_STARTING_REFRESH_SLEEP_TIME
+    return (1 - alpha) * AUTOML_WIDGET_STARTING_REFRESH_SLEEP_TIME + alpha * AUTOML_WIDGET_MAXIMUM_REFRESH_SLEEP_TIME
+
+
+def _update_multirun_widget_refresh_sleep_time(alpha=None):
+    if alpha is None:
+        return MULTIRUN_WIDGET_STARTING_REFRESH_SLEEP_TIME
+    interval = (1 - alpha) * MULTIRUN_WIDGET_STARTING_REFRESH_SLEEP_TIME
+    interval += alpha * MULTIRUN_WIDGET_MAXIMUM_REFRESH_SLEEP_TIME
+    return interval
+
+
+def _update_databricks_widget_refresh_sleep_time(alpha=None):
+    if alpha is None:
+        return WIDGET_STARTING_REFRESH_SLEEP_TIME_DATABRICKS
+    interval = (1 - alpha) * WIDGET_STARTING_REFRESH_SLEEP_TIME_DATABRICKS
+    interval += alpha * WIDGET_REFRESH_MAXIMUM_SLEEP_TIME_DATABRICKS
+    return interval
