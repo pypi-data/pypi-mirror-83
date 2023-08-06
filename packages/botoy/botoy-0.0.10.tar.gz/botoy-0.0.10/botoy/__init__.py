1580dcc59c02576777c242d8401e425d298eb54e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:51 +0000

    Add package botoy/0.0.10/botoy-0.0.10.tar.gz

---
+++ packages/botoy/botoy-0.0.10.tar.gz/botoy-0.0.10/botoy/__init__.py
@@ -0,0 +1,27 @@
+"""See https://github.com/xiyaowong/botoy
+"""
+try:
+    import ujson as json
+except ImportError:
+    import json
+
+from . import decorators, refine
+from .action import Action
+from .async_action import AsyncAction
+from .async_client import AsyncBotoy
+from .client import Botoy
+from .model import EventMsg, FriendMsg, GroupMsg
+
+__all__ = [
+    'refine',
+    'decorators',
+    'sugar',
+    'collection',
+    'Action',
+    'AsyncAction',
+    'Botoy',
+    'AsyncBotoy',
+    'EventMsg',
+    'GroupMsg',
+    'FriendMsg',
+]
