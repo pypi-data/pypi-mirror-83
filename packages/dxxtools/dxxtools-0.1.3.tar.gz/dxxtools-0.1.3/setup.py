897ce50a48c24ac8396a1b96511567c4a1eacb44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:44 +0000

    Add package dxxtools/0.1.3/dxxtools-0.1.3.tar.gz

---
+++ packages/dxxtools/dxxtools-0.1.3.tar.gz/dxxtools-0.1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="dxxtools",
+    version="0.1.3",
+    description="dxxtools is a package of useful tools for .DXX",
+    install_requires=["dxx", "numpy", "matplotlib"],
+    entry_points={
+        "console_scripts": [
+            "vs_plot_dxx = dxxtools.vs_plot:main",
+            "upsampling_dxx = dxxtools.upsampling:main"
+        ]
+    },
+    author="Tetsu Takizawa",
+    author_email="tetsu.varmos@gmail.com",
+    url="https://github.com/tetsuzawa/spatial-research/tree/master/modules/dxxtools"
+)
