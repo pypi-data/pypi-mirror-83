f9c8341f9c3b46bea20be84b605449688d0d350c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:37 +0000

    Add package bostaSDK/1.2.0/BostaSDK-1.2.0.tar.gz

---
+++ packages/bostaSDK/BostaSDK-1.2.0.tar.gz/BostaSDK-1.2.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, Extension
+
+setup(
+    name='BostaSDK',
+    version='1.2.0',
+    author='bosta',
+    author_email='sdk@bosta.com',
+    scripts=[],
+    keywords=["bosta", "client-sdk", "api", "api-integration"],
+    url= 'https://pypi.org/project/BostaSDK/',
+    license='LICENSE',
+    description='Bosta Python SDK',
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'requests'
+    ],
+)
