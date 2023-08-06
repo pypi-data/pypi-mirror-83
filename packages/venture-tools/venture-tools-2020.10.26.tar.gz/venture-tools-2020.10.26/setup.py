84e4535a36ef4f5bcf393c70ea937ea6f934a58d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:23 +0000

    Add package venture-tools/2020.10.26/venture-tools-2020.10.26.tar.gz

---
+++ packages/venture-tools/venture-tools-2020.10.26.tar.gz/venture-tools-2020.10.26/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="venture-tools",
+    version="2020.10.26",
+    author="murmuur",
+    author_email="murmuur@protonmail.com",
+    description="a CLI tool to initialize a new project",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/murmuur-git/venture.git",
+    download_url="https://github.com/murmuur-git/venture/archive/v2020.10.26-alpha.tar.gz",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=[
+        'requests>=2.24.0'
+    ],
+    package_data={
+       'venture' : ['config.ini','templates/python/*', 'templates/python/libs/*'], # Includes python template
+    },
+    entry_points = {
+        'console_scripts': [
+            'venture = venture.__main__:main'
+        ],
+    })
