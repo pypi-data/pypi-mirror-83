12b38726be87616032ae2b84e39b04e98c4c09d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:26 +0000

    Add package ecope/0.1.3/ecope-0.1.3.tar.gz

---
+++ packages/ecope/ecope-0.1.3.tar.gz/ecope-0.1.3/application/cli.py
@@ -0,0 +1,20 @@
+import argparse
+from scrap import Scraper
+from utils import export_to
+
+ap = argparse.ArgumentParser(prog='cli-export',
+                              usage='%(prog)s [options] path',)
+
+ap.add_argument("-c","--category", required=True, 
+   choices=["laptop","celular","audifono","monitor"], default="laptop", help="category to export")
+
+ap.add_argument("-o", "--output", required=False,
+   help="file to export data", default="out")
+ap.add_argument("-f", "--format", required=False,
+   help="format to export data", choices=["csv","json"], default="csv")
+
+args = vars(ap.parse_args())
+
+scrap = Scraper(args['category'])
+all_products = scrap.all_ecommerce()
+exp = export_to(all_products, filename=args['output'], format=args['format'])
