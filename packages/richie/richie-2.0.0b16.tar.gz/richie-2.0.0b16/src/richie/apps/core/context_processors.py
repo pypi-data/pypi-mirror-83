b730d50ac310ad84f002a45d6c174e9ee4fde502
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:02 +0000

    Add package richie/2.0.0b16/richie-2.0.0b16.tar.gz

---
+++ packages/richie/richie-2.0.0b16.tar.gz/richie-2.0.0b16/src/richie/apps/core/context_processors.py
@@ -0,0 +1,49 @@
+"""
+Template context processors
+"""
+import json
+
+from django.conf import settings
+from django.contrib.sites.models import Site
+from django.middleware.csrf import get_token
+
+from . import defaults
+
+
+def site_metas(request):
+    """
+    Context processor to add all information required by Richie CMS templates and frontend.
+
+    If `CDN_DOMAIN` settings is defined we add it in the context. It allows
+    to load statics js on a CDN like cloudfront.
+    """
+    site_current = Site.objects.get_current()
+    protocol = "https" if request.is_secure() else "http"
+    context = {
+        **{
+            f"GLIMPSE_PAGINATION_{k.upper()}": v
+            for k, v in {
+                **defaults.GLIMPSE_PAGINATION,
+                **getattr(settings, "RICHIE_GLIMPSE_PAGINATION", {}),
+            }.items()
+        },
+        "SITE": {
+            "name": site_current.name,
+            "domain": site_current.domain,
+            "web_url": f"{protocol:s}://{site_current.domain:s}",
+        },
+        "FRONTEND_CONTEXT": json.dumps(
+            {
+                "context": {
+                    "csrftoken": get_token(request),
+                    "environment": getattr(settings, "ENVIRONMENT", ""),
+                    "release": getattr(settings, "RELEASE", ""),
+                    "sentry_dsn": getattr(settings, "SENTRY_DSN", ""),
+                }
+            }
+        ),
+    }
+    if getattr(settings, "CDN_DOMAIN", False):
+        context["CDN_DOMAIN"] = settings.CDN_DOMAIN
+
+    return context
