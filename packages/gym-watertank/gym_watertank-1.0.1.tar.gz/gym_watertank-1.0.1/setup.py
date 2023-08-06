1c9d72ef04b7cca2d86ebb03fb8a881f0ac5d8cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:10 +0000

    Add package gym-watertank/1.0.1/gym_watertank-1.0.1.tar.gz

---
+++ packages/gym-watertank/gym_watertank-1.0.1.tar.gz/gym_watertank-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='gym_watertank',
+      version='1.0.1',
+      install_requires=['gym'],  # And any other dependencies foo needs
+      author="Ruoqi Zhang",
+      author_email="ruoqi.zhang.666@gmail.com",
+      description="A gym environment pacakage for watertank and lqr",
+      python_requires='>=3.6'
+)
