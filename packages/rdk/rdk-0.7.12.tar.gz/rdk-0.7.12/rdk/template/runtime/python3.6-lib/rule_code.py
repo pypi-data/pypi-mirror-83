c771dda1eea43e8c72bbd75465e4da39c179f0f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:01 +0000

    Add package rdk/0.7.12/rdk-0.7.12.tar.gz

---
+++ packages/rdk/rdk-0.7.12.tar.gz/rdk-0.7.12/rdk/template/runtime/python3.6-lib/rule_code.py
@@ -0,0 +1,36 @@
+# Copyright 2017-2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You may
+# not use this file except in compliance with the License. A copy of the License is located at
+#
+#        http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for
+# the specific language governing permissions and limitations under the License.
+
+from rdklib import Evaluator, Evaluation, ConfigRule, ComplianceType
+<%ApplicableResources1%>
+class <%RuleName%>(ConfigRule):
+    def evaluate_change(self, event, client_factory, configuration_item, valid_rule_parameters):
+        ###############################
+        # Add your custom logic here. #
+        ###############################
+
+        return [Evaluation(ComplianceType.NOT_APPLICABLE)]
+
+    #def evaluate_periodic(self, event, client_factory, valid_rule_parameters):
+    #    pass
+
+    def evaluate_parameters(self, rule_parameters):
+        valid_rule_parameters = rule_parameters
+        return valid_rule_parameters
+
+
+################################
+# DO NOT MODIFY ANYTHING BELOW #
+################################
+def lambda_handler(event, context):
+    my_rule = <%RuleName%>()
+    evaluator = Evaluator(my_rule<%ApplicableResources2%>)
+    return evaluator.handle(event, context)
