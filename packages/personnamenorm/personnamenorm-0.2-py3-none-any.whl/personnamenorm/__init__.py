052d877283178b66671707e02589f784a9162d1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:43 +0000

    Add package personnamenorm/0.2/personnamenorm-0.2-py3-none-any.whl

---
+++ packages/personnamenorm/personnamenorm-0.2-py3-none-any.whl/personnamenorm/__init__.py
@@ -0,0 +1,11 @@
+
+
+__description__ = "unifying different notations of person names"
+__author__ = "Klaus Lippert"
+__copyright__ = "2020 by Klaus Lippert"
+__license__ = "MIT"
+__email__ = "klaus.lippert@mailbox.org"
+__version__ = "0.2"
+
+from .namenorm import *
+
