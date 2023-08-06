17edf085acc7b6611e93a2954575ffe1678d1e0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:43 +0000

    Add package datatools-mikdowd/0.0.7/datatools-mikdowd-0.0.7.tar.gz

---
+++ packages/datatools-mikdowd/datatools-mikdowd-0.0.7.tar.gz/datatools-mikdowd-0.0.7/setup.py
@@ -0,0 +1,34 @@
+"""Setup for the chocobo package."""
+
+import setuptools
+
+
+with open('README.md') as f:
+    README = f.read()
+
+setuptools.setup(
+    author="Michael Dowd",
+    author_email="mikdowd@gmail.com",
+    name='datatools-mikdowd',
+    license="MIT",
+    description='datatools is a python package for doing basic data summaries and other tasks',
+    version='v0.0.7',
+    long_description=README,
+    url='https://github.com/mdgis/datatools',
+    packages=setuptools.find_packages(),
+    python_requires=">=3.5",
+    install_requires=['pandas', 'seaborn'],
+    classifiers=[
+        # Trove classifiers
+        # (https://pypi.python.org/pypi?%3Aaction=list_classifiers)
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Intended Audience :: Developers',
+    ],
+)
