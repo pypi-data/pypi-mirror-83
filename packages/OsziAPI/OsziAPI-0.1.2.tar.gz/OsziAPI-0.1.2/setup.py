b8706b3ba155f8e79a56c534c8768656322cc8de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:24 +0000

    Add package OsziAPI/0.1.2/OsziAPI-0.1.2.tar.gz

---
+++ packages/OsziAPI/OsziAPI-0.1.2.tar.gz/OsziAPI-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='OsziAPI',
+    version='0.1.2',
+    description='database api',
+    license='MIT License',
+    url='https://github.com/clresonanz/OsziAPI',
+    author='Liu Chang',
+    author_email='chang.liu@resonanzcapital.com',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
+)
