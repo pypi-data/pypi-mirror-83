4644ef4381c80b003ed1db2b470ac4c451e3c0c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:41 +0000

    Add package horkos/0.0.10/horkos-0.0.10-py3-none-any.whl

---
+++ packages/horkos/horkos-0.0.10-py3-none-any.whl/horkos/__init__.py
@@ -0,0 +1,7 @@
+from horkos.cataloger import Catalog
+from horkos.definitions import Field
+from horkos.schemaomatic import load_schema
+from horkos.schemaomatic import Schema
+
+
+__all__ = ('Catalog', 'Field', 'load_schema', 'Schema')
