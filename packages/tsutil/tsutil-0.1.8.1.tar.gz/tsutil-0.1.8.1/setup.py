3662480e4b0adcd55a97655dce2f3815e3659f13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:07 +0000

    Add package tsutil/0.1.8.1/tsutil-0.1.8.1.tar.gz

---
+++ packages/tsutil/tsutil-0.1.8.1.tar.gz/tsutil-0.1.8.1/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf8 -*-
+
+import platform
+from setuptools import setup
+
+reqs = ['requests==2.23.0']
+
+setup(
+      name='tsutil',
+      version='0.1.8.1',
+      py_modules=['tsutil.util', 'tsutil.daemonize', 'tsutil.sync', 'tsutil.sequence', 'tsutil.http_server'],
+      author='Turbidsoul Chen',
+      author_email='sccn.sq+py@gmail.com',
+      url='http://github.com/turbidsoul/tsutil',
+      description='my python tool module',
+      license='MIT',
+      install_requires=reqs,
+      package_dir={'':'.'},
+      include_package_data=True,
+      package_data = {'require': ['*.txt']}
+)
+
