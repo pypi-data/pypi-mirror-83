2f119b7dab4a520d4ed827654eb94b84faf1b4e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:49 +0000

    Add package goprocam/4.1.0/goprocam-4.1.0.tar.gz

---
+++ packages/goprocam/goprocam-4.1.0.tar.gz/goprocam-4.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(name='goprocam',
+      version="4.1.0",
+      description='GoPro WiFi API Wrapper for Python - Compatible with HERO3, HERO3+, HERO4, HERO5, HERO+, HERO6',
+      url='http://github.com/konradit/gopro-py-api',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='Konrad Iturbe',
+      author_email='mail@chernowii.com',
+      license='MIT',
+      packages=['goprocam'],
+      zip_safe=False)
