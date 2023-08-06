e9df57a09176def25390b0e34857eeaf634b3cca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:45 +0000

    Add package blockapi/0.0.81/blockapi-0.0.81.tar.gz

---
+++ packages/blockapi/blockapi-0.0.81.tar.gz/blockapi-0.0.81/blockapi/api/blockscout.py
@@ -0,0 +1,33 @@
+from blockapi.services import BlockchainAPI
+
+
+class BlockscoutAPI(BlockchainAPI):
+    """
+    coins: etc
+    API docs: https://blockscout.com/etc/mainnet/api_docs
+    Explorer: https://blockscout.com
+    """
+
+    active = True
+
+    symbol = 'ETC'
+    base_url = 'https://blockscout.com/etc/mainnet/api?'
+    rate_limit = 0
+    coef = 1e-18
+    max_items_per_page = None
+    page_offset_step = None
+    confirmed_num = None
+
+    supported_requests = {
+        'get_balance': 'module=account&action=eth_get_balance'
+                       '&address={address}',
+    }
+
+    def get_balance(self):
+        response = self.request('get_balance',
+                                address=self.address)
+        if not response:
+            return None
+
+        retval = int(response['result'], 16) * self.coef
+        return [{'symbol': self.symbol, 'amount': retval}]
