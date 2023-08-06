f817c14de36c9cb831386dd967aa4be4f3dd3ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:25:33 +0000

    Add package dinuq/1.1.0/dinuq-1.1.0.tar.gz

---
+++ packages/dinuq/dinuq-1.1.0.tar.gz/dinuq-1.1.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, Extension
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+        
+        
+setup(
+    name='dinuq',
+    version='1.1.0',
+    description='The Dinucleotide Quantification Python package',
+    long_description= readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/spyros-lytras/dinuq',
+    download_url = 'https://github.com/spyros-lytras/dinuq/dist/dinuq-1.1.0.tar',
+    classifiers=[
+    "Intended Audience :: Science/Research",
+    "Topic :: Scientific/Engineering :: Bio-Informatics",
+    "Programming Language :: Python",
+    ],
+    keywords='bioinformatics dinucleotides viruses',
+    project_urls={
+    'CVR Bioinformatics': 'https://bioinformatics.cvr.ac.uk/',
+    },
+    author='Spyros Lytras',
+    author_email='s.lytras.1@research.gla.ac.uk',
+    license='MIT',
+    packages=['dinuq'],
+    install_requires=['biopython'],
+    python_requires="~=3.6")
