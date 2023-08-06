e04c055e1d8085a6f301ba1536ace1d8358cacbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:34 +0000

    Add package sphinx-minipres/0.2.0/sphinx_minipres-0.2.0.tar.gz

---
+++ packages/sphinx-minipres/sphinx_minipres-0.2.0.tar.gz/sphinx_minipres-0.2.0/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+
+version_ns = { }
+exec(open('sphinx_minipres/_version.py').read(), version_ns)
+version = version_ns['__version__']
+del version_ns
+
+setuptools.setup(
+    name="sphinx_minipres",
+    version=version,
+    author="Richard Darst",
+    #author_email="",
+    description="Sphinx extension to turn any web page into a lightweight presentation",
+    long_description=long_description,
+    url="https://github.com/coderefinery/sphinx-minipres",
+    packages=setuptools.find_packages(),
+    package_data={
+        "sphinx_minipres": ['_static/*'],
+        },
+    #py_modules=["minipres"],
+    keywords='sphinx-extension',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Framework :: Sphinx",
+        "Framework :: Sphinx :: Extension",
+    ],
+)
