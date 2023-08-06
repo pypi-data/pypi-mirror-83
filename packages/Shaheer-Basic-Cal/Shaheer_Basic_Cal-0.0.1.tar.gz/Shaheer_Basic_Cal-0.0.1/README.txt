504bcd15efc2ecdcd14e207ac1e23a3aabf79035
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:09 +0000

    Add package Shaheer-Basic-Cal/0.0.1/Shaheer_Basic_Cal-0.0.1.tar.gz

---
+++ packages/Shaheer-Basic-Cal/Shaheer_Basic_Cal-0.0.1.tar.gz/Shaheer_Basic_Cal-0.0.1/README.txt
@@ -0,0 +1,1 @@
+This is a very sinmple calculator that takes two numbers.
---
+++ packages/Shaheer-Basic-Cal/Shaheer_Basic_Cal-0.0.1.tar.gz/Shaheer_Basic_Cal-0.0.1/__init__.py
@@ -0,0 +1,6 @@
+def add_numbers(num1, num2):
+    return num1 +num2
+def sub_numbers(num1, num2):
+    return num1 - num2
+def div_numbers(num1, num2):
+    return num1 / num2
