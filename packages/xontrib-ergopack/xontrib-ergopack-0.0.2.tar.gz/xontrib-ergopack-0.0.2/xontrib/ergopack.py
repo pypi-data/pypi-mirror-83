d84ad6d986a9816dc1648ef62518785dfec6f7aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:40 +0000

    Add package xontrib-ergopack/0.0.2/xontrib-ergopack-0.0.2.tar.gz

---
+++ packages/xontrib-ergopack/xontrib-ergopack-0.0.2.tar.gz/xontrib-ergopack-0.0.2/xontrib/ergopack.py
@@ -0,0 +1,14 @@
+from xonsh import xontribs
+
+_xontribs = [
+  'prompt_bar',
+  'output_search',
+  'whole_word_jumping',
+  'pipeliner',
+  'argcomplete',
+  'onepath',
+  'sh',
+  'back2dir'
+]
+
+xontribs.xontribs_load(_xontribs)
