9c838e34afa35f2d5359c25b2947f3a5036a87e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:16:30 +0000

    Add package google-api-python-client-stubs/0.2.0.post1/google-api-python-client-stubs-0.2.0.post1.tar.gz

---
+++ packages/google-api-python-client-stubs/google-api-python-client-stubs-0.2.0.post1.tar.gz/google-api-python-client-stubs-0.2.0.post1/googleapiclient-stubs/_apis/language/v1beta1/resources.pyi
@@ -0,0 +1,50 @@
+import typing
+
+import httplib2  # type: ignore
+import typing_extensions
+
+import googleapiclient.discovery
+import googleapiclient.http  # type: ignore
+
+from .schemas import *
+@typing.type_check_only
+class CloudNaturalLanguageResource(googleapiclient.discovery.Resource):
+    @typing.type_check_only
+    class DocumentsResource(googleapiclient.discovery.Resource):
+        def analyzeEntities(
+            self, *, body: AnalyzeEntitiesRequest = ..., **kwargs: typing.Any
+        ) -> AnalyzeEntitiesResponseHttpRequest: ...
+        def analyzeSentiment(
+            self, *, body: AnalyzeSentimentRequest = ..., **kwargs: typing.Any
+        ) -> AnalyzeSentimentResponseHttpRequest: ...
+        def analyzeSyntax(
+            self, *, body: AnalyzeSyntaxRequest = ..., **kwargs: typing.Any
+        ) -> AnalyzeSyntaxResponseHttpRequest: ...
+        def annotateText(
+            self, *, body: AnnotateTextRequest = ..., **kwargs: typing.Any
+        ) -> AnnotateTextResponseHttpRequest: ...
+    def documents(self) -> DocumentsResource: ...
+
+@typing.type_check_only
+class AnalyzeEntitiesResponseHttpRequest(googleapiclient.http.HttpRequest):
+    def execute(
+        self, http: typing.Optional[httplib2.Http] = ..., num_retries: int = ...
+    ) -> AnalyzeEntitiesResponse: ...
+
+@typing.type_check_only
+class AnalyzeSentimentResponseHttpRequest(googleapiclient.http.HttpRequest):
+    def execute(
+        self, http: typing.Optional[httplib2.Http] = ..., num_retries: int = ...
+    ) -> AnalyzeSentimentResponse: ...
+
+@typing.type_check_only
+class AnalyzeSyntaxResponseHttpRequest(googleapiclient.http.HttpRequest):
+    def execute(
+        self, http: typing.Optional[httplib2.Http] = ..., num_retries: int = ...
+    ) -> AnalyzeSyntaxResponse: ...
+
+@typing.type_check_only
+class AnnotateTextResponseHttpRequest(googleapiclient.http.HttpRequest):
+    def execute(
+        self, http: typing.Optional[httplib2.Http] = ..., num_retries: int = ...
+    ) -> AnnotateTextResponse: ...
