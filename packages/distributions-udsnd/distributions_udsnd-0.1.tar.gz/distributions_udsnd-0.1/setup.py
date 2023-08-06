f16c9a2285ca4a9b2ace7359f203baee7ec9340b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:29 +0000

    Add package distributions-udsnd/0.1/distributions_udsnd-0.1.tar.gz

---
+++ packages/distributions-udsnd/distributions_udsnd-0.1.tar.gz/distributions_udsnd-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_udsnd',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_udsnd'],
+      zip_safe=False)
