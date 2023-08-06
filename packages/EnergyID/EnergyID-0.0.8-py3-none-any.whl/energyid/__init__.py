a7e3b8593817d52ea17b2d0941e2fddf2155758f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:22:20 +0000

    Add package EnergyID/0.0.8/EnergyID-0.0.8-py3-none-any.whl

---
+++ packages/EnergyID/EnergyID-0.0.8-py3-none-any.whl/energyid/__init__.py
@@ -0,0 +1,7 @@
+from .client import JSONClient, SimpleJSONClient, Scope
+from .pandasclient import PandasClient
+
+__title__ = "energyid"
+__version__ = "0.0.8"
+__author__ = "EnergieID.be"
+__license__ = "MIT"
