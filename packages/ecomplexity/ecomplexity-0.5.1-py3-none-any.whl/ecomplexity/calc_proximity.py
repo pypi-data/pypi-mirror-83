cf6973d6efc639faf6225a9c39165c33502f587b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:10 +0000

    Add package ecomplexity/0.5.1/ecomplexity-0.5.1-py3-none-any.whl

---
+++ packages/ecomplexity/ecomplexity-0.5.1-py3-none-any.whl/ecomplexity/calc_proximity.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+
+
+def calc_discrete_proximity(mcp, ubiquity, asymmetric=False):
+    """Calculate product proximity matrices - discrete
+
+    Args:
+        mcp: numpy ndarray with rows as locations and columns as products
+        ubiquity: numpy array of shape=number of columns in "rca_or_mcp"
+        asymmetric: Whether to generate asymmetric proximity matrix (True) or
+            symmetric (False). *default* False.
+
+    Returns:
+        pandas df with proximity values for every product pair
+    """
+
+    # Calculate discrete proximity
+    phi = mcp.T @ mcp
+    phi = phi / ubiquity[np.newaxis, :]
+
+    if asymmetric == False:
+        # Symmetric proximity matrix
+        phi = np.minimum(phi, phi.T)
+    elif asymmetric == True:
+        # Asymmetric proximity matrix
+        phi = phi.T
+
+    return phi
+
+
+def calc_continuous_proximity(rca, ubiquity):
+    """Calculate product proximity matrices - continuous
+
+    Args:
+        rca: numpy ndarray with rows as locations and columns as products
+        ubiquity: numpy array of shape=number of columns in "rca_or_mcp"
+
+    Returns:
+        pandas df with proximity values for every product pair
+    """
+    # Calculate continuous proximity
+    phi = (1 + np.corrcoef(rca.T)) / 2
+    return phi
