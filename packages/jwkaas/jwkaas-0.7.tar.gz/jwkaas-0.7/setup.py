8c70afd82f65d81d681445deafa35cc085e57839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:24 +0000

    Add package jwkaas/0.7/jwkaas-0.7.tar.gz

---
+++ packages/jwkaas/jwkaas-0.7.tar.gz/jwkaas-0.7/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+     name='jwkaas',
+     version='0.7',
+     scripts=[],
+     author="Bernie van Veen",
+     author_email="b.vanveen@vwt.digital",
+     description="JSON Web Key Advanced Acquiring Store",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://github.com/vwt-digital/jwkaas",
+     packages=setuptools.find_packages(),
+     install_requires=[
+         'pyjwt==1.7.1',
+         'cryptography==3.2'
+     ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+         "Operating System :: OS Independent",
+     ],
+ )
