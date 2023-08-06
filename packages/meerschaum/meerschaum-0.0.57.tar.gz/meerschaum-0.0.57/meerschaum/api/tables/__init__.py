1e33433415d5cbf1ad56c51bed4a3f38266fa799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:10 +0000

    Add package meerschaum/0.0.57/meerschaum-0.0.57.tar.gz

---
+++ packages/meerschaum/meerschaum-0.0.57.tar.gz/meerschaum-0.0.57/meerschaum/api/tables/__init__.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+
+"""
+Define SQLAlchemy tables
+"""
+
+### migrated sqlalchemy tables to another module, importing here for legacy's sake
+from meerschaum.connectors.sql.tables import get_tables
