27a224e493f92e5d72c2e1d0ef7b762c9c2667c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:11 +0000

    Add package kgraph/0.1.2/kgraph-0.1.2.tar.gz

---
+++ packages/kgraph/kgraph-0.1.2.tar.gz/kgraph-0.1.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup_params = dict(name='kgraph',
+                    version='0.1.2',
+                    description='A Python library for relational learning on knowledge graphs.',
+                    url='https://github.com/YaoShuang-long/kgraph',
+                    author='Yao Shuang-long',
+                    author_email='shuanglongyao@gmail.com',
+                    license='Apache 2.0',
+                    packages=find_packages(),
+                    include_package_data=True,
+                    zip_safe=True,
+                    install_requires=[
+                        'numpy>=1.14.3',
+                        # 'joblib>=0.11',
+                        'tqdm>=4.23.4',
+                        'pandas>=0.23.1',
+                        'scipy>=1.3.0',
+                        'setuptools>=36',
+                        'prettytable>=0.7.2',
+                        'arrow>=0.15.6'
+                    ])
+
+if __name__ == '__main__':
+    setup(**setup_params)
