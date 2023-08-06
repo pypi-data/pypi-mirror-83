51c102839c1c0c32dda9fd5149e5712aa03ed034
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:44:35 +0000

    Add package cartoonizer/0.0.3/cartoonizer-0.0.3.tar.gz

---
+++ packages/cartoonizer/cartoonizer-0.0.3.tar.gz/cartoonizer-0.0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setup(
+      name='cartoonizer',
+      version='0.0.3',
+      description='Convert your Image into a Cartoon form',
+      py_modules=["cartoonizer"],
+      package_dir={'':'cartoonizer'},
+      long_description = long_description,
+      long_description_content_type="text/markdown",
+      install_requires=["numpy","cv2","scipy",]
+)
