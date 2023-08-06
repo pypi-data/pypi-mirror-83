51908c3a526cc3cd2cf888460959019b20727157
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:50:24 +0000

    Add package jnt-django-toolbox/0.1.28/jnt-django-toolbox-0.1.28.tar.gz

---
+++ packages/jnt-django-toolbox/jnt-django-toolbox-0.1.28.tar.gz/jnt-django-toolbox-0.1.28/src/jnt_django_toolbox/profiling/profilers/db_queries.py
@@ -0,0 +1,21 @@
+from django.db import connection
+from django.test.utils import CaptureQueriesContext
+
+from jnt_django_toolbox.profiling.profilers.base import BaseProfiler
+
+
+class DatabaseQueriesProfiler(BaseProfiler):
+    """Database queries count profiler."""
+
+    def __init__(self, header: str = "Db-Queries-Count"):
+        """Initializing."""
+        self._header = header
+
+    def before_request(self, request, stack):
+        """Start capturing requests."""
+        self._context = CaptureQueriesContext(connection)
+        stack.enter_context(self._context)
+
+    def after_request(self, request, response):
+        """Add profiling info to response."""
+        response[self._header] = len(self._context)
