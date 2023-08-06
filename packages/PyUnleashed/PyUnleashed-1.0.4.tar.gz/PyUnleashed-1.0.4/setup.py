a9f3329392ef786387e75a0a880ce9925c6b65d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:21:56 +0000

    Add package PyUnleashed/1.0.4/PyUnleashed-1.0.4.tar.gz

---
+++ packages/PyUnleashed/PyUnleashed-1.0.4.tar.gz/PyUnleashed-1.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+     name='PyUnleashed',
+     version='1.0.4',
+     author="John Horton",
+     author_email="pyunleashed@outlook.com",
+     description="A wrapper with a couple of utility functions for the Unleashed Software API",
+     long_description=long_description,
+     long_description_content_type="text/markdown",
+     url="https://www.adminify.me",
+     license='LICENSE.txt',
+     packages=setuptools.find_packages(),
+     install_requires=[
+        'requests'
+     ],
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
