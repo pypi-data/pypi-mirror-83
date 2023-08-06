e782c0f8555f0ab071bdd5109037898a1c6dfd80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:43 +0000

    Add package sqtom/0.4.0/sqtom-0.4.0.tar.gz

---
+++ packages/sqtom/sqtom-0.4.0.tar.gz/sqtom-0.4.0/sqtom/fitting_1d.py
@@ -0,0 +1,137 @@
+# Copyright 2019-2020 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Degenerate squeezer inverse-problem solver
+==========================================
+This module solves the *inverse* problem of given a photon number distribution find the best
+of parameters describing the different quantum states in a single beam producing it.
+
+The ideas behind this module borrow heavily on the work of Burenkok et al. in
+
+Full statistical mode reconstruction of a light field via a photon-number-resolved measurement
+I. A. Burenkov, A. K. Sharma, T. Gerrits, G. Harder, T. J. Bartley, C. Silberhorn, E. A. Goldschmidt, and S. V. Polyakov
+Phys. Rev. A 95, 053806 (2017)
+"""
+
+
+import numpy as np
+from lmfit import Minimizer, Parameters
+from sqtom.forward_solver import degenerate_pmf
+
+
+def marginal_calcs_1d(pd_data, as_dict=True):
+    """Given a one dimensional array of probabilities it calculates the mean photon number
+    and the g2.
+
+    Args:
+        pd_data (array): probability mass function of the photon events
+        as_dict (boolean): whether to return the results as a dictionary
+
+    Returns:
+        dict or array: values of the mean photons number the corresponding g2.
+    """
+
+    intn = pd_data.shape[0]
+    n = np.arange(intn)
+    nmean = pd_data @ n
+    nmean2 = pd_data @ n ** 2
+    g2 = (nmean2 - nmean) / nmean ** 2
+    if as_dict:
+        return {"n": nmean, "g2": g2}
+    return np.array([nmean, g2])
+
+
+def threshold_1d(ps, nmax):
+    """Thresholds a probability distribution by assigning events with nmax
+    photons or more to the nmax bin.
+
+    Args:
+        ps (array): probability distribution
+        nmax (int): threshold value
+
+    Returns:
+        array: thresholded probability distribution
+    """
+    thr = nmax - 1
+    local_ps = np.copy(ps)
+    local_ps[thr] = np.sum(local_ps[thr:])
+    return local_ps[:nmax]
+
+
+def fit_1d(
+    pd_data,
+    guess,
+    do_not_vary=None,
+    method="leastsq",
+    threshold=False,
+    cutoff=50,
+    sq_label="sq_",
+    noise_label="noise",
+):
+    """Takes as input the name of the model to fit to and the jpd of the data
+    and returns the fitted model.
+
+    Args:
+        pd_data (array): one dimensional array of the probability distribution of the data
+        guess (dict): dictionary with the guesses for the different parameters
+        method (string): method to be used by the optimizer
+        do_not_vary (list): list of variables that should be held constant during optimization
+        threshold (boolean or int): whether to threshold the photon probbailitites at value threshold
+        cutoff (int): internal cutoff
+        sq_label (string): string label for the squeezing parameters
+        noise_label (string): label for the noise parameters.
+
+    Returns:
+        Object: object containing the optimized parameter and several goodness-of-fit statistics
+    """
+    if do_not_vary is None:
+        do_not_vary = []
+
+    pars_model = Parameters()
+    n_modes = guess["n_modes"]
+    pars_model.add("n_modes", value=n_modes, vary=False)
+    # Add the squeezing parameters
+    for i in range(n_modes):
+        pars_model.add(sq_label + str(i), value=guess["sq_" + str(i)], min=0.0)
+
+    if "eta" in do_not_vary:
+        pars_model.add("eta", value=guess["eta"], vary=False)
+    else:
+        pars_model.add("eta", value=guess["eta"], min=0.0, max=1.0)
+
+    if noise_label in do_not_vary:
+        pars_model.add(noise_label, value=guess[noise_label], vary=False)
+    else:
+        pars_model.add(noise_label, value=guess[noise_label], min=0.0)
+
+    if threshold:
+
+        def model_1d(params, pd_data):
+            ndim = pd_data.shape[0]
+            dpmf = degenerate_pmf(params, cutoff=cutoff)
+            return threshold_1d(dpmf, ndim) - pd_data
+
+    else:
+
+        def model_1d(params, pd_data):
+            ndim = pd_data.shape[0]
+            return (
+                degenerate_pmf(params, cutoff=cutoff, sq_label=sq_label, noise_label=noise_label)[
+                    :ndim
+                ]
+                - pd_data
+            )
+
+    minner_model = Minimizer(model_1d, pars_model, fcn_args=([pd_data]))
+    return minner_model.minimize(method=method)
---
+++ packages/sqtom/sqtom-0.4.0.tar.gz/sqtom-0.4.0/sqtom/fitting_2d.py
@@ -0,0 +1,213 @@
+# Copyright 2019-2020 Xanadu Quantum Technologies Inc.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""
+Twin-beam squeezer inverse-problem solver
+=========================================
+This module solves the *inverse* problem: given a joint photon number distribution find the best
+parameters describing the different quantum states in a twin-beam producing it.
+
+The ideas behind this module borrow heavily on the work of Burenkok et al. in
+
+Full statistical mode reconstruction of a light field via a photon-number-resolved measurement
+I. A. Burenkov, A. K. Sharma, T. Gerrits, G. Harder, T. J. Bartley, C. Silberhorn, E. A. Goldschmidt, and S. V. Polyakov
+Phys. Rev. A 95, 053806 (2017)
+"""
+
+import numpy as np
+from lmfit import Minimizer, Parameters
+from sqtom.forward_solver import twinbeam_pmf
+
+
+def two_schmidt_mode_guess(jpd_data, sq_label="sq_", noise_label="noise"):
+    """Given a two mode histogram, this function generates a "physically" motivated guess for the loss, Schmidt occupations
+    and dark counts parameters.
+
+    This model is sensible only if the average g2 of signal and idler is above 1.5.
+
+    Args:
+        jpd_data (array): rectangular array with the probability mass functions of the photon events
+        sq_label (string): label for the squeezing parameters.
+        noise_label (string): label for the noise parameters.
+
+    Returns:
+        dict: dictionary containing a set of "reasonable" model parameters
+    """
+    res = marginal_calcs_2d(jpd_data)
+    g2avg = np.max([0.5 * (res["g2_s"] + res["g2_i"]), 1.5])
+    Nbar = 1.0 / (res["g11"] - g2avg)
+    n0 = 0.5 * (1 + np.sqrt(2 * g2avg - 3)) * Nbar
+    n1 = 0.5 * (1 - np.sqrt(2 * g2avg - 3)) * Nbar
+    etas = res["n_s"] / Nbar
+    etai = res["n_i"] / Nbar
+    noise = np.abs(res["g2_s"] - res["g2_i"]) * Nbar
+    return {
+        "eta_s": etas,
+        "eta_i": etai,
+        sq_label + "0": n0,
+        sq_label + "1": n1,
+        noise_label + "_s": noise * etas,
+        noise_label + "_i": noise * etai,
+        "n_modes": 2,
+    }
+
+
+def marginal_calcs_2d(jpd_data, as_dict=True):
+    """Given a two dimensional array of probabilities it calculates
+    the mean photon numbers, their g2's and g11.
+
+    It returns these values as a dictionary or as an array.
+
+    Args:
+        jpd_data (array): probability mass function of the photon events
+        as_dict (boolean): whether to return the results as a dictionary
+
+    Returns:
+        dict or array: values of the mean photons number for signal and idlers,
+            their corresponding g2, their g11 and their noise reduction factor (nrf).
+    """
+    inta, intb = jpd_data.shape
+    na = np.arange(inta)
+    nb = np.arange(intb)
+    ns = np.sum(jpd_data, axis=1) @ na
+    ni = np.sum(jpd_data, axis=0) @ nb
+    ns2 = np.sum(jpd_data, axis=1) @ (na ** 2)
+    ni2 = np.sum(jpd_data, axis=0) @ (nb ** 2)
+    g2s = (ns2 - ns) / ns ** 2
+    g2i = (ni2 - ni) / ni ** 2
+    g11 = (na @ jpd_data @ nb) / (ns * ni)
+    nrf = np.sum([[((i - j) ** 2) * jpd_data[i, j] for i in range(inta)] for j in range(intb)])
+    nrf -= np.sum([[(i - j) * jpd_data[i, j] for i in range(inta)] for j in range(intb)]) ** 2
+    nrf /= ns + ni
+    if as_dict is True:
+        return {
+            "n_s": ns,
+            "n_i": ni,
+            "g11": g11,
+            "g2_s": g2s,
+            "g2_i": g2i,
+            "nrf": nrf,
+        }
+    return np.array([ns, ni, g11, g2s, g2i, nrf])
+
+
+def gen_hist_2d(beam1, beam2):
+    """Calculate the joint probability mass function of events.
+
+    Args:
+        beam1 (array): 1D events array containing the raw click events of first beam
+        beam2 (array): 1D events array containing the raw click events of second beam
+
+    Returns:
+        array: probability mass function of the click patterns for the two beams
+    """
+    nx = np.max(beam1)
+    ny = np.max(beam2)
+    xedges = np.arange(nx + 2)
+    yedges = np.arange(ny + 2)
+    mass_fun, _, _ = np.histogram2d(beam1, beam2, bins=(xedges, yedges), normed=True)
+    return mass_fun
+
+
+def threshold_2d(ps, nmax, mmax):
+    """Thresholds a 2D probability distribution by assigning events with more than nmax (mmax) photons
+    in the first (second) axis to the nmax (mmax) bin.
+
+    Args:
+        ps (array): probability distribution
+        nmax (int): threshold for first axis
+        mmax (int): threshold for second axis
+
+    Returns:
+        array: thresholded probability distribution
+    """
+    probs = np.copy(ps[:nmax, :mmax])
+    for i in range(mmax - 1):
+        probs[nmax - 1, i] += np.sum(ps[nmax:, i])
+    for i in range(nmax - 1):
+        probs[i, mmax - 1] += np.sum(ps[i, mmax:])
+    probs[nmax - 1, mmax - 1] = np.sum(ps[nmax - 1 :, mmax - 1 :])
+    return probs
+
+
+def fit_2d(
+    pd_data,
+    guess,
+    do_not_vary=None,
+    method="leastsq",
+    threshold=False,
+    cutoff=50,
+    sq_label="sq_",
+    noise_label="noise",
+):
+    """Returns a model fit from the parameter guess and the data
+
+    Args:
+        pd_data (array): one dimensional array of the probability distribution of the data
+        guess (dict): dictionary with the guesses for the different parameters
+        method (string): method to be used by the optimizer
+        threshold (boolean or list): threshold photon number for the signal and idlers
+        do_not_vary (list): list of variables that should be held constant during optimization
+        cutoff (int): internal cutoff
+        sq_label (string): label for the squeezing parameters.
+        noise_label (string): label for the noise parameters.
+
+    Returns:
+        Object: object containing the optimized parameter and several goodness-of-fit statistics
+    """
+    if do_not_vary is None:
+        do_not_vary = []
+    pars_model = Parameters()
+    n_modes = guess["n_modes"]
+    pars_model.add("n_modes", value=n_modes, vary=False)
+    for i in range(n_modes):
+        pars_model.add(sq_label + str(i), value=guess["sq_" + str(i)], min=0.0)
+
+    if "eta_s" in do_not_vary:
+        pars_model.add("eta_s", value=guess["eta_s"], vary=False)
+    else:
+        pars_model.add("eta_s", value=guess["eta_s"], min=0.0, max=1.0)
+
+    if "eta_i" in do_not_vary:
+        pars_model.add("eta_i", value=guess["eta_i"], vary=False)
+    else:
+        pars_model.add("eta_i", value=guess["eta_i"], min=0.0, max=1.0)
+
+    if noise_label + "_s" in do_not_vary:
+        pars_model.add(noise_label + "_s", value=guess[noise_label + "_s"], vary=False)
+    else:
+        pars_model.add(noise_label + "_s", value=guess[noise_label + "_s"], min=0.0)
+
+    if noise_label + "_i" in do_not_vary:
+        pars_model.add(noise_label + "_i", value=guess[noise_label + "_i"], vary=False)
+    else:
+        pars_model.add(noise_label + "_i", value=guess[noise_label + "_i"], min=0.0)
+
+    if threshold:
+
+        def model_2d(params, jpd_data):
+            (dim_s, dim_i) = pd_data.shape
+            joint_pmf = twinbeam_pmf(params, cutoff=cutoff)
+            return threshold_2d(joint_pmf, dim_s, dim_i) - threshold_2d(jpd_data, dim_s, dim_i)
+
+    else:
+
+        def model_2d(params, jpd_data):
+            (dim_s, dim_i) = pd_data.shape
+            joint_pmf = twinbeam_pmf(params, cutoff=cutoff)[:dim_s, :dim_i]
+            return joint_pmf - jpd_data
+
+    minner_model = Minimizer(model_2d, pars_model, fcn_args=([pd_data]))
+    result_model = minner_model.minimize(method=method)
+
+    return result_model
