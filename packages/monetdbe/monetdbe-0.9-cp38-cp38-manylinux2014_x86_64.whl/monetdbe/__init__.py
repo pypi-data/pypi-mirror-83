daa5f6b6cbf9bf37455345643547028dbe05045a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:12:52 +0000

    Add package monetdbe/0.9/monetdbe-0.9-cp38-cp38-manylinux2014_x86_64.whl

---
+++ packages/monetdbe/monetdbe-0.9-cp38-cp38-manylinux2014_x86_64.whl/monetdbe/__init__.py
@@ -0,0 +1,6 @@
+from monetdbe.compat import *
+from monetdbe.converters import *
+from monetdbe.dbapi2 import *
+from monetdbe.monetize import PrepareProtocol
+from monetdbe.row import Row
+from monetdbe.version import *
