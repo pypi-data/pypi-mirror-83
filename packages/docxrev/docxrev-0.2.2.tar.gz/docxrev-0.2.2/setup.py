1c84ba1ed59e0a84388efad651d26cedf97905d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:07:19 +0000

    Add package docxrev/0.2.2/docxrev-0.2.2.tar.gz

---
+++ packages/docxrev/docxrev-0.2.2.tar.gz/docxrev-0.2.2/setup.py
@@ -0,0 +1,24 @@
+"""
+Microsoft Word review tools (comments, markup, etc.) with Python
+"""
+
+import pathlib
+
+from setuptools import find_packages, setup
+
+here = pathlib.Path().resolve()
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+setup(
+    name="docxrev",
+    version="0.2.2",
+    description=("Microsoft Word review tools (comments, markup, etc.) with Python"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/blakeNaccarato/docxrev",
+    author="Blake Naccarato",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    python_requires=">=3.7",
+    install_requires=["pywin32", "fire"],
+)
---
+++ packages/docxrev/docxrev-0.2.2.tar.gz/docxrev-0.2.2/src/docxrev/__init__.py
@@ -0,0 +1,4 @@
+"""Microsoft Word review tools (comments, markup, etc.) with Python."""
+
+from docxrev.docxrev import *  # noqa: F401, F403
+from docxrev.com import Document
