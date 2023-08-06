10c60df8c5d9e905d6ea8abe59c6d32adccb2ea5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:24 +0000

    Add package mylib-maureen/2.4.0/mylib_maureen-2.4.0.tar.gz

---
+++ packages/mylib-maureen/mylib_maureen-2.4.0.tar.gz/mylib_maureen-2.4.0/utils/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 _*-  
+"""
+@author: Maureen Hsu
+@file: __init__.py.py 
+@time: 2019/10/25
+"""
+
+# python packages
+
+# 3rd-party packages
+# from configobj import ConfigObj
+# from loguru import logger
+
+# self-defined packages
+# from utils import os_path
+
+# global variables
+# log_path = "./log/"
+# config_path = "./config/"
+
+# set up log & config
+# logger.add(log_path, rotation="5 MB", retention="1 week") # filter=lambda record: record["extra"].get("name") == ""
+# logger = logger.bind(name="")
+# config = ConfigObj(config_path)
