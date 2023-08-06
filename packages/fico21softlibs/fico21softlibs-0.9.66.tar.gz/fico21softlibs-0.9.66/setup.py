9f2e716b08c63750b9e6413a9b3c8dfce8a3e12f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:57 +0000

    Add package fico21softlibs/0.9.66/fico21softlibs-0.9.66.tar.gz

---
+++ packages/fico21softlibs/fico21softlibs-0.9.66.tar.gz/fico21softlibs-0.9.66/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name             = 'fico21softlibs',
+    version          = '0.9.66',
+    description      = 'This is a fico21soft\'s common libraries',
+    long_description = open('README.md').read(),
+    author           = 'Senna Kang',
+    author_email     = 'fico21soft@gmail.com',
+    url              = '',
+    download_url     = '',
+    install_requires = ['pyodbc', 'selenium', 'pynput'],
+    packages         = find_packages(exclude = ['docs', 'example']),
+    keywords         = ['common', 'library', 'fico21soft'],
+    python_requires  = '>=3',
+    zip_safe=False,
+    classifiers      = [
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ]
+)
