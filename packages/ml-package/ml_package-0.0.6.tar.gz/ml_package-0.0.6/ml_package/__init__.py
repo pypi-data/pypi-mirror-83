13e039f850b9d6720b2bea4ba9947e81758187ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:59:06 +0000

    Add package ml-package/0.0.6/ml_package-0.0.6.tar.gz

---
+++ packages/ml-package/ml_package-0.0.6.tar.gz/ml_package-0.0.6/ml_package/__init__.py
@@ -0,0 +1,20 @@
+# twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+# python setup.py sdist
+
+# Basic
+import warnings
+warnings.filterwarnings('ignore') # supress warnings
+
+import pandas as pd
+pd.set_option('display.max_columns', None) # Displaying all the columns in the dataset
+pd.set_option('max_colwidth', None) # Displaying entire contents of columns
+pd.set_option("max_rows", 100) # Displaying all rows
+pd.set_option('max_seq_item', None) # Displaying everything in the list in the dataframe
+pd.set_option('precision', 2) # Displaying precision to just 2 decimal places
+
+
+def read(path, sep):
+    return pd.read_table(filepath_or_buffer = path, sep= sep)
+
+def add_numbers(num1, num2):
+    return num1 + num2
