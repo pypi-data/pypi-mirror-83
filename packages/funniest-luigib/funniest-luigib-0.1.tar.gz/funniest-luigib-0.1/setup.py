12d964f11f28f6dc36b068f282491fa487b9b278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:08:00 +0000

    Add package funniest-luigib/0.1/funniest-luigib-0.1.tar.gz

---
+++ packages/funniest-luigib/funniest-luigib-0.1.tar.gz/funniest-luigib-0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='funniest-luigib',
+      version='0.1',
+      description='The funniest joke in the world',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+        'Topic :: Text Processing :: Linguistic'
+      ],
+      keywords=['funniest', 'joke', 'comedy', 'flying', 'circus'],
+      url='http://github.com/storborg/funniest',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funniest'],
+      install_requires=['markdown'],
+      include_package_data=True,
+      zip_safe=False)
