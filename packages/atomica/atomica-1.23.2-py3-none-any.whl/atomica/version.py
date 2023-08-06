d25668f990bb80b3f7f7259804592f1a1f30cce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:12 +0000

    Add package atomica/1.23.2/atomica-1.23.2-py3-none-any.whl

---
+++ packages/atomica/atomica-1.23.2-py3-none-any.whl/atomica/version.py
@@ -0,0 +1,11 @@
+"""
+Atomica version file.
+
+Standard location for module version number and date.
+"""
+
+from .utils import fast_gitinfo
+
+version = "1.23.2"
+versiondate = "2020-10-22"
+gitinfo = fast_gitinfo(__file__)
