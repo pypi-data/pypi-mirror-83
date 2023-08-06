83d46c5b960a54ac540421495b6468329c1e5266
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:44 +0000

    Add package covid19-jhu-data/0.0.1/covid19_jhu_data-0.0.1.tar.gz

---
+++ packages/covid19-jhu-data/covid19_jhu_data-0.0.1.tar.gz/covid19_jhu_data-0.0.1/README.md
@@ -0,0 +1,3 @@
+# covid19_data_jhu
+
+This is a simple example package. We use pandas to import data from https://github.com/CSSEGISandData/COVID-19.
---
+++ packages/covid19-jhu-data/covid19_jhu_data-0.0.1.tar.gz/covid19_jhu_data-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="covid19_jhu_data",
+    version="0.0.1",
+    author="Kuo, Yao-Jen",
+    author_email="yaojenkuo@datainpoint.com",
+    description="We use pandas to import data from https://github.com/CSSEGISandData/COVID-19",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'requests',
+        'pandas>=1'
+    ]
+)
