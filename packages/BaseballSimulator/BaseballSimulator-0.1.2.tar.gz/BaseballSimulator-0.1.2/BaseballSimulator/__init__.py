17765e61cb52b11cb035c9244343b44ce60c4fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:53 +0000

    Add package BaseballSimulator/0.1.2/BaseballSimulator-0.1.2.tar.gz

---
+++ packages/BaseballSimulator/BaseballSimulator-0.1.2.tar.gz/BaseballSimulator-0.1.2/BaseballSimulator/__init__.py
@@ -0,0 +1,2 @@
+# from .Simulator import *
+# from .Pitchers import *
---
+++ packages/BaseballSimulator/BaseballSimulator-0.1.2.tar.gz/BaseballSimulator-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='BaseballSimulator',
+    version = '0.1.2',
+    description="A module simulating the flight of a basebal, including deflection caused by spin.",
+    url="https://github.com/CD3/BaseballSimulator",
+    packages=find_packages(),
+    install_requires=[
+        'pint',
+        'torch',
+        'plotly',
+        'pyyaml',
+        'numpy',
+        'scipy',
+        'pathos',
+        'click',
+        'tqdm',
+    ],
+    # entry_points='''
+    # [console_scripts]
+    #  launch-sim=BaseballSimulator.scripts.launch_sim:main
+    #  pitch-sim=BaseballSimulator.scripts.pitch_sim:main
+    #  pitch-trainer=BaseballSimulator.scripts.pitch_trainer:main
+    # ''',
+)
