b4eff2915a650d831f56b42611d34d3081a05136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:55 +0000

    Add package intelab-python-sdk/0.4.5/intelab_python_sdk-0.4.5.tar.gz

---
+++ packages/intelab-python-sdk/intelab_python_sdk-0.4.5.tar.gz/intelab_python_sdk-0.4.5/setup.py
@@ -0,0 +1,25 @@
+from codecs import open
+from setuptools import setup, find_packages
+
+
+with open('README.rst', 'r', 'utf-8') as f:
+    readme = f.read()
+
+setup(
+    name='intelab_python_sdk',
+    version='0.4.5',
+    url='https://github.com/zwwangoo/intelab-python-sdk',
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    author='zwwangoo',
+    author_email='w_angzhiwen@163.com',
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    zip_safe=False,
+    include_package_data=True,
+    python_requires='>=3.5',
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
