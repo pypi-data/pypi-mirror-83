a927e3d62af161246e381797beb3b12210137c71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:23:31 +0000

    Add package paji-sdk/0.1.0a2/paji-sdk-0.1.0a2.tar.gz

---
+++ packages/paji-sdk/paji-sdk-0.1.0a2.tar.gz/paji-sdk-0.1.0a2/README.md
@@ -0,0 +1,9 @@
+# 啪唧工具包 paji-sdk
+
+Python 開發工具包
+
+### 透過 pip 安裝
+
+```bash
+pip install paji-sdk
+```
---
+++ packages/paji-sdk/paji-sdk-0.1.0a2.tar.gz/paji-sdk-0.1.0a2/paji_sdk/base/base_types.py
@@ -0,0 +1,26 @@
+import abc
+
+
+class Entity(abc.ABC):
+    def serialize(self) -> dict:
+        pass
+
+
+class UseCase(abc.ABC):
+    """領域的用例"""
+    pass
+
+
+class Service(abc.ABC):
+    """領域的服務"""
+    pass
+
+
+class Repository(abc.ABC):
+    """領域的儲存庫"""
+    pass
+
+
+class Helper(abc.ABC):
+    """工具包"""
+    pass
