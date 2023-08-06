748b0dbaf9d1f456c1fdea30bdbbae83cef9690c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:52:23 +0000

    Add package sphinx-csv-filter/0.3.0/sphinx-csv-filter-0.3.0.tar.gz

---
+++ packages/sphinx-csv-filter/sphinx-csv-filter-0.3.0.tar.gz/sphinx-csv-filter-0.3.0/setup.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8; -*-
+#
+# Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+# license agreements.  See the NOTICE file distributed with this work for
+# additional information regarding copyright ownership.  Crate licenses
+# this file to you under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.  You may
+# obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# However, if you have executed another commercial license agreement
+# with Crate these terms will supersede the license and you may use the
+# software solely pursuant to the terms of the relevant commercial agreement.
+
+import os
+
+from setuptools import find_packages, setup
+
+
+def read(path):
+    with open(os.path.join(os.path.dirname(__file__), path)) as f:
+        return f.read()
+
+
+setup(
+    name='sphinx-csv-filter',
+    version='0.3.0',
+    url='https://github.com/crate/sphinx_csv_filter',
+    author='CRATE Technology GmbH',
+    author_email='office@crate.io',
+    package_dir={'': 'src'},
+    description='Sphinx CSV filter extension for Crate Documentation',
+    long_description=read('README.rst'),
+    long_description_content_type='text/x-rst',
+    platforms=['any'],
+    license='Apache License 2.0',
+    keywords='crate sphinx csv',
+    packages=find_packages('src'),
+    namespace_packages=['crate'],
+    entry_points={},
+    extras_require={
+        "development": [
+            "setuptools",
+            "wheel",
+            "twine",
+        ],
+        "testing": [
+            "pytest",
+            "flake8",
+            "pytest-flake8",
+            "pytest-isort",
+        ]
+    },
+    install_requires=[
+        "Sphinx",
+    ],
+    python_requires=">=3.3",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Database'
+    ],
+)
---
+++ packages/sphinx-csv-filter/sphinx-csv-filter-0.3.0.tar.gz/sphinx-csv-filter-0.3.0/src/crate/sphinx/csv.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8; -*-
+
+import ast
+import re
+
+from docutils.parsers.rst.directives.tables import CSVTable
+from docutils.utils import SystemMessagePropagation
+
+
+def non_negative_int(argument):
+    """
+    Converts the argument into an integer.
+    Raises ValueError for negative or non-integer values.
+    """
+    value = int(argument)
+    if value >= 0:
+        return value
+    else:
+        raise ValueError('negative value defined; must be non-negative')
+
+
+def non_negative_int_list(argument):
+    """
+    Converts a space- or comma-separated list of values into a Python list
+    of integers.
+    Raises ValueError for negative integer values.
+    """
+    if ',' in argument:
+        entries = argument.split(',')
+    else:
+        entries = argument.split()
+    return [non_negative_int(entry) for entry in entries]
+
+
+class CSVFilterDirective(CSVTable):
+    """ The CSV Filter directive renders csv defined in config
+        and filter rows that contains a specified regex pattern
+    """
+    CSVTable.option_spec['include'] = ast.literal_eval
+    CSVTable.option_spec['exclude'] = ast.literal_eval
+    CSVTable.option_spec['included_cols'] = non_negative_int_list
+
+    def parse_csv_data_into_rows(self, csv_data, dialect, source):
+        rows, max_cols = super(
+            CSVFilterDirective, self
+        ).parse_csv_data_into_rows(csv_data, dialect, source)
+        include_filters = exclude_filters = None
+        if 'include' in self.options:
+            include_filters = {
+                k: re.compile(v) for k, v in self.options['include'].items()
+            }
+        if 'exclude' in self.options:
+            exclude_filters = {
+                k: re.compile(v) for k, v in self.options['exclude'].items()
+            }
+        rows = self._apply_filters(rows, max_cols, include_filters, exclude_filters)
+        if 'included_cols' in self.options:
+            rows, max_cols = self._get_rows_with_included_cols(
+                rows, self.options['included_cols']
+            )
+        return rows, max_cols
+
+    def _apply_filters(self, rows, max_cols, include_filters, exclude_filters):
+        result = []
+
+        header_rows = self.options.get('header-rows', 0)
+        # Always include header rows, if any
+        result.extend(rows[:header_rows])
+
+        for row in rows[header_rows:]:
+            # We generally include a row, ...
+            include = True
+            if include_filters:
+                # ... unless include filters are defined, then we generally
+                # exclude them, ...
+                include = False
+                for col_idx, pattern in include_filters.items():
+                    # cell data value is located at hardcoded index pos. 3
+                    # data type is always a string literal
+                    if max_cols - 1 >= col_idx:
+                        if pattern.match(row[col_idx][3][0]):
+                            # ... unless at least one of the defined filters
+                            # matches its cell ...
+                            include = True
+                            break
+
+            # ... unless exclude filters are defined (as well) ...
+            if include and exclude_filters:
+                for col_idx, pattern in exclude_filters.items():
+                    # cell data value is located at hardcoded index pos. 3
+                    # data type is always a string literal
+                    if max_cols - 1 >= col_idx:
+                        if pattern.match(row[col_idx][3][0]):
+                            # ... then we exclude a row if any of the defined
+                            # exclude filters matches its cell.
+                            include = False
+                            break
+
+            if include:
+                result.append(row)
+
+        return result
+
+    def _get_rows_with_included_cols(self, rows, included_cols_list):
+        prepared_rows = []
+        for row in rows:
+            try:
+                idx_row = [row[i] for i in included_cols_list]
+                prepared_rows.append(idx_row)
+            except IndexError:
+                error = self.state_machine.reporter.error(
+                    'One or more indexes of included_cols are not valid. '
+                    'The CSV data does not contain that many columns.')
+                raise SystemMessagePropagation(error)
+        return prepared_rows, len(included_cols_list)
+
+
+def setup(sphinx):
+    sphinx.add_directive('csv-filter', CSVFilterDirective)
