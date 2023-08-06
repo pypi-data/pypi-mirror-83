5cb7aad4bd47b37e748802bef3c9f5bce16881b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:11:46 +0000

    Add package lit2go/0.0.2/lit2go-0.0.2.tar.gz

---
+++ packages/lit2go/lit2go-0.0.2.tar.gz/lit2go-0.0.2/literacy2/lit2go.py
@@ -0,0 +1,13 @@
+def main(args):
+  commands = ['home', 'next', 'back', 'check', 'progress']
+
+  print('\nUsage:')
+  print('  lit2go <command> [options]')
+  print('\nCommands:')
+  for c in commands:
+    print(f'  {c}')
+
+  print()
+
+if __name__ == "__main__":
+    main()
