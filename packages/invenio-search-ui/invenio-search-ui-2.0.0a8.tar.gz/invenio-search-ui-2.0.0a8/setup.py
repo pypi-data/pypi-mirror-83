15ffb9e56c506f25a40292107e6dbed6001c42ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:35 +0000

    Add package invenio-search-ui/2.0.0a8/invenio-search-ui-2.0.0a8.tar.gz

---
+++ packages/invenio-search-ui/invenio-search-ui-2.0.0a8.tar.gz/invenio-search-ui-2.0.0a8/setup.py
@@ -0,0 +1,105 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of Invenio.
+# Copyright (C) 2015-2018 CERN.
+#
+# Invenio is free software; you can redistribute it and/or modify it
+# under the terms of the MIT License; see LICENSE file for more details.
+
+"""UI for Invenio-Search."""
+
+import os
+
+from setuptools import find_packages, setup
+
+readme = open('README.rst').read()
+history = open('CHANGES.rst').read()
+
+tests_require = [
+    'check-manifest>=0.25',
+    'coverage>=4.0',
+    'invenio-db>=1.0.0',
+    'invenio-records>=1.0.0',
+    'isort>=4.3.0',
+    'pydocstyle>=1.0.0',
+    'pytest-cov>=1.8.0',
+    'pytest-pep8>=1.0.6',
+    'pytest>=2.8.0,!=3.3.0',
+]
+
+extras_require = {
+    'docs': [
+        'Sphinx>=1.7.1',
+    ],
+    'tests': tests_require,
+}
+
+extras_require['all'] = []
+for reqs in extras_require.values():
+    extras_require['all'].extend(reqs)
+
+setup_requires = [
+    'Babel>=1.3',
+    'pytest-runner>=2.6.2',
+]
+
+install_requires = [
+    'invenio-base>=1.2.3',
+    'invenio-i18n>=1.2.0',
+    'flask-webpackext>=1.0.0',
+    'invenio-assets>=1.2.0',
+]
+
+packages = find_packages()
+
+# Get the version string. Cannot be done with import!
+g = {}
+with open(os.path.join('invenio_search_ui', 'version.py'), 'rt') as fp:
+    exec(fp.read(), g)
+    version = g['__version__']
+
+setup(
+    name='invenio-search-ui',
+    version=version,
+    description=__doc__,
+    long_description=readme + '\n\n' + history,
+    keywords='invenio search ui',
+    license='MIT',
+    author='CERN',
+    author_email='info@inveniosoftware.org',
+    url='https://github.com/inveniosoftware/invenio-search-ui',
+    packages=packages,
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    entry_points={
+        'invenio_base.apps': [
+            'invenio_search_ui = invenio_search_ui:InvenioSearchUI'
+        ],
+        'invenio_assets.webpack': [
+            'invenio_search_ui = invenio_search_ui.webpack:search_ui',
+        ],
+        'invenio_i18n.translations': [
+            'messages = invenio_search_ui',
+        ],
+    },
+    extras_require=extras_require,
+    install_requires=install_requires,
+    setup_requires=setup_requires,
+    tests_require=tests_require,
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Development Status :: 5 - Production/Stable',
+    ],
+)
