cfc38613665345dedbac0cf285e380b6a1b63346
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:47:46 +0000

    Add package methodcrm-client/0.0.1/methodcrm_client-0.0.1-py3-none-any.whl

---
+++ packages/methodcrm-client/methodcrm_client-0.0.1-py3-none-any.whl/openapi_client/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from openapi_client.api.api_key_api import APIKeyApi
+from openapi_client.api.files_api import FilesApi
+from openapi_client.api.me_api import MeApi
+from openapi_client.api.tables_api import TablesApi
